62a020415793bfb32ee3fc464b36535721a5e205
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-27 01:58:56 +0000

    protobuf@7.4: update 3.12.2 bottle.

--- Formula/protobuf@7.4.rb
+++ Formula/protobuf@7.4.rb
@@ -10,6 +10,8 @@ class ProtobufAT74 < AbstractPhp74Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    cellar :any_skip_relocation
+    sha256 "21e5ae305237f3370d74a726b81b7380d31b025c382e36b06bdda813426c966c" => :catalina
   end
 
   def install
