bf81026173d3d79049fe386af07174cd241a9731
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-13 04:25:32 +0000

    xdebug@8.0: update 3.0.0 bottle.

--- Formula/xdebug@8.0.rb
+++ Formula/xdebug@8.0.rb
@@ -10,8 +10,8 @@ class XdebugAT80 < AbstractPhp80Extension
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any_skip_relocation
-    rebuild 7
-    sha256 "d038c64fc07df43a8a910eca753db77b0044b7608bac7d4cf22206318deab358" => :catalina
+    rebuild 8
+    sha256 "3c94940b4251f6ed2afe40cc36392a395b4e96b00c3f5e30f04b4960c4fbe1ae" => :catalina
   end
 
   def install
