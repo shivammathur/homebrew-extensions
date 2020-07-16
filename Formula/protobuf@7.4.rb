c8ace390860f6bff3e7ae33215b0fae968950466
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-16 04:49:40 +0000

    protobuf@7.4: update 3.12.3 bottle.

--- Formula/protobuf@7.4.rb
+++ Formula/protobuf@7.4.rb
@@ -4,14 +4,14 @@ class ProtobufAT74 < AbstractPhp74Extension
   init
   desc "Protobuf PHP extension"
   homepage "https://github.com/protocolbuffers/protobuf"
-  url "https://pecl.php.net/get/protobuf-3.12.2.tgz"
-  sha256 "1f4c4927007bebee3622ee0f05a71e7087e0c0f638ec9f3839ba2454fb4d11cb"
+  url "https://pecl.php.net/get/protobuf-3.12.3.tgz"
+  sha256 "056b815dea3067abf8fe3d7779b2357039129dd847f66b49b80c9ab05d7557a0"
   head "https://github.com/protocolbuffers/protobuf.git"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any_skip_relocation
-    sha256 "21e5ae305237f3370d74a726b81b7380d31b025c382e36b06bdda813426c966c" => :catalina
+    sha256 "7e1a8267720355767aef73f14242497af082bc1a659fc4a9a09089c300a27e9c" => :catalina
   end
 
   def install
