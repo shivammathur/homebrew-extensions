d1e8eb75d024130c24a4332c914628948252b5a3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-17 16:21:29 +0000

    imagick@7.4: update 3.4.4 bottle.

--- Formula/imagick@7.4.rb
+++ Formula/imagick@7.4.rb
@@ -7,13 +7,14 @@ class ImagickAT74 < AbstractPhp74Extension
   init
   desc "Imagick PHP extension"
   homepage "https://github.com/Imagick/imagick/releases"
-  url "https://github.com/Imagick/imagick/archive/3.4.4.tar.gz"
+  url "https://github.com/imagick/imagick/archive/3.4.4.tar.gz"
   sha256 "8204d228ecbe5f744d625c90364808616127471581227415bca18857af981369"
   head "https://github.com/Imagick/imagick"
   license "PHP-3.01"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    sha256 "d37ea2ca136415ef7ecc52f3e618043fbe2e0919518bed048ff31560c30be0e4" => :catalina
   end
 
   depends_on "pkg-config" => :build
