a5fedd63505b31958293274387efa9c31e4b4c58
Shivam Mathur <shivam_jpr@hotmail.com>
2020-12-23 22:15:46 +0530

    Add xdebug 2 formulae for PHP 7.2 to 7.4

---
+++ Formula/xdebug2@7.2.rb
@@ -0,0 +1,27 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Xdebug Extension
+class Xdebug2AT72 < AbstractPhp72Extension
+  init
+  desc "Xdebug PHP extension"
+  homepage "https://github.com/xdebug/xdebug"
+  url "https://github.com/xdebug/xdebug/archive/2.9.8.tar.gz"
+  sha256 "28f8de8e6491f51ac9f551a221275360458a01c7690c42b23b9a0d2e6429eff4"
+  head "https://github.com/xdebug/xdebug.git"
+  license "PHP-3.0"
+
+  bottle do
+    root_url "https://dl.bintray.com/shivammathur/extensions"
+  end
+
+  def install
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-xdebug"
+    system "make"
+    prefix.install "modules/xdebug.so"
+    write_config_file
+  end
+end
