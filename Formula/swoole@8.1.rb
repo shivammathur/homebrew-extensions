b2f6442d240ffc573d81fee8fad3bd11397b400d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-24 08:27:01 +0000

    swoole@8.1: update 4.5.10 bottle.

--- Formula/swoole@8.1.rb
+++ Formula/swoole@8.1.rb
@@ -15,6 +15,8 @@ class SwooleAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    cellar :any
+    sha256 "0814bcbd46c945a426e3010328e1b54d1e5027d26d7e90db0eed293b2c103424" => :catalina
   end
 
   def install
