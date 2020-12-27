d18580846cbc2dddfad630166cebf89956006c08
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-27 04:25:17 +0000

    igbinary@7.0: update 3.2.0 bottle.

--- Formula/igbinary@7.0.rb
+++ Formula/igbinary@7.0.rb
@@ -8,15 +8,15 @@ class IgbinaryAT70 < AbstractPhp70Extension
   init
   desc "Igbinary PHP extension"
   homepage "https://github.com/igbinary/igbinary"
-  url "https://github.com/igbinary/igbinary/archive/3.1.6.tar.gz"
-  sha256 "86079a3a0e0ea46292ed0ebe69748c5e09c68fe5b0e274d0dd45f3d9c80f61a8"
+  url "https://github.com/igbinary/igbinary/archive/3.2.0.tar.gz"
+  sha256 "9a1e1e6fd982aeff70e0ae05b822173d8492cfc9550d6172041d905c5e51da68"
   head "https://github.com/igbinary/igbinary.git"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any_skip_relocation
-    sha256 "f87b62eeb707eb9d142cdff4c09faf6feaa2b09022df4183643efd8dfbe8bcdf" => :catalina
+    sha256 "75fae6c375ee7f7cfdeaf1c9b88209f6f40b6fb21119d34f54f631f350024967" => :catalina
   end
 
   def install
