620397368fd8fc88613a0b1fa3384ec09905e856
Shivam Mathur <shivam_jpr@hotmail.com>
2020-12-29 03:03:33 +0530

    Fix version check --skip-build --skip-test

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -59,7 +59,7 @@ else
     sed -i '' "s/^  sha256.*/  sha256 \"$checksum\"/g" ./Formula/"$VERSION".rb
   fi
 fi
-new_version=$(cat Formula/"$VERSION".rb | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
+new_version=$(cat Formula/"$VERSION".rb | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+" | head -n 1)
 existing_version=$(curl --user "$HOMEBREW_BINTRAY_USER":"$HOMEBREW_BINTRAY_KEY" -s https://api.bintray.com/packages/"$HOMEBREW_BINTRAY_USER"/"$HOMEBREW_BINTRAY_REPO"/"$package" | sed -e 's/^.*"latest_version":"\([^"]*\)".*$/\1/')
 echo "existing label: $existing_version"
 echo "new label: $new_version"
