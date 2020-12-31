c2bc62e099f64254fa7708b3a4e724fc8f54fb73
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-31 05:06:17 +0000

    imap@8.1: update 8.1.0 bottle.

--- Formula/imap@8.1.rb
+++ Formula/imap@8.1.rb
@@ -8,15 +8,15 @@ class ImapAT81 < AbstractPhp81Extension
   init
   desc "Imap PHP extension"
   homepage "https://github.com/php/php-src"
-  url "https://github.com/php/php-src/archive/master.tar.gz?v=8.1.0&build_time=1609302601"
+  url "https://github.com/php/php-src/archive/master.tar.gz?v=8.1.0&build_time=1609390513"
   version "8.1.0"
-  sha256 "1c29a67d3e90d873419bcdcbc4c5d4da94bd86d24a22dba02c3f3ca1f7f61fa2"
+  sha256 "a8f3ecb208ded4eac543a4b999f8a5a012103d8076f6c217c3d5569c59f5dea1"
   license "PHP-3.01"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 17
-    sha256 "11bd7aa057f111228553763969f2d5c912f36053ee8f0edcace36374344897db" => :catalina
+    rebuild 18
+    sha256 "6e2cec33d4d023bf62fed2c31bbc34d57906c8c60dcfd5b575d3365bb6b940f1" => :catalina
   end
 
   depends_on "imap-uw"
