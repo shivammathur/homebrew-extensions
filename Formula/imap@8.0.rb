e001657b1301a6b6c1d8307f1ed7b8df45f8e713
Shivam Mathur <shivam_jpr@hotmail.com>
2021-01-11 21:10:11 +0530

    Do not copy imap header --build-only-imap

--- Formula/imap@8.0.rb
+++ Formula/imap@8.0.rb
@@ -33,7 +33,6 @@ class ImapAT80 < AbstractPhp80Extension
            "--with-kerberos"
     system "make"
     prefix.install "modules/imap.so"
-    (include/"php/ext"/extension).install Dir["php_*.h"]
     write_config_file
   end
 end
