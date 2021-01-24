a646ddd1ead0e6dbf119b50d2e1a2bfce4ca387e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-24 18:24:05 +0000

    grpc@7.0: update 1.35.0 bottle.

--- Formula/grpc@7.0.rb
+++ Formula/grpc@7.0.rb
@@ -16,6 +16,8 @@ class GrpcAT70 < AbstractPhp70Extension
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any_skip_relocation
+    sha256 "5b32110ec6ed369953d48b395811e87e3635de610214713c7bdbd9f1833d3d7d" => :big_sur
+    sha256 "f625723c7561f444e66be6b1941678f3fe170d98ad24b7b1a2b71f991bdc02e3" => :catalina
   end
 
   depends_on "grpc"
