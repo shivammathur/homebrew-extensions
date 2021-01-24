c76557429070d5e70ea7dac9366f45736432f383
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-24 14:06:59 +0000

    Update grpc@7.0

--- Formula/grpc@7.0.rb
+++ Formula/grpc@7.0.rb
@@ -8,16 +8,14 @@ class GrpcAT70 < AbstractPhp70Extension
   init
   desc "gRPC PHP extension"
   homepage "https://github.com/grpc/grpc"
-  url "https://pecl.php.net/get/grpc-1.34.0.tgz"
-  sha256 "70a0f34ac30608e52d83bba9639b2cf6878942813f4aee6d6e842e689ea27485"
+  url "https://pecl.php.net/get/grpc-1.35.0.tgz"
+  sha256 "d8de1ad5df0bc424699a44133141d9d9c936d3803ae01e5510350590b8c1e4ae"
   head "https://github.com/grpc/grpc.git"
   license "Apache-2.0"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any_skip_relocation
-    sha256 "452a57e73be574228f0bea7ab2ac966390fdada5ac5d023397142f49c2fc907b" => :big_sur
-    sha256 "6989b8736d1a6d492c847e6fe98a99ec183ba545473e30bee646303ec811200f" => :catalina
   end
 
   depends_on "grpc"
