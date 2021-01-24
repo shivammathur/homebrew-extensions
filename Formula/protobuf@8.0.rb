e9f340ef66dcecae3928df4aa21fce19d3c538d1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-24 09:55:41 +0000

    protobuf@8.0: update 3.15.0 bottle.

--- Formula/protobuf@8.0.rb
+++ Formula/protobuf@8.0.rb
@@ -18,6 +18,7 @@ class ProtobufAT80 < AbstractPhp80Extension
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "7859bb91c3d6c269b710a782352e4c384d29818829a564982129d5b0bf3ba031" => :big_sur
     sha256 "dafadd86428c98192bfe7484de971e25a68ed0bfc360ae5a55e9d6a008511ed4" => :arm64_big_sur
     sha256 "1a6e5f4fa12bcc796a92c5ae4e7138b4ce93ccf1d1342ce638ae6bc1f20e5b06" => :catalina
   end
