42326b4ac991b1b0c094986e3e606756d46d80f4
Shivam Mathur <shivam_jpr@hotmail.com>
2021-01-24 22:36:59 +0530

    Fix ci --build-only-grpc

---
+++ Formula/raphf@5.6.rb
@@ -0,0 +1,28 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Raphf Extension
+class RaphfAT56 < AbstractPhp56Extension
+  init
+  desc "Raphf PHP extension"
+  homepage "https://github.com/m6w6/ext-raphf"
+  url "https://pecl.php.net/get/raphf-1.1.2.tgz?init=true"
+  sha256 "d35a49672e72d0e03751385e0b8fed02aededcacc5e3ec27c98a5849720483a7"
+  head "https://github.com/m6w6/ext-raphf.git"
+  license "BSD-2-Clause"
+
+  bottle do
+    root_url "https://dl.bintray.com/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "raphf-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-raphf"
+    system "make"
+    prefix.install "modules/raphf.so"
+    write_config_file
+  end
+end
