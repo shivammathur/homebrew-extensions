cda50f9a3ffe02624cad2bb64d6c0433a8b73d0a
Shivam Mathur <shivam_jpr@hotmail.com>
2021-01-24 19:34:28 +0530

    Add support for propro and raphf

---
+++ Formula/raphf@8.0.rb
@@ -0,0 +1,28 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Raphf Extension
+class RaphfAT80 < AbstractPhp80Extension
+  init
+  desc "Raphf PHP extension"
+  homepage "https://github.com/m6w6/ext-raphf"
+  url "https://pecl.php.net/get/raphf-2.0.1.tgz"
+  sha256 "da3566db17422e5ef08b7ff144162952aabc14cb22407cc6b1d2a2d095812bd0"
+  head "https://github.com/m6w6/ext-raphf.git"
+  license "BSD-2-Clause"
+
+  bottle do
+    root_url "https://dl.bintray.com/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "raphf-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-raphf"
+    system "make"
+    prefix.install "modules/raphf.so"
+    write_config_file
+  end
+end
