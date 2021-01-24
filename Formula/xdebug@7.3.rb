0fd547a6917eed81c8d5d4403db40e557b7388e6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-24 09:16:43 +0000

    xdebug@7.3: update 3.0.2 bottle.

--- Formula/xdebug@7.3.rb
+++ Formula/xdebug@7.3.rb
@@ -15,6 +15,7 @@ class XdebugAT73 < AbstractPhp73Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    sha256 "424b1525234c894dcb92b934d5326af48bdf9b2fded26022ebac07eef77b777d" => :big_sur
     sha256 "d54c54c6bad0fc5c58fc209b6a0ba9fa0272212f8b1b5266c8fb9c054bf142c0" => :arm64_big_sur
     sha256 "5b179bef58f870aef3d7b75c5f928aa5b916b014dd72f3949eefe6494dda45ce" => :catalina
   end
