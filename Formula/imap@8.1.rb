6f274744c1cdc75fe11645b2ee05e131c62b6f67
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-24 11:06:29 +0000

    imap@8.1: update 8.1.0 bottle.

--- Formula/imap@8.1.rb
+++ Formula/imap@8.1.rb
@@ -16,6 +16,7 @@ class ImapAT81 < AbstractPhp81Extension
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     rebuild 21
+    sha256 "386f34de927f91abd2c9a2f04970cc88b3e52ac183104b43bbbe17ec1a56e724" => :big_sur
     sha256 "a2d3e602a44af470699f3f1fb2c1af4677fd85b6ad8dd27f9a24ca47dc2542a5" => :arm64_big_sur
     sha256 "757e843aa2c1cd85b4f3fb88e2413f7342c8866c76f5a96e096be18b74e9255c" => :catalina
   end
