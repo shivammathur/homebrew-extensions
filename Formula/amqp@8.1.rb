10688616d9839761416d639cdb412e85455e8f60
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-25 15:15:44 +0000

    Update amqp@8.1

--- Formula/amqp@8.1.rb
+++ Formula/amqp@8.1.rb
@@ -8,7 +8,7 @@ class AmqpAT81 < AbstractPhp81Extension
   init
   desc "Amqp PHP extension"
   homepage "https://github.com/php-amqp/php-amqp"
-  url "https://github.com/php-amqp/php-amqp/archive/df1241852b359cf12c346beaa68de202257efdf1.tar.gz?init=true"
+  url "https://github.com/php-amqp/php-amqp/archive/df1241852b359cf12c346beaa68de202257efdf1.tar.gz"
   version "1.11.0"
   sha256 "c9b27857fc7e39654518af5c35eb947b371556375550412b7166f75c03c8b5c8"
   head "https://github.com/php-amqp/php-amqp"
@@ -16,9 +16,6 @@ class AmqpAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 9
-    sha256 "775bafe4997f0a7e646059f0aa20bb8b7833e9d70a50c7654adc04b26b2b7ed7" => :big_sur
-    sha256 "783c7230102c0ce7ddc676382662f00e03c09d2d4b1ec86c90119c2460b3f0fd" => :catalina
   end
 
   depends_on "rabbitmq-c"
