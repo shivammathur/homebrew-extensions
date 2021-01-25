1e4947ad4ba2e7f357495f244a360eb68afad29e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-25 02:47:26 +0000

    imap@8.1: rebuild
    Update imap@8.1
    
    Closes #166.
    
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/imap@8.1.rb
+++ Formula/imap@8.1.rb
@@ -8,17 +8,13 @@ class ImapAT81 < AbstractPhp81Extension
   init
   desc "Imap PHP extension"
   homepage "https://github.com/php/php-src"
-  url "https://github.com/php/php-src/archive/master.tar.gz?v=8.1.0&build_time=1611541781"
+  url "https://github.com/php/php-src/archive/master.tar.gz?v=8.1.0&build_time=1611542845"
   version "8.1.0"
   sha256 "1da1e72b375fd7e43ca79907bc66be28878d8b285ca6e9b781e388d87a314f76"
   license "PHP-3.01"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 21
-    sha256 "386f34de927f91abd2c9a2f04970cc88b3e52ac183104b43bbbe17ec1a56e724" => :big_sur
-    sha256 "a2d3e602a44af470699f3f1fb2c1af4677fd85b6ad8dd27f9a24ca47dc2542a5" => :arm64_big_sur
-    sha256 "757e843aa2c1cd85b4f3fb88e2413f7342c8866c76f5a96e096be18b74e9255c" => :catalina
   end
 
   depends_on "imap-uw"
