c3b14a7c34b2caaf6c2204c107d2843e3bf30e5e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-25 05:24:33 +0000

    propro@7.4: update 2.1.0 bottle.

--- Formula/propro@7.4.rb
+++ Formula/propro@7.4.rb
@@ -16,6 +16,10 @@ class ProproAT74 < AbstractPhp74Extension
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any_skip_relocation
+    rebuild 1
+    sha256 "b7dda981b173790ac5c3d1190360a506dacafdf4e84be97cbe7b3043af6ce3f5" => :big_sur
+    sha256 "3a9793a4ea6f617a1fd256d8c79895416b2f514cf9d74652787c8d3ef4f2ca44" => :arm64_big_sur
+    sha256 "7b92e7c3fcb0836d6fe9d00677590678be6ed0979571cf3bccfce1e4f6b65879" => :catalina
   end
 
   def install
