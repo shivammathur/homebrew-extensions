aadeab69dd7480f428e0eae2fe708d639bcfd530
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-25 02:47:09 +0000

    Update propro@7.1

--- Formula/propro@7.1.rb
+++ Formula/propro@7.1.rb
@@ -16,9 +16,6 @@ class ProproAT71 < AbstractPhp71Extension
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any_skip_relocation
-    sha256 "ed35568cf420691fecb51fb89fb43dc9cefd1e0c9a1ea22a04305103be24dbf7" => :big_sur
-    sha256 "1b53dc4444eade8ecef68ad847457b02c2630fbf32757f6653a3d0f4624d8c2d" => :arm64_big_sur
-    sha256 "6554b50300bcf14e36b264372e2d9e079e079a82148a6167f2ebd650a8c66c6d" => :catalina
   end
 
   def install
