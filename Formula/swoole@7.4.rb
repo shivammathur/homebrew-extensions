85a903146bd43e5dee57c30c453d7fc216c847d5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-26 05:20:32 +0000

    swoole@7.4: update 4.6.2 bottle.

--- Formula/swoole@7.4.rb
+++ Formula/swoole@7.4.rb
@@ -16,6 +16,9 @@ class SwooleAT74 < AbstractPhp74Extension
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any
+    sha256 "f32ce44d8bd97f7722de1eea0eb1416da149dc3edab9bb26c4e08376652c2c23" => :big_sur
+    sha256 "4745d79d4d2026afc7b852fc6cd5fc5c0a49ae4b449714ee091958d28f5f024f" => :arm64_big_sur
+    sha256 "7b9aa5446b869ec4e4ee4fc52b365d8a087c0b26adcf7d3190d6b34ebe0f4d6c" => :catalina
   end
 
   def install
