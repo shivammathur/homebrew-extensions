2d12494757b0e3bcd2bfaab3ca8a6e30fc925f9e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-26 04:17:02 +0000

    pcov@8.1: rebuild
    Update pcov@8.1
    
    Closes #226.
    
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/pcov@8.1.rb
+++ Formula/pcov@8.1.rb
@@ -16,10 +16,6 @@ class PcovAT81 < AbstractPhp81Extension
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any_skip_relocation
-    rebuild 22
-    sha256 "a7720c339610982902bfdfc28dc614aca7e7dac8ce6960da041c695e1e65b096" => :big_sur
-    sha256 "3b39916a4fe66f37c822a54c6956b80f647df39eb4a6f243adb2d88eeff6dd52" => :arm64_big_sur
-    sha256 "83c9002efba31755e295aa1ff5dc7dbc03fe7c77bcd42946d58acb039fcceba5" => :catalina
   end
 
   def install
