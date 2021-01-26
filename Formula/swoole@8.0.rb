a496203ca3450fa3efbcdcac3356ac6958e1df5e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-26 04:17:24 +0000

    Update swoole@8.0

--- Formula/swoole@8.0.rb
+++ Formula/swoole@8.0.rb
@@ -8,16 +8,14 @@ class SwooleAT80 < AbstractPhp80Extension
   init
   desc "Swoole PHP extension"
   homepage "https://github.com/swoole/swoole-src"
-  url "https://github.com/swoole/swoole-src/archive/v4.6.1.tar.gz"
-  sha256 "d4c9b1c3966faeee996344920554993885465377e40fa5ebb4e716593072eb57"
+  url "https://github.com/swoole/swoole-src/archive/v4.6.2.tar.gz"
+  sha256 "ba0f95b85da77096c535e4919935bdcd07742641e9664b715041343c902807c5"
   head "https://github.com/swoole/swoole-src.git"
   license "Apache-2.0"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any
-    sha256 "69150a8daf7b5f31f8882d11b1663178be2bd95d859184ab0333c815553b6d3b" => :big_sur
-    sha256 "faf34a59449d804a37045b4b38eb0c609f5759a2500b0353aef8a6f2949a71a5" => :catalina
   end
 
   def install
