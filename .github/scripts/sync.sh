ed0409273f0405a9dc8682f6dfb5487554f54793
Shivam Mathur <shivam_jpr@hotmail.com>
2021-02-09 02:36:11 +0530

    Fix sync workflow

--- .github/scripts/sync.sh
+++ .github/scripts/sync.sh
@@ -1,23 +1,27 @@
-extension=$1
-shift
-deps=("$@")
-git config --local user.email homebrew-test-bot@lists.sfconservancy.org
-git config --local user.name BrewTestBot
-git config --local pull.rebase true
-sudo rm -rf ./*txt ./*.json ./*.gz
-sudo mkdir -p "./.github/deps/$extension"
-for formula in "${deps[@]}"; do
-  formula=$(echo "$formula" | xargs)
-  sudo cp "$(brew --prefix)/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/$formula.rb" "./.github/deps/$extension/"
+brew tap shivammathur/extensions
+mapfile -t extensions < <(find "$(brew --repository)"/Library/Taps/shivammathur/homebrew-extensions/Formula -name "*.rb" -print0 | xargs -0 basename -a | sed "s/@.*//" | sort | uniq)
+to_wait=()
+for extension in "${extensions[@]}"; do
+  deps=
+  sudo mkdir -p "./.github/deps/$extension"
+  mapfile -t deps < <(grep "depends_on" "$(brew --repository)"/Library/Taps/shivammathur/homebrew-extensions/Formula/"$extension"@7.4.rb | tr -d '"' | cut -d' ' -f 4)
+  for formula in "${deps[@]}"; do
+    curl -o "./.github/deps/$extension/$formula.rb" -sL "https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/$formula.rb" &
+    to_wait+=($!)
+  done
 done
+wait "${to_wait[@]}"
 ls "./.github/deps/$extension"
 if [ "$(git status --porcelain=v1 2>/dev/null | wc -l)" != "0" ]; then
+  git config --local user.email homebrew-test-bot@lists.sfconservancy.org
+  git config --local user.name BrewTestBot
+  git config --local pull.rebase true
   git stash
   git pull -f https://"$GITHUB_REPOSITORY_OWNER":"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY".git master
   git stash apply || true
   if [ "$(git status --porcelain=v1 2>/dev/null | wc -l)" != "0" ]; then
     git add .
-    git commit -m "Update PHP dependencies for $extension"
+    git commit -m "Update extension dependencies"
     git push -f https://"$GITHUB_REPOSITORY_OWNER":"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY".git master || true
   fi
 fi
