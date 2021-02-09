9a114ad0f23483047bb46312179a50ea4949842c
Shivam Mathur <shivam_jpr@hotmail.com>
2021-02-09 06:11:22 +0530

    Add zmq --build-only-zmq

---
+++ Formula/zmq@8.0.rb
@@ -0,0 +1,40 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Zmq Extension
+class ZmqAT80 < AbstractPhp80Extension
+  init
+  desc "Zmq PHP extension"
+  homepage "https://github.com/zeromq/php-zmq"
+  url "https://github.com/zeromq/php-zmq/archive/43464c42a6a47efdf8b7cab03c62f1622fb5d3c6.tar.gz?init=true"
+  sha256 "cbf1d005cea35b9215e2830a0e673b2edd8b526203f731de7a7bf8f590a60298"
+  version "1.1.3"
+  head "https://github.com/zeromq/php-zmq.git"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://dl.bintray.com/shivammathur/extensions"
+  end
+
+  depends_on "czmq"
+  depends_on "zeromq"
+
+  def install
+    ENV["PKG_CONFIG"] = "#{HOMEBREW_PREFIX}/bin/pkg-config"
+    ENV.append "PKG_CONFIG_PATH", "#{Formula["libsodium"].opt_prefix}/lib/pkgconfig"
+    args = %W[
+      --with-czmq=#{Formula["czmq"].opt_prefix}
+    ]
+    inreplace "package.xml", "@PACKAGE_VERSION@", version
+    inreplace "php-zmq.spec", "@PACKAGE_VERSION@", version
+    inreplace "php_zmq.h", "@PACKAGE_VERSION@", version
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{module_name}.so"
+    write_config_file
+    add_include_files
+  end
+end
