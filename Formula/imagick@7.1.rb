738f35328f65b590fe5bfa8e3ce2e94ad80c0af2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-09 08:24:58 +0000

    imagick@7.1: update 3.4.4 bottle.

--- Formula/imagick@7.1.rb
+++ Formula/imagick@7.1.rb
@@ -16,6 +16,10 @@ class ImagickAT71 < AbstractPhp71Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    rebuild 2
+    sha256 arm64_big_sur: "d8d067189be03101518244eaed06db24d2bb518a43bb07fd4b7053ba0cd9fa8c"
+    sha256 big_sur:       "9ac092d325a9e0ade7e1379e821f8c918dffee556825f21859867eabb0b00f42"
+    sha256 catalina:      "eed49ec6ae16f71171c88bcdfc800429eff415a3a75d20f83ba9acbc490f8e31"
   end
 
   depends_on "imagemagick"
