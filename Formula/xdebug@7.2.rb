0f47f83ff8225c5dbe0faa771da3bc098bf36b3a
Shivam Mathur <shivam_jpr@hotmail.com>
2021-02-09 09:03:38 +0530

    xdebug@7.2 3.0.2
    Switch xdebug@7.2 to Xdebug 3.x
    
    Closes #295.
    
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/xdebug@7.2.rb
+++ Formula/xdebug@7.2.rb
@@ -8,18 +8,13 @@ class XdebugAT72 < AbstractPhp72Extension
   init
   desc "Xdebug PHP extension"
   homepage "https://github.com/xdebug/xdebug"
-  url "https://github.com/xdebug/xdebug/archive/2.9.8.tar.gz"
-  sha256 "28f8de8e6491f51ac9f551a221275360458a01c7690c42b23b9a0d2e6429eff4"
+  url "https://github.com/xdebug/xdebug/archive/3.0.2.tar.gz"
+  sha256 "9b1b53468d35dfb3aeabf07c81e6de3b9ab6a7b15f5f5f35af3cac41bd70762c"
   head "https://github.com/xdebug/xdebug.git"
   license "PHP-3.0"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    cellar :any_skip_relocation
-    rebuild 3
-    sha256 "40c69d44e827f48b38261dd53d3baeef6297a32f0014587e93e5e4452302424e" => :big_sur
-    sha256 "83c970c210e5b85a743e81485c87527c950b502fe73ec3fc00a2878a2489999e" => :arm64_big_sur
-    sha256 "374d9c6edccef56d901b64c7f27baea4757307da7eaa96b566dfc6627e8fdb9c" => :catalina
   end
 
   def install
