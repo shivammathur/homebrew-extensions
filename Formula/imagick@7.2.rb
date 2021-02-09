9c59c50583ea96c2e2eeee3da4195aa31dc290ae
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-09 05:40:34 +0000

    Update imagick@7.2

--- Formula/imagick@7.2.rb
+++ Formula/imagick@7.2.rb
@@ -9,13 +9,12 @@ class ImagickAT72 < AbstractPhp72Extension
   desc "Imagick PHP extension"
   homepage "https://github.com/Imagick/imagick/releases"
   url "https://github.com/imagick/imagick/archive/3.4.4.tar.gz"
-  sha256 "8204d228ecbe5f744d625c90364808616127471581227415bca18857af981369"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   head "https://github.com/Imagick/imagick"
   license "PHP-3.01"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 1
     sha256 "c15e4a1b34134141da350d82c290cb9190ad805a59c7b76340b9d12eb4ee80ff" => :big_sur
     sha256 "f9bd9dcee6dc6af9ebd0316f9cfb03fc304c258b4c66e8228d173ef1acf099c0" => :arm64_big_sur
     sha256 "6bb90d857c39f369b8efaa1a04199c25d1b3c9d881963bee454ec69c0c890bd5" => :catalina
