a63f6710f9ac23600854ba264b44f5cf8eea0d43
Shivam Mathur <shivam_jpr@hotmail.com>
2021-02-09 12:19:51 +0530

    Fix imagick --build-only-imagick

--- Formula/imagick@7.2.rb
+++ Formula/imagick@7.2.rb
@@ -8,25 +8,17 @@ class ImagickAT72 < AbstractPhp72Extension
   init
   desc "Imagick PHP extension"
   homepage "https://github.com/Imagick/imagick/releases"
-  url "https://github.com/imagick/imagick/archive/3.4.4.tar.gz"
-  sha256 "8204d228ecbe5f744d625c90364808616127471581227415bca18857af981369"
+  url "https://github.com/Imagick/imagick/archive/448c1cd0d58ba2838b9b6dff71c9b7e70a401b90.tar.gz?init=true"
+  sha256 "b1b32d524d11ffbd530b270f15205c5523ce81efde3c1bee94f4d2730246c867"
+  version "3.4.4"
   head "https://github.com/Imagick/imagick"
   license "PHP-3.01"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 1
-    sha256 "c15e4a1b34134141da350d82c290cb9190ad805a59c7b76340b9d12eb4ee80ff" => :big_sur
-    sha256 "f9bd9dcee6dc6af9ebd0316f9cfb03fc304c258b4c66e8228d173ef1acf099c0" => :arm64_big_sur
-    sha256 "6bb90d857c39f369b8efaa1a04199c25d1b3c9d881963bee454ec69c0c890bd5" => :catalina
   end
 
   depends_on "imagemagick"
-
-  patch do
-    url "https://github.com/Imagick/imagick/commit/b4e676607c23e3b280d5312c0cd2d79786772547.patch"
-    sha256 "3aac4980d1d38ddca16e0469ea257ec6405474ee8438b47d4c00aac94911cc49"
-  end
 
   def install
     safe_phpize
--- Formula/imagick@8.0.rb
+++ Formula/imagick@8.0.rb
@@ -8,25 +8,17 @@ class ImagickAT80 < AbstractPhp80Extension
   init
   desc "Imagick PHP extension"
   homepage "https://github.com/Imagick/imagick/releases"
-  url "https://github.com/Imagick/imagick/archive/132a11fd26675db9eb9f0e9a3e2887c161875206.tar.gz"
-  version "3.5.0"
-  sha256 "f94982135603811cd15da19b80959c7456ada9a916af5bd7162b5bad80fa34c4"
+  url "https://github.com/Imagick/imagick/archive/448c1cd0d58ba2838b9b6dff71c9b7e70a401b90.tar.gz?init=true"
+  sha256 "b1b32d524d11ffbd530b270f15205c5523ce81efde3c1bee94f4d2730246c867"
+  version "3.4.4"
   head "https://github.com/Imagick/imagick"
   license "PHP-3.01"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    sha256 "291d56d0d45beba209b198af59a4579444e001a4a6a6f2fb7ab558f84238ebeb" => :big_sur
-    sha256 "6e02e1d602552f3613f572b10e386144e4fa2ed51b7c74f625a3ae166b7df391" => :arm64_big_sur
-    sha256 "b8c6372efdeb95f7c30eeefa47fb03c98e4ed316a33c231207d16d7f151a3173" => :catalina
   end
 
   depends_on "imagemagick"
-
-  patch do
-    url "https://github.com/Imagick/imagick/commit/b4e676607c23e3b280d5312c0cd2d79786772547.patch"
-    sha256 "3aac4980d1d38ddca16e0469ea257ec6405474ee8438b47d4c00aac94911cc49"
-  end
 
   def install
     safe_phpize
