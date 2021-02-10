4249b54f16b807b60094a2f7b73d61391670dd15
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-10 06:55:21 +0000

    Update pcov@8.1

--- Formula/pcov@8.1.rb
+++ Formula/pcov@8.1.rb
@@ -15,10 +15,6 @@ class PcovAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 25
-    sha256 cellar: :any_skip_relocation, big_sur:       "f22410971515bee3c1172dccc8a9461bf4dd92f005a61128246b0fae9112d39b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c5c71f24bc5549c3b29d6d1133d268907d728d1b0cf62a5e501fefaef6282a55"
-    sha256 cellar: :any_skip_relocation, catalina:      "e8eedfd15989c6643129ac3bcda30a2a210ebdbece0fee8c683f6271096f40bd"
   end
 
   def install
