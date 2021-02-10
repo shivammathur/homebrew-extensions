38d58a89da208b275eca534a813e2a756c63e9ac
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-10 06:55:22 +0000

    raphf@8.1: rebuild
    Update raphf@8.1
    
    Closes #338.
    
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/raphf@8.1.rb
+++ Formula/raphf@8.1.rb
@@ -15,10 +15,6 @@ class RaphfAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 5
-    sha256 cellar: :any_skip_relocation, big_sur:       "dba3dc7db684f7d45a3bf420d819f9c3e5876787480b2f4f7d687b83e304a51a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "58f43dc85f09f28ee40f2602cc445d0c382ceb9b1858ab5932bcb24bcfdbe354"
-    sha256 cellar: :any_skip_relocation, catalina:      "354dd8a97bd8cfbe0aae7e67ea800d4ae6f36f361077430139081aef50b80b95"
   end
 
   def install
