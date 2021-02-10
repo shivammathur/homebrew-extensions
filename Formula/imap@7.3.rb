189dff10fd3ad50acfae758a7077fd68a6ed5744
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-10 06:55:34 +0000

    Update imap@7.3

--- Formula/imap@7.3.rb
+++ Formula/imap@7.3.rb
@@ -8,13 +8,12 @@ class ImapAT73 < AbstractPhp73Extension
   init
   desc "Imap PHP extension"
   homepage "https://github.com/php/php-src"
-  url "https://www.php.net/distributions/php-7.3.26.tar.xz"
-  sha256 "d93052f4cb2882090b6a37fd1e0c764be1605a2461152b7f6b8f04fa48875208"
+  url "https://www.php.net/distributions/php-7.3.27.tar.xz"
+  sha256 "65f616e2d5b6faacedf62830fa047951b0136d5da34ae59e6744cbaf5dca148d"
   license "PHP-3.01"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 1
     sha256 "f2c2502270b53581c6aebc33d6c0d91b6fdd3f3f2e9d19f6a8c46618b3396b5b" => :big_sur
     sha256 "c3d60a248ea9846c32de8aae2d2b211ce6ae252216d9fb79879ecc41c14e68bf" => :arm64_big_sur
     sha256 "3b56f683b2727dbb3ceeec06271d3a56599d04aa902ef90feed8bd9bd7f8d0b0" => :catalina
