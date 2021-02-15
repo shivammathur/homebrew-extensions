de04d3d03b06af10ac8f128bdd96952001921b8b
Shivam Mathur <shivam_jpr@hotmail.com>
2021-02-15 10:18:47 +0530

    Rebuild imagick --build-only-imagick --build-all

--- Formula/imagick@8.1.rb
+++ Formula/imagick@8.1.rb
@@ -8,7 +8,7 @@ class ImagickAT81 < AbstractPhp81Extension
   init
   desc "Imagick PHP extension"
   homepage "https://github.com/Imagick/imagick/releases"
-  url "https://github.com/Imagick/imagick/archive/448c1cd0d58ba2838b9b6dff71c9b7e70a401b90.tar.gz"
+  url "https://github.com/Imagick/imagick/archive/448c1cd0d58ba2838b9b6dff71c9b7e70a401b90.tar.gz?init=true"
   sha256 "b1b32d524d11ffbd530b270f15205c5523ce81efde3c1bee94f4d2730246c867"
   version "3.4.4"
   head "https://github.com/Imagick/imagick"
