f0b3b9951ffbf307c115c82bc9208217f938514a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-16 16:25:12 +0000

    igbinary@8.1: update 3.2.1 bottle.

--- Formula/igbinary@8.1.rb
+++ Formula/igbinary@8.1.rb
@@ -15,6 +15,10 @@ class IgbinaryAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    rebuild 9
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4c7c404dc1baa077b983f1b5c29443a65cc9560ef0b70511125185565488752b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "23da53c28549a04d43c91ddb50db94bb93ee70a545cfdfd167b1f391cbd866c0"
+    sha256 cellar: :any_skip_relocation, catalina:      "ed7afcd1fdf343c90b17d97f11248cb47a37f5a6b2b109fad7103a7983327f85"
   end
 
   def install
