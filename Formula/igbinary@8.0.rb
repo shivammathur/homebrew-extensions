8a5264da9197e0875eea03848e1d52f00801cd15
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-17 00:51:59 +0000

    Update igbinary@8.0

--- Formula/igbinary@8.0.rb
+++ Formula/igbinary@8.0.rb
@@ -8,17 +8,13 @@ class IgbinaryAT80 < AbstractPhp80Extension
   init
   desc "Igbinary PHP extension"
   homepage "https://github.com/igbinary/igbinary"
-  url "https://github.com/igbinary/igbinary/archive/3.2.1.tar.gz?init=true"
+  url "https://github.com/igbinary/igbinary/archive/3.2.1.tar.gz"
   sha256 "bc26f361f750bc9894741201e6851a3445a20d185969c08bf311264b7dc9cd6b"
   head "https://github.com/igbinary/igbinary.git"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "eecbf931582831a0010b51908b2fc69e5cf550e4620208d2eecfa73f8fb287c8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d5a0e8c6bdda07168eaeb95336844bb375673eb07d9dca5606e21cf2681806cf"
-    sha256 cellar: :any_skip_relocation, catalina:      "6995e1c113623a51be2752f69de5348cb23c58876081aead439c9f7979a71493"
   end
 
   def install
