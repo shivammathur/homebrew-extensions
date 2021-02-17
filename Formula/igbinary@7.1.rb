1cc38b677014efa9ca2713c01df0cd8d015062e5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-17 00:51:58 +0000

    igbinary@7.1: rebuild
    Update igbinary@7.1
    
    Closes #378.
    
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/igbinary@7.1.rb
+++ Formula/igbinary@7.1.rb
@@ -8,17 +8,13 @@ class IgbinaryAT71 < AbstractPhp71Extension
   init
   desc "Igbinary PHP extension"
   homepage "https://github.com/igbinary/igbinary"
-  url "https://github.com/igbinary/igbinary/archive/3.2.1.tar.gz?init=true"
+  url "https://github.com/igbinary/igbinary/archive/3.2.1.tar.gz"
   sha256 "bc26f361f750bc9894741201e6851a3445a20d185969c08bf311264b7dc9cd6b"
   head "https://github.com/igbinary/igbinary.git"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "01cf894cb07ed01a4780077055015c1e7b3c319ccd382042774df83fca522f97"
-    sha256 cellar: :any_skip_relocation, big_sur:       "13f5625e9daab25060e30bc693201212975fa3865b30634f016323da2a45c1c8"
-    sha256 cellar: :any_skip_relocation, catalina:      "489645fa4309d245ec4dba0e95822307a4f6938af728e92aecab5d5248ba7381"
   end
 
   def install
