f9f954fdfff81cc7bc599818875fd855c74c9af7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-18 04:13:05 +0000

    igbinary@8.1: rebuild
    Update igbinary@8.1
    
    Closes #393.
    
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/igbinary@8.1.rb
+++ Formula/igbinary@8.1.rb
@@ -15,10 +15,6 @@ class IgbinaryAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 10
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "252e5c1d6203615c5b010163c2bf3900bbb40e2713a87e8484753ee40c79fbeb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8249efe551060d616201cca21670620cd5b1df8ffcee53bf5cb99f85e80f2fb9"
-    sha256 cellar: :any_skip_relocation, catalina:      "22a269a597da99c15336f5435cc5cb4204ab2ddfe5c8f54e3f00629638ed6fe0"
   end
 
   def install
