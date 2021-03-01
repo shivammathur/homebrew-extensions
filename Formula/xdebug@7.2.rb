feea487f46e4e0333cfd1824803c2b5ae6dec9ff
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-01 13:21:11 +0000

    Update xdebug@7.2

--- Formula/xdebug@7.2.rb
+++ Formula/xdebug@7.2.rb
@@ -8,16 +8,13 @@ class XdebugAT72 < AbstractPhp72Extension
   init
   desc "Xdebug PHP extension"
   homepage "https://github.com/xdebug/xdebug"
-  url "https://github.com/xdebug/xdebug/archive/3.0.2.tar.gz"
-  sha256 "9b1b53468d35dfb3aeabf07c81e6de3b9ab6a7b15f5f5f35af3cac41bd70762c"
+  url "https://github.com/xdebug/xdebug/archive/3.0.3.tar.gz"
+  sha256 "1143f8f9677c677106e846f0d22fc82f4ee5ffb52bdd1498a28cb0748186a8c9"
   head "https://github.com/xdebug/xdebug.git"
   license "PHP-3.0"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    sha256 arm64_big_sur: "76c31b2450d0297307f15bc050f0233da429f98a6570da61c9005124959cf792"
-    sha256 big_sur:       "6c52fee175fe4bc246b3431cb36c640108ae807b54f56a5b12df8dc75982f001"
-    sha256 catalina:      "1497766280727373ac55c9d91d81fb565b3c6c9fac4ecd5fd6f549caa649e54c"
   end
 
   def install
