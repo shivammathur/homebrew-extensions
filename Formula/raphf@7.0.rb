abd70ac671c45ee2648bd544c3dbe922019fac0f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-04 07:42:56 +0000

    Update raphf@7.0

--- Formula/raphf@7.0.rb
+++ Formula/raphf@7.0.rb
@@ -16,7 +16,6 @@ class RaphfAT70 < AbstractPhp70Extension
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
     cellar :any_skip_relocation
-    rebuild 1
     sha256 "6f9a7916360a1a0df0734354487be16e7e807eefceb986ad41f747f661e2e7ab" => :big_sur
     sha256 "067f68246a163708e16ea487f5441520eb553807d472904e9dcb0b83ea64906e" => :arm64_big_sur
     sha256 "f058eb64731d701919b91f89e682b9aa13a21118201e62617b95f3a5bbf3e7a5" => :catalina
