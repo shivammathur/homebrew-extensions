684d062cd1addbe6417ded8b9b5ec116a5c02f49
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-11 06:27:19 +0000

    amqp@8.1: update 1.11.0 bottle.

--- Formula/amqp@8.1.rb
+++ Formula/amqp@8.1.rb
@@ -16,6 +16,10 @@ class AmqpAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    rebuild 18
+    sha256 arm64_big_sur: "d6cd909b306d0568abac58e12b38782d2c975ab7b71531464a0ac74645a67d00"
+    sha256 big_sur:       "03deaff532c3faeb0aa9803df0e85ade31a406af736c260e3ee9158a7b83b024"
+    sha256 catalina:      "e2e5196f9651f899d844ce6433845b9e9021754a6502334b5847a6452b6b8fb7"
   end
 
   depends_on "rabbitmq-c"
