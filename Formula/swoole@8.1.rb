7e170f578eea502fef0b0eeddf150da37ab80f48
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-11 08:29:07 +0000

    swoole@8.1: update 4.6.3 bottle.

--- Formula/swoole@8.1.rb
+++ Formula/swoole@8.1.rb
@@ -15,6 +15,10 @@ class SwooleAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    rebuild 4
+    sha256 cellar: :any, arm64_big_sur: "b63bf2c63ffcd84bd8584e9af364f08d9dd0f732f04badd0c2cffe213c0eccd7"
+    sha256 cellar: :any, big_sur:       "fc34c3db91d2fd753cc9273c31fd08aef02e45f3279be1eb25eec5bde6fc63f7"
+    sha256 cellar: :any, catalina:      "5e624e176b64f602bd74f79edc65651d10a17934bd3f8dc5f0998995599d9bf0"
   end
 
   def install
