78846617b78c237ba5d6855b82357cbe4ddae66b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-16 12:33:08 +0000

    grpc@8.1: update 1.36.0 bottle.

--- Formula/grpc@8.1.rb
+++ Formula/grpc@8.1.rb
@@ -15,6 +15,10 @@ class GrpcAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "093cfc74d0bd244c154766f9f288aebf19aa7baf05221ae03c867e3f3d477b8f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c2aa3b49abac58b23c262fa6709e44f96fda93a04f9a2420f7a4dae4938b4f94"
+    sha256 cellar: :any_skip_relocation, catalina:      "e62d5ec09afb50948dbe0a8d740945132c06862eeef8befd19073368bc6f03c8"
   end
 
   depends_on "grpc"
