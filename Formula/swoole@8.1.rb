290509f5317c28ef48b15e78d1ba847c24275d7d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-18 04:20:22 +0000

    swoole@8.1: rebuild
    * Update swoole@8.1
    * Patch swoole@8.1
    * Patch swoole@8.1
    
    Closes #562.
    
    Co-authored-by: Shivam Mathur <shivam_jpr@hotmail.com>
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/swoole@8.1.rb
+++ Formula/swoole@8.1.rb
@@ -15,13 +15,12 @@ class SwooleAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    sha256 cellar: :any, arm64_big_sur: "c3e2ba6adaa2dc989928e4fb773397aaee262382d6891c707a8ba834a8da6f68"
-    sha256 cellar: :any, big_sur:       "556a71cf35c38f5e6c2c840973f5bc754c177f8dd77300ec821c117db04819c8"
-    sha256 cellar: :any, catalina:      "f086af2532805b3babff0418d6846d22974c3a536625acd8492a7c828b839583"
   end
 
   def install
     patch_spl_symbols
+    inreplace "ext-src/php_swoole_cxx.cc", "_ex(file.c_str(), ", "_ex("
+    inreplace "ext-src/php_swoole_cxx.cc", "zend_file_handle_dtor", "zend_destroy_file_handle"
     safe_phpize
     system "./configure"
     system "make"
