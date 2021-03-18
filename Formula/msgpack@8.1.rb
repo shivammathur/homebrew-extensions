2905ce57fe6e5d1da4cc7f0fd9380fad6bad69d5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-18 06:28:17 +0000

    msgpack@8.1: update 2.1.2 bottle.

--- Formula/msgpack@8.1.rb
+++ Formula/msgpack@8.1.rb
@@ -15,6 +15,10 @@ class MsgpackAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    rebuild 6
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "58a40f7e0ce453217e40f6103f7e97d80b1706346bac7a59eef424411761c929"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8150be926560b11c6646dd160c0f5333d0a3e7d56807a63567d1484f9d222a9e"
+    sha256 cellar: :any_skip_relocation, catalina:      "dc6d49dd1a0783d4195614a5441fb83e4b1091e6101fef9e6ec16a1b7411d572"
   end
 
   def install
