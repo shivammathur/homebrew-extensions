020b286767fe176f05876e7bcd795963c7472ca2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-25 06:12:34 +0000

    raphf@8.1: update 2.0.1 bottle.

--- Formula/raphf@8.1.rb
+++ Formula/raphf@8.1.rb
@@ -15,6 +15,10 @@ class RaphfAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    rebuild 11
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8a76ae0bc2a483005a618754d024f0e3547aeda74f0ea4b2e64a8cf69a1d81f4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8ab260a7fbb1f3c56730444e33f611b94b7b1d84f032d6b58713f885f8ec067e"
+    sha256 cellar: :any_skip_relocation, catalina:      "f17b29ec6485dc58aa30bdf927ad5ef9945e01b38fbaa698a1c3336e9845b434"
   end
 
   def install
