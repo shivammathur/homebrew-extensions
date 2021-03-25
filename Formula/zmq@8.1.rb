c382e8839e3b7668a4872f8f35ca08426e99688e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-25 04:09:32 +0000

    zmq@8.1: rebuild
    Update zmq@8.1
    
    Closes #590.
    
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/zmq@8.1.rb
+++ Formula/zmq@8.1.rb
@@ -16,10 +16,6 @@ class ZmqAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 7
-    sha256 arm64_big_sur: "a81f93fa5336cea7d2c49fcfc804e11979f7f04db4f0b14e9374380420d09164"
-    sha256 big_sur:       "533b0fccb3d5e43f077936b4f54abcc57807a74aabce6e1ba2ca4a23f2ce5def"
-    sha256 catalina:      "70d44df9e55abba048548a0f18b1da7cfe21f489e5153fee72d05b546dae2fcc"
   end
 
   depends_on "czmq"
