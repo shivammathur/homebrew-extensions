0988fbd962205a4403d43145cc033be7f52a4221
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-25 04:08:51 +0000

    pcov@8.1 1.0.8
    Update pcov@8.1
    
    Closes #576.
    
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/pcov@8.1.rb
+++ Formula/pcov@8.1.rb
@@ -8,17 +8,13 @@ class PcovAT81 < AbstractPhp81Extension
   init
   desc "PCOV PHP extension"
   homepage "https://github.com/krakjoe/pcov"
-  url "https://github.com/krakjoe/pcov/archive/v1.0.6.tar.gz"
-  sha256 "3be3b8af91c43db70c4893dd2552c9ee2877e9cf32f59a607846c9ceb64a173b"
+  url "https://github.com/krakjoe/pcov/archive/v1.0.8.tar.gz"
+  sha256 "f67a1c1aabe798e5bb85d0ea8c7b1d27b226b066d12460da8d78b48bfc6f455e"
   head "https://github.com/krakjoe/pcov.git"
   license "PHP-3.01"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 31
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "adbccc713db6d7cf667c326a999aecccb5ad0a4a2c3b723ca91a3c1c2a1f6afb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "9d244352ec711f409786a507068d3bc01e886b4872cfdd79865f3b0c77a91d46"
-    sha256 cellar: :any_skip_relocation, catalina:      "cc9f5b688af62adfd8f85c257a6185d548b7178cd5f6dd6e15ab1f34187e4673"
   end
 
   def install
