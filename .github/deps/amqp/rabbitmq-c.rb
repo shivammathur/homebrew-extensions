8b3c38dd15925034e5910a1766409b980f215cec
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-01 10:05:10 +0000

    Update extension dependencies

--- .github/deps/amqp/rabbitmq-c.rb
+++ .github/deps/amqp/rabbitmq-c.rb
@@ -1,17 +1,16 @@
 class RabbitmqC < Formula
   desc "C AMQP client library for RabbitMQ"
   homepage "https://github.com/alanxz/rabbitmq-c"
-  url "https://github.com/alanxz/rabbitmq-c/archive/v0.10.0.tar.gz"
-  sha256 "6455efbaebad8891c59f274a852b75b5cc51f4d669dfc78d2ae7e6cc97fcd8c0"
+  url "https://github.com/alanxz/rabbitmq-c/archive/v0.11.0.tar.gz"
+  sha256 "437d45e0e35c18cf3e59bcfe5dfe37566547eb121e69fca64b98f5d2c1c2d424"
   license "MIT"
   head "https://github.com/alanxz/rabbitmq-c.git"
 
   bottle do
-    sha256               arm64_big_sur: "ca5405b2852bfbecbe4e8145e688df1ffb4c97e2147e81684dc949b336dc8649"
-    sha256 cellar: :any, big_sur:       "f76f526ae3c37b8f686cbd796d2fc9f1ec6210e0ae2f8986260efa834b99c9f3"
-    sha256 cellar: :any, catalina:      "6434a9100eeadfcd57d35fd31d1863d75b71ec163a3a1be29076c217712bda55"
-    sha256 cellar: :any, mojave:        "5f99c633ece8efad2ef2085955b22d0558d8fc2dedcac67b3ba8b58a2640c2c3"
-    sha256 cellar: :any, high_sierra:   "53d883744a185e5daab18c8bd18fd70fed56dd009cc507356f128663947c2453"
+    sha256 arm64_big_sur: "8c99de9859b3c8fd847d02a777f9f607942742c8e8c7ab11961c9f82cdd7521c"
+    sha256 big_sur:       "d8e5e9610c9209804710a07ad2accdd9e07df7c6ec0dbbc987193fb724801c0f"
+    sha256 catalina:      "2f15cd210e177b5d9156d3884f2b5fe735352820cc7b900a1c9dc951f56cb8e0"
+    sha256 mojave:        "ad16aaef6f362020519025f174bd72c4c74d007965209d9c89b55d0d989752cc"
   end
 
   depends_on "cmake" => :build
