afa44c42b0ac14d61e76ccb7603daf61453da507
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-02 18:18:47 +0000

    apcu@8.1: update 5.1.20 bottle.

--- Formula/apcu@8.1.rb
+++ Formula/apcu@8.1.rb
@@ -15,6 +15,9 @@ class ApcuAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a851e9c4b5d4c1cae488f5c9619bfdcd580131692b42a24109bdca88515cc473"
+    sha256 cellar: :any_skip_relocation, big_sur:       "03898e892ed8723184d1d5ab1d59a3d53f5f0d7d4a6e1bdc64c7ff3a11b86cd0"
+    sha256 cellar: :any_skip_relocation, catalina:      "23408c9b6f47203b3d9be71a26ea828ab4c3758fab2d5055bdc5bb6da5b8e05b"
   end
 
   def install
