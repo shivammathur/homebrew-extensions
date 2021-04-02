bc743c5478b72f3fdaf37f9bbbe954d5e00bfe23
Shivam Mathur <shivam_jpr@hotmail.com>
2021-04-02 21:58:26 +0530

    Add apcu --build-only-apcu

---
+++ Formula/apcu@8.0.rb
@@ -0,0 +1,29 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Apcu Extension
+class ApcuAT80 < AbstractPhp80Extension
+  init
+  desc "Apcu PHP extension"
+  homepage "https://github.com/krakjoe/apcu"
+  url "https://pecl.php.net/get/apcu-5.1.20.tgz?init=true"
+  sha256 "b99d40fafec06f4d132fcee53e7526ddbfc1d041ea6e04e17389dfad28f9c390"
+  head "https://github.com/krakjoe/apcu.git"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://dl.bintray.com/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "apcu-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-apcu"
+    system "make"
+    prefix.install "modules/#{module_name}.so"
+    write_config_file
+    add_include_files
+  end
+end
