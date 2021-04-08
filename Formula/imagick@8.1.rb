1cf11c9e15d0132442c0be45962f53ce57ad5217
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-08 04:08:13 +0000

    Update imagick@8.1

--- Formula/imagick@8.1.rb
+++ Formula/imagick@8.1.rb
@@ -16,10 +16,6 @@ class ImagickAT81 < AbstractPhp81Extension
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    rebuild 9
-    sha256 arm64_big_sur: "2e6599317b261ec93040c517af295fc6ca585fe16db30da130415d9535fe7056"
-    sha256 big_sur:       "a39291d548e83b3ee38cd6db09f8efa6d7db76407d621c68ac1561e052b80e16"
-    sha256 catalina:      "872f158d4f11a976b015264aca70fcff5b9750e96b88f4c128c5dd6334e3fa25"
   end
 
   depends_on "imagemagick"
