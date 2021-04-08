130ef50c766f8308cd7e542c6e88b8273fdc766b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-08 04:09:00 +0000

    protobuf@7.4 3.15.7
    Update protobuf@7.4
    
    Closes #629.
    
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/protobuf@7.4.rb
+++ Formula/protobuf@7.4.rb
@@ -8,16 +8,13 @@ class ProtobufAT74 < AbstractPhp74Extension
   init
   desc "Protobuf PHP extension"
   homepage "https://github.com/protocolbuffers/protobuf"
-  url "https://pecl.php.net/get/protobuf-3.15.6.tgz"
-  sha256 "9caa70247dd449ff7819446967883c82496971a6fa58208633badc4f66fb262b"
+  url "https://pecl.php.net/get/protobuf-3.15.7.tgz"
+  sha256 "6ee82f36ec8fa6b8f15b38d014233f9f3dabda02517b82d436d28ac40594b6f1"
   head "https://github.com/protocolbuffers/protobuf.git"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fffded7fd710282275c6e21f0e0ba11a1a2e3441a200cd444d2aa701878ec2d7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "866968316ee0e40738e64b26574a2273931b1fea328ca2fbd91166f23a7166be"
-    sha256 cellar: :any_skip_relocation, catalina:      "2cf3f478c0fd8dcefbcb85c1c0a78da916da5e9801528fa4cd319636a14343e8"
   end
 
   def install
