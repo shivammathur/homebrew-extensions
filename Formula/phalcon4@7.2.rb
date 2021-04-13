a45ddfd6a9ce4248b0ba71dd9c3e18457ccd0712
Shivam Mathur <shivam_jpr@hotmail.com>
2021-04-13 22:40:40 +0530

    Add psr and phalcon --build-only-psr

---
+++ Formula/phalcon4@7.2.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+class Phalcon4AT72 < AbstractPhp72Extension
+  init
+  desc "Full-stack PHP framework"
+  homepage "https://phalcon.io/en-us"
+  url "https://github.com/phalcon/cphalcon/archive/v4.1.0.tar.gz"
+  sha256 "3b98df3fd15560f30abbcf37b498536ad4d287699f5957b3ec37703491d3b594"
+  head "https://github.com/phalcon/cphalcon.git"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "pcre"
+  depends_on "shivammathur/extensions/psr@7.2"
+
+  def install
+    Dir.chdir "build/php7/64bits"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-phalcon"
+    system "make"
+    prefix.install "modules/phalcon.so"
+    write_config_file
+  end
+end
---
+++ Formula/psr@5.6.rb
@@ -0,0 +1,25 @@
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+class PsrAT56 < AbstractPhp56Extension
+  init
+  desc "PHP extension providing the accepted PSR interfaces "
+  homepage "https://github.com/jbboehr/php-psr"
+  url "https://pecl.php.net/get/psr-0.6.1.tgz?init=true"
+  sha256 "57ccc6293ddb56b3cae2620bb3dc00f145d5edb42e38b160d93ed968fcbb1bae"
+  head "https://github.com/jbboehr/php-psr.git"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "pcre"
+
+  def install
+    Dir.chdir "psr-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-psr"
+    system "make"
+    prefix.install "modules/psr.so"
+    write_config_file
+  end
+end
