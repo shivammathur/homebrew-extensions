5381507ece4d35de6a0357c0c04a9dde4eafc041
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-13 19:14:19 +0000

    Update psr@7.3

--- Formula/psr@7.3.rb
+++ Formula/psr@7.3.rb
@@ -4,7 +4,7 @@ class PsrAT73 < AbstractPhp73Extension
   init
   desc "PHP extension providing the accepted PSR interfaces "
   homepage "https://github.com/jbboehr/php-psr"
-  url "https://pecl.php.net/get/psr-1.1.0.tgz?init=true"
+  url "https://pecl.php.net/get/psr-1.1.0.tgz"
   sha256 "4feb8eeea4237894bcab1ea064cefc3421c909778d39c977184c16e725cfbfb2"
   head "https://github.com/jbboehr/php-psr.git"
 
