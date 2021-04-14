f75794a0310ab7b44397634ae210594ef0710780
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-14 00:26:15 +0000

    phalcon4@7.3: update 4.1.0 bottle.

--- Formula/phalcon4@7.3.rb
+++ Formula/phalcon4@7.3.rb
@@ -10,6 +10,9 @@ class Phalcon4AT73 < AbstractPhp73Extension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4599c79039e8c714155a15f97666f3887893576e2844137d704cd9ab8cc3f425"
+    sha256 cellar: :any_skip_relocation, big_sur:       "51f80189bde2b7f4563c2289ded069fb06b1aeba57fc99d2a134a908b39927a1"
+    sha256 cellar: :any_skip_relocation, catalina:      "9ea42e8e5c1f9dc94a9d205cd3bad961d390c8930f7434f09268317fa7dda84f"
   end
 
   depends_on "pcre"
