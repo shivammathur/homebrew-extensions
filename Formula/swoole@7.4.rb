1468e0d419dd5388f420fe35dda49f87129bc5d1
Shivam Mathur <shivam_jpr@hotmail.com>
2021-04-15 13:39:48 +0530

    Update swoole@7.4 [skip ci]

--- Formula/swoole@7.4.rb
+++ Formula/swoole@7.4.rb
@@ -8,16 +8,13 @@ class SwooleAT74 < AbstractPhp74Extension
   init
   desc "Swoole PHP extension"
   homepage "https://github.com/swoole/swoole-src"
-  url "https://github.com/swoole/swoole-src/archive/v4.6.4.tar.gz"
-  sha256 "fca3337a269db0533c0c7db10b48aeb9e6e5d7b84188d17ac59576b3f365fba3"
+  url "https://github.com/swoole/swoole-src/archive/v4.6.5.tar.gz"
+  sha256 "c93f6a0623b90ca6a6c96f481e6a817f2930b4a582ca0388575b51aa4fb91d38"
   head "https://github.com/swoole/swoole-src.git"
   license "Apache-2.0"
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any, arm64_big_sur: "236eafc37a00fe97e2b31b934afc365a446ecde6b1d2f4fb36fb4024e65d5e8e"
-    sha256 cellar: :any, big_sur:       "b495ce7e4d07da6f242a47f1f29f490853498e88a7caffd7da2b85af16002338"
-    sha256 cellar: :any, catalina:      "2575b91fd4c52d19e86ee4bd04f4a7655dae5989c121fc3d1865a23080414b00"
   end
 
   def install
