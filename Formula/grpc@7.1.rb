67c4b7fb0459c44e7d9f7fb1c5d6c47e03b39381
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-15 06:10:55 +0000

    grpc@7.1: update 1.37.0 bottle.

--- Formula/grpc@7.1.rb
+++ Formula/grpc@7.1.rb
@@ -15,6 +15,9 @@ class GrpcAT71 < AbstractPhp71Extension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a71c8f76214c5eb022cc761429fe9617b0e6d505a164f806ae444660b5df8224"
+    sha256 cellar: :any_skip_relocation, big_sur:       "4ff12924262ae26338867fe73893fdd64661f262b8e5542c99ee5fa3aa8aa347"
+    sha256 cellar: :any_skip_relocation, catalina:      "21b93690ce1ad4c3260cdbaafba46a4e0ee66b9eab1c3c4c680c6a5afeb7cc31"
   end
 
   depends_on "grpc"
