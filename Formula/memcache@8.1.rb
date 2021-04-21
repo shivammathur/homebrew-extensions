0ffa0929dece4bc473a2fcf07d75dcf86926a621
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-21 16:11:28 +0000

    memcache@8.1: update 8.0 bottle.

--- Formula/memcache@8.1.rb
+++ Formula/memcache@8.1.rb
@@ -15,6 +15,9 @@ class MemcacheAT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "25a37b92b721a298abe4272b82b8c3d731f74fd44a06648a6f329574342e229a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c25a3b04a09ec28a25801848e8653d1b54bf082cab17d8b5ce08c0a2270251fd"
+    sha256 cellar: :any_skip_relocation, catalina:      "4ee18380e6e43ae991310e4d4cb6fd2dc196ea2f2a287e73bfdd74d6dfab056e"
   end
 
   def install
