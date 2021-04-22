9bcf410f70a9e35af62a803e44042a67c9dacf83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-22 06:11:20 +0000

    igbinary@7.3: update 3.2.2 bottle.

--- Formula/igbinary@7.3.rb
+++ Formula/igbinary@7.3.rb
@@ -15,6 +15,9 @@ class IgbinaryAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7a5b1f5f8bb0cf9046a4be07eb9f1f3e6a8fe126f227ecf9972fa14d4d50c09f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "e412889d5b91894060c38f97e90982df5bcf0881bc233cd40df332e43408c75b"
+    sha256 cellar: :any_skip_relocation, catalina:      "018a7ca6560f0b8b0a3bd6fbddcdaa94e4424bfae59fdaa8ab30be5994520d27"
   end
 
   def install
