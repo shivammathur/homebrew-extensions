48e21d0dfd925f2aaf84730ee591f874d83fc435
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-22 04:09:42 +0000

    Update pecl_http@8.0

--- Formula/pecl_http@8.0.rb
+++ Formula/pecl_http@8.0.rb
@@ -8,18 +8,13 @@ class PeclHttpAT80 < AbstractPhpExtension
   init
   desc "Pecl_http PHP extension"
   homepage "https://github.com/m6w6/ext-http"
-  url "https://pecl.php.net/get/pecl_http-4.0.0.tgz"
-  sha256 "ea9a508578cffd428baf7b78f6d1618badedf3175be06b0809588a8b48889d5f"
+  url "https://pecl.php.net/get/pecl_http-4.1.0.tgz"
+  sha256 "f8b3a0827c4c8c97ef00d51d09c63fa4203f895bd737f954062c16bae3b8ab1e"
   head "https://github.com/m6w6/ext-http.git"
   license "BSD-2-Clause"
-  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 3
-    sha256 arm64_big_sur: "0a9a5d972630f69a3ea397893de412cc05d18ca08c51c97f6a49c0f1316db3dd"
-    sha256 big_sur:       "6c2bd5aa0da067cd2af2239d168a1188d77ae56e5efcf75258cd6c715cdf81ea"
-    sha256 catalina:      "08cfaa404a990653354140e07354f2701b1f9fcd23cfdb8b0ea77efe73a08323"
   end
 
   depends_on "brotli"
