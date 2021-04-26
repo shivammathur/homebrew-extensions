29d86268205cbf159885fbfadcf5e3a08a9ae537
shivammathur <shivam_jpr@hotmail.com>
2021-04-26 19:51:29 +0530

    Fix sync workflow [skip ci]

--- .github/scripts/sync.sh
+++ .github/scripts/sync.sh
@@ -1,27 +1,30 @@
-brew tap shivammathur/extensions
-mapfile -t extensions < <(find "$(brew --repository)"/Library/Taps/shivammathur/homebrew-extensions/Formula -name "*.rb" -print0 | xargs -0 basename -a | sed "s/@.*//" | sort | uniq)
-to_wait=()
+# configure git
+git config --local user.email 1589480+BrewTestBot@users.noreply.github.com
+git config --local user.name BrewTestBot
+git config --local pull.rebase true
+
+mapfile -t extensions < <(find ./Formula -maxdepth 1 -name "*.rb" -print0 | xargs -0 basename -a | sed "s/@.*//" | sort | uniq)
 for extension in "${extensions[@]}"; do
-  deps=
-  sudo mkdir -p "./.github/deps/$extension"
-  mapfile -t deps < <(grep "depends_on" "$(brew --repository)"/Library/Taps/shivammathur/homebrew-extensions/Formula/"$extension"@7.4.rb | tr -d '"' | cut -d' ' -f 4)
-  for formula in "${deps[@]}"; do
-    curl -o "./.github/deps/$extension/$formula.rb" -sL "https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/$formula.rb" &
-    to_wait+=($!)
-  done
-done
-wait "${to_wait[@]}"
-ls "./.github/deps/$extension"
-if [ "$(git status --porcelain=v1 2>/dev/null | wc -l)" != "0" ]; then
-  git config --local user.email 1589480+BrewTestBot@users.noreply.github.com
-  git config --local user.name BrewTestBot
-  git config --local pull.rebase true
-  git stash
-  git pull -f https://"$GITHUB_REPOSITORY_OWNER":"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY".git master
-  git stash apply || true
-  if [ "$(git status --porcelain=v1 2>/dev/null | wc -l)" != "0" ]; then
-    git add .
-    git commit -m "Update extension dependencies"
-    git push -f https://"$GITHUB_REPOSITORY_OWNER":"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY".git master || true
+  mapfile -t deps < <(grep "depends_on" ./Formula/"$extension"@7.2.rb | tr -d '"' | cut -d' ' -f 4)
+  if [[ -n "${deps// }" ]]; then
+    printf "\n----- %s -----\n" "$extension"
+    echo "${deps[@]//shivammathur*}"
+    sudo mkdir -p "./.github/deps/$extension"
+    for formula in "${deps[@]//shivammathur*}"; do
+      if [ "x$formula" != "x" ]; then
+        sudo curl -o "./.github/deps/$extension/$formula.rb" -sL "https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/$formula.rb"
+      fi
+    done
+    ls "./.github/deps/$extension"
   fi
+done
+sudo git status
+sudo git add .
+if [ "$(git status --porcelain=v1 2>/dev/null | wc -l)" != "0" ]; then
+  sudo git stash
+  sudo git pull -f https://"$GITHUB_REPOSITORY_OWNER":"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY".git master
+  sudo git stash apply
+  sudo git add .
+  sudo git commit -m "Update extension dependencies"
+  sudo git push -f https://"$GITHUB_REPOSITORY_OWNER":"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY".git master || true
 fi
