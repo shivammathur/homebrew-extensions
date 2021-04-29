bbc474dc7fcdd54bd85b3e8f0e487b5ffc34415b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-29 04:08:18 +0000

    Update swoole@8.1

--- Formula/swoole@8.1.rb
+++ Formula/swoole@8.1.rb
@@ -8,21 +8,19 @@ class SwooleAT81 < AbstractPhpExtension
   init
   desc "Swoole PHP extension"
   homepage "https://github.com/swoole/swoole-src"
-  url "https://github.com/swoole/swoole-src/archive/v4.6.5.tar.gz"
-  sha256 "c93f6a0623b90ca6a6c96f481e6a817f2930b4a582ca0388575b51aa4fb91d38"
+  url "https://github.com/swoole/swoole-src/archive/v4.6.6.tar.gz"
+  sha256 "8cd397bda05da1f3a546a6c537b8896e668af1ae96af8b6c4b2bbf8af57c7dee"
   head "https://github.com/swoole/swoole-src.git"
   license "Apache-2.0"
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "685f36b0bc738d50381d607793275bc5efb31b213676b50432afd9ea2e7a1cac"
-    sha256 cellar: :any, big_sur:       "b5a4bd33caa80b58d51dcfe25a7946f321f5a0eccc22f035d8416f3cb897f538"
-    sha256 cellar: :any, catalina:      "0c98a284d4631dbb335840bf1b961e91636682235bc96f261df67cfe3faf367b"
   end
 
   def install
     patch_spl_symbols
+    inreplace "ext-src/swoole_coroutine.cc", "const char *error_filename", "_zend_string *error_filename"
+    inreplace "ext-src/swoole_server.cc", "? PG(last_error_file)", "? PG(last_error_file)->val"
     safe_phpize
     system "./configure"
     system "make"
