ac84cad66926e900d9060a35040d78b635262133
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-29 04:08:18 +0000

    Update swoole@8.0

--- Formula/swoole@8.0.rb
+++ Formula/swoole@8.0.rb
@@ -8,16 +8,13 @@ class SwooleAT80 < AbstractPhpExtension
   init
   desc "Swoole PHP extension"
   homepage "https://github.com/swoole/swoole-src"
-  url "https://github.com/swoole/swoole-src/archive/v4.6.5.tar.gz"
-  sha256 "c93f6a0623b90ca6a6c96f481e6a817f2930b4a582ca0388575b51aa4fb91d38"
+  url "https://github.com/swoole/swoole-src/archive/v4.6.6.tar.gz"
+  sha256 "8cd397bda05da1f3a546a6c537b8896e668af1ae96af8b6c4b2bbf8af57c7dee"
   head "https://github.com/swoole/swoole-src.git"
   license "Apache-2.0"
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any, arm64_big_sur: "1cb0205de5bb83d9ada789eb949eac70625b2cd3b2de3e679c92d2723a2e1cab"
-    sha256 cellar: :any, big_sur:       "f7fa5260af6929fe2b31cc7b3eca97168b9dfddcf70ee7f6b74a50b46043d34b"
-    sha256 cellar: :any, catalina:      "66966b4eccf75e06308086bc06595c8e1ef1ff66827b2b38e6447fd948b80825"
   end
 
   def install
