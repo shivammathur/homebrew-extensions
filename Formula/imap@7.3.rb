7970753e34a74eef65c553fcb149ba82ec2dd22b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-03 16:21:56 +0000

    imap@7.3 7.3.28
    Update imap@7.3
    
    Closes #750.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/imap@7.3.rb
+++ Formula/imap@7.3.rb
@@ -8,15 +8,12 @@ class ImapAT73 < AbstractPhpExtension
   init
   desc "Imap PHP extension"
   homepage "https://github.com/php/php-src"
-  url "https://www.php.net/distributions/php-7.3.27.tar.xz"
-  sha256 "65f616e2d5b6faacedf62830fa047951b0136d5da34ae59e6744cbaf5dca148d"
+  url "https://www.php.net/distributions/php-7.3.28.tar.xz"
+  sha256 "a2a84dbec8c1eee3f46c5f249eaaa2ecb3f9e7a6f5d0604d2df44ff8d4904dbe"
   license "PHP-3.01"
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 arm64_big_sur: "c1f0d83ffb72b3f182604bdd7bfc3fb13aa2c190733c2325762eb0ba66824693"
-    sha256 big_sur:       "f2a427989799b09b1d8becbb220e23bc273d44d42f3ad0f838464e4a007d3f82"
-    sha256 catalina:      "a66038bd8c3c698aba3e3fe7e8439e9f919572c5135bd99015fadb1015acd9fb"
   end
 
   depends_on "imap-uw"
