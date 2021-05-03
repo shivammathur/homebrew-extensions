2944116081ca1e3f6c147581606067f6f881a339
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-03 16:21:48 +0000

    Update grpc@8.0

--- Formula/grpc@8.0.rb
+++ Formula/grpc@8.0.rb
@@ -8,16 +8,13 @@ class GrpcAT80 < AbstractPhpExtension
   init
   desc "gRPC PHP extension"
   homepage "https://github.com/grpc/grpc"
-  url "https://pecl.php.net/get/grpc-1.37.0.tgz"
-  sha256 "591317e428276a05ca578dce778d4ade47d777a1efa9509db4dd480f82a5e822"
+  url "https://pecl.php.net/get/grpc-1.37.1.tgz"
+  sha256 "2abefeea06491ac76862bacf16e78732ffbf4ffb0b0e4f74263d4f1a5c7745d6"
   head "https://github.com/grpc/grpc.git"
   license "Apache-2.0"
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8d710a2122283f3471b1c0569bd9e32654a00b8e6ffc90be15806bec862929fd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2172bd971f625f798cfa9bc0483ecb2545a1f79633ffc8de76bc4c042616a48b"
-    sha256 cellar: :any_skip_relocation, catalina:      "a318227e6b4761c8e43003ae63650eb5a0fc834deced802b8264b902ecef1d59"
   end
 
   depends_on "grpc"
