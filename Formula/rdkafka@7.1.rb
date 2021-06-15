d182885d26c29009f4ba81f7bf524de52db21c1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-15 18:08:18 +0000

    rdkafka@7.1: update 5.0.0 bottle.

--- Formula/rdkafka@7.1.rb
+++ Formula/rdkafka@7.1.rb
@@ -15,6 +15,10 @@ class RdkafkaAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 arm64_big_sur: "5fe5a5213bbfba62bccc579e8b5c844a476a7e776b1a9fe440cd8385ba180869"
+    sha256 big_sur:       "7a517368f3e074e0b0fcdfcc31f1aeaf2885903c0f098bcd12fdab34b778485c"
+    sha256 catalina:      "5ec5fc5cb67358db4bbd42fe2375cd82d74110c76a362c53fd13a6f961d8b09d"
   end
 
   depends_on "librdkafka"
