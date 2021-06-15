1efe4c024cdb036574f1d421b271d89d2f7174dd
shivammathur <shivam_jpr@hotmail.com>
2021-06-15 22:12:31 +0530

    Add rdkafka --build-only-rdkafka

---
+++ Formula/rdkafka@5.6.rb
@@ -0,0 +1,31 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Rdkafka Extension
+class RdkafkaAT56 < AbstractPhpExtension
+  init
+  desc "Rdkafka PHP extension"
+  homepage "https://github.com/arnaud-lb/php-rdkafka"
+  url "https://pecl.php.net/get/rdkafka-4.1.2.tgz?init=true"
+  sha256 "8ae04c240ce810bc08c07ea09f90daf9df72f0dde220df460985945a3ceec7fc"
+  head "https://github.com/arnaud-lb/php-rdkafka.git"
+  license "MIT"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "librdkafka"
+
+  def install
+    Dir.chdir "rdkafka-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--with-rdkafka=#{Formula["librdkafka"].opt_prefix}"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
