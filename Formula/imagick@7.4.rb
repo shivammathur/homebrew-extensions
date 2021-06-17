ee29cac57e1baf2e47bacc3284e80e32205d5f41
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-17 04:07:08 +0000

    Update imagick@7.4

--- Formula/imagick@7.4.rb
+++ Formula/imagick@7.4.rb
@@ -16,10 +16,6 @@ class ImagickAT74 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 2
-    sha256 arm64_big_sur: "afbf41eb62227c02d6867c6cddec50e42b54b13e58832890155dd3326690664d"
-    sha256 big_sur:       "026d3ed413f47adc321b15aa59f37cf1cf5f23f96cd16c9f2bb8e4a13731df99"
-    sha256 catalina:      "237b05b2dd5b7c6fb70fab62c9ac52f15dc47054825ef85df323107982bf72ae"
   end
 
   depends_on "imagemagick"
