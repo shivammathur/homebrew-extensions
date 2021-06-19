db075c0855d1687182edc818d4083c531cc0cafd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-19 05:07:58 +0000

    Update imagick@7.2

--- Formula/imagick@7.2.rb
+++ Formula/imagick@7.2.rb
@@ -8,18 +8,14 @@ class ImagickAT72 < AbstractPhpExtension
   init
   desc "Imagick PHP extension"
   homepage "https://github.com/Imagick/imagick/releases"
-  url "https://github.com/Imagick/imagick/archive/448c1cd0d58ba2838b9b6dff71c9b7e70a401b90.tar.gz"
-  sha256 "b1b32d524d11ffbd530b270f15205c5523ce81efde3c1bee94f4d2730246c867"
+  url "https://github.com/imagick/imagick/archive/3.5.0.tar.gz"
+  sha256 "f32fb86fefb8531fc32768b9732305f8ada14f249cf3b74c9aaa664c6ae38c27"
   version "3.4.4"
   head "https://github.com/Imagick/imagick"
   license "PHP-3.01"
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 2
-    sha256 arm64_big_sur: "239befd6017286bc24a2820af5ee3dd020de2d82e88295427370c41e4e266dd3"
-    sha256 big_sur:       "3350f0b77a9871bc7c0b751a0b5ad94d6e88778de9d43db2ec2a815072952197"
-    sha256 catalina:      "23e825ab2d13d46ed6e924060ec0aa26efcb356e41f5385f4934c9c1147c47e4"
   end
 
   depends_on "imagemagick"
