3b3d541e40228259bfc46c0bbcc112236c289c1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-30 21:40:02 +0000

    imap@7.2: update 7.2.34 bottle.

--- Formula/imap@7.2.rb
+++ Formula/imap@7.2.rb
@@ -14,6 +14,10 @@ class ImapAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 2
+    sha256 arm64_big_sur: "3626f1cb97192f303e5a8f4a4396c68854d72e78537f66112786c194e1be03c5"
+    sha256 big_sur:       "3a0a507c367557068328520faaf00a0c74b00c199f0bc8e5a4d0da7052a3b022"
+    sha256 catalina:      "54e9c60ac72f280d3599ae7eea985a0f6198452cbc0ee1843d148ff50801dbef"
   end
 
   depends_on "imap-uw"
