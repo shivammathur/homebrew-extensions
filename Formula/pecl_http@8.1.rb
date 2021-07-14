b7eb56c40052465484d5286957d970456f877adf
shivammathur <shivam_jpr@hotmail.com>
2021-07-14 12:48:22 +0530

    pecl_http@8.1: switch to new serialization mechanism

--- Formula/pecl_http@8.1.rb
+++ Formula/pecl_http@8.1.rb
@@ -8,17 +8,14 @@ class PeclHttpAT81 < AbstractPhpExtension
   init
   desc "Pecl_http PHP extension"
   homepage "https://github.com/m6w6/ext-http"
-  url "https://pecl.php.net/get/pecl_http-4.1.0.tgz"
-  sha256 "f8b3a0827c4c8c97ef00d51d09c63fa4203f895bd737f954062c16bae3b8ab1e"
+  url "https://github.com/m6w6/ext-http/archive/88ef34102763b9bd64cc3787ca704b3141c8a905.tar.gz"
+  sha256 "83d4cedd4ec447b9b32b2471d17bd9ccc00b0e5903e438d541230f122a71d726"
+  version "4.1.0"
   head "https://github.com/m6w6/ext-http.git"
   license "BSD-2-Clause"
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 2
-    sha256 arm64_big_sur: "20bac8cd102a48c4bd4aea51b23a2e3e01ec67796b4fa07c4eacb0a8ea388f75"
-    sha256 big_sur:       "cc4b4d7504a3396b1caf7fcfadb661c9d1f5efb3caa48d7290627bab37c2dc46"
-    sha256 catalina:      "b952d696aa2c7a32008f197f922f773df5d97ea3a5be9353c79084f41ffb7239"
   end
 
   depends_on "brotli"
@@ -38,7 +35,6 @@ class PeclHttpAT81 < AbstractPhpExtension
       -I#{Formula["shivammathur/extensions/raphf@8.1"].opt_include}/php
     ]
     ENV["EXTRA_INCLUDES"] = extra_includes * " "
-    Dir.chdir "pecl_http-#{version}"
     inreplace "src/php_http_api.h", "ext/raphf", "ext/raphf@8.1"
     patch_spl_symbols
     safe_phpize
