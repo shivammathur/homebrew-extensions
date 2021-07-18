4e3baead3c2b5370ceb61bea3b41c1b0b66dcb88
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-18 10:03:38 +0000

    Update extension dependencies

--- .github/deps/imagick/imagemagick.rb
+++ .github/deps/imagick/imagemagick.rb
@@ -1,10 +1,9 @@
 class Imagemagick < Formula
   desc "Tools and libraries to manipulate images in many formats"
   homepage "https://www.imagemagick.org/"
-  url "https://www.imagemagick.org/download/releases/ImageMagick-7.1.0-2.tar.xz"
-  sha256 "039006f616bb326598e7b910932694e2a3ca925586560e1b8d153a7048f52980"
+  url "https://www.imagemagick.org/download/releases/ImageMagick-7.1.0-3.tar.xz"
+  sha256 "8b97804f26263e234a5a7a8ab8c7ad1ba458fa5363dfb52ebb755af8bf4add0f"
   license "ImageMagick"
-  revision 1
   head "https://github.com/ImageMagick/ImageMagick.git", branch: "main"
 
   livecheck do
@@ -13,11 +12,11 @@ class Imagemagick < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "a470a24f29e381360a002801386c6013fe8b272f2e8af6b962aefb3cd90052e2"
-    sha256 big_sur:       "81dc2d0a4b698004e170eda1888dd5ba20dc0e925b3a15ed8d219052e40bff09"
-    sha256 catalina:      "580774d0e2374075e78da44768ec8d8e461d128f7d18855325b518b324901611"
-    sha256 mojave:        "a3c97ef87b6d7ba4a50365bd32348d57fbb28227c058f4591695ede8a9743ebc"
-    sha256 x86_64_linux:  "4a4a9b77b5fc715a131b2332df38e64d906862a4b959a80a4e4b409e2f4206da"
+    sha256 arm64_big_sur: "1b47043fa17564c0e1b06bddc852e046775d977ef9931c32630819d7eaeff7f6"
+    sha256 big_sur:       "38307f8f58bfabfde3488c47b4019328b09b0fba22878684bb8c9b8a609b8f18"
+    sha256 catalina:      "5a89251c484cfb8d16b818ec1cb34b841b94dc924737a9ff2eb4876195d947f1"
+    sha256 mojave:        "5b3c0b6025d6dd5d40d53ad4d96338aa6481e983f41352aa39a8817e57feeb16"
+    sha256 x86_64_linux:  "69e47ca62cc210254a33246f6b78df6079de6a59e96016f73af28ebc60a7fb1d"
   end
 
   depends_on "pkg-config" => :build
