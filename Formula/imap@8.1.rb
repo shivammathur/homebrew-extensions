017f34acbdde7cbba70e774cd031b51c6dd88aaf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-21 06:08:00 +0000

    Update imap@8.1

--- Formula/imap@8.1.rb
+++ Formula/imap@8.1.rb
@@ -8,17 +8,13 @@ class ImapAT81 < AbstractPhpExtension
   init
   desc "Imap PHP extension"
   homepage "https://github.com/php/php-src"
-  url "https://github.com/php/php-src/archive/master.tar.gz?commit=339ce9430ebeeae146bb8d59c865ad4cedca7b59"
+  url "https://github.com/php/php-src/archive/master.tar.gz?commit=ae8647d9d3fa420e1116af02ac431772f81c9968"
   version "8.1.0"
-  sha256 "0521e0ed7b86af7061d2c3e8629d1fd107b446c261111180deed4ef1831ba123"
+  sha256 "334168111197a7c1c9d422cf25ef0f65d58d0c0e026075534a7f4e627b84160c"
   license "PHP-3.01"
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 12
-    sha256 arm64_big_sur: "49af8624f9c164d2263bc7c827ad7c344a704ceb2a9e0922a4a1d7fcacf24f14"
-    sha256 big_sur:       "4616ea522716733eb4c2dc082f9df7f6f843e2b18d8177de664ac1adb26bca8a"
-    sha256 catalina:      "6bfedf6be562b257833a33dce7086a04d9c4b53da23f3badcf5efc50f32f2f17"
   end
 
   depends_on "imap-uw"
