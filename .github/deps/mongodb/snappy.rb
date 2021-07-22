1c7f5f8760ba8031b16ecb4bc12582aaee615c2e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-22 22:42:42 +0000

    Update extension dependencies

---
+++ .github/deps/mongodb/snappy.rb
@@ -0,0 +1,103 @@
+class Snappy < Formula
+  desc "Compression/decompression library aiming for high speed"
+  homepage "https://google.github.io/snappy/"
+  url "https://github.com/google/snappy/archive/1.1.9.tar.gz"
+  sha256 "75c1fbb3d618dd3a0483bff0e26d0a92b495bbe5059c8b4f1c962b478b6e06e7"
+  license "BSD-3-Clause"
+  head "https://github.com/google/snappy.git"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_big_sur: "19b5a3afc6646dcec7a1803921b44fb5c57b6734fc0e32f025633f14d1da05ec"
+    sha256 cellar: :any,                 big_sur:       "d73fd47c36e1559d49e1c4c4346c754a9d2ff2af9a0bef25631f52763f19f0ef"
+    sha256 cellar: :any,                 catalina:      "e62a5ab8aa407d6e7d8ddbecdc66fdd1fb256b87730dfe4abbdf8996b3db2869"
+    sha256 cellar: :any,                 mojave:        "b5c89925c1e54ea1e1992d076836092fa754681b373b4834766236abb779cfab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c6708c99972bc8ff6d2ad298cf0cd498853d73b45f9ac95a9370fc70b2c59297"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+
+  # Fix issue where Mojave clang fails due to entering a __GNUC__ block
+  on_macos do
+    depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1100
+  end
+
+  on_linux do
+    # Fix for build failure. Remove with next release.
+    patch do
+      url "https://github.com/google/snappy/commit/0c716d435abe65250100c2caea0e5126ac4e14bd.patch?full_index=1"
+      sha256 "12ff7d1182a35298de3287db32ef8581b8ef600efd6d9509fcc894d3d2056c80"
+    end
+  end
+
+  fails_with :clang do
+    build 1100
+    cause "error: invalid output constraint '=@ccz' in asm"
+  end
+
+  # Fix issue where `snappy` setting -fno-rtti causes build issues on `folly`
+  # `folly` issue ref: https://github.com/facebook/folly/issues/1583
+  patch :DATA
+
+  def install
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
+    on_macos do
+      ENV.llvm_clang if DevelopmentTools.clang_build_version <= 1100
+    end
+
+    # Disable tests/benchmarks used for Snappy development
+    args = std_cmake_args + %w[
+      -DSNAPPY_BUILD_TESTS=OFF
+      -DSNAPPY_BUILD_BENCHMARKS=OFF
+    ]
+
+    system "cmake", ".", *args
+    system "make", "install"
+    system "make", "clean"
+    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON", *args
+    system "make", "install"
+  end
+
+  test do
+    # Force use of Clang on Mojave
+    on_macos { ENV.clang }
+
+    (testpath/"test.cpp").write <<~EOS
+      #include <assert.h>
+      #include <snappy.h>
+      #include <string>
+      using namespace std;
+      using namespace snappy;
+
+      int main()
+      {
+        string source = "Hello World!";
+        string compressed, decompressed;
+        Compress(source.data(), source.size(), &compressed);
+        Uncompress(compressed.data(), compressed.size(), &decompressed);
+        assert(source == decompressed);
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++11", "test.cpp", "-L#{lib}", "-lsnappy", "-o", "test"
+    system "./test"
+  end
+end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 672561e..2f97b73 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -76,10 +76,6 @@ else(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+   # Disable C++ exceptions.
+   string(REGEX REPLACE "-fexceptions" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-exceptions")
+-
+-  # Disable RTTI.
+-  string(REGEX REPLACE "-frtti" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-rtti")
+ endif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+ 
+ # BUILD_SHARED_LIBS is a standard CMake variable, but we declare it here to make
---
+++ .github/deps/mongodb/zstd.rb
@@ -0,0 +1,39 @@
+class Zstd < Formula
+  desc "Zstandard is a real-time compression algorithm"
+  homepage "https://facebook.github.io/zstd/"
+  url "https://github.com/facebook/zstd/archive/v1.5.0.tar.gz"
+  sha256 "0d9ade222c64e912d6957b11c923e214e2e010a18f39bec102f572e693ba2867"
+  license "BSD-3-Clause"
+  head "https://github.com/facebook/zstd.git", branch: "dev"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_big_sur: "e8962c7923904213f312c86372b670b6b5a7ac7103ee63254ab3d1c349913246"
+    sha256 cellar: :any,                 big_sur:       "eae17621cfc664d6e527a6d6aa6a000343eced0f60c81b4e2dd9a9aed7b79c3f"
+    sha256 cellar: :any,                 catalina:      "571d031a8fe1b96f68c4c50c2e72532adbad273c565420cb0825cf4745f512bc"
+    sha256 cellar: :any,                 mojave:        "8089b1b5c398c95af5eaacea6033829dd8d255c9f32d6fa2f0c436821c902087"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0fcd15f865d47d9140af51b05c99a3bab12fdfa10f78b47e296eb82f53f685ba"
+  end
+
+  depends_on "cmake" => :build
+
+  uses_from_macos "zlib"
+
+  def install
+    cd "build/cmake" do
+      system "cmake", "-S", ".", "-B", "builddir",
+                      "-DZSTD_BUILD_CONTRIB=ON",
+                      "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                      *std_cmake_args
+      system "cmake", "--build", "builddir"
+      system "cmake", "--install", "builddir"
+    end
+  end
+
+  test do
+    assert_equal "hello\n",
+      pipe_output("#{bin}/zstd | #{bin}/zstd -d", "hello\n", 0)
+
+    assert_equal "hello\n",
+      pipe_output("#{bin}/pzstd | #{bin}/pzstd -d", "hello\n", 0)
+  end
+end
