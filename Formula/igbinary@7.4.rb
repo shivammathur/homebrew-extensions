37d4c909f17e2c7506a045e57d4bbbaa5ec12eeb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-25 11:11:10 +0000

    Update igbinary@7.4

--- Formula/igbinary@7.4.rb
+++ Formula/igbinary@7.4.rb
@@ -8,16 +8,13 @@ class IgbinaryAT74 < AbstractPhpExtension
   init
   desc "Igbinary PHP extension"
   homepage "https://github.com/igbinary/igbinary"
-  url "https://github.com/igbinary/igbinary/archive/3.2.3.tar.gz"
-  sha256 "c6bb38235e166ddf5713f464f9ab6d16e85783eefa7825824efd252eea6ac4e5"
+  url "https://github.com/igbinary/igbinary/archive/3.2.4.tar.gz"
+  sha256 "30a70eca00d0acaf4979ee532143aebe11cb325a5356b086f357cc3f69fe5550"
   head "https://github.com/igbinary/igbinary.git"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a3a9097fd42db1b0c63a43c8ce6a578c076895bfdbbc0178b63c02a018501a30"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3f5129b6abdac5496a5ed89231f54da8dc37c138bea7884d95ca189dd1bf3d4e"
-    sha256 cellar: :any_skip_relocation, catalina:      "c0abf8770246dea2ffc5c2325d6f0a6722b0885716387e9eb167a4de539c8b97"
   end
 
   def install
