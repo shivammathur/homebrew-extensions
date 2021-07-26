f58fd4d8c1d877bc85eaf89754a479eebfd264b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 03:07:41 +0000

    Update igbinary@7.1

--- Formula/igbinary@7.1.rb
+++ Formula/igbinary@7.1.rb
@@ -8,8 +8,8 @@ class IgbinaryAT71 < AbstractPhpExtension
   init
   desc "Igbinary PHP extension"
   homepage "https://github.com/igbinary/igbinary"
-  url "https://github.com/igbinary/igbinary/archive/3.2.3.tar.gz"
-  sha256 "c6bb38235e166ddf5713f464f9ab6d16e85783eefa7825824efd252eea6ac4e5"
+  url "https://github.com/igbinary/igbinary/archive/3.2.4.tar.gz"
+  sha256 "30a70eca00d0acaf4979ee532143aebe11cb325a5356b086f357cc3f69fe5550"
   head "https://github.com/igbinary/igbinary.git"
   license "BSD-3-Clause"
 
