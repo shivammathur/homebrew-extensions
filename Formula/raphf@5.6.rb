231104bbd9d3b53dab5b2ee11c745edc3a2a6d74
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 05:27:24 +0000

    raphf@5.6: update 1.1.2 bottle.

--- Formula/raphf@5.6.rb
+++ Formula/raphf@5.6.rb
@@ -18,6 +18,7 @@ class RaphfAT56 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "6f828ddb01134c57d9d8aaf8a5c739671f6a7744995a80a0bc5040b29b5216c2"
     sha256 cellar: :any_skip_relocation, big_sur:       "cc06f69d08d8d5b9710ab5858c799788e7a1662361e1640902b819b379006ff9"
     sha256 cellar: :any_skip_relocation, catalina:      "50b3b5781c82add904f128e5c8b82b2da2543093feab1e1c41a1cfb8744d257e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f59b8af341853d110ff905bbacb4e5da172a42d7de7331a0893299ab63897d34"
   end
 
   def install
