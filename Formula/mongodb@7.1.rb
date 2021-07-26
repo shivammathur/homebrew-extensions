a2a8062201b495e87d550ef8a78cbe6066210f90
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 05:16:56 +0000

    mongodb@7.1: update 1.10.0 bottle.

--- Formula/mongodb@7.1.rb
+++ Formula/mongodb@7.1.rb
@@ -16,9 +16,10 @@ class MongodbAT71 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 1
-    sha256 arm64_big_sur: "067c66da5a4fe8e485691a731e9b3d2dbc1a8da645d18cbb5a78a38f99e16059"
-    sha256 big_sur:       "6b4b8a2393abd2af4362b1bd2289ad1952e04cf87d2289145110c7efbf2faa7b"
-    sha256 catalina:      "24c64a8817aca0d7a8a47e843126f12d1ff486ce677024030b1cb9a4461f119b"
+    sha256                               arm64_big_sur: "067c66da5a4fe8e485691a731e9b3d2dbc1a8da645d18cbb5a78a38f99e16059"
+    sha256                               big_sur:       "6b4b8a2393abd2af4362b1bd2289ad1952e04cf87d2289145110c7efbf2faa7b"
+    sha256                               catalina:      "24c64a8817aca0d7a8a47e843126f12d1ff486ce677024030b1cb9a4461f119b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ca03926eec674d37cff618f51251e5a6bd40fe57d650946001cd2ff31bb7c116"
   end
 
   depends_on "icu4c"
