0563d17016e8ec734a33bbe25a21490b235baf93
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 05:32:28 +0000

    xdebug2@7.4: update 2.9.8 bottle.

--- Formula/xdebug2@7.4.rb
+++ Formula/xdebug2@7.4.rb
@@ -18,6 +18,7 @@ class Xdebug2AT74 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "b7a780f4925f7ec3becafd5bedd6e56256aab21d1ace4330158775aa90be0f58"
     sha256 cellar: :any_skip_relocation, big_sur:       "cad5e99bd3b9b35c9bfd9c2325630a1e7f34f40f74eadaa1f0f0fcfa445db77f"
     sha256 cellar: :any_skip_relocation, catalina:      "bdae44ba3d4b4ce595efc702599e2aa5ef690e109886f6c966f138b9f8bc105c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6fdbe89bd5ac04639ba437472057cb3bdfb4eebe086a8409a6abb45af65624d4"
   end
 
   def install
