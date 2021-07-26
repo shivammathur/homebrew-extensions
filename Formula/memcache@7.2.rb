3670652b46a749f6ae7873e52619e98d4f9caa8e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 05:15:29 +0000

    memcache@7.2: update 4.0.5.2 bottle.

--- Formula/memcache@7.2.rb
+++ Formula/memcache@7.2.rb
@@ -18,6 +18,7 @@ class MemcacheAT72 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "b47e577a71ec37a02000a5df1fcbeda5cf9bce28633daeff6acbde7d147fd7dc"
     sha256 cellar: :any_skip_relocation, big_sur:       "42e093cdcfb1b52bcb97b9b56c8eeddb7d0851d391c26ec921b2024fc3184e97"
     sha256 cellar: :any_skip_relocation, catalina:      "a71ae5bd6aa1211c1cfcdeb69dfb6c7a254735d01660efdb56587e43ba7c89ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "da595555c4038d5cdcd80d6a8f4b3cc76019d42662d44181decec1e534902c23"
   end
 
   def install
