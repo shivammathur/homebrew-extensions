c630b592b86906bdf067cbc747a2915c1f06fee2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 05:26:07 +0000

    pcov@7.4: update 1.0.9 bottle.

--- Formula/pcov@7.4.rb
+++ Formula/pcov@7.4.rb
@@ -18,6 +18,7 @@ class PcovAT74 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "cec867cd083630436128112956d7f356bb2c1d97815b518a3cbb1c6585f4e956"
     sha256 cellar: :any_skip_relocation, big_sur:       "6c518a2cb0fcf4016f01c62e4487f858b1578322c298a7273208089f2f0395d6"
     sha256 cellar: :any_skip_relocation, catalina:      "688a2fc74d1c8b9e822439be8689063c956894c5c852d8958f0c28e5a4a3452e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c4ebf3c578a3c333225f9ea68d1cb8cea4008f365780521f14610b9c7bf50a1f"
   end
 
   def install
