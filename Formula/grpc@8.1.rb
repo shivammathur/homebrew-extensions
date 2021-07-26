c53c04319a6c7bbf105a02448f9412ddaf7e6fbf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 04:07:37 +0000

    grpc@8.1: update 1.39.0 bottle.

--- Formula/grpc@8.1.rb
+++ Formula/grpc@8.1.rb
@@ -15,9 +15,10 @@ class GrpcAT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4e0e700e9c63aa3691cc3cec4d4ffb220c02f78153b5175419807f75d2003ab6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "cca80c66a5a3894df3efd0f60d9a8e71a25524b8f7d1da97a36c8942b9abb1ff"
-    sha256 cellar: :any_skip_relocation, catalina:      "98891003d2d2e6c7fbafaed13f87c6b728cc4f267f7f6111e10a5dfca722e348"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6f42bea4590d864c47bb2d9f8bb8cf0eb71232877ff4b59d3a764d6fc5edc3ed"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6d39d7f10f0574e7074813c6538dfa4bea1dda9a3d6d558b94f0eb94621431c9"
+    sha256 cellar: :any_skip_relocation, catalina:      "1e264e0e09e3c40d287d05cb9026978e304d93af79cb9d65bffccdbb4060f4b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "873c956b241590b77ea8b1f22dce59ea2aba2df07bdbfa439b09cbfe27669406"
   end
 
   depends_on "grpc"
