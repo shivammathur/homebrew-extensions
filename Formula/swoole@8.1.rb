f2dd1665eebed61e021384be52777eb3e1ebe2ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 05:30:48 +0000

    swoole@8.1: update 4.7.0 bottle.

--- Formula/swoole@8.1.rb
+++ Formula/swoole@8.1.rb
@@ -15,9 +15,10 @@ class SwooleAT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any, arm64_big_sur: "88afe9e7ed74fc831f8caaa06b74bc771dbb08ca05b7f632783211b84bd1eeaa"
-    sha256 cellar: :any, big_sur:       "b80ac8e0e7f488231217fb8976b8fb75a5436436229a9a941630aacc45f66912"
-    sha256 cellar: :any, catalina:      "4ab14eb3acd746b8aa6064c75d2f1fc2ff1c2b1be9b8a7e7aed27df5581b6d63"
+    sha256 cellar: :any,                 arm64_big_sur: "88afe9e7ed74fc831f8caaa06b74bc771dbb08ca05b7f632783211b84bd1eeaa"
+    sha256 cellar: :any,                 big_sur:       "b80ac8e0e7f488231217fb8976b8fb75a5436436229a9a941630aacc45f66912"
+    sha256 cellar: :any,                 catalina:      "4ab14eb3acd746b8aa6064c75d2f1fc2ff1c2b1be9b8a7e7aed27df5581b6d63"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0c46dcccd7085078730eeb5d487d5d05c2cba682919427a13ddbe4751b59fb64"
   end
 
   def install
