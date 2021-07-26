abf64ce7a4f4fd16e7954d3bd6d33de50768e7c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 05:29:42 +0000

    rdkafka@7.2: update 5.0.0 bottle.

--- Formula/rdkafka@7.2.rb
+++ Formula/rdkafka@7.2.rb
@@ -16,9 +16,10 @@ class RdkafkaAT72 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 1
-    sha256 arm64_big_sur: "103fde7c9c1ee84ceb703622facf6d27ee0dce3bf912db731ea74a7f41c2fd5c"
-    sha256 big_sur:       "8fefc40f48e2012f8f96d907dfa04748aca0eb3c406cb4dc9ff30d530c9fcecf"
-    sha256 catalina:      "4391b9be7c498cd9d6eb2efd9285975ac6671b96167b23bd4412370ab8adcdf6"
+    sha256                               arm64_big_sur: "103fde7c9c1ee84ceb703622facf6d27ee0dce3bf912db731ea74a7f41c2fd5c"
+    sha256                               big_sur:       "8fefc40f48e2012f8f96d907dfa04748aca0eb3c406cb4dc9ff30d530c9fcecf"
+    sha256                               catalina:      "4391b9be7c498cd9d6eb2efd9285975ac6671b96167b23bd4412370ab8adcdf6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ebd4734835063efd6475e505ec1a1d28b2076fa1d55dd9ec7fb3e0cf79034bb8"
   end
 
   depends_on "librdkafka"
