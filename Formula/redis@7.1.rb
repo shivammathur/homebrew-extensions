e35c718d4eb11a8843dfae6c40c3d97072149bf8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 07:44:35 +0000

    redis@7.1: update 5.3.4 bottle.

--- Formula/redis@7.1.rb
+++ Formula/redis@7.1.rb
@@ -15,9 +15,10 @@ class RedisAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 arm64_big_sur: "a21c03b001ad3fa2c49e8586def2ea42a6b018c793842644b66a004bdededc9e"
-    sha256 big_sur:       "d10e64f96b53a58834d8d44e94d60dece392c5c8868b1c445ac7cdea67e9987e"
-    sha256 catalina:      "d176f6035d7948f8c60fc008ebb3adfdfb7ca3f1cca4c1a99ddd007194aa95f5"
+    sha256                               arm64_big_sur: "a21c03b001ad3fa2c49e8586def2ea42a6b018c793842644b66a004bdededc9e"
+    sha256                               big_sur:       "d10e64f96b53a58834d8d44e94d60dece392c5c8868b1c445ac7cdea67e9987e"
+    sha256                               catalina:      "d176f6035d7948f8c60fc008ebb3adfdfb7ca3f1cca4c1a99ddd007194aa95f5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9539fc7a5a23b58af4fe39f652ddb315af82e58ba015b15f35934f7e2b9d22d5"
   end
 
   depends_on "liblzf"
