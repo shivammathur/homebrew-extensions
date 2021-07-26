eb3f6d3d9a74b4dfa8bca2e0e6f7abe38bbe716a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 05:12:49 +0000

    amqp@7.1: update 1.10.2 bottle.

--- Formula/amqp@7.1.rb
+++ Formula/amqp@7.1.rb
@@ -15,9 +15,10 @@ class AmqpAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 arm64_big_sur: "e2da087bef1b943a869b0c21abf890b108a44f59ab0949e3d64b8cfb509efcd3"
-    sha256 big_sur:       "6f4ee43df8601cb43291f4419fc3dee35574a73a702625d48f16b4ca4dd17e71"
-    sha256 catalina:      "55a487f4915f9389d5d9a0c153694ba6bd48eb0cb5163ca4b2832cb33cf6603d"
+    sha256                               arm64_big_sur: "e2da087bef1b943a869b0c21abf890b108a44f59ab0949e3d64b8cfb509efcd3"
+    sha256                               big_sur:       "6f4ee43df8601cb43291f4419fc3dee35574a73a702625d48f16b4ca4dd17e71"
+    sha256                               catalina:      "55a487f4915f9389d5d9a0c153694ba6bd48eb0cb5163ca4b2832cb33cf6603d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0d7022ed09796403d9aa717d58dd85b5cd33d253c08e0fb4add63f2c46444538"
   end
 
   depends_on "rabbitmq-c"
