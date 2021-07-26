a03559c3e268ba3fd84a49cfa20ecfaad8bd9747
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-26 05:27:43 +0000

    protobuf@5.6: update 3.12.4 bottle.

--- Formula/protobuf@5.6.rb
+++ Formula/protobuf@5.6.rb
@@ -18,6 +18,7 @@ class ProtobufAT56 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "f9c5111e2001a08c73ed8fec8ad350331dda5c576caf7299b67299ab2f0664f5"
     sha256 cellar: :any_skip_relocation, big_sur:       "17a65f7f1ee7428f394e807d822e0c958b70d9954b32c690500ecf867e7bf1a8"
     sha256 cellar: :any_skip_relocation, catalina:      "2e986ed0bc71368895eba85f187fe5acd514f5f80bd9bff7ae35ae82361ed0c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e19c7f2eb4090d64d8a10a029ff09be01b4f5115819415ce8404c235234d4177"
   end
 
   def install
