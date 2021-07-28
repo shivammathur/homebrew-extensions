73140a0e69f59833694f816a8a6376b836e27e9e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-28 10:08:23 +0000

    Update extension dependencies

--- .github/deps/imap/krb5.rb
+++ .github/deps/imap/krb5.rb
@@ -1,8 +1,8 @@
 class Krb5 < Formula
   desc "Network authentication protocol"
   homepage "https://web.mit.edu/kerberos/"
-  url "https://kerberos.org/dist/krb5/1.19/krb5-1.19.1.tar.gz"
-  sha256 "fa16f87eb7e3ec3586143c800d7eaff98b5e0dcdf0772af7d98612e49dbeb20b"
+  url "https://kerberos.org/dist/krb5/1.19/krb5-1.19.2.tar.gz"
+  sha256 "10453fee4e3a8f8ce6129059e5c050b8a65dab1c257df68b99b3112eaa0cdf6a"
   license :cannot_represent
 
   livecheck do
@@ -11,11 +11,11 @@ class Krb5 < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "0a2528fac8efc2c289411944004f75c5bed52942bc5c2258636e47a1fab635f9"
-    sha256 big_sur:       "d544c1111503eb27b253e190998b948889ea224b1ebecbceb6a4dd912317eb53"
-    sha256 catalina:      "6a41de7c23c35b555a22349963df4114c498c5b91c345126a1e7deb7acbe31ea"
-    sha256 mojave:        "7697a5ffefab32cacc4d34aec34cb03dab185776025d56d7442d6350cd9fadc1"
-    sha256 x86_64_linux:  "66786d7f83dda619a4a3687045b686fd0507abd475ec9053a94fd2341c31ea12"
+    sha256 arm64_big_sur: "62a2882f4193d6871d9c8116b897a8d33d6a4fedb954646a0eefdc4bfe623110"
+    sha256 big_sur:       "6ae8cffb08f9cba4842a21fc92618e22403a474fbd6d05820c3755401d55688d"
+    sha256 catalina:      "5d0157ff13610f06bc262350978ef1f754e2b7ed0721ba27c431cfd5b5b16639"
+    sha256 mojave:        "9dd6fca906e634be1c1b05a99fa8f28735a969e3eb4d939048e89f322c3a4278"
+    sha256 x86_64_linux:  "2626ed3b8a8e5448aabeac56f6fe91b199fe896a5fd84394d5475213fde1a139"
   end
 
   keg_only :provided_by_macos
