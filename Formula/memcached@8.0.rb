8dad899e9c4eff3b273aeb1177bdacce58f8447b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-08-15 14:39:00 +0000

    memcached@8.0: update 3.1.5 bottle.

--- Formula/memcached@8.0.rb
+++ Formula/memcached@8.0.rb
@@ -16,9 +16,10 @@ class MemcachedAT80 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 arm64_big_sur: "156d31183c41f62c19ac9fab86444116fe0d4c0b546497d54139703314547e7a"
-    sha256 big_sur:       "70e0a23e8a6e12611e7f3068ea3fb9fc2286cca73d94fe677b637548522b3c8d"
-    sha256 catalina:      "283589376dc4a73c966e0d48374cb156c39ddd148443fbc4432abd7b1806c041"
+    sha256                               arm64_big_sur: "156d31183c41f62c19ac9fab86444116fe0d4c0b546497d54139703314547e7a"
+    sha256                               big_sur:       "70e0a23e8a6e12611e7f3068ea3fb9fc2286cca73d94fe677b637548522b3c8d"
+    sha256                               catalina:      "283589376dc4a73c966e0d48374cb156c39ddd148443fbc4432abd7b1806c041"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f18bec2862d40b67a3d9a6bf147056fdad1996037f04ae808b1cc3f7689ccc55"
   end
 
   depends_on "libevent"
