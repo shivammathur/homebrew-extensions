0ec4a2b198d8b1119cc436945b43ef76b04b052c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-01 03:37:35 +0000

    pcov@8.1: update 1.0.9 bottle.

--- Formula/pcov@8.1.rb
+++ Formula/pcov@8.1.rb
@@ -15,10 +15,11 @@ class PcovAT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "676e37e7a9177101eba884745bfc4a02ef022e228507400bdb63c6fb940a6d9b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "db358fc96661381215b3cd6ccea8c14ca06de1ca7708e1b1c1b46b16a378ac2e"
-    sha256 cellar: :any_skip_relocation, catalina:      "718d44a40baebd50fe26aca4b863b83b0e496ac465e62ce959c562cfe6ddc911"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a4007cd038771638e37e2be23679e7f0467d8e674c04b118bbd62b446849ab9"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "87f343b0ea498be44809295921121d46fb8b66a9fe1828cdd4b0db3142bdf3bc"
+    sha256 cellar: :any_skip_relocation, big_sur:       "70254ce643e523bf8426865ed1041364255500bff0c9799fd18467ed0e40ecbc"
+    sha256 cellar: :any_skip_relocation, catalina:      "88d82b347bb89d1759eb0c5da813055bf696a95a3834b4b3f8be404a6ae2a63c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b8a50b9ccb1a1f6c21b947f1bcb8093dd37ccb66f2529555c96be9489a2238e"
   end
 
   def install
