b4688c92c7fcf47b3c462c2f72d74f78fed40983
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-01 16:09:30 +0000

    zmq@8.2: update 1.1.3 bottle.

--- Formula/zmq@8.2.rb
+++ Formula/zmq@8.2.rb
@@ -16,6 +16,11 @@ class ZmqAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_big_sur: "3e24222e75cd52be6d80a634a973196ea1d297bf01d2b4092bac571ddb96b47a"
+    sha256 cellar: :any,                 big_sur:       "bed451d8f88aa0715e52dbb7f5fcfd314cd402d1ee8ae967e923bb1fe6726b2d"
+    sha256 cellar: :any,                 catalina:      "7facc3dda1110e3e16418f0669bc8f4e3bb8c6761dc700b83c0b6b07c036595a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c15fe37adcba65b214b6051cc9d1263e35e7bd2ecdcf3167d7a6325b5a17aa9b"
   end
 
   depends_on "zeromq"
