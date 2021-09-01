d163c8a0f46deac068b5f9b0c398e09ca90d3dac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-01 15:06:27 +0000

    apcu@8.2: update 5.1.20 bottle.

--- Formula/apcu@8.2.rb
+++ Formula/apcu@8.2.rb
@@ -16,6 +16,11 @@ class ApcuAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3b34deb7efed95a9ab33234dcf8c7d7bd66b4eefbcdde3d9ddb63da1166b61f9"
+    sha256 cellar: :any_skip_relocation, big_sur:       "2122d8f1612f663a7f17d0e8f49f05dab4c15fba2e6745252321be927b6ffaf3"
+    sha256 cellar: :any_skip_relocation, catalina:      "a64f6cf9dcde52181ac527acec7d47c4a10df5b3fa01e16bb842adb8b2b15ed3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50c4f23d2ae3fcce387c2512b9e3519fdbc75e86fd92635944e8da767b838ecd"
   end
 
   def install
