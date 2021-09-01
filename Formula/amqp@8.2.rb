a3805c03a5c86bb1e1494122e3bc75c2d7f8e65c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-01 15:06:22 +0000

    amqp@8.2: update 1.11.0 bottle.

--- Formula/amqp@8.2.rb
+++ Formula/amqp@8.2.rb
@@ -16,6 +16,11 @@ class AmqpAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_big_sur: "3d5fd1cf07a3244fe3897f8fe48d7271976af569d29dcdafa6d0d0e066a38d92"
+    sha256 cellar: :any,                 big_sur:       "13203de849d34a257b2a59c7b287a127756fa0087c31a536f8adab9841f4fba5"
+    sha256 cellar: :any,                 catalina:      "46474c1aca9334a479a3de98d36b6e0ab36f91d704e8766f922edbff4959a0fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e91c686449a5e99b0e29dc36d72e2cef4fb0e94067a5f1da2fc96840315992da"
   end
 
   depends_on "rabbitmq-c"
