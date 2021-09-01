f73d3a9c00e88ca3903d7abe24051ad97e16e5ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-01 15:21:33 +0000

    msgpack@8.2: update 2.1.2 bottle.

--- Formula/msgpack@8.2.rb
+++ Formula/msgpack@8.2.rb
@@ -16,6 +16,11 @@ class MsgpackAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f2a14ab6c1d64752695cd29d98b6ea5da4274313fb5fe110719d282e66f54413"
+    sha256 cellar: :any_skip_relocation, big_sur:       "27387323d6a8b1727914ff25ce3ef1b082fcdca51f7aa714fcdc5050bd3d16ee"
+    sha256 cellar: :any_skip_relocation, catalina:      "f5686b58e182730952eb45ebe412d799965b2526874cd185201a8e3e6d6482e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "238e5740d0ad68d620f3486dbd075e90437ba7e3c58525b28aff6e3502de72ca"
   end
 
   def install
