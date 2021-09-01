c48785ba19c310ecb3dfbad40593a5e000dabc48
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-01 05:40:55 +0000

    yaml@8.1: update 2.2.1 bottle.

--- Formula/yaml@8.1.rb
+++ Formula/yaml@8.1.rb
@@ -15,11 +15,11 @@ class YamlAT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_big_sur: "0b9f31627a2c73c2d0cc065e7eeee24cfb15ac60c417485884a00fb42d029a39"
-    sha256 cellar: :any,                 big_sur:       "3a435f6025c9cdcb7abe22fd32057f679b2b785037fe8c8efc22fda4f4604e45"
-    sha256 cellar: :any,                 catalina:      "5983c42802d718548d82e8a2b7978959b56e8846b983e6a0471d6f65b2b968e0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ac35ee326160888c08d57f1ff4a80030e547eac16cabef1dd7b1c38a22819902"
+    rebuild 2
+    sha256 cellar: :any,                 arm64_big_sur: "1c9af33385bd20fa3fa26e8ca9934484a2da91f598dedc65dab7a4b85e47111d"
+    sha256 cellar: :any,                 big_sur:       "24527b29d04384b3b6bdc7461d3369446e1493168a39656f94539ba97d9d583e"
+    sha256 cellar: :any,                 catalina:      "ab8a267b710942dbdfe7b4b4c879acde22379b02ff46711d09dc3ecda460baac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2840fb9736bf5fac7c9f5a936f355b1e3f99994e727df7b0650dee691316a14b"
   end
 
   depends_on "libyaml"
