22f2224c5e37ef0c26a7789775ae15aa4f6b2326
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-01 10:13:48 +0000

    vips@7.1: update 1.0.12 bottle.

--- Formula/vips@7.1.rb
+++ Formula/vips@7.1.rb
@@ -15,6 +15,10 @@ class VipsAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any, arm64_big_sur: "26a74c40683660cc42c1cd7d331dd95d72c5867e6a5ec39ab46891de97525b40"
+    sha256 cellar: :any, big_sur:       "ef4aac2b52a4a22278701d540dc188e44c861ceedd59fceff7ba319ac017eee5"
+    sha256 cellar: :any, catalina:      "a329bfbdda932c271057c468b3533eef5475ba1de791d477e5faee71e2b21bf0"
   end
 
   depends_on "vips"
