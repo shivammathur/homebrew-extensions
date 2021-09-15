db95288225cd65a400241d5dd3d5799a3d981391
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-15 23:03:20 +0000

    expect@8.2: update 0.4.0 bottle.

--- Formula/expect@8.2.rb
+++ Formula/expect@8.2.rb
@@ -15,6 +15,11 @@ class ExpectAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_big_sur: "833e257d97ebb3a7ae59eb499b7c9395be426d00d4394024f3e4e1fa53b85eff"
+    sha256 cellar: :any,                 big_sur:       "5cda292469b8a9815b6023fafbdd184e472f9b2ab07c4f29496a0c19adf4714b"
+    sha256 cellar: :any,                 catalina:      "e5f18ebc22aae3f8fb8bf216dca0a8dd438b2e3a7281c404bd7123f28999b55e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d0323d117fcd1154d6b10e224b6b7ca08ee897293c26ac63a1d4002d757e4c5"
   end
 
   depends_on "expect"
