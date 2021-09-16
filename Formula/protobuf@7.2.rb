3e0e73c237e042552cef3bd4b5b861291bda5f64
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-16 06:08:37 +0000

    protobuf@7.2: update 3.18.0 bottle.

--- Formula/protobuf@7.2.rb
+++ Formula/protobuf@7.2.rb
@@ -15,10 +15,10 @@ class ProtobufAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5c145897fb233ad16c859592d637a4ea9a07fc3a6ddb3c98c0194ec6e050f836"
-    sha256 cellar: :any_skip_relocation, big_sur:       "994f6dcf44d9b1a86a62bea47558bc934299eaec9f8632a76da7a1db82a3e986"
-    sha256 cellar: :any_skip_relocation, catalina:      "77fea2b78f1a9fee5250ae4cdde21c46bcacbe6145d18d80c2128f4867221241"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d298ea3606bcff295cdbcf1c97ce078c3864cc59b73420bde7f7cd1b7503ed51"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e6e49067c5e3505e6effc6c1059281c9966e4ef8bb5c19e15bd7fc6e92975833"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a0110d9e325c74f0b3141c05573f8dd2c394760c21a2883a92911753f49a94e5"
+    sha256 cellar: :any_skip_relocation, catalina:      "740c8d2518fd6f814bac44596530303c0dacb2c3bf7be842b3453eb6e4caa79c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76aca171ccc31aff7247089408a543e2580361797a6cc06260cae1eec08895bb"
   end
 
   def install
