60c884adb2b4fe1b9f43ce79856c23fc5bf002d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-16 04:08:34 +0000

    Update protobuf@7.3

--- Formula/protobuf@7.3.rb
+++ Formula/protobuf@7.3.rb
@@ -8,8 +8,8 @@ class ProtobufAT73 < AbstractPhpExtension
   init
   desc "Protobuf PHP extension"
   homepage "https://github.com/protocolbuffers/protobuf"
-  url "https://pecl.php.net/get/protobuf-3.17.3.tgz"
-  sha256 "b4995573171e61f0e85a1bf4f021b64b7dadbff714019ef2ca7c8723f435d616"
+  url "https://pecl.php.net/get/protobuf-3.18.0.tgz"
+  sha256 "598305799f14aa9e947bbc39679778c374d77f2c93a39ab3dd3fa4f78f68cdad"
   head "https://github.com/protocolbuffers/protobuf.git"
   license "BSD-3-Clause"
 
