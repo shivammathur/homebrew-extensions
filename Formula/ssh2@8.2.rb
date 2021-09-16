fd43ba49b7d4858369e664d408d5b23c93cb5550
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-16 02:20:47 +0000

    ssh2@8.2: update 1.3.1 bottle.

--- Formula/ssh2@8.2.rb
+++ Formula/ssh2@8.2.rb
@@ -15,6 +15,11 @@ class Ssh2AT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_big_sur: "747c159f6d7480a42830121198abf898ffb3c1f6229af9a2255f185c286d98fd"
+    sha256 cellar: :any,                 big_sur:       "cd8302277b1226936f9da8dc1f0d458e338dfcdd358a3e0edd7d819c9157358e"
+    sha256 cellar: :any,                 catalina:      "cb2e4e1dbe52b729f5f4ddc50f75309031a862f2f1c1610249b85fa17fc52331"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d64fdaa9264b12b53f8def1ec0aee23e60aeb179613df7ad9504b6d6894f26e9"
   end
 
   depends_on "libssh2"
