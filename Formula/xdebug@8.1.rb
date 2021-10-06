1c5504dd4f3b5e9fe0dbf88a21659892840f4cca
Michael Lundbøl <michael.lundboel@gmail.com>
2021-10-06 12:20:17 +0200

    update xdebug@8.1
    Signed-off-by: Michael Lundbøl <michael.lundboel@gmail.com>

--- Formula/xdebug@8.1.rb
+++ Formula/xdebug@8.1.rb
@@ -8,18 +8,13 @@ class XdebugAT81 < AbstractPhpExtension
   init
   desc "Xdebug PHP extension"
   homepage "https://github.com/xdebug/xdebug"
-  url "https://github.com/xdebug/xdebug/archive/3.0.4.tar.gz"
-  sha256 "7e4f28fc65c8b535de43b6d2ec57429476a6de1d53c4d440a9108ae8d28e01f4"
+  url "https://github.com/xdebug/xdebug/archive/3.1.0.tar.gz"
+  sha256 "0a023d9847a60bb0aff9509d92457e9c0077942d09e8bedb8e97bd3b90abd7a0"
   head "https://github.com/xdebug/xdebug.git"
   license "PHP-3.0"
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 2
-    sha256 arm64_big_sur: "398f6760560b75ee195d0d5b8d91bc8f161975cfd0e2faa71dcbd33fe8cd39e7"
-    sha256 big_sur:       "9035ebe27442c1a7a540bf639f70af2585340bca60b1850b73b57ac610b38588"
-    sha256 catalina:      "f388c7f96af988e527677f5a394050a08497ddaac3412b97acecbb4db7d1ebf9"
-    sha256 x86_64_linux:  "a397c06605169d9cec5dc9d4edad747fbb141e46a8160b8ab2d176441be96636"
   end
 
   def install
