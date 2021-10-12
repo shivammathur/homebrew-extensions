f1248e697d2028f20a844652161ed4cf9c2f6966
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-12 15:02:24 +0000

    xlswriter@7.3: update 1.5.0 bottle.

--- Formula/xlswriter@7.3.rb
+++ Formula/xlswriter@7.3.rb
@@ -15,6 +15,11 @@ class XlswriterAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "466e11cc5baac14a89f89d3460782c51a053625b0701dea2262c71da72b219be"
+    sha256 cellar: :any_skip_relocation, big_sur:       "2e521122f5b468ba766a0b4de8977f209f77b5dd9a7e3daae667964246e8e7ff"
+    sha256 cellar: :any_skip_relocation, catalina:      "046f156a07372298b2c93e65e89c50ccaec1edd7bf1d746fadd7425771ad6d85"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ac2a090573eb5f3fb332386d245b668a4d8b0e81c4726a7dfd2c0b5e29e1b58"
   end
 
   def install
