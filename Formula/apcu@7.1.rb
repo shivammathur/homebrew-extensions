3502c1595b79202279d9c309ba391952af0064d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-14 05:03:35 +0000

    apcu@7.1: update 5.1.21 bottle.

--- Formula/apcu@7.1.rb
+++ Formula/apcu@7.1.rb
@@ -15,10 +15,10 @@ class ApcuAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "121c6bdcc966f805c367785063a3a20b04adb52b68fb154f90a6a946015e1e5c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "725cb5263b31d2f11f2d6b511e1fd76681c091b7c6d2c9f6723c96c0331526c2"
-    sha256 cellar: :any_skip_relocation, catalina:      "6a28ef46f0b5dd23a8a0a6f5f8e5e253754082e61f0603a12deef0c2bc4f2277"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "95b90fe4bd1150e1d1154033177d4402f7e64539b1d92cadc7c6cde17756a855"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "dc3f3d878bdf3195f819cc187ef948c33baa4815b397fd9189390e0f4069738e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ea6e77aa28d8a95fe378ebda65ed70776286693c60b46e62ab790e91cb768403"
+    sha256 cellar: :any_skip_relocation, catalina:      "c2ef3472a6488c2ea3b1d108059ffcf1ce5f1fa660264efbfa097673a79666b1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "61dee77d50cb91471179de76342b10d2e11b6571c20bb5e9dd9fe46a461ce883"
   end
 
   def install
