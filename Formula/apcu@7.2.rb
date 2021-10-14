7c67abbee3d06a16fe2401c7fdb1d194ea1b1daf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-14 04:10:01 +0000

    Update apcu@7.2

--- Formula/apcu@7.2.rb
+++ Formula/apcu@7.2.rb
@@ -8,8 +8,8 @@ class ApcuAT72 < AbstractPhpExtension
   init
   desc "Apcu PHP extension"
   homepage "https://github.com/krakjoe/apcu"
-  url "https://pecl.php.net/get/apcu-5.1.20.tgz"
-  sha256 "b99d40fafec06f4d132fcee53e7526ddbfc1d041ea6e04e17389dfad28f9c390"
+  url "https://pecl.php.net/get/apcu-5.1.21.tgz"
+  sha256 "1033530448696ee7cadec85050f6df5135fb1330072ef2a74569392acfecfbc1"
   head "https://github.com/krakjoe/apcu.git"
   license "PHP-3.01"
 
