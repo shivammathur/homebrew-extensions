811d0251271271a46c65714f161fa72a3c5e6b4f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-15 15:28:19 +0000

    xdebug@8.0: update 3.1.1 bottle.

--- Formula/xdebug@8.0.rb
+++ Formula/xdebug@8.0.rb
@@ -15,10 +15,10 @@ class XdebugAT80 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 arm64_big_sur: "7529c379d368343fc4f66ac641b075a8a3605ed530dbb779321fd3e81b576673"
-    sha256 big_sur:       "3ff6144fcfe8f549876eb1c1dc0e69506aafc07a69abf3adba9a106d5e6e6b8b"
-    sha256 catalina:      "ec58f4fdd0d86cdd492de7b1af4f16a2c15077f53e20b1f0e76e41685c5a8908"
-    sha256 x86_64_linux:  "5f9d97a622e70d8e3f122fbb02165990d321110faad460238293994e6b59eda3"
+    sha256 arm64_big_sur: "d32882870735d93c3df63ab88513563f6e1b93698a28c8a904253e0fbadaf14e"
+    sha256 big_sur:       "6cabf89fe8fe6696983db1b7892c3dd050ec707e31936be21c7a550db3abdc31"
+    sha256 catalina:      "ff2f43369720c0470c804888cb205f247f43f59f19c7e66b09347d66b605699c"
+    sha256 x86_64_linux:  "8e63f2a0bf719cec547f00715e1b435c63deebb22c11e3da2550078cf8b58c9c"
   end
 
   def install
