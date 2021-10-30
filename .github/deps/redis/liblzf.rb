471f6b3a73dfcdcf086b7e745ff4e44e25cf0679
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-10-30 10:03:56 +0000

    Update extension dependencies

--- .github/deps/redis/liblzf.rb
+++ .github/deps/redis/liblzf.rb
@@ -12,12 +12,14 @@ class Liblzf < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e321946e647108f4f478e84270ef6a49463e18d412fc94a4bc260c5009bd2dba"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1eb80ac962ecb5b94ba1ed3dc86d2baa8a13f231d113a77428879e0a8423ebaf"
-    sha256 cellar: :any_skip_relocation, catalina:      "9aa8a1495947fe1fd6249abe33de7245f9ae4a58dcf900276253b013f7f148e8"
-    sha256 cellar: :any_skip_relocation, mojave:        "62c558b1b9562038c49c1e83b73dfb08d8fca8b924eb36428a5c0bb566408f9d"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "66c9ec26bce56b59ffb317d5a415e6358e8246588a3f247c33b8a8d24e714570"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "04201793eebd3ab8a64694c1422e4d5801be13fd26d1ed257b6c31d4a231124c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a929d667239b01aae65391c9bdb50b8979cf00746728274801dfcb69e7dba54b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e321946e647108f4f478e84270ef6a49463e18d412fc94a4bc260c5009bd2dba"
+    sha256 cellar: :any_skip_relocation, monterey:       "c0a3c8f9311082eb797c848798f98da622a9ec648669298090fa7ef5cdec6b52"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1eb80ac962ecb5b94ba1ed3dc86d2baa8a13f231d113a77428879e0a8423ebaf"
+    sha256 cellar: :any_skip_relocation, catalina:       "9aa8a1495947fe1fd6249abe33de7245f9ae4a58dcf900276253b013f7f148e8"
+    sha256 cellar: :any_skip_relocation, mojave:         "62c558b1b9562038c49c1e83b73dfb08d8fca8b924eb36428a5c0bb566408f9d"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "66c9ec26bce56b59ffb317d5a415e6358e8246588a3f247c33b8a8d24e714570"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "04201793eebd3ab8a64694c1422e4d5801be13fd26d1ed257b6c31d4a231124c"
   end
 
   def install
