6d0742fe0ad14a23f68d61a8249c904b10777d4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-18 04:08:16 +0000

    Update imap@7.4

--- Formula/imap@7.4.rb
+++ Formula/imap@7.4.rb
@@ -8,8 +8,8 @@ class ImapAT74 < AbstractPhpExtension
   init
   desc "Imap PHP extension"
   homepage "https://github.com/php/php-src"
-  url "https://www.php.net/distributions/php-7.4.25.tar.xz"
-  sha256 "12a758f1d7fee544387a28d3cf73226f47e3a52fb3049f07fcc37d156d393c0a"
+  url "https://www.php.net/distributions/php-7.4.26.tar.xz"
+  sha256 "e305b3aafdc85fa73a81c53d3ce30578bc94d1633ec376add193a1e85e0f0ef8"
   license "PHP-3.01"
 
   bottle do
