0edf4dfc6e4459ee13ace251d68ce88527a4b3c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-25 14:08:25 +0000

    mailparse@7.1: update 3.1.2 bottle.

--- Formula/mailparse@7.1.rb
+++ Formula/mailparse@7.1.rb
@@ -15,6 +15,11 @@ class MailparseAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9b3afccd474fd0f7cf3c51eb899892976f6dd0cf07de847decd9813a17910dbb"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d2c166fc1a31488165fadc5d2062249f3e3a963f28c3c4e9da8c91c632ac8e68"
+    sha256 cellar: :any_skip_relocation, catalina:      "f7eafef699871c50e1d6baa7123c73e694c9bbe4e6dd9193dd28a5f4f15c836e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "70c9e66f9392aa9d9d4476a03f4f6c341c34657f03bd66cc1e172a6665f25997"
   end
 
   def install
