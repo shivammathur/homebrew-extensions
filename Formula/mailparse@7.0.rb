e6a313c336739d8a14ec762e37b108c4f9464830
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-11-25 13:23:14 +0000

    Update mailparse@7.0

--- Formula/mailparse@7.0.rb
+++ Formula/mailparse@7.0.rb
@@ -8,7 +8,7 @@ class MailparseAT70 < AbstractPhpExtension
   init
   desc "Mailparse PHP extension"
   homepage "https://github.com/php/pecl-mail-mailparse"
-  url "https://pecl.php.net/get/mailparse-3.1.2.tgz?init=true"
+  url "https://pecl.php.net/get/mailparse-3.1.2.tgz"
   sha256 "b0647ab07ea480fcc13533368e38fdb4f4bb45d30dce65fc90652a670a4f4010"
   head "https://github.com/php/pecl-mail-mailparse.git"
   license "PHP-3.01"
