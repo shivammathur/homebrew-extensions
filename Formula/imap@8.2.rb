74748be24342edf8dc99f33a11a503c47cee6ccc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-16 11:57:53 +0000

    imap@8.2: update 8.2.0 bottle.

--- Formula/imap@8.2.rb
+++ Formula/imap@8.2.rb
@@ -15,11 +15,11 @@ class ImapAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 22
-    sha256 cellar: :any,                 arm64_big_sur: "69ef2cf9e0bd53a169c516fbb2c0f2275f7a164f603c5e6098e34c62a43cd5d8"
-    sha256 cellar: :any,                 big_sur:       "5a516ebdeb8a22c4ee9d43cc2ffd095d79f91d38b486d492814553d971e15219"
-    sha256 cellar: :any,                 catalina:      "29aead635a6f004621431b9779c5075c3ceb7c101045e86491761c0558b7fb2f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8f442a9d5ed8405a49c40891bce893676e47bacaac47da8fa3531ae36f538444"
+    rebuild 23
+    sha256 cellar: :any,                 arm64_big_sur: "bf2423873021475a02ae79d19ad079c77f4eb7e006ec0d4bfbfdb13e4b7997e7"
+    sha256 cellar: :any,                 big_sur:       "2364192728b871824f7c3b1830a5c3d68319de2471b82896c2254d205a9c382f"
+    sha256 cellar: :any,                 catalina:      "46a10b49905d882631ccfa9ec052fbe8cb60b47f1effb76f5488ad35cc0fa4f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7c2ca178f98308178d16ada215d635be08b985ada8b78ddb03d58010f016b6a6"
   end
 
   depends_on "krb5"
