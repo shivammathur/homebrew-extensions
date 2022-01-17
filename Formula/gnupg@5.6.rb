b6dfbcf3f71acce753038f4e97157e98b7cf5eaf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-17 16:04:58 +0000

    gnupg@5.6: update 1.5.1 bottle.

--- Formula/gnupg@5.6.rb
+++ Formula/gnupg@5.6.rb
@@ -15,6 +15,11 @@ class GnupgAT56 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_big_sur: "6a04bd147dddb9a272548d434836fd2e2884eab3ba0bbd34784b3e67d4ca7f48"
+    sha256 cellar: :any,                 big_sur:       "95416d09ded0afd26db0595b7f35d211bcf1d369c5bf2ecbde9cba5ea9dc796d"
+    sha256 cellar: :any,                 catalina:      "db5795e33083c637c2b4c6a992d0f06da8831d002624bbd886476d259d248adb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "83c38b89a2475737769150c71816a37fdc60f45194f43461b54d198b90f3e1a6"
   end
 
   depends_on "gpgme"
