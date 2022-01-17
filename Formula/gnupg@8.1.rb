3837c53d11bc26088f53c6ad3e68809e3a61dd54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-17 14:23:42 +0000

    Update gnupg@8.1

--- Formula/gnupg@8.1.rb
+++ Formula/gnupg@8.1.rb
@@ -8,7 +8,7 @@ class GnupgAT81 < AbstractPhpExtension
   init
   desc "Gnupg PHP extension"
   homepage "https://github.com/php-gnupg/php-gnupg"
-  url "https://pecl.php.net/get/gnupg-1.5.1.tgz?init=true"
+  url "https://pecl.php.net/get/gnupg-1.5.1.tgz"
   sha256 "a9906f465ab2343cb2f3127ee209c72760238745c34878d9bbc1576486219252"
   head "https://github.com/php-gnupg/php-gnupg.git"
   license "BSD-2-Clause"
