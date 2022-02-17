8c139dab5dcb299cb7a3ec52feed5a8147292608
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-17 04:03:25 +0000

    Update xdebug@7.3

--- Formula/xdebug@7.3.rb
+++ Formula/xdebug@7.3.rb
@@ -8,7 +8,7 @@ class XdebugAT73 < AbstractPhpExtension
   init
   desc "Xdebug PHP extension"
   homepage "https://github.com/xdebug/xdebug"
-  url "https://github.com/xdebug/xdebug/archive/3.1.3.tar.gz?revision=1"
+  url "https://github.com/xdebug/xdebug/archive/3.1.3.tar.gz"
   sha256 "6620bf33db616ba52cc6b5976265d8962d8d23321ad5fd63b862c8d47eb5152f"
   head "https://github.com/xdebug/xdebug.git"
   license "PHP-3.0"
