cfb01ba9025aa8ff595523d28f7866ae8f4518bb
Shivam Mathur <shivam_jpr@hotmail.com>
2022-02-25 04:40:24 +0530

    Add vips dependencies
    Refactor sync script

--- .github/scripts/sync.sh
+++ .github/scripts/sync.sh
@@ -3,16 +3,22 @@ git config --local user.email 1589480+BrewTestBot@users.noreply.github.com
 git config --local user.name BrewTestBot
 git config --local pull.rebase true
 
+trunk=https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula
 mapfile -t extensions < <(find ./Formula -maxdepth 1 -name "*@*.rb" -print0 | xargs -0 basename -a | sed "s/@.*//" | sort | uniq)
 for extension in "${extensions[@]}"; do
   mapfile -t deps < <(grep "depends_on" ./Formula/"$extension"@7.2.rb | tr -d '"' | cut -d' ' -f 4)
+  if [ "$extension" = "vips" ]; then
+    mapfile -t vips_deps < <(curl -sL "$trunk"/vips.rb | grep "depends_on" | tr -d '"' | cut -d' ' -f 4)
+    deps=( "${deps[@]}" "${vips_deps[@]}" )
+  fi
+  
   if [[ -n "${deps// }" ]]; then
     printf "\n----- %s -----\n" "$extension"
     echo "${deps[@]//shivammathur*}"
     sudo mkdir -p "./.github/deps/$extension"
     for formula in "${deps[@]//shivammathur*}"; do
       if [ "x$formula" != "x" ]; then
-        sudo curl -o "./.github/deps/$extension/$formula.rb" -sL "https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/$formula.rb"
+        sudo curl -o "./.github/deps/$extension/$formula.rb" -sL "$trunk"/"$formula".rb
       fi
     done
     ls "./.github/deps/$extension"
