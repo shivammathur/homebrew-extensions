3df6e03e1da62ce34e45b502b4bff2d31a8919fc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-24 21:22:47 +0000

    vips@7.1: update 1.0.13 bottle.

--- Formula/vips@7.1.rb
+++ Formula/vips@7.1.rb
@@ -15,10 +15,11 @@ class VipsAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_big_sur: "bfd11fae72526f68d8dd1d80a32e24df91e5e7327beb78a080c896f9ca34ee66"
-    sha256 cellar: :any,                 big_sur:       "94855fb47ecbe01a807dfb6292dbe76a9a42d4e97f50975a464ba87f6cb2c322"
-    sha256 cellar: :any,                 catalina:      "4a4edf79d234f9492a63cc4b06dbe96c3e3f5fb3e92a9df34b58fa3b2628e83c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "25aaaf0bfe45c6fb0f1bc955efdfbdcf276fe8cf4d3b12bd752218760ee6b51a"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_big_sur: "62be9579c0b50cbc43b3fec2954f573c554885e3ee7580ac2021691e59453569"
+    sha256 cellar: :any,                 big_sur:       "5f2a565619b8d5f23bc229d76aef4e149a7d896f3ab484bbe0e3db1214641ece"
+    sha256 cellar: :any,                 catalina:      "1ba5218fa8aea3fa47c98a0ce2e4f1d9cf764f7b8eb6b6d71c7234cf4d2382c7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f8a8f2d3882d592c97d6c973f1826b6febdda24e581adf9ce419f15d77609b2c"
   end
 
   depends_on "vips"
