5dc67c158fc63a5aa1c56f75339a4cd7baab8d29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-02-24 06:32:24 +0000

    mailparse@8.0: update 3.1.3 bottle.

--- Formula/mailparse@8.0.rb
+++ Formula/mailparse@8.0.rb
@@ -15,11 +15,10 @@ class MailparseAT80 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bcc2ca18aa778a2f9757bd26aca003dcbbce59fffdd54eefe97687d2cbcb608f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8eeccef4093fc11ebc2ffde2e710f849f18d69419c344e6965389b784c580ea6"
-    sha256 cellar: :any_skip_relocation, catalina:      "2abae38b4b7748491fd6f1b69acc9898de21ec6376f042a93e6f247f519756f2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d138027e2ef93723fc5cddbd5d520fecef277fba454b72e6167bb895142f552"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "877e777b119d7e116cab6d9a4b525e29f702d32334e162565df31d6bc5cd3d99"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a8173026cbb94819c8f1557cead0c321bef8ff1f0dadf56af19ec723c912d0b7"
+    sha256 cellar: :any_skip_relocation, catalina:      "cb59e5a5f550d0dc38a74c4be198e5278a63c6a6468296db82c931e4cb75b5f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c9d43e964c3bc30b0ac456467e7a302ad9e5625095ec0c0427071818ec753e9c"
   end
 
   def install
