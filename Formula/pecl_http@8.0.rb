bbd7dd316d3c42d12b2ba9dd1535933827784efa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-03-03 04:08:33 +0000

    Update pecl_http@8.0

--- Formula/pecl_http@8.0.rb
+++ Formula/pecl_http@8.0.rb
@@ -8,8 +8,8 @@ class PeclHttpAT80 < AbstractPhpExtension
   init
   desc "Pecl_http PHP extension"
   homepage "https://github.com/m6w6/ext-http"
-  url "https://pecl.php.net/get/pecl_http-4.2.1.tgz"
-  sha256 "ab07d55ccdfa897c897c26f1c0851b557b363431766c9ea4c799ea61443bdcdc"
+  url "https://pecl.php.net/get/pecl_http-4.2.2.tgz"
+  sha256 "c338afd29259f0093f07f4e99b80705a2d5bb046c7ab32e5938eef29bbb63a6e"
   head "https://github.com/m6w6/ext-http.git"
   license "BSD-2-Clause"
 
