cbc4772ea861e9d99222e5addd8c4130a301e2e1
shivammathur <shivam_jpr@hotmail.com>
2022-04-06 07:42:04 +0530

    Attempt to fix pr script --init

--- .github/scripts/pr.sh
+++ .github/scripts/pr.sh
@@ -2,14 +2,16 @@ git config --global user.name "BrewTestBot"
 git config --global user.email "1589480+BrewTestBot@users.noreply.github.com"
 for formula in ./Formula/"$EXTENSION"@*.rb; do
   VERSION="$EXTENSION"@"$(echo "$formula" | grep -Eo '[0-9]+\.[0-9]+')"
+  BRANCH="update-$VERSION-${GITHUB_SHA:0:7}"
   export VERSION
   git checkout "$GITHUB_DEFAULT_BRANCH" || true
-  git checkout -b "update-$VERSION-${GITHUB_SHA:0:7}"
+  git checkout -b "$BRANCH"
   bash .github/scripts/edit.sh
-  if ! git diff --quiet --exit-code; then
+  if ! git diff --exit-code; then
     git add "$formula"
     git commit -m "Update $VERSION"
-    gh pr create --title "Update $VERSION" \
+    gh pr create --head "$BRANCH" \
+                 --title "Update $VERSION" \
                  --body "Build $VERSION" \
                  --base "$GITHUB_DEFAULT_BRANCH" \
                  --assignee "$GITHUB_REPOSITORY_OWNER" \
