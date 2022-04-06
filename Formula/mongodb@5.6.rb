217ca9edd1834aa3991540dfb6c3ea6b352b75d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-06 02:33:28 +0000

    Update mongodb@5.6

--- Formula/mongodb@5.6.rb
+++ Formula/mongodb@5.6.rb
@@ -10,7 +10,6 @@ class MongodbAT56 < AbstractPhpExtension
   homepage "https://github.com/mongodb/mongo-php-driver"
   url "https://pecl.php.net/get/mongodb-1.7.5.tgz"
   sha256 "e48a07618c0ae8be628299991b5f481861c891a22544a2365a63361cc181c379"
-  revision 1
   head "https://github.com/mongodb/mongo-php-driver.git"
   license "Apache-2.0"
 
