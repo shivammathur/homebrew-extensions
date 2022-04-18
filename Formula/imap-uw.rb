d5f73ff31b2f6aa057f103ca2c7acc812a2675bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-18 01:10:10 +0000

    imap-uw: update 2007f bottle.

--- Formula/imap-uw.rb
+++ Formula/imap-uw.rb
@@ -16,6 +16,7 @@ class ImapUw < Formula
     rebuild 2
     sha256 cellar: :any,                 arm64_monterey: "242217503356b37382c539019d7d62f54d4108dbf6c51fbab1dccb4e9a6b25bb"
     sha256 cellar: :any,                 arm64_big_sur:  "06b10eaafa103894a8b75287cb56b58e7d3f0338d6230dc189c9a4112e322dac"
+    sha256 cellar: :any,                 monterey:       "b36fa2a2dfff5bae7e6f9d837385a71a99f0082fd0f7d110472ac4272c25977a"
     sha256 cellar: :any,                 big_sur:        "9ac09b14756bfe8779a436c36d19eb7660fb236beb9aaf9b1f24d1283c387b4e"
     sha256 cellar: :any,                 catalina:       "3a3a035f6149a5f5471e452bb988b79bbda12c5da0af29905637f2633c1e488a"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "f58099004929c2c1270633f40ac8ee86c14a6cfe887ec1f5ea6465d08a4db0bb"
