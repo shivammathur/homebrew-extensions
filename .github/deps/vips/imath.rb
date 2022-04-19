769ede8c5362cf48c0020c9c6f59867538947446
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-04-19 10:08:46 +0000

    Update extension dependencies

--- .github/deps/vips/imath.rb
+++ .github/deps/vips/imath.rb
@@ -1,17 +1,17 @@
 class Imath < Formula
   desc "Library of 2D and 3D vector, matrix, and math operations"
   homepage "https://www.openexr.com/"
-  url "https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v3.1.4.tar.gz"
-  sha256 "fcca5fbb37d375a252bacd8a29935569bdc28b888f01ef1d9299ca0c9e87c17a"
+  url "https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v3.1.5.tar.gz"
+  sha256 "1e9c7c94797cf7b7e61908aed1f80a331088cc7d8873318f70376e4aed5f25fb"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "a062f442cb1477e1191f7c8bc5c679dd58add7b49dee162e1c7aaa84ee2fc0de"
-    sha256 cellar: :any,                 arm64_big_sur:  "990f0df58bc099fee22bc9fbe53e5384e8f5c347840ce59b6c67c50858ca6a8b"
-    sha256 cellar: :any,                 monterey:       "f76322bb6521326e7931313779021735b64d4964e32db9ac645ecbb567b74f8a"
-    sha256 cellar: :any,                 big_sur:        "9415df9168a38e49695164c6469baf4942a54e88da0d001757d0fc9f55aed9ea"
-    sha256 cellar: :any,                 catalina:       "9c18609d792b0969753cfaefe7418ae37d120dff1aaad1da57dd1e2bc8bdf16d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1eec5fa234963e53dfbdf32d814165927323ea98d266993e754e75c7d009d314"
+    sha256 cellar: :any,                 arm64_monterey: "c9a1333aef15ce338d27e3cf73885750c1dadd7cf0a664cf6cd887a841e95c9b"
+    sha256 cellar: :any,                 arm64_big_sur:  "1e8b8cef1b3fee6809cac3d9e73182d1218044960e3295411e6b8c68d3601d42"
+    sha256 cellar: :any,                 monterey:       "60265951d4debb77e090194e08369ace9957411da8d94efd1ec16487b42cbf1b"
+    sha256 cellar: :any,                 big_sur:        "5acea94faccfa4fd8ca1964bce545d751d9404c6e6830a0d9a75031a529cc06f"
+    sha256 cellar: :any,                 catalina:       "cbbb632a0b17f931262850308af109b1ccb9dd533a861069d4de1edb092ae00e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "62d672df3af0e944fcc190526981aede2b48c65c65de7b602a5b55cd882602f3"
   end
 
   depends_on "cmake" => :build
