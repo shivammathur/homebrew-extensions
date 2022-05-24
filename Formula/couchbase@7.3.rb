7be69b99d7460226048afad1914082d74904a8a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-05-24 07:02:55 +0000

    couchbase@7.3: update 3.2.2 bottle.

--- Formula/couchbase@7.3.rb
+++ Formula/couchbase@7.3.rb
@@ -15,6 +15,13 @@ class CouchbaseAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "f4334d946fe5d8736c4f1758e960f08f5aa34fd780675e7ef7e5d7e36b8164fb"
+    sha256 cellar: :any,                 arm64_big_sur:  "d6dc416b3d0f3ae804df21d85ac426cb2277db6e32c8019d9db8d3ef4a766042"
+    sha256 cellar: :any,                 monterey:       "3466abd062360a2bd3ca69800aa47a1618ef518df4c846772687506220fd04ad"
+    sha256 cellar: :any,                 big_sur:        "24f2f40486fd6d9b7c350bad9f32687cbc039c98a6d5580cac8c4a48d4362d0d"
+    sha256 cellar: :any,                 catalina:       "8c1d3b6adee7b56cd90eea0d8a4627175e16dbbc4d695a12773f813ca22e974e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a2db20fa6d0368f787a61e338b12306144d59a9171287d0bed528dff213e6410"
   end
 
   depends_on "libcouchbase"
