e939e431e7ed79f039befe370a75836a740c4287
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-06 18:06:23 +0000

    xdebug@7.4: update 3.1.5 bottle.

--- Formula/xdebug@7.4.rb
+++ Formula/xdebug@7.4.rb
@@ -15,10 +15,12 @@ class XdebugAT74 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 arm64_big_sur: "7413441ebc43a0b83bf672872113ae3e2f77a2deee470fb9c7b916bb1a7e7355"
-    sha256 big_sur:       "e468106bba0ba98a2c10ed0dcb3c14c9beab8fcab01a805f2a1f695922deeb53"
-    sha256 catalina:      "3793f0a90be61d5d998be78c0ba3ffe2070a04ab65f3437a36c422ebf3fffaa2"
-    sha256 x86_64_linux:  "46e174dc338bb0ee261c14c3e226fd9500265f2de0dfccedc47d3b3f8a11bbeb"
+    sha256 arm64_monterey: "35baaca5764b64748da5c883e38b0a9ae73bbccd066f750a2ea585cdbe05e6c8"
+    sha256 arm64_big_sur:  "b02510c762cb11e54cfa596bcb1373dc74b00025038b293c3501dbff1107649e"
+    sha256 monterey:       "e6c15d3be5f41fd4994b32bc692e4790adb125c9d98106c3a7e78859f9eb938d"
+    sha256 big_sur:        "dcc9b8028958151e87b5885722c472f549d32ce35961ca05c679c72200658299"
+    sha256 catalina:       "01a2b2fc675232dc6553d04805f98c1fe0fe2c055fed36c0c7d7b3b97f01e801"
+    sha256 x86_64_linux:   "c3f031465412ec06400b61ddad306bf7a6a908493464819f1c3203f7071951c5"
   end
 
   uses_from_macos "zlib"
