0330d006b46b1197ee8f45ad50f261ee1bc4832a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-23 10:02:07 +0000

    pecl_http@8.0: update 4.2.3 bottle.

--- Formula/pecl_http@8.0.rb
+++ Formula/pecl_http@8.0.rb
@@ -16,6 +16,7 @@ class PeclHttpAT80 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any,                 arm64_big_sur: "6b6c4dd1e16eff65457c7f20f353cc5188cb7575c2195a9e432e81f068b6b0d9"
+    sha256 cellar: :any,                 monterey:      "47737f8f3dac7d2f9e77a76ec25582c0515bb3457589122a7a9127325bf688ed"
     sha256 cellar: :any,                 big_sur:       "6388e7d7c061652150fbb5db61d6209eaccc4efbd6fb6e072885e490cc7c8e2a"
     sha256 cellar: :any,                 catalina:      "e5d23a665a03b9a2b716bc03545205d8108951023199d9d8ee07798ef1e4c67f"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "02d3d73ad1fd9e81eee8a46ba8d09604513e0cc983075aad5ea66228ec2b457e"
