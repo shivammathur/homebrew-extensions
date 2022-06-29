2e2c00ac49bf7d9dd8b16bebd69f5878fdfa022a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-06-29 09:37:22 +0000

    msgpack@7.3: update 2.1.2 bottle.

--- Formula/msgpack@7.3.rb
+++ Formula/msgpack@7.3.rb
@@ -15,12 +15,13 @@ class MsgpackAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6453a6d5a7b7237a1e69dc747620a1d5082da2f7fef3b3a7e01f8ef8211d8041"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1daac878d3d85b4044518e096cf57d98d75a6100a8a05d2df8114abf635fbe62"
-    sha256 cellar: :any_skip_relocation, monterey:       "5f2e37880e265ce475f08e106a813ba1e647fd07c2d299e4f20cf38c7e31c3c6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "79ebdc76c521375d6427e51a905f81381da74deee2d0e24c5c1285318e7fabc8"
-    sha256 cellar: :any_skip_relocation, catalina:       "4efe6796c44eedc85a62196fcf66d9cc2aac1958254b89d995c43e3490859ebe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6851106016c98fec5be6a1cd8a038519000e36e90c02d3719adddf5f90071bb6"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "66a196a29f8f564478039db591c7199370e9fa2a8a954a6d9883055d16a9d23c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "42360c2d8652e29c06ac9f9dbd445bdbf124feecc91dd95ad5835b5280e2d8b4"
+    sha256 cellar: :any_skip_relocation, monterey:       "18a9d7a499b11e0513b37e3fea4c5148a8beadade6d2245465f68cfd01cd5883"
+    sha256 cellar: :any_skip_relocation, big_sur:        "8fdad3e50928455921fff01c8e8c8ee18da17b187b7ef10ce438c3691e037452"
+    sha256 cellar: :any_skip_relocation, catalina:       "e274fff3476b853436221787211178e4fe7e3bad44a3862706ac3983ad68d0cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9a056a5114c0d6aea7db59f860db5c54c0c9dbe53e34faf45518eb53f1246e36"
   end
 
   def install
