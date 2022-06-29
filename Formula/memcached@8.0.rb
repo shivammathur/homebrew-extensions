24730657038c9fc539a96f87966c7caf66c97165
Shivam Mathur <shivam_jpr@hotmail.com>
2022-06-29 02:07:39 +0000

    Update memcached@8.0

--- Formula/memcached@8.0.rb
+++ Formula/memcached@8.0.rb
@@ -8,9 +8,8 @@ class MemcachedAT80 < AbstractPhpExtension
   init
   desc "Memcached PHP extension"
   homepage "https://github.com/php-memcached-dev/php-memcached"
-  url "https://github.com/php-memcached-dev/php-memcached/archive/19a02bb5bfaeb520b857a2d64172f7d2a9615fb3.tar.gz"
-  sha256 "63fdeff6398ebbe1c15c89b86a628a2e00305fb36f4304b3a1f5aa097ad098f6"
-  version "3.1.5"
+  url "https://pecl.php.net/get/memcached-3.2.0.tgz"
+  sha256 "2a41143a7b29f4a962a3805b77aa207a99e4566e2d314ce98a051cd24d6e9636"
   head "https://github.com/php-memcached-dev/php-memcached.git"
   license "PHP-3.01"
 
@@ -33,7 +32,7 @@ class MemcachedAT80 < AbstractPhpExtension
     %w[igbinary msgpack].each do |e|
       mkdir_p "include/php/ext/#{e}"
       headers = Dir["#{Formula["#{e}@8.0"].opt_include}/**/*.h"]
-      (buildpath/"include/php/ext/#{e}").install_symlink headers unless headers.empty?
+      (buildpath/"memcached-#{version}/include/php/ext/#{e}").install_symlink headers unless headers.empty?
     end
   end
 
@@ -48,6 +47,7 @@ class MemcachedAT80 < AbstractPhpExtension
       --with-libmemcached-dir=#{Formula["libmemcached"].opt_prefix}
       --with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr
     ]
+    Dir.chdir "memcached-#{version}"
     patch_memcached
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig, *args
