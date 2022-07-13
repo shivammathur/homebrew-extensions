7aeb1487a03f1121ae601117512fd67872ddb32e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-13 10:04:19 +0000

    Update extension dependencies

--- .github/deps/pecl_http/libidn2.rb
+++ .github/deps/pecl_http/libidn2.rb
@@ -1,10 +1,10 @@
 class Libidn2 < Formula
   desc "International domain name library (IDNA2008, Punycode and TR46)"
   homepage "https://www.gnu.org/software/libidn/#libidn2"
-  url "https://ftp.gnu.org/gnu/libidn/libidn2-2.3.2.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libidn/libidn2-2.3.2.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/libidn/libidn2-2.3.2.tar.gz"
-  sha256 "76940cd4e778e8093579a9d195b25fff5e936e9dc6242068528b437a76764f91"
+  url "https://ftp.gnu.org/gnu/libidn/libidn2-2.3.3.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libidn/libidn2-2.3.3.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/libidn/libidn2-2.3.3.tar.gz"
+  sha256 "f3ac987522c00d33d44b323cae424e2cffcb4c63c6aa6cd1376edacbf1c36eb0"
   license any_of: ["GPL-2.0-or-later", "LGPL-3.0-or-later"]
 
   livecheck do
@@ -13,13 +13,12 @@ class Libidn2 < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "fcc51d2c385b19d647da5a53f7041f13f97d2c1119a7cbbfd8433e9e55bf5012"
-    sha256 arm64_big_sur:  "dbaac7e6e29ffa8c7c2b5e152fd6ee0118e547f90dc4b180c7f168c2f681c5f4"
-    sha256 monterey:       "29b1ea810ddad662b0c766429c2384495d643baa253dc31eed0300f5d4c4d7f4"
-    sha256 big_sur:        "d21350f576f9b9cd0512149164622671b71854da69947183bc84e09a3a257b89"
-    sha256 catalina:       "71c5f183ae570f9a77eb759ab2bd04d84eb5cb9cf9c9a3b7cd8879aad5966bcd"
-    sha256 mojave:         "9402e3774f00c5485dd341cf34c35c24de0dc9bb90b2d4057c22e432848e0f1f"
-    sha256 x86_64_linux:   "57a2bf8955bcc8c2661aec2b26acfb90ec50402d78afebb000a1f6b0c27421e4"
+    sha256 arm64_monterey: "e2d5ad350bd11194d179b2bf199103ce3a0f4aa57cca95fc8ac1f0bc104912c3"
+    sha256 arm64_big_sur:  "8ff0838515b514f57fffa8964669815e5fe9e9fc21eeb559210e1f5634ac5483"
+    sha256 monterey:       "1ed7a729a0e671778e89599ccf3df8536e41b95b30052827ee2a53a252a06519"
+    sha256 big_sur:        "80fd3d0e971eb6b2620141249d863232a382b0db606d7e95923cf2f753a41e3a"
+    sha256 catalina:       "fc09aa5df61b841c40a23490df6b2d556772c927520d7e0aac099029a11dd768"
+    sha256 x86_64_linux:   "0ceff03509ea09a0784aa40b7b724f40857dfac8e9e34b36ee93570c57eb1780"
   end
 
   head do
