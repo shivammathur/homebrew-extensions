0d05011804a7a95b689a7f7426d16fbc486fbacc
Shivam Mathur <shivam_jpr@hotmail.com>
2022-07-14 07:41:25 +0000

    Update swoole@8.2

--- Formula/swoole@8.2.rb
+++ Formula/swoole@8.2.rb
@@ -8,8 +8,8 @@ class SwooleAT82 < AbstractPhpExtension
   init
   desc "Swoole PHP extension"
   homepage "https://github.com/swoole/swoole-src"
-  url "https://github.com/swoole/swoole-src/archive/v4.8.10.tar.gz"
-  sha256 "0bf908cee05b0aafec9fbbd3bf4077f1eeac334756f866c77058eb1bfca66fd7"
+  url "https://github.com/swoole/swoole-src/archive/v4.8.11.tar.gz"
+  sha256 "b81c682e4b865d6e3839b8b83640242f54127f669550111f5e99fae80ef1e142"
   head "https://github.com/swoole/swoole-src.git"
   license "Apache-2.0"
 
@@ -29,6 +29,11 @@ class SwooleAT82 < AbstractPhpExtension
 
   def install
     patch_spl_symbols
+    inreplace "ext-src/swoole_coroutine.cc" do |s|
+      s.gsub! "zend_bool *zend_vm_interrupt", "zend_atomic_bool *zend_vm_interrupt"
+      s.gsub! "zend_vm_interrupt = &EG(vm_interrupt)", "zend_atomic_bool_load_ex(&EG(vm_interrupt))"
+      s.gsub! "*zend_vm_interrupt = 1", "zend_atomic_bool_store_ex(&EG(vm_interrupt), true)"
+    end
     safe_phpize
     system "./configure"
     system "make"
