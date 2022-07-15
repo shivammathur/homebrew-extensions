100eca9505902a6364128d0024d46c631af9442d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-15 14:54:20 +0000

    psr@7.2: update 1.1.0 bottle.

--- Formula/psr@7.2.rb
+++ Formula/psr@7.2.rb
@@ -11,11 +11,12 @@ class PsrAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1174d0390fa8ebdad9c352490709e5e9ba5a3c26307bd662a6d44735bb653b51"
-    sha256 cellar: :any_skip_relocation, monterey:      "e8a3e3cbb4cf1f836733f8ae3b68a4ea12727b5a30d0feea3523569faa6e0db3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6f628e993bd86d669febb15b30017def78885cda5a2e861b223f355090a32844"
-    sha256 cellar: :any_skip_relocation, catalina:      "7631d58de4468f14c478545ec51cde02dbb71c1b14f5bb63eb494d96f46434bd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b6aec61b8820a7c2f197873321855703e4442b5e51582b571d1764db30d73cec"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4494e93cbf3c8c7f5854cc8921bd43ad0bb900eb43ee69dd6b883ed9015d8a9c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1174d0390fa8ebdad9c352490709e5e9ba5a3c26307bd662a6d44735bb653b51"
+    sha256 cellar: :any_skip_relocation, monterey:       "e8a3e3cbb4cf1f836733f8ae3b68a4ea12727b5a30d0feea3523569faa6e0db3"
+    sha256 cellar: :any_skip_relocation, big_sur:        "6f628e993bd86d669febb15b30017def78885cda5a2e861b223f355090a32844"
+    sha256 cellar: :any_skip_relocation, catalina:       "7631d58de4468f14c478545ec51cde02dbb71c1b14f5bb63eb494d96f46434bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b6aec61b8820a7c2f197873321855703e4442b5e51582b571d1764db30d73cec"
   end
 
   depends_on "pcre"
