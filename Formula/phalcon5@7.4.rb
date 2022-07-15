6398365e5cbc504a5f10a2474b5d978b0c06c343
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-15 17:44:42 +0000

    phalcon5@7.4: update 5.0.0RC3 bottle.

--- Formula/phalcon5@7.4.rb
+++ Formula/phalcon5@7.4.rb
@@ -15,6 +15,13 @@ class Phalcon5AT74 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e3a95d192541a3a0029f761f60c70d92dec89edd5f78b4521c74d623f8e480c5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "863974e0e4975c080d1051cb53cbe258ac577b700fe1c9e96d8465d4333f14b4"
+    sha256 cellar: :any_skip_relocation, monterey:       "1804794a3cf3be4de29d64d0adaa00183686a324370e245521716ea9320979dd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0cb0ecb69a7d2b9f156d88f30867ae6ba2cd46183e3524b7646fc434749158ba"
+    sha256 cellar: :any_skip_relocation, catalina:       "f82362e2fa41b45fceabc7ecb0d263989ff5858dff8afdb382c70eef6ae5b887"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "401e401ab248d0a89d3b00e0e7ce15154f659fa14abcdab67061df6415dbb43e"
   end
 
   depends_on "pcre"
