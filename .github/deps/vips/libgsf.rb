9964b5a9c0581f4dda9ff8bada7482a99cba53ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-09 10:04:24 +0000

    Update extension dependencies

--- .github/deps/vips/libgsf.rb
+++ .github/deps/vips/libgsf.rb
@@ -1,17 +1,17 @@
 class Libgsf < Formula
   desc "I/O abstraction library for dealing with structured file formats"
   homepage "https://gitlab.gnome.org/GNOME/libgsf"
-  url "https://download.gnome.org/sources/libgsf/1.14/libgsf-1.14.49.tar.xz"
-  sha256 "e9ebe36688f010c9e6e40c8903f3732948deb8aca032578d07d0751bd82cf857"
-  license "LGPL-2.1-only"
+  url "https://download.gnome.org/sources/libgsf/1.14/libgsf-1.14.50.tar.xz"
+  sha256 "6e6c20d0778339069d583c0d63759d297e817ea10d0d897ebbe965f16e2e8e52"
+  license all_of: ["GPL-2.0-or-later", "LGPL-2.1-only"]
 
   bottle do
-    sha256 arm64_monterey: "b4f7cf3af8caa8b37b04297651f835ad4817f2eadd801053929701b89a5a20aa"
-    sha256 arm64_big_sur:  "09eec762124167dbba66e3c9f66eb6720ddb99f93f720da78161a57adda6bbf8"
-    sha256 monterey:       "5e179d8367799493a823c13dddcd960f6a94335b61190531c6830b0d7139259e"
-    sha256 big_sur:        "6256f65f5cfbc1f5a7ac01713c36490c6bb7be1ca779b1f30a076850a6091821"
-    sha256 catalina:       "4bbd87e330bcbadd98b49655fffc7e4ab18e27c6015cd295a2fca378ac5575a7"
-    sha256 x86_64_linux:   "d9ffdb9168375f025302a615f3976954eb297cdc17784aa4a665e0f7c09f0a51"
+    sha256 arm64_monterey: "5c2386595e059d1cead2c6faf9b57544ff41d9b306e2ad60f2be57157256b966"
+    sha256 arm64_big_sur:  "6ed258aa2e65be0a98bd5778d88883533f4814d74a4d8311b1875184ff3c5ed1"
+    sha256 monterey:       "d405f26c28d748604664dbed110bbbe520a9fb10c109a06b3fbaf8409b7ef6a0"
+    sha256 big_sur:        "3f7214b79d4035c79a3b505d78e65e15fd55a14a5eecce4efb5e078af3afc2a1"
+    sha256 catalina:       "79404074c9a4ac0af485e76375b60cfc8fd5c03f10fc42bc7c517be717a5b33c"
+    sha256 x86_64_linux:   "f601eba91c5184233aa5ad6b43776453f6b183484ded55280cfafd9350cb214e"
   end
 
   head do
