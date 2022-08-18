16511a7dd5f79a03af184e2921fda4877cfca3a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-18 10:04:20 +0000

    Update extension dependencies

--- .github/deps/redis/lz4.rb
+++ .github/deps/redis/lz4.rb
@@ -1,10 +1,10 @@
 class Lz4 < Formula
   desc "Extremely Fast Compression algorithm"
   homepage "https://lz4.github.io/lz4/"
-  url "https://github.com/lz4/lz4/archive/v1.9.3.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/lz4-1.9.3.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/lz4-1.9.3.tar.gz"
-  sha256 "030644df4611007ff7dc962d981f390361e6c97a34e5cbc393ddfbe019ffe2c1"
+  url "https://github.com/lz4/lz4/archive/v1.9.4.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/lz4-1.9.4.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/lz4-1.9.4.tar.gz"
+  sha256 "0b0e3aa07c8c063ddf40b082bdf7e37a1562bda40a0ff5272957f3e987e0e54b"
   license "BSD-2-Clause"
   head "https://github.com/lz4/lz4.git", branch: "dev"
 
@@ -14,14 +14,12 @@ class Lz4 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "d222923849a6fefd391ab6705f1468c10d287c08ab9c4b5053a18a552139e262"
-    sha256 cellar: :any,                 arm64_big_sur:  "1d915415cff308983a50f873db4f0de6acab7b57d65a93ec127c06c824ca0269"
-    sha256 cellar: :any,                 monterey:       "5ed09cfe61aa92ae7e28a5a9acf0dfddd5e327f8b2995d1bbe96f63d79fd4f54"
-    sha256 cellar: :any,                 big_sur:        "7024d0b6ee857352cbd3138f752496b87fa27252adbc6daefa4a6c64d3e347e5"
-    sha256 cellar: :any,                 catalina:       "899aeb12833a982e06013a60aa9b1ee69e3f77f969a5aa2dcec02ad329f369bb"
-    sha256 cellar: :any,                 mojave:         "e6adc6da46164495cf129c9e54bd69c6620eb4622a38e403edf1b5f488d044a8"
-    sha256 cellar: :any,                 high_sierra:    "46e99b27c33fd51a4394850be3559ea7b69fc26060ab2095dae315be14aa5e94"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "902257ec34dd2beebcf22bb68c9ccd179008c2ba8d725436c3c5cd5a503e4665"
+    sha256 cellar: :any,                 arm64_monterey: "284fa580570efdc8056e4fc95dc05f7b0546aa0c346795dd616d4cec8eb99426"
+    sha256 cellar: :any,                 arm64_big_sur:  "8cf59a354786ad0ed95a7b531d7149ae03612081818dcdf2d9ca8cb4fe28c07a"
+    sha256 cellar: :any,                 monterey:       "88b369cea90a0a119c24aa96a614fe7d77de58d18cb1803023dc925679eb905f"
+    sha256 cellar: :any,                 big_sur:        "aafb93487e108d302d060265898e4eaa82f5c806ff36dec50871db1c33fdc04d"
+    sha256 cellar: :any,                 catalina:       "ddb59c42498843638f1f9d80bd0c7b7126910c4fc8ee7c69fa8784dd4bc95c1f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1757fefc3840e11c4822e4c2a95aa62aca44a4eaccce6f5c414ea51d1e58bf8e"
   end
 
   def install
