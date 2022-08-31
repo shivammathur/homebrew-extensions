e1c9e06e3407b891dcf95ee1dfd53f94b1d60c92
shivammathur <shivam_jpr@hotmail.com>
2022-08-31 08:54:47 +0530

    Add grpc@8.3

---
+++ Formula/grpc@8.3.rb
@@ -0,0 +1,31 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for GRPC Extension
+class GrpcAT83 < AbstractPhpExtension
+  init
+  desc "gRPC PHP extension"
+  homepage "https://github.com/grpc/grpc"
+  url "https://pecl.php.net/get/grpc-1.48.0.tgz"
+  sha256 "4b4ccb491355f938d28e63a476df92d5109263ea63ffee1e0249616461e26963"
+  head "https://github.com/grpc/grpc.git", branch: "master"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "grpc"
+
+  def install
+    Dir.chdir "grpc-#{version}"
+    patch_spl_symbols
+    safe_phpize
+    system "./configure", "--enable-grpc"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+  end
+end
