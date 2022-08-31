ef766352e1fc62ef3a260e652f84db4c1631046a
shivammathur <shivam_jpr@hotmail.com>
2022-08-31 09:19:07 +0530

    Add yaml@8.3

---
+++ Formula/yaml@8.3.rb
@@ -0,0 +1,35 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Yaml Extension
+class YamlAT83 < AbstractPhpExtension
+  init
+  desc "Yaml PHP extension"
+  homepage "https://github.com/php/pecl-file_formats-yaml"
+  url "https://pecl.php.net/get/yaml-2.2.2.tgz"
+  sha256 "119052f0461d57d86f44c252f9c9b2dd743486c701c1a0aba0aebecdd0d8b82a"
+  head "https://github.com/php/pecl-file_formats-yaml.git", branch: "php7"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "libyaml"
+
+  def install
+    args = %W[
+      --with-yaml=#{Formula["libyaml"].opt_prefix}
+    ]
+    Dir.chdir "yaml-#{version}"
+    inreplace "detect.c", "ZEND_ATOL(*lval, buf)", "*lval = ZEND_ATOL(buf)"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
