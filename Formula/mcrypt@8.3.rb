a8fe47b7ba9464b669bc750449e25feb1ad3605d
shivammathur <shivam_jpr@hotmail.com>
2022-08-31 08:55:50 +0530

    Add mcrypt@8.3

---
+++ Formula/mcrypt@8.3.rb
@@ -0,0 +1,54 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Mcrypt Extension
+class McryptAT83 < AbstractPhpExtension
+  init
+  desc "Mcrypt PHP extension"
+  homepage "https://github.com/php/pecl-encryption-mcrypt"
+  url "https://pecl.php.net/get/mcrypt-1.0.5.tgz"
+  sha256 "c9f51e211640a15d2a983f5d80e26660656351651d6f682d657bdf1cfa07d8a3"
+  head "https://github.com/php/pecl-encryption-mcrypt.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "automake" => :build
+  depends_on "libtool"
+
+  resource "libmcrypt" do
+    url "https://downloads.sourceforge.net/project/mcrypt/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz"
+    sha256 "e4eb6c074bbab168ac47b947c195ff8cef9d51a211cdd18ca9c9ef34d27a373e"
+  end
+
+  def install
+    # Work around configure issues with Xcode 12
+    ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
+
+    resource("libmcrypt").stage do
+      # Workaround for ancient config files not recognising aarch64 macos.
+      %w[config.guess config.sub].each do |fn|
+        cp "#{Formula["automake"].opt_prefix}/share/automake-#{Formula["automake"].version.major_minor}/#{fn}", fn
+      end
+
+      # Avoid flat_namespace usage on macOS
+      inreplace "./configure", "${wl}-flat_namespace ${wl}-undefined ${wl}suppress", "" if OS.mac?
+
+      system "./configure", "--prefix=#{prefix}",
+                            "--mandir=#{man}"
+      system "make", "install"
+    end
+
+    Dir.chdir "mcrypt-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--with-mcrypt=#{prefix}"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
