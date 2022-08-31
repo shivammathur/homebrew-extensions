13aa825e970e3c4ac655b5174ab7f0ec9b1ee1ef
shivammathur <shivam_jpr@hotmail.com>
2022-08-31 08:44:05 +0530

    Add amqp@8.3

---
+++ Formula/amqp@8.3.rb
@@ -0,0 +1,35 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Amqp Extension
+class AmqpAT83 < AbstractPhpExtension
+  init
+  desc "Amqp PHP extension"
+  homepage "https://github.com/php-amqp/php-amqp"
+  url "https://pecl.php.net/get/amqp-1.11.0.tgz"
+  sha256 "dc5212b4785f59955118a219bbfbcedb7aa6ab2a91e8038a0ad1898f331c2f08"
+  head "https://github.com/php-amqp/php-amqp.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "rabbitmq-c"
+
+  def install
+    args = %W[
+      --with-amqp=shared
+      --with-librabbitmq-dir=#{Formula["rabbitmq-c"].opt_prefix}
+    ]
+    Dir.chdir "amqp-#{version}"
+    patch_spl_symbols
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+  end
+end
