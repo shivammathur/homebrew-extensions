d8c8a1073747423bbc7fac53e8be18833c512bf2
shivammathur <shivam_jpr@hotmail.com>
2022-08-31 08:47:43 +0530

    Add apcu@8.3

---
+++ Formula/apcu@8.3.rb
@@ -0,0 +1,29 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Apcu Extension
+class ApcuAT83 < AbstractPhpExtension
+  init
+  desc "Apcu PHP extension"
+  homepage "https://github.com/krakjoe/apcu"
+  url "https://pecl.php.net/get/apcu-5.1.21.tgz"
+  sha256 "1033530448696ee7cadec85050f6df5135fb1330072ef2a74569392acfecfbc1"
+  head "https://github.com/krakjoe/apcu.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "apcu-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-apcu"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
