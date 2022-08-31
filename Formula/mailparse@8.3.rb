146c75bfc7dd0550522b0ba21eea25545b172ac9
shivammathur <shivam_jpr@hotmail.com>
2022-08-31 08:55:35 +0530

    Add mailparse@8.3

---
+++ Formula/mailparse@8.3.rb
@@ -0,0 +1,29 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Mailparse Extension
+class MailparseAT83 < AbstractPhpExtension
+  init
+  desc "Mailparse PHP extension"
+  homepage "https://github.com/php/pecl-mail-mailparse"
+  url "https://pecl.php.net/get/mailparse-3.1.3.tgz"
+  sha256 "8659ca62dc9a4d7d15f07f97a0e2142cb58251c8e772cd36669ec740d2292471"
+  head "https://github.com/php/pecl-mail-mailparse.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "mailparse-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-mailparse"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
