b6b71ffac438d535ae7be7a4dd8999bfddf0007d
shivammathur <shivam_jpr@hotmail.com>
2022-08-31 08:58:47 +0530

    Add xlswriter@8.3

---
+++ Formula/xlswriter@8.3.rb
@@ -0,0 +1,33 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Xlswriter Extension
+class XlswriterAT83 < AbstractPhpExtension
+  init
+  desc "Xlswriter PHP extension"
+  homepage "https://github.com/viest/php-ext-xlswriter"
+  url "https://pecl.php.net/get/xlswriter-1.5.2.tgz"
+  sha256 "fc363ef816c8efc46f9b5f6c86a7ab4469a803659b8d6b46421d143654361ea0"
+  head "https://github.com/viest/php-ext-xlswriter.git", branch: "master"
+  license "BSD-2-Clause"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    args = %w[
+      --with-xlswriter
+      --enable-reader
+    ]
+    Dir.chdir "xlswriter-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
