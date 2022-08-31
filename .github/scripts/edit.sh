53781786692a8cab96c4265995ffd547e2aa9bbb
shivammathur <shivam_jpr@hotmail.com>
2022-08-31 08:00:07 +0530

    Add support for PHP 8.3

--- .github/scripts/edit.sh
+++ .github/scripts/edit.sh
@@ -15,15 +15,15 @@ fetch() {
     [ "$checksum" != "" ] && sed -i "s/^  sha256.*/  sha256 \"$checksum\"/g" ./Formula/"$VERSION".rb
   else
     if [[ "$EXTENSION" =~ amqp|event|expect|gnupg|pcov|imagick ]] ||
-       [[ "$VERSION" =~ (couchbase)@(7.4|8.[0-2]) ]] ||
+       [[ "$VERSION" =~ (couchbase)@(7.4|8.[0-3]) ]] ||
        [[ "$VERSION" =~ (phalcon5)@(7.4|8.[0-1]) ]] ||
        [[ "$VERSION" =~ (propro)@7.[0-4] ]] ||
        [[ "$VERSION" =~ (msgpack)@(7.[0-4]|8.0) ]] ||
-       [[ "$VERSION" =~ (apcu|grpc|igbinary|mailparse|memcached|pecl_http|protobuf|raphf|rdkafka|redis|ssh2|vips|xlswriter)@(7.[0-4]|8.[0-2]) ]] ||
-       [[ "$VERSION" =~ (yaml)@(7.[1-4]|8.[0-2]) ]] ||
-       [[ "$VERSION" =~ (ast|mcrypt|mongodb|xdebug)@(7.[2-4]|8.[0-2]) ]] ||
-       [[ "$VERSION" =~ (psr)@(7.[3-4]|8.[0-2]) ]] ||
-       [[ "$VERSION" =~ (memcache|swoole)@8.[0-2] ]]; then
+       [[ "$VERSION" =~ (apcu|grpc|igbinary|mailparse|memcached|pecl_http|protobuf|raphf|rdkafka|redis|ssh2|vips|xlswriter)@(7.[0-4]|8.[0-3]) ]] ||
+       [[ "$VERSION" =~ (yaml)@(7.[1-4]|8.[0-3]) ]] ||
+       [[ "$VERSION" =~ (ast|mcrypt|mongodb|xdebug)@(7.[2-4]|8.[0-3]) ]] ||
+       [[ "$VERSION" =~ (psr)@(7.[3-4]|8.[0-3]) ]] ||
+       [[ "$VERSION" =~ (memcache|swoole)@8.[0-3] ]]; then
       sudo chmod a+x .github/scripts/update.sh && bash .github/scripts/update.sh "$EXTENSION" "$VERSION"
       url=$(grep '^  url' < ./Formula/"$VERSION".rb | cut -d\" -f 2)
       checksum=$(curl -sSL "$url" | shasum -a 256 | cut -d' ' -f 1)
