9d4f7da401d5adb961d7bf7e539b83bcb56d4fbc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-31 07:15:54 +0000

    swoole@8.3: update 4.8.11 bottle.

--- Formula/swoole@8.3.rb
+++ Formula/swoole@8.3.rb
@@ -15,6 +15,12 @@ class SwooleAT83 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_monterey: "44c0e8d0031161f565b1435fb060e9f4591daaf60974438c9df609d0d2afb3f3"
+    sha256 cellar: :any,                 arm64_big_sur:  "2f63256d6131c6801e977460a4ac2832b79199f25f3dc7dcd25fa7760640ea5f"
+    sha256 cellar: :any,                 monterey:       "bfbaad254a996a54576dad749d80a9cee9107ffc5a40cbcc68cd832139c0775b"
+    sha256 cellar: :any,                 big_sur:        "4647615ff2efb31722298b7c29bdf966120e73ce1b53d107cf99d0b158314df3"
+    sha256 cellar: :any,                 catalina:       "482ca0b1e7c575b0536c3066a8003ae5ec8379c55205cef2d88b3ffbde6fb965"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3f5be5682fda51861f6e23db4b8b676546e3bbdeb27a01236f0be2ad34f1b3b0"
   end
 
   depends_on "brotli"
