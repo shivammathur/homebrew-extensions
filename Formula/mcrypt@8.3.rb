a972c34415276dd8490b9aca54783306e1b85a69
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-08-31 07:00:26 +0000

    mcrypt@8.3: update 1.0.5 bottle.

--- Formula/mcrypt@8.3.rb
+++ Formula/mcrypt@8.3.rb
@@ -15,6 +15,12 @@ class McryptAT83 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_monterey: "42f6541e0421f814644a66ead8250161947a3dc41a63132559a5b483b683a488"
+    sha256 cellar: :any,                 arm64_big_sur:  "61168a552e6989586926deddc4e54853a15163efaf08bcf6c3b72135c8e23669"
+    sha256 cellar: :any,                 monterey:       "d21a66b5e24ab097961130fe9d543651ab8b151b9bfa6e16ed94695432c9fd86"
+    sha256 cellar: :any,                 big_sur:        "80048fdcd2884eedcd4b3686380a6b5971f4b9d179dd3164a9c2ca882077cf55"
+    sha256 cellar: :any,                 catalina:       "70d44449960d0d4af70f188d083e2ddb87054d2a996ed0bf93e8478b4ff1a721"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1f9ff0cfc7e7bf6750ae84517e4962ec852b1ebfac699c78946f46b39ba4f23c"
   end
 
   depends_on "automake" => :build
