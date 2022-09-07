b0425f0a5eb877b29025009893d97f6fb6161810
Shivam Mathur <shivam_jpr@hotmail.com>
2022-09-07 08:00:36 +0530

    pecl_http@7.2: update 3.3.0 bottle.

--- Formula/pecl_http@7.2.rb
+++ Formula/pecl_http@7.2.rb
@@ -15,12 +15,13 @@ class PeclHttpAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_monterey: "1e9f0a3713e4e3b8a6be829460aa0757827f2284cb15bb8356c6116820b40a7a"
-    sha256 cellar: :any,                 arm64_big_sur:  "caec9583c340cf8c8b708a52f6a7f135c24380ddb13b0bc65ee050b1f963c4d1"
-    sha256 cellar: :any,                 monterey:       "9bfbef6815eea9766c15ee3d834d771aef9fd3bf0634d28165446bbefb84884f"
-    sha256 cellar: :any,                 big_sur:        "1ffa6346c545d93c22923953ff4f1795857420c8144e93aca1e21e111a04083d"
-    sha256 cellar: :any,                 catalina:       "3c8a4ed4499f9aaebf08e8066715e12cb6e2b261ef1ecccf9e6af3a54a052cd0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "70574cf2f1f8fbc45dae548115e2047b6d974d73ccd3f57a08f944caf6f793a8"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "4d00a7195cc7b442ad63f2e4a9d2cca84cc1814409bd1f344f37b7834df1aac1"
+    sha256 cellar: :any,                 arm64_big_sur:  "1772b077ea088393b87d00875db176b2e4b19be51e8e89eab28faa88cbe8618b"
+    sha256 cellar: :any,                 monterey:       "1439b026b4c76d5f5cbbc7e2f509c3e900e281387bf36fddb4c12c74be486340"
+    sha256 cellar: :any,                 big_sur:        "e839dba3c453cf7bc5d23d0e9ce7b16634d907b6ef289082eecfc26bb6018b3a"
+    sha256 cellar: :any,                 catalina:       "862b041b3b59bea9a1a641e35b44425b7afdfeaf80d39b22fea825e93c2c5228"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7170541b6f40c5d73d51d6fbd0724d738f09b49822cfa87e0d43530ec2842864"
   end
 
   depends_on "brotli"
