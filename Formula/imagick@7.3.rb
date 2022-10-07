aa52632a02c9e69c5fc3ec65f12ecb5c24441c27
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-07 09:36:17 +0000

    imagick@7.3: update 3.7.0 bottle.

--- Formula/imagick@7.3.rb
+++ Formula/imagick@7.3.rb
@@ -15,10 +15,13 @@ class ImagickAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_big_sur: "265f386e394c229a9e467e600beae2e4b3b937da616c8a1ac23d9984b7091558"
-    sha256 cellar: :any,                 big_sur:       "9758e7bde63f4edcb03dfe9d1393b0a8db3bb83398603dcbc0273acb65c8ba78"
-    sha256 cellar: :any,                 catalina:      "7255ff983639931bcb51a746e35205abb77b6e576fd9447753f73dcc43a532c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "60b8cdbdaf887666f4173bfe144b58bb1458367ea0310e37ba89484c88e345b2"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "b6efd794354ed8c213a4708eb8886fd925e2460109e0b944c3c91135d887ffc8"
+    sha256 cellar: :any,                 arm64_big_sur:  "361a275665b22314a7b45aee4d9e2f0abbf41ef3ee1e8c2416b100c407485b68"
+    sha256 cellar: :any,                 monterey:       "d9a5672398b2c846964a0dff1006d68b72e7a36414c06342394b0dd041892875"
+    sha256 cellar: :any,                 big_sur:        "0c7ac3bbf4218ede1b4f25607bdc1be914cd08116737b3f4680611ab44161288"
+    sha256 cellar: :any,                 catalina:       "12adee6731661055b4ae984045dd403c451a2c4cca173b02e65bb1de9e79d45c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b98ea9db65ce09b0d4eb28c7bd9d2e8216b30765e3aaee91fc389bec87b0d2b4"
   end
 
   depends_on "imagemagick"
