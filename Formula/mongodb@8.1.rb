e2b7db56000b910e24a0b010d93c724d48fad5e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-24 01:13:03 +0000

    mongodb@8.1: update 1.14.2 bottle.

--- Formula/mongodb@8.1.rb
+++ Formula/mongodb@8.1.rb
@@ -15,12 +15,12 @@ class MongodbAT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_monterey: "5330690895e2824ca33ae0424f198fb8b1a56071ff4d145b373161e67c3fe75a"
-    sha256 cellar: :any,                 arm64_big_sur:  "092188feea8f11536ffcf832d8dc16bd9a89ecb4df9e726e19dd607a95e54e2d"
-    sha256 cellar: :any,                 monterey:       "ea72a4d78aeebbf3ab5a0ea57de4f91f946514ce8c8591d484d449ceb8dcf92c"
-    sha256 cellar: :any,                 big_sur:        "e288da7400cc45f7e74b6aa70d88647a5d522f8385451b8452132ac139e2a86b"
-    sha256 cellar: :any,                 catalina:       "fba770f3b02a685277855473f1628ce52f3c3305fe5b4a7aafd0cbb3b2f88abe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "941d5480590b72b25ab20ca814d178b54aa0d15d8e7ee2c39a101659525c331b"
+    sha256 cellar: :any,                 arm64_monterey: "44eab61c71b1e4b913ba6daa786ec509cbed3585f18b766a4ca2b07e3af4a085"
+    sha256 cellar: :any,                 arm64_big_sur:  "b444ac98fffcb41e4ea16766ae58eae959a6d5eefe4339f61b64e431f997dba2"
+    sha256 cellar: :any,                 monterey:       "b7b9ad0417d86b78d1d267a3ac4e5faf806def87b5af7faeb54186997142caf8"
+    sha256 cellar: :any,                 big_sur:        "a0ef14bb64233b106f46e290de7e4751770c53e3a114fdfd4c8f37e0453725d8"
+    sha256 cellar: :any,                 catalina:       "9fc62d5fc4d031a84e079d5c2bd584943e3d0c596ef367062bd34db3997c9041"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "000314bb2c18c4a4fbb664c28a85070df9c494d65d8572f01c3a929602b64974"
   end
 
   depends_on "icu4c"
