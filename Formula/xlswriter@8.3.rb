e7a37ae6052b53b88eed1c5fe2da471e1035726c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-03 08:05:50 +0000

    xlswriter@8.3: update 1.5.2 bottle.

--- Formula/xlswriter@8.3.rb
+++ Formula/xlswriter@8.3.rb
@@ -15,13 +15,13 @@ class XlswriterAT83 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e654e04bae4e53e5a14a9e1e9eefbcdcb8042a7887fd8850f826fa7d24926104"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c3f3502c273262f7b9344f7b18c1e77c0f7a1cab15e500acd6b5be192d08c983"
-    sha256 cellar: :any_skip_relocation, monterey:       "8f22ab1529533aeda3a34344b8b368beb69b0f5ac9020ffb9ccd86860a204a43"
-    sha256 cellar: :any_skip_relocation, big_sur:        "00396a7668cd9d16b78b33d7b7ac0b195a413f039329fd66b1b47251eee96692"
-    sha256 cellar: :any_skip_relocation, catalina:       "8f7ab69c1d9cdd749aac7ae5a6a9520b34d945d2ba9f6b3bba46dd99bf92fad7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5fc3d4a0251e076457c53a74e82895d2816b1f6759aebc163517df2b00b42278"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e255c35672d97c9b6d69c00a02113edd9ffc37c97b85bc3f44f4495b89d93e64"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dfb82f8ac216abebf5c5fcee086733a4f42020f10f06ea0c59323a9a6c32ce48"
+    sha256 cellar: :any_skip_relocation, monterey:       "10914550bc6d05a66da7f310e5119b5a3b865fdc92af964907797eeff84e0dec"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a002c94cc25efb526710a0194a862233aaca01f745824aa205483baa6962c3db"
+    sha256 cellar: :any_skip_relocation, catalina:       "48b8249c2807409048e77ed602f8a46cfad902929c3acc82070948e86a1ac566"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7e9cfb57c89dd572cd17df4be73ec933f4605b8411b3dc06463c0a001ce11c47"
   end
 
   def install
