d9c92daddcf9a4f9e28e4e3b8e9229c3557c9805
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-12 18:53:06 +0000

    Update ds@8.0

--- Formula/ds@8.0.rb
+++ Formula/ds@8.0.rb
@@ -8,7 +8,7 @@ class DsAT80 < AbstractPhpExtension
   init
   desc "Ds PHP extension"
   homepage "https://github.com/php-ds/ext-ds"
-  url "https://pecl.php.net/get/ds-1.4.0.tgz?init=true"
+  url "https://pecl.php.net/get/ds-1.4.0.tgz"
   sha256 "a9b930582de8054e2b1a3502bec9d9e064941b5b9b217acc31e4b47f442b93ef"
   head "https://github.com/php-ds/ext-ds.git"
   license "MIT"
