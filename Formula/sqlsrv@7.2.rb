80539bd490b79cce45ffb31a77ccef8c9f5e8268
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-12 18:35:19 +0000

    sqlsrv@7.2: update 5.9.0 bottle.

--- Formula/sqlsrv@7.2.rb
+++ Formula/sqlsrv@7.2.rb
@@ -15,6 +15,13 @@ class SqlsrvAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "7cd2bd68dc8c6ec66ba2eea8f11b909735c9541df901ea40228342a5bcc94ca5"
+    sha256 cellar: :any,                 arm64_big_sur:  "e8b07d6500f01138ae7eb653dd4090a671dd7824d3cb8dc2c1ef0d9ae3489770"
+    sha256 cellar: :any,                 monterey:       "81259ea8536efc8a608d8e89ae0ce8d70354ac7cdeef90aed15cf3701513f556"
+    sha256 cellar: :any,                 big_sur:        "0a0bfcf787a2307d5330aa530c544e2a0d1419f2757e004f76f6be65a253dd00"
+    sha256 cellar: :any,                 catalina:       "da41006ca751d497869f1f7f3f31ec5100cbc10667bde3b1164403f74341a8f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e33382550dcf535513ab0cc7b61e90438b0e7f1129ee0f83a84d8e436cbb852d"
   end
 
   depends_on "unixodbc"
