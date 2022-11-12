4ae50ecd64e82f6f97bda567ae55365b9b541c92
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-12 18:06:51 +0000

    pdo_sqlsrv@8.2: update 5.10.1 bottle.

--- Formula/pdo_sqlsrv@8.2.rb
+++ Formula/pdo_sqlsrv@8.2.rb
@@ -15,6 +15,13 @@ class PdoSqlsrvAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "142cd014c0a837a48062e89156b618ffc582aab68b1e0eea45526c640224e41a"
+    sha256 cellar: :any,                 arm64_big_sur:  "8a14ce425c6e68ed69fa7e9981016e92e93119eae138c951585fef46b1c9aa5e"
+    sha256 cellar: :any,                 monterey:       "4e6657cb05bed854053280a12b1f94e4ab86f8b72c0ee6aa2b2bd9a2fe36eec4"
+    sha256 cellar: :any,                 big_sur:        "4e7f5026e4cb74d770acd35894b4b65978a282340dcabf6ea273f375ec1c623b"
+    sha256 cellar: :any,                 catalina:       "3a660d0f228266a5c922d84dc38db89c8b36d45c21b07b30f097d371b9ec4e89"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9d7914fa772745d7931e6a7307e502f068143c27ddf3f8f83c0221a0fbc6f4ca"
   end
 
   depends_on "unixodbc"
