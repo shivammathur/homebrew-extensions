5d7533d6ec6fc63fb4b4c18e4e718c1d5be2a176
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-12 15:52:24 +0000

    Update sqlsrv@7.2

--- Formula/sqlsrv@7.2.rb
+++ Formula/sqlsrv@7.2.rb
@@ -8,7 +8,7 @@ class SqlsrvAT72 < AbstractPhpExtension
   init
   desc "Sqlsrv PHP extension"
   homepage "https://github.com/Microsoft/msphpsql"
-  url "https://pecl.php.net/get/sqlsrv-5.9.0.tgz?init=true"
+  url "https://pecl.php.net/get/sqlsrv-5.9.0.tgz"
   sha256 "0a108e0408e8b984e5ae8bc52824ed32872d72e3a571cd2a5d2b63b200215ab3"
   head "https://github.com/Microsoft/msphpsql.git"
   license "MIT"
