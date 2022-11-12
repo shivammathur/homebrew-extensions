df7e176fc98e8c7d390d6d8d65086873ff1f1d1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-12 19:26:14 +0000

    ds@7.1: update 1.3.0 bottle.

--- Formula/ds@7.1.rb
+++ Formula/ds@7.1.rb
@@ -15,6 +15,13 @@ class DsAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a95af1503ba898378168bd918fbb8f2fcf79f747d255a5b51625509ce4c09e47"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fd0157a2c0bd3b811e999f3bf12dbfb93fd681027acf754e06c495ed6bcf6621"
+    sha256 cellar: :any_skip_relocation, monterey:       "760b3771efa21edd8ddd3491fb0111bbf6559841669f57509907feb3efae16df"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0ff34900d0a3f87eb8262a16743a2ed7de0de0eaabe115d832fd807800777799"
+    sha256 cellar: :any_skip_relocation, catalina:       "7e654514d3a7dd776935c5cbe53e8469d332ea72b99dfb61a084ac9b9711901e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8d656a42cfb56092514e0eed6e6d8a5be28508286e2ea20ee8e91b503ef49ee5"
   end
 
   def install
