8c8d9388fa822efb58c63cef4858451a16d08ba3
Shivam Mathur <shivam_jpr@hotmail.com>
2022-11-13 08:36:41 +0000

    Update lua@5.6

--- Formula/lua@7.1.rb
+++ Formula/lua@7.1.rb
@@ -8,7 +8,7 @@ class LuaAT71 < AbstractPhpExtension
   init
   desc "Lua PHP extension"
   homepage "https://github.com/laruence/php-lua"
-  url "https://pecl.php.net/get/lua-2.0.7.tgz?init=true"
+  url "https://pecl.php.net/get/lua-2.0.7.tgz"
   sha256 "86545e1e09b79e3693dd93f2a5a8f15ea161b5a1928f315c7a27107744ee8772"
   head "https://github.com/laruence/php-lua.git"
   license "PHP-3.01"
@@ -24,6 +24,10 @@ class LuaAT71 < AbstractPhpExtension
       --with-lua=#{Formula["lua"].opt_prefix}
     ]
     Dir.chdir "lua-#{version}"
+    inreplace "config.m4", "include/lua.h", "include/lua/lua.h"
+    inreplace "php_lua.h", "include \"l", "include \"lua/l"
+    inreplace "lua_closure.c", "include \"l", "include \"lua/l"
+    inreplace "lua_closure.c", "lua/lua_closure.h", "lua_closure.h"
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig, *args
     system "make"
