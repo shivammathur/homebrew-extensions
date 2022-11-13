36493573b6accf5d3cb1a9c5fcf5026ccba71ebe
shivammathur <shivam_jpr@hotmail.com>
2022-11-13 07:46:43 +0530

    Add gearman --init

---
+++ Formula/gearman@5.6.rb
@@ -0,0 +1,34 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Gearman Extension
+class GearmanAT56 < AbstractPhpExtension
+  init
+  desc "Gearman PHP extension"
+  homepage "https://github.com/php/pecl-networking-gearman"
+  url "https://pecl.php.net/get/gearman-1.1.2.tgz?init=true"
+  sha256 "c30a68145b4e33f4da929267f7b5296376ca81d76dd801fc77a261696a8a5965"
+  head "https://github.com/php/pecl-networking-gearman.git"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "gearman"
+
+  def install
+    args = %W[
+      --with-gearman=#{Formula["gearman"].opt_prefix}
+    ]
+    Dir.chdir "gearman-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
---
+++ Formula/gearman@8.0.rb
@@ -0,0 +1,34 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Gearman Extension
+class GearmanAT80 < AbstractPhpExtension
+  init
+  desc "Gearman PHP extension"
+  homepage "https://github.com/php/pecl-networking-gearman"
+  url "https://pecl.php.net/get/gearman-2.1.0.tgz?init=true"
+  sha256 "77075fc250fac2d3878deb255d44efc3ad76cf7b2c7f1af420cb17839a03c0bc"
+  head "https://github.com/php/pecl-networking-gearman.git"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "gearman"
+
+  def install
+    args = %W[
+      --with-gearman=#{Formula["gearman"].opt_prefix}
+    ]
+    Dir.chdir "gearman-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
