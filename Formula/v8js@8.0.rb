a353860d592de89d8b026f493e743203afad1694
Shivam Mathur <shivam_jpr@hotmail.com>
2022-11-13 17:34:18 +0000

    Update v8js@8.0

--- Formula/v8js@8.0.rb
+++ Formula/v8js@8.0.rb
@@ -8,8 +8,9 @@ class V8jsAT80 < AbstractPhpExtension
   init
   desc "V8js PHP extension"
   homepage "https://github.com/phpv8/v8js"
-  url "https://pecl.php.net/get/v8js-2.1.2.tgz?init=true"
-  sha256 "65b7730111ad6d92c00195280ccbc1e55d52cdab3103ae28a057037278cc03ca"
+  url "https://github.com/phpv8/v8js/archive/461230be276dc423d8eebf0c9ea769c71d47b7f6.tar.gz"
+  version "2.1.2"
+  sha256 "61effd3cde61dc002e9ddda28fbea86654a8fcfe70f0a895e8a46f27b914cc78"
   head "https://github.com/phpv8/v8js.git"
   license "MIT"
 
@@ -19,11 +20,19 @@ class V8jsAT80 < AbstractPhpExtension
 
   depends_on "v8"
 
+  patch do
+    url "https://patch-diff.githubusercontent.com/raw/phpv8/v8js/pull/490.patch"
+    sha256 "2590f7ba4df798078e6c3a9a73a9ec3028d7e2e66b49f5561e40bdc1577536cd"
+  end
+
   def install
     args = %W[
       --with-v8js=#{Formula["v8"].opt_prefix}
     ]
-    Dir.chdir "v8js-#{version}"
+    ENV.append "CPPFLAGS", "-DV8_COMPRESS_POINTERS"
+    ENV.append "CXXFLAGS", "-Wno-c++11-narrowing"
+    ENV.append "LDFLAGS", "-lstdc++"
+    inreplace "config.m4", "$PHP_LIBDIR", "libexec"
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig, *args
     system "make"
