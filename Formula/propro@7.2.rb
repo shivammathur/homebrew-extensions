a37a967500942e363bc0753d40a40b10498cb4f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-15 18:06:51 +0000

    propro@7.2: update 2.1.0_1 bottle.

--- Formula/propro@7.2.rb
+++ Formula/propro@7.2.rb
@@ -16,6 +16,13 @@ class ProproAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "54d7775290a67c39ddb20df1b4a001be2bd5df376a131c36749b96341e45c8c1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ef878175013d73dd82be74196ad3bbbfce583d1095cc7c01671033efc7bd0027"
+    sha256 cellar: :any_skip_relocation, monterey:       "3ef68b99ece93a17dd7191017546a07bb7597844da2267885dce20c3b964bd7d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cc30c98b22d661c670cf55cfcdf051800aa1290936e9bc8c1f22fb99dc367c1d"
+    sha256 cellar: :any_skip_relocation, catalina:       "53d95b07b3e2143d557788dd530a44203ca9b98be63a52cb85487d63c0ded62e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "37a590b5840c40f3b9c6806018b439a7794c47c7dd42c0e3c41e2ee2bbf024c0"
   end
 
   def install
