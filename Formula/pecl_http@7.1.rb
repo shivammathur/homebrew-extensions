b837e1d657143493e7a8551b3dc5364a3d0f7496
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-15 22:06:38 +0000

    pecl_http@7.1: update 3.3.0_1 bottle.

--- Formula/pecl_http@7.1.rb
+++ Formula/pecl_http@7.1.rb
@@ -16,6 +16,13 @@ class PeclHttpAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_monterey: "9c7e9db88dee2bb16fdd7a9fb964405ca9b455f39208ca83d39fbfba3fbd1bdc"
+    sha256 cellar: :any,                 arm64_big_sur:  "d7fe5fb1cb6bb969d837c9735e8fc242fa67d023c68f3c59494a662ea05194e0"
+    sha256 cellar: :any,                 monterey:       "5f330e5fe7219960c57eb81b17f8ce192625246125fef74cebe66ef7730893e7"
+    sha256 cellar: :any,                 big_sur:        "15fd7f1ddd4b9b738e09fc84908d66ee5955383a84fc62e4f95b6653fcf8106d"
+    sha256 cellar: :any,                 catalina:       "cd9e966b6e4147ba47dd24556c8eb736dc4cf163aa48c3fa24f392a7f2ebdd46"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "407496be6891332065aab9f298589b8e53f2e95d81e4ea7e388013801adc48b4"
   end
 
   depends_on "brotli"
