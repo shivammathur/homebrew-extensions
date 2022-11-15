e31f91f96ec41ffa221354dadff9d8d31cddce2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-15 09:08:35 +0000

    raphf@5.6: update 1.1.2 bottle.

--- Formula/raphf@5.6.rb
+++ Formula/raphf@5.6.rb
@@ -15,12 +15,13 @@ class RaphfAT56 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1151470d528ae6ca7e90be0cb5710a2a058656bc9120883f0a886a4991d09812"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6f828ddb01134c57d9d8aaf8a5c739671f6a7744995a80a0bc5040b29b5216c2"
-    sha256 cellar: :any_skip_relocation, monterey:       "8968cb965c32636d6fd4427a3aa05883b498dd590b68fa018ee7f98100813c12"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cc06f69d08d8d5b9710ab5858c799788e7a1662361e1640902b819b379006ff9"
-    sha256 cellar: :any_skip_relocation, catalina:       "50b3b5781c82add904f128e5c8b82b2da2543093feab1e1c41a1cfb8744d257e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f59b8af341853d110ff905bbacb4e5da172a42d7de7331a0893299ab63897d34"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dc1f7eb55443a5d12efebdf502a6884b76fd5b1cabe429db7527a39ee71f70db"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9d4d4d53f38af0dc057898e5fbafacb416a7d7c790591d39fea2b36c59c4272e"
+    sha256 cellar: :any_skip_relocation, monterey:       "91339bd86acb7c26a647d9af5a1e9a9b93d1ef60ffedf36337eac56ac46a13a7"
+    sha256 cellar: :any_skip_relocation, big_sur:        "20789dd30fd504220d88516acb41b57062411275fae423014b151a5c9bf67c17"
+    sha256 cellar: :any_skip_relocation, catalina:       "7a33e709afe0fd48e728fb03af84b14b4fa2b4aa0b06b07af9b7e5c5af62643a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9e3c3a7f3536eafb0f9bba721244799ad6993689ac9a291debb915872cb12ec8"
   end
 
   def install
