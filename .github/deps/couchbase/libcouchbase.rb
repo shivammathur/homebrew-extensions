6e928fed8cb61a1b9eab95e9aa172efb95017fc5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-22 10:05:55 +0000

    Update extension dependencies

--- .github/deps/couchbase/libcouchbase.rb
+++ .github/deps/couchbase/libcouchbase.rb
@@ -10,6 +10,7 @@ class Libcouchbase < Formula
     sha256 arm64_ventura:  "330bcf91ee7f1072b49d9f4659271d1ad218b8f285c9fd7c2fa9f1ad891b6e90"
     sha256 arm64_monterey: "22c4e6e156a17916e584d5c536b367f11225f53e5d9fd7ebec0c32a9cb2541b6"
     sha256 arm64_big_sur:  "c42f0807a6d3a2428d69bc8a14e0a12d731f341c86f6c0fd9b647492607acdc7"
+    sha256 ventura:        "3b1e65edb78f078229edfe5325844c01c52136fcb0e3e33922834008070991cb"
     sha256 monterey:       "ac5bed438447e1b9568fa7556f9aba7f4fe7b83914818375aabbde79e7bc1246"
     sha256 big_sur:        "5e71748685a148e7ab08302ffc0e52bf6267c2cf33af3e67be1c8330e0ffb5f8"
     sha256 catalina:       "f7a9b3d187941f8a0ce90478f5aabad88333e6fed21f09590102e2f4d5e5195e"
