657d360d4ea11f87008f03d55c6afa9ccf343080
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-26 10:04:41 +0000

    Update extension dependencies

--- .github/deps/vips/openslide.rb
+++ .github/deps/vips/openslide.rb
@@ -29,8 +29,8 @@ class Openslide < Formula
 
   uses_from_macos "sqlite"
 
-  resource "svs" do
-    url "https://openslide.cs.cmu.edu/download/openslide-testdata/Aperio/CMU-1-Small-Region.svs"
+  resource "homebrew-svs" do
+    url "https://github.com/libvips/libvips/raw/d510807e/test/test-suite/images/CMU-1-Small-Region.svs"
     sha256 "ed92d5a9f2e86df67640d6f92ce3e231419ce127131697fbbce42ad5e002c8a7"
   end
 
@@ -40,7 +40,7 @@ class Openslide < Formula
   end
 
   test do
-    resource("svs").stage do
+    resource("homebrew-svs").stage do
       system bin/"openslide-show-properties", "CMU-1-Small-Region.svs"
     end
   end
