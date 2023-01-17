938e64fa7c9638ba891ab53c18c961e6871efb2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-17 17:52:48 +0000

    msgpack@7.0: update 2.1.2 bottle.

--- Formula/msgpack@7.0.rb
+++ Formula/msgpack@7.0.rb
@@ -14,14 +14,13 @@ class MsgpackAT70 < AbstractPhpExtension
   license "BSD-3-Clause"
 
   bottle do
-    root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
+    root_url "https://github.com/shivammathur/homebrew-extensions/releases/download/msgpack@7.0-2.1.2"
+    rebuild 2
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8942e15b2f19eccfc5c703faeac33ac4c25187d6868a39cb8ba7079aec253e8c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "12bb646cefa7b38bdc5e6a3205a60a9b533165abfbc143cf6405344479abdcac"
-    sha256 cellar: :any_skip_relocation, monterey:       "df620050205b6e6a287910e0bc12aaec8bd77653defccd2537551b836c82a5dc"
+    sha256 cellar: :any_skip_relocation, monterey:       "ac9bdb078f494f37adba0f50cb8b77d73b0ff52fbf4d4b365c6c48f07c360575"
     sha256 cellar: :any_skip_relocation, big_sur:        "87dc6efbd7ffc4c53facbdcca9034ea64e730632d27f34842eb47c7cc0a94aab"
-    sha256 cellar: :any_skip_relocation, catalina:       "2c614b7abf100847510a17a199463859b7b8e977fb4343bc1f9080e9c077a079"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9acb36c30d69b23d26f30011a06524e01cdbe91d3b6f3669c6fcabc147af4129"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "39060cc0d55597084fc2a042015c55225dbd1c83ce5f627020c1d67a0e5405c2"
   end
 
   def install
