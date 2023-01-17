156ec97336106c7a8a822b705084436dd90ede60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-17 18:55:55 +0000

    igbinary@8.0: update 3.2.12 bottle.

--- Formula/igbinary@8.0.rb
+++ Formula/igbinary@8.0.rb
@@ -14,12 +14,12 @@ class IgbinaryAT80 < AbstractPhpExtension
   license "BSD-3-Clause"
 
   bottle do
-    root_url "https://ghcr.io/v2/shivammathur/extensions"
+    root_url "https://github.com/shivammathur/homebrew-extensions/releases/download/igbinary@8.0-3.2.12"
+    rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_monterey: "462065d82828fa871f156751224fe3d8f23635dd1b8bfb59fbc1705e00e56968"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ca3e3e685c0a2c13ccc5a962685b1af50dc46af7a8f6ce19bfe3a1d976500edb"
-    sha256 cellar: :any_skip_relocation, monterey:       "8b13c2b6a524e72e9c4bdb502835459d76ba8c1ee4668f2046841c1b4b71a4b5"
+    sha256 cellar: :any_skip_relocation, monterey:       "4b8da8e46bdf126a3713b382d7170f3addb83cb4c388f3e4492ee45f5e98689d"
     sha256 cellar: :any_skip_relocation, big_sur:        "e22dd021bb0650c2fce0a612ce6b0c959e03f4531da85300f5d9966852719151"
-    sha256 cellar: :any_skip_relocation, catalina:       "211a6ebbcde7adc04a092d545742616740ab1f2ae0dfb409748b4a81b3a88fea"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb0e972d8181c520b2e44e91dd5e6900e1f91104134b62754a3fa6ea32d5bb2a"
   end
 
