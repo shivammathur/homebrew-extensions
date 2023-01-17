d46758f6fd7ed529113e5855fc117d2e253d1fa3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-17 17:01:32 +0000

    imagick@8.0: update 3.7.0 bottle.

--- Formula/imagick@8.0.rb
+++ Formula/imagick@8.0.rb
@@ -14,14 +14,13 @@ class ImagickAT80 < AbstractPhpExtension
   license "PHP-3.01"
 
   bottle do
-    root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "509d31c19588d66fb24d0f79a253c7a94e3d75f58b42c2e43b23262659d21f3c"
-    sha256 cellar: :any,                 arm64_big_sur:  "4fc31a687a005d382c7415e54c14b4aa05efd74bdada0102f1dad2108cac2207"
-    sha256 cellar: :any,                 monterey:       "bd361c0d595fafc1b74624dc3325c679a35dc2eb079c9899d08a149d073a8bd4"
-    sha256 cellar: :any,                 big_sur:        "d87dbef2a0369d3111a58cc36b2536db5ad37f0c7c43a807ef8d33529a17f6bc"
-    sha256 cellar: :any,                 catalina:       "7776a8d40bfe8b691a52474cf2b05977ff383561a3d74489caf00e8ea38f8439"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a546f2dbadfde3bbaf93301ec2b218d7b7b6cb99e3d464741cc850721b074293"
+    root_url "https://github.com/shivammathur/homebrew-extensions/releases/download/imagick@8.0-3.7.0"
+    rebuild 2
+    sha256 cellar: :any,                 arm64_monterey: "14dfaf65a088ca70772532df59edce1f9324432ac0a1aa7e1447cc1746c1444c"
+    sha256 cellar: :any,                 arm64_big_sur:  "2829b8f4036fac90e94e32b87b5726daf0763b2373c20538e25bd9e84135c076"
+    sha256 cellar: :any,                 monterey:       "0a84be64c44cfa21b0c3b6800639521b954138d85d7ec281cc5b4a3fa9788319"
+    sha256 cellar: :any,                 big_sur:        "673fa95b7aa305cc853d225e894c6669f68e33110a8ea53b3ab41c38e1ff0b2c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb8a3f7e5db8f3b5a00472826e7abe10907bbcec743257aa43f597dca84cec92"
   end
 
   depends_on "imagemagick"
