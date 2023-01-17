70f9c06cae99e6e1b3c26a9768649b6fbbbf7347
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-17 16:01:21 +0000

    vips@7.1: update 1.0.13 bottle.

--- Formula/vips@7.1.rb
+++ Formula/vips@7.1.rb
@@ -14,14 +14,13 @@ class VipsAT71 < AbstractPhpExtension
   license "MIT"
 
   bottle do
-    root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 3
-    sha256 cellar: :any,                 arm64_monterey: "55bcd7ec12913889b45e9aaf9cfaae556a89fd9fa5c610daa09114b3f26417cf"
-    sha256 cellar: :any,                 arm64_big_sur:  "2f2fea1c02b8e1b2b64a547e0cd25ac01250ba259dbf898fe9f65c301e5c4bc4"
-    sha256 cellar: :any,                 monterey:       "2434763ba5948a4a1f8cff04cd3e75e47f481cdec0625ac7b40cfd02d7bc8450"
-    sha256 cellar: :any,                 big_sur:        "c76b7699945b21e7ba2f0cc7dc2d621f005a32821c7bb723874539de65eb05de"
-    sha256 cellar: :any,                 catalina:       "bd3846ca4e308a6e07588c6c212d8ef25c38ff8113b7200a29b0448804bfeb3f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6bb89cc3e6deaef8c299fce0059c2dffe2ae032385e11ccab24b79d94bc4ebcb"
+    root_url "https://github.com/shivammathur/homebrew-extensions/releases/download/vips@7.1-1.0.13"
+    rebuild 4
+    sha256 cellar: :any,                 arm64_monterey: "fc33f9232b0f84ba80e7acf53f976679a4971b80036f19e38382db3ee05e8335"
+    sha256 cellar: :any,                 arm64_big_sur:  "ae8e11dbfc6b640cc4a7a923b29124ce0aec058bd12523f744e8f6a48bc9f202"
+    sha256 cellar: :any,                 monterey:       "96094fe0fd4ceb9f71954c75a94bfc0fdff6fc0e498fb1ced27405aa17186058"
+    sha256 cellar: :any,                 big_sur:        "37a438e5ad391e05bb7fcf4972a9e5716f59948834a2814f2dc36fd84c113546"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1023f158783df74dc55f30df6208c2e6575e4d2e60d2d00ff6ca5c43e8a4656b"
   end
 
   depends_on "vips"
