0ef8e9a9ecb49976acf202531711fa4d2c92e553
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-17 20:41:45 +0000

    propro@5.6: update 1.0.2_1 bottle.

--- Formula/propro@5.6.rb
+++ Formula/propro@5.6.rb
@@ -15,13 +15,12 @@ class ProproAT56 < AbstractPhpExtension
   revision 1
 
   bottle do
-    root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
+    root_url "https://github.com/shivammathur/homebrew-extensions/releases/download/propro@5.6-1.0.2_1"
+    rebuild 2
     sha256 cellar: :any_skip_relocation, arm64_monterey: "fa1d9ba80bc35b72a1d14eb021df2b44f5fadeb9d05925d7909ef594a011d44e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8494a61c58f376e7b0f310b3af367e91bd467677048ca710cbc057f6f497de3f"
-    sha256 cellar: :any_skip_relocation, monterey:       "85192dd8184ef4c391169d1def19af4057b816686a5fa20a5631a8fe454fa775"
+    sha256 cellar: :any_skip_relocation, monterey:       "ba4bd081bb948b15721b0ce378123bfe500fa649b733e012f6f46c2b5088104e"
     sha256 cellar: :any_skip_relocation, big_sur:        "4ca7d96f03bfff66e14264a9c9e4a2dcf5be440731d0800991847805305799cd"
-    sha256 cellar: :any_skip_relocation, catalina:       "ee61181ab48b820b317ef909d51ac50e951126b19f37aca57a67f0179e4da81f"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "bd0d3a3e52a4c1af46f3d5719c30b9654ff84937cad2648650586ea7e4104798"
   end
 
