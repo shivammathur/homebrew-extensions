730b36336aa2c67f5958d1a4318f482b83bf1a51
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-17 22:21:04 +0000

    redis@8.2: update 5.3.7 bottle.

--- Formula/redis@8.2.rb
+++ Formula/redis@8.2.rb
@@ -15,13 +15,12 @@ class RedisAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 3
+    rebuild 4
     sha256 cellar: :any,                 arm64_monterey: "db8c0cd28e67fec0d9a6d27fc39bebb7bac4fe88f3ea247a5a34c2c7e0756155"
     sha256 cellar: :any,                 arm64_big_sur:  "6be1c1a1fdf52d94ee8c522bad4d7d6dc7f89f77f78987b6dd1ee3d2ad72c785"
-    sha256 cellar: :any,                 monterey:       "685e042eb01434de0c8ddfcf24df57ee80fb10e612c752a80d74bf745290c233"
+    sha256 cellar: :any,                 monterey:       "cf70bc9c3f176211d594f61ccdc9b215ffab09a3ed8ec8d4875157edd2d63de3"
     sha256 cellar: :any,                 big_sur:        "6cdc738be806fc30b68ce4fd4704cac2bc0b9064d2d79418c0d4657ef276acb7"
-    sha256 cellar: :any,                 catalina:       "910bd0cfe8fd379300cb97e961fc0e36f78970194a8121e0a52fd9bcddcd9e55"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d43822c949202cfc439fcf6a4ea2767265afa39ce8d1d7455ede3980bbde6e48"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "458b5c3d47f08121cd12f46c8d4dab8c2d155da6cbe6dd651c40bd85b59e99e7"
   end
 
   depends_on "liblzf"
