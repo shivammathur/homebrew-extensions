dbc026f1c5d14ab6979388a229ff34751dfc9915
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-17 23:43:48 +0000

    pecl_http@5.6: update 2.6.0_2 bottle.

--- Formula/pecl_http@5.6.rb
+++ Formula/pecl_http@5.6.rb
@@ -16,13 +16,12 @@ class PeclHttpAT56 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6875416b3f2700593edc145092aee55f26e481b02c62b8e8d7007e29b7b5ea4e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6855221b69104d8cc473ea936936db12b2a648d6a9c81807ea168450de865003"
-    sha256 cellar: :any,                 monterey:       "c3984dfc2a9ce30fdd3c4fe82fceb90716e239c688fad5eacf20906964bc67dd"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7d9da040e308f7e605edf9cdf4676b32c71ce1490bc3a9f41a2c69a1c520b828"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6c7982aabc981a64a6db568f5138632e902121b529b469084a204741ea2965c8"
+    sha256 cellar: :any,                 monterey:       "e6c28dc1f033cf562349183361098ae487786197ca43e63deab2d7eaf3e50aa7"
     sha256 cellar: :any,                 big_sur:        "1f55a5dec4494ab375399bde7b216caa95e92c802a10c48cbe30cacba9be6fd7"
-    sha256 cellar: :any,                 catalina:       "0a5e12dc2dca705890ff7534135f24b392e1823281c4cd1ac1772817fc2a8a5f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1168725e5b4895b454f19f7a12ef0e8eb093407f9b57adeff75071656e44def4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "70fe798dcae208aca28f3d5c625642a865331004b5928d734d69df6200564b1e"
   end
 
   depends_on "brotli"
