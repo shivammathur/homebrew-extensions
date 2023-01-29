ff48e45fd4411203b8afbc6e8a33e5ac40b21f4c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-01-29 02:52:57 +0000

    uuid@7.2: update 1.2.0 bottle.

--- Formula/uuid@7.2.rb
+++ Formula/uuid@7.2.rb
@@ -15,6 +15,11 @@ class UuidAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_monterey: "0cbfbaad372a035fecff26643c86654a8de4a7eddcfe6691e1983c0b737f1c53"
+    sha256 cellar: :any,                 arm64_big_sur:  "193297e3031c47bb990050044d932e3bde3aa8324bd29039304195a3ee0a2ff9"
+    sha256 cellar: :any,                 monterey:       "7c6bfa670a93b5bb1b3e1e140fb65c5efa11d87dbbe06270302cfe5bda609f90"
+    sha256 cellar: :any,                 big_sur:        "f6fecbe91ae04355d6f8b124f580ae0cec58e46ff574a873805ad3e54a824540"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f1bdc59396f56e3f6ed465204317d931c312652abbb5052f8ca57373765b8766"
   end
 
   def uuid_dependency
