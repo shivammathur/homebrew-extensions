86ae505f0e4ac357feab564cba09ceee13e7d888
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-04 10:07:37 +0000

    Update extension dependencies

--- .github/deps/uuid/e2fsprogs.rb
+++ .github/deps/uuid/e2fsprogs.rb
@@ -1,6 +1,8 @@
 class E2fsprogs < Formula
   desc "Utilities for the ext2, ext3, and ext4 file systems"
   homepage "https://e2fsprogs.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.46.6/e2fsprogs-1.46.6.tar.gz"
+  sha256 "bf2fcc7ee5178fe73a3057f7e2aa3fe52e98b7bb461509c67b021ba00f94c6f7"
   license all_of: [
     "GPL-2.0-or-later",
     "LGPL-2.0-or-later", # lib/ex2fs
@@ -10,30 +12,19 @@ class E2fsprogs < Formula
   ]
   head "https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git", branch: "master"
 
-  stable do
-    url "https://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.46.5/e2fsprogs-1.46.5.tar.gz"
-    sha256 "b7430d1e6b7b5817ce8e36d7c8c7c3249b3051d0808a96ffd6e5c398e4e2fbb9"
-
-    # Remove `-flat_namespace` flag and fix M1 shared library build.
-    # Sent via email to theodore.tso@gmail.com
-    patch :DATA
-  end
-
   livecheck do
     url :stable
     regex(%r{url=.*?/e2fsprogs[._-]v?(\d+(?:\.\d+)+)\.t}i)
   end
 
   bottle do
-    rebuild 2
-    sha256 arm64_ventura:  "ea5c3806ca772a4f15759d1372a2b37b5390d5a4be6cf6a7b0fb6107a8d7d029"
-    sha256 arm64_monterey: "474e58e01910204a775eb12ab6322025c5468088f857d71dbde0a37462412255"
-    sha256 arm64_big_sur:  "5296cb03cc130751689a14bbdedb21018f07e3cd3ada7b88f811d748a79dc38c"
-    sha256 ventura:        "a232d3349b0b702b5d109cd4778ee305563c713b5b312dd5bc64e68ad0bbf504"
-    sha256 monterey:       "2d0d0cf4044a8d608ccadd4c57ea6e9e4f0a0a380fc03386993053cb715e9bfa"
-    sha256 big_sur:        "a2e07e058b28329521afa738526942e344d107630f8312eacd43b57ce752bde8"
-    sha256 catalina:       "956dbc649227546cc36afcc88a89651bd99b89116f8d8c2e00cfcf375f9e98c7"
-    sha256 x86_64_linux:   "964d0ca9f5517c4137dbac355540b97b21b35229e918072de437d4528dfa5a69"
+    sha256 arm64_ventura:  "e0f6bc2a122f8907b42911d45cf0d30f7acbc9ab9a30e8488a712b458307d42b"
+    sha256 arm64_monterey: "c152340d0ebce9eb437a15b747cc92ef5f08fa61c58b7a1d9386104728fa8fc3"
+    sha256 arm64_big_sur:  "bd4c36e5f5741471df258d4164ffaf53790467e4206a4c248c47f6667ec1e564"
+    sha256 ventura:        "a5d8fcd407f09d7ef11e7180ec5e1ad2db7143e66dfbb5f0cb2276b53b508a89"
+    sha256 monterey:       "899d70651a9a571aa55ab7b8a3f6e4616e1f4cc0856e22c2a5c79b5b64bbd25f"
+    sha256 big_sur:        "d9251812c87a30083a9f1b9a12605f8dce0e443a9aa09e85a5891544f356c54c"
+    sha256 x86_64_linux:   "b2854383f1ee6e3b360012d2a53b7e0043c480084d5b35a9d1b8d9f639e26252"
   end
 
   keg_only "this installs several executables which shadow macOS system commands"
@@ -87,19 +78,3 @@ class E2fsprogs < Formula
     system bin/"lsattr", "-al"
   end
 end
-
-__END__
-diff --git a/lib/Makefile.darwin-lib b/lib/Makefile.darwin-lib
-index 95cdd4b..95e8ee0 100644
---- a/lib/Makefile.darwin-lib
-+++ b/lib/Makefile.darwin-lib
-@@ -24,7 +24,8 @@ image:		$(BSD_LIB)
- $(BSD_LIB): $(OBJS)
- 	$(E) "	GEN_BSD_SOLIB $(BSD_LIB)"
- 	$(Q) (cd pic; $(CC) -dynamiclib -compatibility_version 1.0 -current_version $(BSDLIB_VERSION) \
--		-flat_namespace -undefined warning -o $(BSD_LIB) $(OBJS))
-+		-install_name $(BSDLIB_INSTALL_DIR)/$(BSD_LIB) \
-+		-undefined dynamic_lookup -o $(BSD_LIB) $(OBJS))
- 	$(Q) $(MV) pic/$(BSD_LIB) .
- 	$(Q) $(RM) -f ../$(BSD_LIB)
- 	$(Q) (cd ..; $(LN) $(LINK_BUILD_FLAGS) \
