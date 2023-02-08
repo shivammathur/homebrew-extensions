21dbbd221b40e60259d7ed5c653a2613451e04b5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-08 15:08:55 +0000

    snmp@7.4: update 7.4.33 bottle.

--- Formula/snmp@7.4.rb
+++ Formula/snmp@7.4.rb
@@ -16,6 +16,11 @@ class SnmpAT74 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_monterey: "9ec2cacd5686aed9d242f86343bc4410d71b197b79440321cef5d74a047b79ff"
+    sha256 cellar: :any,                 arm64_big_sur:  "5e4dd519017022a7273e225b378d16291c3ac351a14ee37d6063003a08143ef6"
+    sha256 cellar: :any,                 monterey:       "9949aa137d93d6de0579d370506f1b40e0ace3e979627474238a71acb43c2371"
+    sha256 cellar: :any,                 big_sur:        "40dfe678a061ab43957e9aa62ae1c19bc8bcd7015a49489da5862dc7eb5942ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3d663b12cf3fa7a969f1eebb527b09e4be4f6e26f0efee2189e7ed06d54a4ebe"
   end
 
   depends_on "net-snmp"
