a7d9c1399ca1b53f4411db0b6cfc8530b94dbf83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-09 05:50:16 +0000

    igbinary@7.1: update 3.2.13 bottle.

--- Formula/igbinary@7.1.rb
+++ Formula/igbinary@7.1.rb
@@ -15,12 +15,11 @@ class IgbinaryAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3752fd5765570706698a39ebbf2f518ee7919a3d03ced940002c96ecf6774855"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6193b91bbb8984dc8baad15ddaedb7809494f5df74fcc02f48b8555ff2ebea49"
-    sha256 cellar: :any_skip_relocation, monterey:       "ee820c8e4f6a2c07fdf361af84fc1e0fa3979be82b7068c9a6cb6d895108a37c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c83fdae1e2fab6efd5e64bff1b000a4a07adc7ff484e0d276e7230d5e55ddcfd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee92610cd79944e9bacc33bdfa6c6347b589b6e816c30805cc71dabca5d7e60a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "88687e75e2598a0823201be76ee7aaa0bb6b0657abe3e0c15c6f83c690f0b2c7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "703c08cc3a3745368a72adce2ae2371eef3892a64274f53e581f08d466c0e92b"
+    sha256 cellar: :any_skip_relocation, monterey:       "c82196237972d0e72a6f90cc256caf749cb9239ce4ebe52ecc0fdb56146578d4"
+    sha256 cellar: :any_skip_relocation, big_sur:        "e5a8061a6237c7a195adc729884ecc2a6ae8c63c7f51191f3f3657c4e0a7fbee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "68dee0c6d62e29b7119d08f5f88867a13efda5451da3b2f1ce8de4a3360bb7ad"
   end
 
   def install
