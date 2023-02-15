7d794a16dc73e2505679de0839d4f9a56cd5d852
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-15 03:31:08 +0000

    Update mongodb@7.3

--- Formula/mongodb@7.3.rb
+++ Formula/mongodb@7.3.rb
@@ -8,8 +8,8 @@ class MongodbAT73 < AbstractPhpExtension
   init
   desc "Mongodb PHP extension"
   homepage "https://github.com/mongodb/mongo-php-driver"
-  url "https://pecl.php.net/get/mongodb-1.15.0.tgz"
-  sha256 "eeb6268d34bd0b4a3dcc60dde4e484f5cf4fa2439ca3d9f024c0000e99ee7240"
+  url "https://pecl.php.net/get/mongodb-1.15.1.tgz"
+  sha256 "b2038e778d71f45cadb8c93a30eb548e5c2c1e4f832807a775ed516b7816b749"
   head "https://github.com/mongodb/mongo-php-driver.git"
   license "Apache-2.0"
 
