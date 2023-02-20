fff1da0fe125529f83002d06105e1066b5f21710
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-20 10:08:13 +0000

    Update extension dependencies

--- .github/deps/pdo_sqlsrv/unixodbc.rb
+++ .github/deps/pdo_sqlsrv/unixodbc.rb
@@ -1,13 +1,13 @@
 class Unixodbc < Formula
   desc "ODBC 3 connectivity for UNIX"
-  homepage "http://www.unixodbc.org/"
-  url "http://www.unixodbc.org/unixODBC-2.3.11.tar.gz"
+  homepage "https://www.unixodbc.org/"
+  url "https://www.unixodbc.org/unixODBC-2.3.11.tar.gz"
   mirror "https://fossies.org/linux/privat/unixODBC-2.3.11.tar.gz"
   sha256 "d9e55c8e7118347e3c66c87338856dad1516b490fb7c756c1562a2c267c73b5c"
   license "LGPL-2.1-or-later"
 
   livecheck do
-    url "http://www.unixodbc.org/download.html"
+    url "https://www.unixodbc.org/download.html"
     regex(/href=.*?unixODBC[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
