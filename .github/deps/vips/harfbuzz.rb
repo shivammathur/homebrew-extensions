31368f502783e6adfc6f34a0d1b7a30dc5c2b9da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-04 10:06:29 +0000

    Update extension dependencies

--- .github/deps/vips/harfbuzz.rb
+++ .github/deps/vips/harfbuzz.rb
@@ -1,19 +1,19 @@
 class Harfbuzz < Formula
   desc "OpenType text shaping engine"
   homepage "https://github.com/harfbuzz/harfbuzz"
-  url "https://github.com/harfbuzz/harfbuzz/archive/7.0.1.tar.gz"
-  sha256 "9f6601010fc471b8a41f56d529b6000bd3a6df3dca10565794669fa66292457c"
+  url "https://github.com/harfbuzz/harfbuzz/archive/7.1.0.tar.gz"
+  sha256 "6c7a358c6e134bd6da4fe39f59ec273ff0ee461697945027b7538287b8c73b1e"
   license "MIT"
   head "https://github.com/harfbuzz/harfbuzz.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_ventura:  "9be79b5d81a65ebceddb4edb19681c686c79673cab9f254782388f00ebc156a3"
-    sha256 cellar: :any, arm64_monterey: "18ee2c2e6f4f2e71c687d9876bac7e76853b255643b405500e12ec5c2e3d17f5"
-    sha256 cellar: :any, arm64_big_sur:  "667dc03a21d739c1062285987991fffe1c9a374114bf54a91f331c97a8dcac9f"
-    sha256 cellar: :any, ventura:        "d7350f3ef9ad1b8df09da44e51354aa18f1c4f4eb91e0cf61d49a860b0627ad7"
-    sha256 cellar: :any, monterey:       "6dd99cf8b279c86e86c656ef020172f3347090f41d71143cd04313056fff74d8"
-    sha256 cellar: :any, big_sur:        "4fe4ddb06c26ac051758f26500ffc00dccf1c4e1c91220e2c63f8458c681bfcf"
-    sha256               x86_64_linux:   "a64f01f6fd0b4627582dfdc121fadfd8ff521122fbe0e32ef565cf03554f4912"
+    sha256 cellar: :any, arm64_ventura:  "49464bacf4e6af3dfcbb6c7ad6b3837bff639ad7938909fd5ca412593a9203ff"
+    sha256 cellar: :any, arm64_monterey: "a93182e4d1b76b7a5abe3be51d41c65531c603cbdfc069632b1e52f7c6efbd37"
+    sha256 cellar: :any, arm64_big_sur:  "7042d7647cfad6bbd8f82768bfc6441d9f6f1b9403492ab3195645902c569dc0"
+    sha256 cellar: :any, ventura:        "706c7425225c602226f177c3e08776dcb75cffe271bb8721a406d90a92c2610a"
+    sha256 cellar: :any, monterey:       "54a3d3b15ea59065ee0d890ecf1734b2d0dd3d1c1e741b0968ae7fe10e2f1133"
+    sha256 cellar: :any, big_sur:        "56eef99328f9979190d1dedb442ec2c363aa5106fa394b204ff5194e288e1afa"
+    sha256               x86_64_linux:   "9c8b1d11ca5923600226c21ceba5b755676f910971fd465a79478cc887f55866"
   end
 
   depends_on "gobject-introspection" => :build
