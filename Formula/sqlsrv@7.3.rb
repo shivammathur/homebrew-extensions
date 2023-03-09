d24c94db75bb6e3db0bfc6feacbc59322c519dd7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-09 06:10:27 +0000

    sqlsrv@7.3: update 5.11.0 bottle.

--- Formula/sqlsrv@7.3.rb
+++ Formula/sqlsrv@7.3.rb
@@ -15,13 +15,11 @@ class SqlsrvAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "5eb3afd68a4904659b2e18fae1289785bd495f12dee180179e9e7421a29deb4e"
-    sha256 cellar: :any,                 arm64_big_sur:  "3a93cc3288568fae6799816be277e16147075197d162f28612928ff477db1b6a"
-    sha256 cellar: :any,                 monterey:       "f338a6a0ccb4493bd86397bd9a5664fd2e7a27f9de64acc038d8c47d2049987a"
-    sha256 cellar: :any,                 big_sur:        "9531d34142ad698b0c284780f829f0d4a7b34b7a200b81694cd48968906f54ec"
-    sha256 cellar: :any,                 catalina:       "bf3789942d33d3e0f34528668cc4b5841f388483f66bfb593f737e24b335b3e7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "87a0e2a142a10ea56487049fae5b2e6b89c717f54e0be6a89045255f19afba9b"
+    sha256 cellar: :any,                 arm64_monterey: "1b19469574e30a9b770d7502e9bfb01f7ec73cf53997ed1a9e8f3758e9b49837"
+    sha256 cellar: :any,                 arm64_big_sur:  "a85a2fc309cb2b5699a8e731e271b46e19dd0d0c124d046782f3e4a4a0e0325f"
+    sha256 cellar: :any,                 monterey:       "73eb215d4bdff6cebb6ca29efe5fb5495e6ee48ed0efd3d21afd8241dc800581"
+    sha256 cellar: :any,                 big_sur:        "f1ab9c53513251c14a2cf86d2c8a48bc032570019d01a96e1d4d5a83e94fdb1e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e8c2e023c8fa45f14446c765e0eb105d8d3c46fdbe76c60e49f1f095d44a1bbd"
   end
 
   depends_on "unixodbc"
