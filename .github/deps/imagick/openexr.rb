a8ce3e6fb9fe7fa98ca384478c194ef9fe97a0c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-02 10:05:47 +0000

    Update extension dependencies

--- .github/deps/imagick/openexr.rb
+++ .github/deps/imagick/openexr.rb
@@ -2,18 +2,18 @@ class Openexr < Formula
   desc "High dynamic-range image file format"
   homepage "https://www.openexr.com/"
   # NOTE: Please keep these values in sync with imath.rb when updating.
-  url "https://github.com/AcademySoftwareFoundation/openexr/archive/v3.1.6.tar.gz"
-  sha256 "daa33d93a7b706e27368a162060df0246a7750c39a01a122d33b13f5c45d2029"
+  url "https://github.com/AcademySoftwareFoundation/openexr/archive/v3.1.7.tar.gz"
+  sha256 "78dbca39115a1c526e6728588753955ee75fa7f5bb1a6e238bed5b6d66f91fd7"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "d693491cf6fbdfcb1af8182224f221e05d9616102154c2b89c204171fd0771a7"
-    sha256 cellar: :any,                 arm64_monterey: "5e284bee8b44bf804ad98e89ec1651a5692675a444a34ff100c9c7364d7f014e"
-    sha256 cellar: :any,                 arm64_big_sur:  "c35545e0fcf2649f8a109038b5111244b1439db93455e72117d60967a9de2b20"
-    sha256 cellar: :any,                 ventura:        "e63936a21f703b5cb14046e933bc7107691de334406a91c87cb7e1aa7115246f"
-    sha256 cellar: :any,                 monterey:       "e1ed7ef06f854f826505cf7ebd0eab915dcbbef7bbec2a34647b9c08ce4dd4a1"
-    sha256 cellar: :any,                 big_sur:        "8244aa8ae5241b0871d5799ccfd92ea5c9982deef565759a0987f4e4aa4daa42"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6cff41929d6d580903bbaca126eef5a09894ba2ac421140c0a7b85fb125393c1"
+    sha256 cellar: :any,                 arm64_ventura:  "af3bf3c18721d74bd6e3ae7ca18af940b18d598539b69f5872e2d5e130f6e83c"
+    sha256 cellar: :any,                 arm64_monterey: "4eadfb370990348330f6926081e23dfbf24824ca114f5c5e346251f0bb766b89"
+    sha256 cellar: :any,                 arm64_big_sur:  "c967d62720920952652225959c9720fa23095d9d4f7c51c6bcef3fdee8f19b68"
+    sha256 cellar: :any,                 ventura:        "a56b65bb841761e73cef93fb0d29a3419603a469e82dbe981bd4241adaa6f3a1"
+    sha256 cellar: :any,                 monterey:       "49735200012f8571dcad5aed537dd3f11fda0736c8d1a283ca4357538b9d5f16"
+    sha256 cellar: :any,                 big_sur:        "33e64c16c41ffdae87a17fad4e4ef5f7783e35f93e849a2be6fc36e648c96654"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cc85fb2f2b332515511f56211372262f707dbe90f7f4cb9a60be1cc701ad0956"
   end
 
   depends_on "cmake" => :build
