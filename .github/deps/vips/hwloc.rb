65c8b90de7c1524fddf14a7a7157d2144538af6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-05 10:06:44 +0000

    Update extension dependencies

--- .github/deps/vips/hwloc.rb
+++ .github/deps/vips/hwloc.rb
@@ -1,8 +1,8 @@
 class Hwloc < Formula
   desc "Portable abstraction of the hierarchical topology of modern architectures"
   homepage "https://www.open-mpi.org/projects/hwloc/"
-  url "https://download.open-mpi.org/release/hwloc/v2.9/hwloc-2.9.0.tar.bz2"
-  sha256 "2070e963596a2421b9af8eca43bdec113ee1107aaf7ccb475d4d3767a8856887"
+  url "https://download.open-mpi.org/release/hwloc/v2.9/hwloc-2.9.1.tar.bz2"
+  sha256 "7cc4931a20fef457e0933af3f375be6eafa7703fde21e137bfb9685b1409599e"
   license "BSD-3-Clause"
 
   livecheck do
@@ -11,13 +11,13 @@ class Hwloc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "ebc0569119f947e8fb8d7fe6afd16c0837b4cea63bdf3da4aa6780a34b602255"
-    sha256 cellar: :any,                 arm64_monterey: "b21514e88d48aaa2337f885d20ec652518de6592e9e2e31d02f9f77b40ca8d21"
-    sha256 cellar: :any,                 arm64_big_sur:  "44f5c7c1fa40a2e92547848f94bf8b5d3342c332440c17c924a2d723bb99c265"
-    sha256 cellar: :any,                 ventura:        "f5177bb1cee6726d0ce1f5e38f1565cc6ef57bc7d012ecd171142ebc6b383d43"
-    sha256 cellar: :any,                 monterey:       "5f7e91070d53048025f859c7818061bf45f439e905a651de6f4797e7a67fcc3e"
-    sha256 cellar: :any,                 big_sur:        "72763e5ea7bc83e12e27b2ba45e00d2bbd7275f8f78192037b0e3c70a7b22e68"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "823f01a9103475e97c90b55256380d4b12e662c6282b379b842edc4908dd340b"
+    sha256 cellar: :any,                 arm64_ventura:  "c27f24627823ce209b3249737f6dbd4b001f7ca23ed3e59896125a11bac579eb"
+    sha256 cellar: :any,                 arm64_monterey: "f1fb60aa94a0409d191960c9e15b66061d0c1843a7f051cc2c7fc543fe85bda8"
+    sha256 cellar: :any,                 arm64_big_sur:  "685c4d5588e835fdb628f36653218addc6f22ddf73ba2dc7b930a0e5ac97345f"
+    sha256 cellar: :any,                 ventura:        "e8d0c2594d39615247ee63c1333482929b00321fee0feee0ce5f32e6b2a735ef"
+    sha256 cellar: :any,                 monterey:       "e460c87fc7c3f905572610617d60bf761bea65eea81c52ee9089b73cb58c6f36"
+    sha256 cellar: :any,                 big_sur:        "7661285898f8baf26255dd46388d767ca4459f96d19083762e8f5eed449d2c35"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2bd836b802e40d21dce48e98a973cc0fb54e56ccb31d0f7054791c052946737d"
   end
 
   head do
