c862c3b5fb12a46faa665a00b6f6ca5abbf51847
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-25 22:54:43 +0000

    apcu@7.0: update 5.1.22 bottle.

--- Formula/apcu@7.0.rb
+++ Formula/apcu@7.0.rb
@@ -17,6 +17,7 @@ class ApcuAT70 < AbstractPhpExtension
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c958b62677d6108962ec9a8dc78d093ba0a32572df233a416aa0c62c0f2e006d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "009dcb9d7b683498eb9450a16ab6e71f34ad8750da8afa0adf4ed1ce3c7b60a0"
+    sha256 cellar: :any_skip_relocation, ventura:        "f76d1bc01b59c552fad86f2e935a4d58cde916357051781d16997547ff1ebb1e"
     sha256 cellar: :any_skip_relocation, monterey:       "8fc953b71be3f62f1b9b5941c6e1ae0146be7aeffdc50c247455803802dd2219"
     sha256 cellar: :any_skip_relocation, big_sur:        "4d974c1b11d9c508add2534a1b22ed46db1f3c225ab3c87a48d4cf99abebe979"
     sha256 cellar: :any_skip_relocation, catalina:       "b32c88b213419dca8ecadb6bea7d66851c46f5cdf714742aee72584bb75021b8"
