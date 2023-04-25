143c3f84efc017aa27987d479c654127fc90fd16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-25 22:46:06 +0000

    amqp@7.4: update 1.11.0 bottle.

--- Formula/amqp@7.4.rb
+++ Formula/amqp@7.4.rb
@@ -16,6 +16,7 @@ class AmqpAT74 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any,                 arm64_big_sur: "7a935a9d1675e2be8144094ace1a97b8bd01e1b07448df896039ea22b9383478"
+    sha256 cellar: :any,                 ventura:       "28281cd09c1adac8ebf75f208c5d59c6f8582c61ce846691a01dba22d471228e"
     sha256 cellar: :any,                 big_sur:       "304606252ecd9c31a6b9639d15f75f4c5cab6840a32a66ec1f74efceddd9a677"
     sha256 cellar: :any,                 catalina:      "906dcb8cb251b94b2f4d39772642e87607d281c7460d6f4cc5e4e414209a8eb5"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "692fa0934d3d279cbdb45525b2c622ae214103c301fe25bf0fe5ce1ca3627ed2"
