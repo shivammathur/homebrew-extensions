de9705b7c6d99ca96be18e664307bd1258447470
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 05:28:54 +0000

    msgpack@8.3: update 2.2.0RC1 bottle.

--- Formula/msgpack@8.3.rb
+++ Formula/msgpack@8.3.rb
@@ -18,6 +18,7 @@ class MsgpackAT83 < AbstractPhpExtension
     rebuild 2
     sha256 cellar: :any_skip_relocation, arm64_monterey: "5828858d1ba6ce8cef098a77bf92b7348fd2f9a62268d083e0e626f713be46f6"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "45347e09adb828af25fd7e3c8a16a14798e3ecb240a4616afcc7b552633c42f5"
+    sha256 cellar: :any_skip_relocation, ventura:        "350c72bb4b2259fd4f360a69b2bacede4a9c0e9bde6e50a06b94929bd70c0b2c"
     sha256 cellar: :any_skip_relocation, monterey:       "6380a7042b6234b65ae021f86373a354b2b7a9798e0ff1ead26e34774ec8d0e6"
     sha256 cellar: :any_skip_relocation, big_sur:        "d574a8d5522cc7f3bb20889e7c000c23a61dabd078ef3f2015c6f07489cf7583"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "fe944a357b13b7efae2fb07133ae99e2ccb4908537a5da2e0c4401262687d3ad"
