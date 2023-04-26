36a97b9e9fd3bbf0b397f170205cdd78fb138289
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 02:23:39 +0000

    igbinary@7.0: update 3.2.14 bottle.

--- Formula/igbinary@7.0.rb
+++ Formula/igbinary@7.0.rb
@@ -17,6 +17,7 @@ class IgbinaryAT70 < AbstractPhpExtension
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "71a437645d2cc5f4f646398625a906a54ff3ba42336728320dbf85b3c4ec6aa7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5342a4d6e4483874de660f2a971236a8a9dd7373b5dedc35dc31787140b79d3c"
+    sha256 cellar: :any_skip_relocation, ventura:        "700109b665997fa03a91169b79299a9d2a9663d81e12d8cb7d3c1312470bded7"
     sha256 cellar: :any_skip_relocation, monterey:       "bc20e4e2a45e09c279c0d33e37db7a61cfda61f480aa51c12de78e7693d74a73"
     sha256 cellar: :any_skip_relocation, big_sur:        "df24ad3cbc26b5e6010c3e53085885d1e0aa6cae89d6b3a6d813a010cf665c21"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "b34a980608258d676ff31217ff927bd0be672ebcb88251b438e4db2c15d70a70"
