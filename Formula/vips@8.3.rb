01ba4e41e8a47c8cfa361a2cb4f7c2ea8e95d102
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 11:20:47 +0000

    vips@8.3: update 1.0.13 bottle.

--- Formula/vips@8.3.rb
+++ Formula/vips@8.3.rb
@@ -18,6 +18,7 @@ class VipsAT83 < AbstractPhpExtension
     rebuild 5
     sha256 cellar: :any,                 arm64_monterey: "107b5077f6be1faec1f759b8400abb80ef3a8a50641c84b3a4f1f922f10367c0"
     sha256 cellar: :any,                 arm64_big_sur:  "d1ba93c2c68532331d9579192830afa567eac5d09694601ed81de7d7213a409a"
+    sha256 cellar: :any,                 ventura:        "620c6e5ce1d5f3b68369ba47ff216416cf36720624ccbbf3130d558f75302e45"
     sha256 cellar: :any,                 monterey:       "fe5e6ef68e032c0d50b4e3bcb9ddb9426d702806921a7489fbb91c35fbd9c702"
     sha256 cellar: :any,                 big_sur:        "17d72b716be2d577c8a1c2dfab88754000c9f80e903eccadce4b4b021103c081"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "81d51fcc30a03748657d36bdbc7066cc318716b31ca8957cd5d4182376b48dc7"
