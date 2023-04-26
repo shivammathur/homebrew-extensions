db3010cfc39079c1861ec6f387fdc6dcabfdc812
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 12:11:21 +0000

    yaml@7.4: update 2.2.3 bottle.

--- Formula/yaml@7.4.rb
+++ Formula/yaml@7.4.rb
@@ -17,6 +17,7 @@ class YamlAT74 < AbstractPhpExtension
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any,                 arm64_monterey: "517a830a8b503888833a37f1148cdbf9170e67bceb0cd886f9e15f7c574cd6a0"
     sha256 cellar: :any,                 arm64_big_sur:  "d48696e6267fe21a05cfc33fdd786b4a6a63d8c318df177b63c37809c2349219"
+    sha256 cellar: :any,                 ventura:        "5fa6949c8ad043e4ea3dc452fd59caf4b3044f6cbcdcfced7e7a1b12370c8856"
     sha256 cellar: :any,                 monterey:       "7e6f6f869407bfc36b3bd472d7278291589e6336245f91d089be5483a2dfb01a"
     sha256 cellar: :any,                 big_sur:        "28cf61fa926fc6c3f74f9b055570fb56e95a9bae4457ed56c0e7d470a34f8dd0"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "abf74ba02a9c95107c8f145d7432138d1b72d5457bcdc8d59b355cf905db2b26"
