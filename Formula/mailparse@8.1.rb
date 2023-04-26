4e7d380b8938d6b5f521cea8a536876d945fed23
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 03:57:56 +0000

    mailparse@8.1: update 3.1.4 bottle.

--- Formula/mailparse@8.1.rb
+++ Formula/mailparse@8.1.rb
@@ -17,6 +17,7 @@ class MailparseAT81 < AbstractPhpExtension
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "485eb5dd598487b71b0a8c86e4156ec02d9084f26c9561b7e0ae7f42fbeec22f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "aea9018a619bd76ec153a746be31c219d0e728ceaefe83987d5f28f0442f86b4"
+    sha256 cellar: :any_skip_relocation, ventura:        "745ad368400efea2759ae0e35d3a34a50bcaaa7772b1205d732bd276132a72ed"
     sha256 cellar: :any_skip_relocation, monterey:       "a3ed4d75e15bca778eb550d641398ade1766eccb2c085939f320c44f96217a4f"
     sha256 cellar: :any_skip_relocation, big_sur:        "dd0580b43c56aaa15dec61cb76f12cd7abb391e7612676f40dcecf9eb341192d"
     sha256 cellar: :any_skip_relocation, catalina:       "8393301e99f969f3f5589c7bf8f8fffeb0a0f8628af7b1b276b364d4c4b50cb7"
