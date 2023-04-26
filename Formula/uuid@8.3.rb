a3c05ed5d76abf5fdc93b5d9c0b7a700b14ee7ab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 10:19:27 +0000

    uuid@8.3: update 1.2.0 bottle.

--- Formula/uuid@8.3.rb
+++ Formula/uuid@8.3.rb
@@ -17,6 +17,7 @@ class UuidAT83 < AbstractPhpExtension
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any,                 arm64_monterey: "65b1475a489414f9c87d123b092e98915ee833820d8f0987a2d1f85d13d98071"
     sha256 cellar: :any,                 arm64_big_sur:  "6eabe12ffdba996f98116fe30d4967c77fc9104f5e8a96fb9e504965913e1d96"
+    sha256 cellar: :any,                 ventura:        "6c1494b74f8196ef887b002ff2d9af280ec172c9a8c27b4fc1f0db71a499d3c4"
     sha256 cellar: :any,                 monterey:       "4f34598db73f02f3905a98ba367c04fc7514f23dc475b757625a038fd532a4ab"
     sha256 cellar: :any,                 big_sur:        "90f29375a25e2dea9e3c141a4a62fbb328d607f78a28696dca927e26946e3dd6"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c7e63546f7818f08696b2f105a06c2d81f9a31efb83a194a0a4ebbf88690fd45"
