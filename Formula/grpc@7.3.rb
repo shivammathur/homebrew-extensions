1e01cbad3c7e199359e99be02d21521c915c035e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 02:04:41 +0000

    grpc@7.3: update 1.54.0 bottle.

--- Formula/grpc@7.3.rb
+++ Formula/grpc@7.3.rb
@@ -17,6 +17,7 @@ class GrpcAT73 < AbstractPhpExtension
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3c10a93280199030b35a1bbdf44a52bd3582b097cea7d4e90ceb24c8f8ee5688"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c0bfeeee371d9a223377adbcfc65ee1018bb955f902a4bf68d71efbb4ad5c433"
+    sha256 cellar: :any_skip_relocation, ventura:        "8537a459b3336309a253a07ebd1c5bf8a63ee5e9ee156a90bc2872b1eff42133"
     sha256 cellar: :any_skip_relocation, monterey:       "e64686ec021831aaabf8c02aa8f9608cc7859b13ac180c750f99344f68769d73"
     sha256 cellar: :any_skip_relocation, big_sur:        "0b437840469cd8be2c09dffc007c6fbc1451cf23a9c5a56a639e22e11a6b12fc"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "815c9a1a8920d9ae70d14799cceb178cd89ecaa743a063324ab60ca8cfe777b2"
