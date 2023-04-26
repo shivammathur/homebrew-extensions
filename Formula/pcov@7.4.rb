391cf0df2bc5f750f3abb26113f89460b30a5c76
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 05:47:32 +0000

    pcov@7.4: update 1.0.11 bottle.

--- Formula/pcov@7.4.rb
+++ Formula/pcov@7.4.rb
@@ -16,6 +16,7 @@ class PcovAT74 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "83147b62160d224a8910d8677471340a61ef103cf7b4c7a8f65da3bec5469ba8"
+    sha256 cellar: :any_skip_relocation, ventura:       "b7793e75b4b76eafdbbb7aafee28e6a93983a6deb601dc320f6985e5c7848cfc"
     sha256 cellar: :any_skip_relocation, big_sur:       "0f3ed1c83a48de680127e42f68af517316a76139ea055b08a6093f3a4a2e7de7"
     sha256 cellar: :any_skip_relocation, catalina:      "fe7820bbad6681c90104578581a2b3254ce69807d36ecdbc716e7a0a181e3cff"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "321ccbbb6c918e25a1a0458469ea3ea6f726cc65aff00972a81ebc3986d56056"
