e1a546ce567582092e32ef1058e2d8a3deadfef8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 12:40:21 +0000

    zmq@8.2: update 1.1.3 bottle.

--- Formula/zmq@8.2.rb
+++ Formula/zmq@8.2.rb
@@ -19,6 +19,7 @@ class ZmqAT82 < AbstractPhpExtension
     rebuild 5
     sha256 cellar: :any,                 arm64_monterey: "82c5d4a440082933452c760be75ea3b773e433288290d20bb12250fb74b556df"
     sha256 cellar: :any,                 arm64_big_sur:  "0e3690ac7c07d13d022ee7eb42a4454a3ed4ebe0228f267c8965a03e72f20c93"
+    sha256 cellar: :any,                 ventura:        "3c9d78612afbb83c21de73e1e428273356e69407fcad67135a5fdd74e4ff4af8"
     sha256 cellar: :any,                 monterey:       "ef5522efaf949a2bd20dd5f6e2c5930eafc98c57cb61a2a6ba3abf5d847c2171"
     sha256 cellar: :any,                 big_sur:        "4edaf3740c17b841ad1eaa17450929e819e38855f9b18b70d70812b905b23f16"
     sha256 cellar: :any,                 catalina:       "e531d2b3a0396ec9ff52f5488463b4e4e1827ca58976e2ea191de8cd589aacf5"
