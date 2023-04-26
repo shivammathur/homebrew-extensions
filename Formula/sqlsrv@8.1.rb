f032cd203055607225ed7725408238962d6801de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 09:31:02 +0000

    sqlsrv@8.1: update 5.11.0 bottle.

--- Formula/sqlsrv@8.1.rb
+++ Formula/sqlsrv@8.1.rb
@@ -17,6 +17,7 @@ class SqlsrvAT81 < AbstractPhpExtension
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any,                 arm64_monterey: "6f6cd8d3d55eb781d9306db3aeffb1f975317513feb8d910e99ce92c756d0835"
     sha256 cellar: :any,                 arm64_big_sur:  "6434f53116997bafeb62e04dfdabd2c9b1030c275d7e4b9fb61fb967ba5e04d4"
+    sha256 cellar: :any,                 ventura:        "9831ceca21bc8ffad1c0d9e8e0b56b0abb9bc5e8a297af91f3862d25daaa9ca9"
     sha256 cellar: :any,                 monterey:       "b7c4fbde5354380e26941dbcb13ba2250b1a3c4c138de42db6e07a867935f5ce"
     sha256 cellar: :any,                 big_sur:        "7f0cad989c89147418f5254fcf7f8899efa1f209be574f523979c232ec04a1b4"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "2dc5a6bbaa94dbbcab3cbe9fe39c5c40d42ff9877662073a962bdd30bc4f82b3"
