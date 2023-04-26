df845e041891d05aae87339ce3462c007cbf18b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 11:28:10 +0000

    xdebug@8.1: update 3.2.1 bottle.

--- Formula/xdebug@8.1.rb
+++ Formula/xdebug@8.1.rb
@@ -17,6 +17,7 @@ class XdebugAT81 < AbstractPhpExtension
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 arm64_monterey: "b0fb5f38c3f8589ead0942de6b9891e0bd084ce675ec8126e0da1d5956dba7c4"
     sha256 arm64_big_sur:  "ca6d45cb1b8a627dac2d207603bff52d15e20ce9c69baa7036153a718ea259d3"
+    sha256 ventura:        "32b543f9a2365e5b9c37460c8ab52a739a71cf6fa692595531e6cb150ff46a5c"
     sha256 monterey:       "42b14cbe70d897ba5386f1825f07b62bc96b175063f99f6fc11f331e427780fc"
     sha256 big_sur:        "24f50eb031511f9f03a103099bed34fec86e76765ce7395d8397eca77fefbab5"
     sha256 x86_64_linux:   "3bf4bf6113f7f90a7d2fb5d25a1a9ba305aa1b0eebadd99c7972a77d936f1bfc"
