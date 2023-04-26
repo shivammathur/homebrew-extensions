fdaa1edd57c3e5f4ef47011287b9c95c3d4ef630
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 08:45:04 +0000

    rdkafka@8.3: update 6.0.3 bottle.

--- Formula/rdkafka@8.3.rb
+++ Formula/rdkafka@8.3.rb
@@ -18,6 +18,7 @@ class RdkafkaAT83 < AbstractPhpExtension
     rebuild 2
     sha256 cellar: :any,                 arm64_monterey: "8a4e9182a33e391a2a2801c124524bfa17274f57526451b23d03adb7d94bd8ef"
     sha256 cellar: :any,                 arm64_big_sur:  "a95a5b3cf772f5120f8979339ce8bf505039f35e026815cb3ce1661e84bfcf31"
+    sha256 cellar: :any,                 ventura:        "4f7792f43d783be308be74e9f1f9e2f03fc7297405e884e63a7f68732101f508"
     sha256 cellar: :any,                 monterey:       "005d7886ed77b5e93495ab003581fe6bb4abc81e73effb61cf6583983cd40c9e"
     sha256 cellar: :any,                 big_sur:        "2b1c3d7056a77e701aab21d6130261cd0261789846350ae8df8da2d0662fe512"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "61a5ca4d70c83b3a0723efa6aafa2e462873deb44581a1ea605426cc331bbd5e"
