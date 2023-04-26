ba8e55594a43195b87347d666bd0ec527c448870
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 07:04:43 +0000

    propro@7.0: update 2.1.0_1 bottle.

--- Formula/propro@7.0.rb
+++ Formula/propro@7.0.rb
@@ -19,6 +19,7 @@ class ProproAT70 < AbstractPhpExtension
     rebuild 2
     sha256 cellar: :any_skip_relocation, arm64_monterey: "bccbaa9b3fa407d60429025d3957ba63dc2b3d9a1038dacd76fe2b680cbc3754"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6e20aa5721d8d97b992c866845f2096eb11c89ba1defc3d589240f7707d52fd4"
+    sha256 cellar: :any_skip_relocation, ventura:        "867f5d81d1244fdd81b0e009bf0b24697ca80abf4eef309cc579f21c2760b8f9"
     sha256 cellar: :any_skip_relocation, monterey:       "97efe61fba56c705dea36908cd06ad25e519b5215207566544b7b39c3306f318"
     sha256 cellar: :any_skip_relocation, big_sur:        "2fc7721b01075968e6d39443b9d755d83e91aeffec338d6769fa92bf336bf646"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c530f72f4fc589c8500929126f62fbb1708d73b5cb69451812022b241b802949"
