74a8c9164d1407b3006ae68b30993630ad5a2a3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 00:04:38 +0000

    ds@8.1: update 1.4.0 bottle.

--- Formula/ds@8.1.rb
+++ Formula/ds@8.1.rb
@@ -18,6 +18,7 @@ class DsAT81 < AbstractPhpExtension
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4802c60e38bee695d2a8c65900e2420d6a247db30bdc63832d30cf0efc985bd3"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ae0170f17a983197ce7bb0594aa6e7613f7d7aefe1d8b1921b7089881289c18e"
+    sha256 cellar: :any_skip_relocation, ventura:        "343337caedddac62dfca419a7a32f6a4fc23cff794e7a1d283fec490e5b0f3d6"
     sha256 cellar: :any_skip_relocation, monterey:       "dc85a0efc7116ac0552dbafd37d0a6f5cb7334b5aefafbbbf9e22a59bcbc36f7"
     sha256 cellar: :any_skip_relocation, big_sur:        "ef8a2467bf62cf834532f3be8f0bb6f831f51600ffa2f0f4466880ad7998c7ad"
     sha256 cellar: :any_skip_relocation, catalina:       "ab4d57835e316b887bcdad68a0255d02f7c4c72788cbbcfc5f83fc64b728d778"
