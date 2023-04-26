cf653ec2791f8c5dd6195a839235cb6aa4e07937
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 00:24:53 +0000

    event@8.1: update 3.0.8 bottle.

--- Formula/event@8.1.rb
+++ Formula/event@8.1.rb
@@ -18,6 +18,7 @@ class EventAT81 < AbstractPhpExtension
     rebuild 1
     sha256 cellar: :any,                 arm64_monterey: "e0211dd675fc943d3e6e7fe06d2782bdbd2607c5317907ba20de2970e514a9f1"
     sha256 cellar: :any,                 arm64_big_sur:  "ab59c3a5e98f95bb5ecd1201209850cdada5a6d665bc3302ee43e8785e22ff91"
+    sha256 cellar: :any,                 ventura:        "cdc29ca10326404953a848a7b43a6b726bfa624469f3e0cc6d6b767318520050"
     sha256 cellar: :any,                 monterey:       "7e944245c4e8fc7c96e83fbb509e6c05dcd1689699a9f0ac0c6d978f70857768"
     sha256 cellar: :any,                 big_sur:        "fc2169865a4121624e54fb4b9f1f5e3d44a57416b35fa0944a4090d4eff424bf"
     sha256 cellar: :any,                 catalina:       "a88cb1073dec03460560cb014243f4b2d10527690b57d12328ac7c587b0896c9"
