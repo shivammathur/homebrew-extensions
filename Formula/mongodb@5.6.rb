d2674032c9228098230cef126102924bc2ebed48
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 05:06:33 +0000

    mongodb@5.6: update 1.7.5_1 bottle.

--- Formula/mongodb@5.6.rb
+++ Formula/mongodb@5.6.rb
@@ -19,6 +19,7 @@ class MongodbAT56 < AbstractPhpExtension
     rebuild 2
     sha256 cellar: :any,                 arm64_monterey: "7e146037f33f161c7f0eb5420aef87f074ffd964005080902494116118fb49b9"
     sha256 cellar: :any,                 arm64_big_sur:  "3362354da45e16cc0a24811ef49abf49ea0df93a6044cbedd1bb052aa4ab08cc"
+    sha256 cellar: :any,                 ventura:        "beaf2a234175d072e540e9352b6060b981a55e97362608b1748300156e55366b"
     sha256 cellar: :any,                 monterey:       "0e9da4775fea214e03d1bd23e90009ac9d471a7939ddf078ca15c05e49120d76"
     sha256 cellar: :any,                 big_sur:        "036e1aa82b6c24ec4c18e03c70f5082a5fdef7d710eefb3db4d0e6dbc097a787"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9247088926a7715f0d80056c139df6d9fc132f3076398ab7c185a96a05d3067"
