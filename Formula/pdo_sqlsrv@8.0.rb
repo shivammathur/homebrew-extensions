7dbff90e6fa254ce6c57d19760eeb46c79d6e535
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 06:14:38 +0000

    pdo_sqlsrv@8.0: update 5.11.0 bottle.

--- Formula/pdo_sqlsrv@8.0.rb
+++ Formula/pdo_sqlsrv@8.0.rb
@@ -17,6 +17,7 @@ class PdoSqlsrvAT80 < AbstractPhpExtension
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any,                 arm64_monterey: "75dcb553cbae23779d7886d2f582cc5358e5f0a2702b835a4bf12b9efffdbd9c"
     sha256 cellar: :any,                 arm64_big_sur:  "77db18d1b1976b1f6c62c4d7883088e6eebee4e400a7fad979cd2f0e1a6171a0"
+    sha256 cellar: :any,                 ventura:        "eed395c4cfe64f19c6287d329a8c2d83c57330873dd7536c11a90823885f8a65"
     sha256 cellar: :any,                 monterey:       "fe9636aa698b50474a406ec7ea2f031ad87a4b7dbba607c9583e9c818dc8117c"
     sha256 cellar: :any,                 big_sur:        "eeeb7fefc8b9e3557789da10f0d8c16b518fe25bd8d4e400fb6e9a9f4855a20a"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "5fab9d71db23012233022bdce6a68cb4a1a0866738c1b37d64b76d7e85c5b0ae"
