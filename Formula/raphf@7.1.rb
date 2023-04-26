8c48d1f35492356131326c13a34a88b53ac9a91f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-26 07:57:23 +0000

    raphf@7.1: update 2.0.1_1 bottle.

--- Formula/raphf@7.1.rb
+++ Formula/raphf@7.1.rb
@@ -19,6 +19,7 @@ class RaphfAT71 < AbstractPhpExtension
     rebuild 2
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e043b655f4019b45a578033e22c3a879dcd44413275d411439de809ff8485d37"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c1c7f2573bc1b30aacbad3d03bb5756ced54479cc4d5f92f37645a50b2737b51"
+    sha256 cellar: :any_skip_relocation, ventura:        "5482afedf38f2438cfdf3a250e13893248a447b0b200cb72e389ad1d2efd9857"
     sha256 cellar: :any_skip_relocation, monterey:       "fda60a197361945d0dd5ae1d8c52a08d2c46c275e498db775c6c5096750a1b07"
     sha256 cellar: :any_skip_relocation, big_sur:        "5979bc35365f67ddd3453a10ccffcaf90064f9004abb3a1c370fbe536b202f3e"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "66243c4eda2dd0ba3340e517025a9c7eee8d04d36d3f10530fad4d0d8a92bfb8"
