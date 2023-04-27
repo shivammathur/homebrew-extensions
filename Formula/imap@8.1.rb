3e4b331141c3e9dcfabe931031ea3723f1d73a16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-27 03:40:11 +0000

    imap@8.1: update 8.1.18 bottle.

--- Formula/imap@8.1.rb
+++ Formula/imap@8.1.rb
@@ -16,6 +16,7 @@ class ImapAT81 < AbstractPhpExtension
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any,                 arm64_monterey: "47c4d6c4973b46dc7aecf854d79c7cef571b5c53b443825ca52d8a2cee26092f"
     sha256 cellar: :any,                 arm64_big_sur:  "6ede910a2e89c3946d139143072ad626641c86d4b88e59a855370d71fddfd690"
+    sha256 cellar: :any,                 ventura:        "0bedfac21d609061dce453eaaed178ceb8f6c1d1184f93d002036fc8e4577bca"
     sha256 cellar: :any,                 monterey:       "f73a2cf8d128a100e73ebc71dd152ac358d067fe8ce53270fc189485b776be6c"
     sha256 cellar: :any,                 big_sur:        "1dd0ef1129b557832229c7c9c6b72e1ffe24fc4117ab770e279e4376d1942ba4"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "62eaa7063fa70280742a7644f08bda5c26b523af511899b0d032e439d9700b4a"
