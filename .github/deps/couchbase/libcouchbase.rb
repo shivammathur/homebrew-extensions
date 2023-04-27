0cd3f6b4b50316cc6315e053e0c635a1877ec8f9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-27 10:06:26 +0000

    Update extension dependencies

--- .github/deps/couchbase/libcouchbase.rb
+++ .github/deps/couchbase/libcouchbase.rb
@@ -1,19 +1,19 @@
 class Libcouchbase < Formula
   desc "C library for Couchbase"
   homepage "https://docs.couchbase.com/c-sdk/current/hello-world/start-using-sdk.html"
-  url "https://packages.couchbase.com/clients/c/libcouchbase-3.3.5.tar.gz"
-  sha256 "75bf6770b1a98289bbc079c302fe40286f2e7003aaffe64403fe04aaa84e0fc1"
+  url "https://packages.couchbase.com/clients/c/libcouchbase-3.3.6.tar.gz"
+  sha256 "3c2029ae06e4b84ea6c8ed99636ce918e965eb702cbc4a326b19d23bdef725d9"
   license "Apache-2.0"
   head "https://github.com/couchbase/libcouchbase.git", branch: "master"
 
   bottle do
-    sha256 arm64_ventura:  "d9214a7dee5702813a29b54eb1609161e4caf74cdfc426bfc4453c9e9a2853be"
-    sha256 arm64_monterey: "ccc61af9bf33de16f1ae67f25a8313e08f72a4e0f8e6ea05335edbe608713342"
-    sha256 arm64_big_sur:  "bbbc7d284801bb8a6acedc11155bea2aab94c6acc1866316b0001e59a80adbd9"
-    sha256 ventura:        "45d30cd6b52d62085175dc4a33f03d8be1be318402d6391bbc2b579fdc2354d5"
-    sha256 monterey:       "32d83fdc521c2d5965c7aadecd775fcc1f46c51e8feb81b2b9678574f3128c89"
-    sha256 big_sur:        "01e9b91accbed749daf98ec9bad32799b384e765a4b999ac6316f77de824d523"
-    sha256 x86_64_linux:   "f6bae5e18d79df4e6270fbd5b716eeee92adedc08d5ac74118b8c2c815ad307f"
+    sha256 arm64_ventura:  "1de2c75def3dd56e37beb86f466cff79ba6ca35e15ded56d3dd42961acf22553"
+    sha256 arm64_monterey: "4f42739a0ebb51b21194fc997a4d5f131993b97cdd9bbf6431f8b67c9395840f"
+    sha256 arm64_big_sur:  "8d5053266f32ff50caa22fd28c8405be989937e3f95564fc8101992867e2f244"
+    sha256 ventura:        "48c0ee9d0003f903ae296afb8b1812f6f51edddf27378e7b41b6a3b95a101f4f"
+    sha256 monterey:       "5c74af87e1acd807becfc26a8f69e2fa9ed7840f6e8796d96221aafe012e2ae1"
+    sha256 big_sur:        "f263fc6282e31432ab2e7f7f97f69bb008acebdb000213517b061da514636d70"
+    sha256 x86_64_linux:   "3b1a34a2184b254bb37049096daabc1806466332879ffbbe66da35e95584e42b"
   end
 
   depends_on "cmake" => :build
@@ -23,14 +23,14 @@ class Libcouchbase < Formula
   depends_on "openssl@1.1"
 
   def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args,
-                            "-DLCB_NO_TESTS=1",
-                            "-DLCB_BUILD_LIBEVENT=ON",
-                            "-DLCB_BUILD_LIBEV=ON",
-                            "-DLCB_BUILD_LIBUV=ON"
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DLCB_NO_TESTS=1",
+                    "-DLCB_BUILD_LIBEVENT=ON",
+                    "-DLCB_BUILD_LIBEV=ON",
+                    "-DLCB_BUILD_LIBUV=ON",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
