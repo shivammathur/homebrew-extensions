bbf77c8e4103df577bd109cdfa752358dc434e33
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-27 03:39:57 +0000

    mcrypt@5.6: update 5.6.40 bottle.

--- Formula/mcrypt@5.6.rb
+++ Formula/mcrypt@5.6.rb
@@ -18,6 +18,7 @@ class McryptAT56 < AbstractPhpExtension
     rebuild 6
     sha256 cellar: :any,                 arm64_monterey: "aed698768a2990edbc5d3b8c7939e4c3edca7cec5bb44414da397c36cff90f2a"
     sha256 cellar: :any,                 arm64_big_sur:  "00d120da6fdd31449d4b93ea61c8d683cd8383b51c54536033db2e9cb79bf611"
+    sha256 cellar: :any,                 ventura:        "eeb218aec02f9798a1179d7a0daeaf30442e0e61db1fd14904654e0c0e7cc619"
     sha256 cellar: :any,                 monterey:       "64e9fa1fd5d6a90ca3f1e9195c244789c76f0f125572a816febe2f0638bb48e9"
     sha256 cellar: :any,                 big_sur:        "46f89e4c9d7c66b9c4c0b87f26c1c855e647b37592f143dc4f0b62024c63d950"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "375e671e34fe533f707ccacbfd061f57cddc730e66024d3d4f60aadfdbea1acf"
