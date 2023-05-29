a23e3d1d7f99f68207f08eac28a7db8909bc4f46
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-29 10:06:39 +0000

    Update extension dependencies

--- .github/deps/imagick/jbig2dec.rb
+++ .github/deps/imagick/jbig2dec.rb
@@ -1,19 +1,9 @@
 class Jbig2dec < Formula
   desc "JBIG2 decoder and library (for monochrome documents)"
-  homepage "https://jbig2dec.com/"
-  url "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9531/jbig2dec-0.19.tar.gz"
-  sha256 "279476695b38f04939aa59d041be56f6bade3422003a406a85e9792c27118a37"
+  homepage "https://github.com/ArtifexSoftware/jbig2dec"
+  url "https://github.com/ArtifexSoftware/jbig2dec/archive/refs/tags/0.19.tar.gz"
+  sha256 "e81b787ad0b147a437a52d9ce7de1a8f429655e8aa030383b6b2dd8919373717"
   license "AGPL-3.0-or-later"
-
-  # Not every GhostPDL release on GitHub provides a jbig2dec archive, so it's
-  # necessary to check releases until we find one. Since the assets list HTML
-  # is no longer part of release pages, it would take several requests to do
-  # this. As it stands, this checks the homepage, even though it has typically
-  # been slow to update the tarball link when a new version is released.
-  livecheck do
-    url :homepage
-    regex(%r{href=.*?/jbig2dec[._-]v?(\d+(?:\.\d+)+)\.t}i)
-  end
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "fd976897a71bad7195c7a248a9d12183dfb93c5e42f2a82cce542987cf3c4fec"
