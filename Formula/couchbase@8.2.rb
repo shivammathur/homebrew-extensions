c936b905164cac980d2dd5ab08e3004b22b8dcc1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-01 07:12:04 +0000

    couchbase@8.2: update 4.1.4 bottle.

--- Formula/couchbase@8.2.rb
+++ Formula/couchbase@8.2.rb
@@ -15,12 +15,12 @@ class CouchbaseAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256                               arm64_monterey: "3ff07911c10f781002e7090ae42e74b317b0651b9d0ca625e90961eb3f295ef1"
-    sha256                               arm64_big_sur:  "214d456cd153f31b34c3df7674693c2121457633fc5ebf0083320c3ff7222864"
-    sha256                               ventura:        "ffbb056f28155c76af5aba0d427e354143bd7cbd59479003334519b472ed4a38"
-    sha256                               monterey:       "f082b67fcc070a34305f779d7288bcaf5ae74514121ed70bf637887f0184b8bb"
-    sha256                               big_sur:        "7699fce1b3e25a8b668eacd8e80b76122c1d87e1a0a897fa26cf753c97803a59"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5dac3a89d3a8bc7d4d5a1c2b3058339d558dcfe37118e1c26e12dd01c4b9961b"
+    sha256                               arm64_monterey: "da14bcd05eeaeeb13cd991b422a84802f35a5f3f766ec14e5fc65a578f80da4f"
+    sha256                               arm64_big_sur:  "6397193e15185742e4ac8f74c296d594f305fe3779185702d0eda05e5ad099df"
+    sha256                               ventura:        "a10f09a44e6e454256d4699d87f7ad6c73cefe85fc54efac3e56ff0c1985de04"
+    sha256                               monterey:       "82d8ed53ddf61237845b24ba4ccfbac492f22d257968ce6f1f5f53b0c18cabd0"
+    sha256                               big_sur:        "d02b2ae41341779353c0a97fdcde803da95f36cd5d5b13f9343794cc0d01171a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "26db5910db3b6717609c8c9535431a1b3b2616e431d6bb4a60f8cf07ce91cb83"
   end
 
   depends_on "cmake" => :build
