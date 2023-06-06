f2b09e22687f16bdc668dbcf6117762f71bfd5d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-06 10:06:25 +0000

    Update extension dependencies

--- .github/deps/vips/nss.rb
+++ .github/deps/vips/nss.rb
@@ -1,8 +1,8 @@
 class Nss < Formula
   desc "Libraries for security-enabled client and server applications"
   homepage "https://firefox-source-docs.mozilla.org/security/nss/index.html"
-  url "https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_89_1_RTM/src/nss-3.89.1.tar.gz"
-  sha256 "3adaedb9e70c3c5f40603bf60a01e336190a6dbe01929d395f16b01fe84a0156"
+  url "https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_90_RTM/src/nss-3.90.tar.gz"
+  sha256 "9acd6534c41d8ead19fca6fcb3fffed2f9f09c437c3d79fee6a4ee668aaa93b6"
   license "MPL-2.0"
 
   livecheck do
@@ -14,13 +14,13 @@ class Nss < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "7a6a7b36b69e0454962f2c7a1cb58772d7cc56b035075b3baa94e2e047db12bb"
-    sha256 cellar: :any,                 arm64_monterey: "cb5d32c139fda15c153ddfbb198737083e7ac4a3f723f8cff965dc53752118e6"
-    sha256 cellar: :any,                 arm64_big_sur:  "7c14dd1932efa6006f6476f53292fdcf614b120244bcf645559f5c7f7615edaf"
-    sha256 cellar: :any,                 ventura:        "ff4be1fa79da7ac54cc006ba4e9895cab208d469775a32fd41eb13d972cd8a17"
-    sha256 cellar: :any,                 monterey:       "0558d396068aa1b0907e15637f145816f8edae562e105ebc1995e9c22b79d29b"
-    sha256 cellar: :any,                 big_sur:        "b281e4beffac3e41bce1bb9abcf5ce08a431b4b0fb2fa9bfb4cdfa6d947adea5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c23fac555c8987b5fab507851ed09c9e8e2bcb18234c961fee49c3906a174cb6"
+    sha256 cellar: :any,                 arm64_ventura:  "06e80a7720d63974bf705bf94855dab436f99775ea58c25645585aff1fa2f7fc"
+    sha256 cellar: :any,                 arm64_monterey: "021d488f0b75a8c6bfef7fd9b5ec21480f26ca46e936c2ff3713604c86e6ee96"
+    sha256 cellar: :any,                 arm64_big_sur:  "28ee42e78abc7ad3bb7393ba71f449b8418fc9a10227625860b495d04e1c0b6a"
+    sha256 cellar: :any,                 ventura:        "340ed3bd037d44c064ff7499fb4185a91ed59975bdae12d9ad0e2d391fa3c54f"
+    sha256 cellar: :any,                 monterey:       "048beeb4005b250ae7fb22099a061c051cc2ee36166bf60105a7be7ead572fb7"
+    sha256 cellar: :any,                 big_sur:        "8dec3947f3a3e4131d011516704a3c23bc9408f9a829f53cc4a1c3a2500da689"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8bfc4abaf05af28df1a87c8f8b516568e816bb9258cad97fe1d39a963b45631b"
   end
 
   depends_on "nspr"
