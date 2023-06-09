cf87a4ec7f8da18fb4852bbdf9dc22d4d18b0a21
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-09 10:06:05 +0000

    Update extension dependencies

--- .github/deps/gnupg/nettle.rb
+++ .github/deps/gnupg/nettle.rb
@@ -1,19 +1,19 @@
 class Nettle < Formula
   desc "Low-level cryptographic library"
   homepage "https://www.lysator.liu.se/~nisse/nettle/"
-  url "https://ftp.gnu.org/gnu/nettle/nettle-3.9.tar.gz"
-  mirror "https://ftpmirror.gnu.org/nettle/nettle-3.9.tar.gz"
-  sha256 "0ee7adf5a7201610bb7fe0acbb7c9b3be83be44904dd35ebbcd965cd896bfeaa"
+  url "https://ftp.gnu.org/gnu/nettle/nettle-3.9.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/nettle/nettle-3.9.1.tar.gz"
+  sha256 "ccfeff981b0ca71bbd6fbcb054f407c60ffb644389a5be80d6716d5b550c6ce3"
   license any_of: ["GPL-2.0-or-later", "LGPL-3.0-or-later"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "1eede72be5e777dcaf56bedd3d1e3025721802a6bb76ddadfaa7e00687473d29"
-    sha256 cellar: :any,                 arm64_monterey: "dd7c688e3670f0bd70de95986940863c36d6520b1367b4644a5107d10a2011db"
-    sha256 cellar: :any,                 arm64_big_sur:  "9f2ac8a6f7c3e59567963fb58b54cdb356398dcc5ca7ded600c4e69fd2b9f3c5"
-    sha256 cellar: :any,                 ventura:        "ce695fb7726b6e23ffa11f986200e57beb6491dafaee565b425e1bd6a6df99e7"
-    sha256 cellar: :any,                 monterey:       "e89e182fa226751c0dccc50f1c879a406afaf8d2a87a9bc0dfa2c922052fe0f8"
-    sha256 cellar: :any,                 big_sur:        "96c9f8a3e4492142b5a1945aa61fecc237340fd0b828bdfaaebf1556b100d541"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "178762434a32894286c1ee1b3f7221dd82a198a7bb60343b30c750ddbfa0a848"
+    sha256 cellar: :any,                 arm64_ventura:  "751e140ceac7711c462f1c05d74297c79f0abea26666f974370886c01d7bec83"
+    sha256 cellar: :any,                 arm64_monterey: "0438f593bb82ef4bfbe1c3ebba3e42dd168f031674975832823f45b6b528a997"
+    sha256 cellar: :any,                 arm64_big_sur:  "0b2001b44f417826e8463d83ce936105af71022cbe9f1e3ca0288b4389e6c460"
+    sha256 cellar: :any,                 ventura:        "180c6997e8df4c9e686250ef8388fa61bfde55ccd1f64c093122c10f18ce430f"
+    sha256 cellar: :any,                 monterey:       "8125044368a0ca002deed4578d868d9a90dd8532403eaad9237be8ae3b8f4ef5"
+    sha256 cellar: :any,                 big_sur:        "3892e22ccbf2c0305c33445b591a398ec9df758cb3589a4d872ecf266f737831"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "89cb9f1c702bee6f67ef0a3d4c075b6d6b0dddf54853120a33272ce704d5a6d7"
   end
 
   depends_on "gmp"
