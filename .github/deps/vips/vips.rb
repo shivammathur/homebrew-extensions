4777958d27d3ea442773b9bbd7813b6a24ef02b5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-21 01:44:29 +0000

    Update extension dependencies

--- .github/deps/vips/vips.rb
+++ .github/deps/vips/vips.rb
@@ -1,10 +1,9 @@
 class Vips < Formula
   desc "Image processing library"
   homepage "https://github.com/libvips/libvips"
-  url "https://github.com/libvips/libvips/releases/download/v8.14.2/vips-8.14.2.tar.xz"
-  sha256 "27dad021f0835a5ab14e541d02abd41e4c3bd012d2196438df5a9e754984f7ce"
+  url "https://github.com/libvips/libvips/releases/download/v8.14.3/vips-8.14.3.tar.xz"
+  sha256 "f884d61a6b54c99cdae855001c8b9523e13b4982be7e76cac03faccb91be105c"
   license "LGPL-2.1-or-later"
-  revision 1
 
   livecheck do
     url :stable
@@ -12,13 +11,13 @@ class Vips < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "834ecc709b7bbc6b355c333c8bc04b4cad503f1276e832a1d3f2f178ed7c7670"
-    sha256 arm64_monterey: "73a5076121e80c058be25ee27aad981a74d642dc301261559b51d432e56eb97b"
-    sha256 arm64_big_sur:  "e83f8d00ed9cb9a5561b6e7e76693d0e36d20830f30411970fa51f78e438e0b2"
-    sha256 ventura:        "986c3dd78113f49bfa03794218e46805cf31907c8ad181c50c3189acc81fa9ce"
-    sha256 monterey:       "9f5f29390c5e040dd154a690dce38689958db456d7bff5a7b0d1962196ca7945"
-    sha256 big_sur:        "28ec6abf206a66a1186791044780218264b00131f9ab725a96b96e49f7505ee2"
-    sha256 x86_64_linux:   "0b377b7181b90a843893aea0295e625ac5a4f97488109b98d27b391b052d1dad"
+    sha256 arm64_ventura:  "738b579bf0a9ca1451084a3e6131d76d9a65630bc7ed919d6afa9ecb3e83c1fe"
+    sha256 arm64_monterey: "d203896a72ff98855d0cd16f65d436844b3a0c52740f558e0e4f2a8bf91ce171"
+    sha256 arm64_big_sur:  "290d46d540a48e99cf8f03cde06905f5bd9c97eef56465f38d8b3bccf1015b2d"
+    sha256 ventura:        "4e7b98aaa3e75eacd2a74602be2674cda01a1667b643925e3df0699f141a1523"
+    sha256 monterey:       "9677cfb3676af73199c8574200586979041cf2aef8e9803dfb676624bd86c6c6"
+    sha256 big_sur:        "f028a3d1a5d3b2a4b19a837fee657e3f72c7eba951dce480a329a7c22682f1b3"
+    sha256 x86_64_linux:   "ed66bca9afe0046a6e25b2ad33aa2751fc42f89280f1f14c70af9a6cafc73662"
   end
 
   depends_on "gobject-introspection" => :build
