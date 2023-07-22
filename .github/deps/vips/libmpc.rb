6d5d0ebcf114b3d4a2c1c42b04dc890625193d81
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-07-22 10:07:02 +0000

    Update extension dependencies

--- .github/deps/vips/libmpc.rb
+++ .github/deps/vips/libmpc.rb
@@ -1,6 +1,6 @@
 class Libmpc < Formula
   desc "C library for the arithmetic of high precision complex numbers"
-  homepage "https://www.multiprecision.org/mpc/"
+  homepage "https://www.multiprecision.org/"
   url "https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz"
   mirror "https://ftpmirror.gnu.org/mpc/mpc-1.3.1.tar.gz"
   sha256 "ab642492f5cf882b74aa0cb730cd410a81edcdbec895183ce930e706c1c759b8"
