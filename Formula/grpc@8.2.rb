6e63d3c8ebabf94b7b9837d4338dffe7a91bbf69
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-17 03:36:14 +0000

    Update grpc@8.2

--- Formula/grpc@8.2.rb
+++ Formula/grpc@8.2.rb
@@ -8,8 +8,8 @@ class GrpcAT82 < AbstractPhpExtension
   init
   desc "gRPC PHP extension"
   homepage "https://github.com/grpc/grpc"
-  url "https://pecl.php.net/get/grpc-1.56.0.tgz"
-  sha256 "bb3c58314cc4c4c043b70bf7162a4ebae507834bf5c2a014b67ebd8d70109dfe"
+  url "https://pecl.php.net/get/grpc-1.57.0.tgz"
+  sha256 "b1ae19706fd3968584ed3079986b4cf1d6e557fc336761a336d73a435b9a7e60"
   head "https://github.com/grpc/grpc.git"
   license "Apache-2.0"
 
