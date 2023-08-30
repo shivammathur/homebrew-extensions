2180b2899026a4e99140f52d4d68897be8a29083
Shivam Mathur <shivam_jpr@hotmail.com>
2023-08-30 12:13:07 +0530

    Update event@8.4

---
+++ Formula/event@8.4.rb
@@ -0,0 +1,41 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Event Extension
+class EventAT84 < AbstractPhpExtension
+  init
+  desc "Event PHP extension"
+  homepage "https://bitbucket.org/osmanov/pecl-event"
+  url "https://pecl.php.net/get/event-3.0.8.tgz"
+  sha256 "e3e91edd3dc15e0969b9254cc3626ae07825e39bf26d61b49935f66f603d7b6b"
+  revision 1
+  head "https://bitbucket.org/osmanov/pecl-event.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "libevent"
+  depends_on "openssl@3"
+
+  def install
+    args = %W[
+      --with-event-core
+      --with-event-extra
+      --with-event-openssl
+      --enable-event-sockets
+      --with-openssl-dir=#{Formula["openssl@3"].opt_prefix}
+      --with-event-libevent-dir=#{Formula["libevent"].opt_prefix}
+    ]
+    Dir.chdir "event-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
