1fe2cd154f6a629c3b415acfd9ac896d87faf9e3
Shivam Mathur <shivam_jpr@hotmail.com>
2023-08-30 05:20:49 +0000

    Add pcov@8.4 and xdebug@8.4

---
+++ Formula/xdebug@8.4.rb
@@ -0,0 +1,31 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Xdebug Extension
+class XdebugAT84 < AbstractPhpExtension
+  init
+  desc "Xdebug PHP extension"
+  homepage "https://github.com/xdebug/xdebug"
+  url "https://github.com/xdebug/xdebug/archive/master.tar.gz"
+  sha256 "3d8777da27bca3a20dab3e4d7372ce8b974de53f87898f622389e5da3bad334f"
+  version "3.2.2"
+  head "https://github.com/xdebug/xdebug.git", branch: "master"
+  license "PHP-3.0"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  uses_from_macos "zlib"
+
+  def install
+    inreplace "config.m4", "80400", "80500"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-xdebug"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+  end
+end
