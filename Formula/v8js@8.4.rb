c487b19fb501510ee0207c26e5740bf410918c72
Shivam Mathur <shivam_jpr@hotmail.com>
2023-08-30 13:37:23 +0530

    Update v8js@8.4

---
+++ Formula/v8js@8.4.rb
@@ -0,0 +1,41 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for V8js Extension
+class V8jsAT84 < AbstractPhpExtension
+  init
+  desc "V8js PHP extension"
+  homepage "https://github.com/phpv8/v8js"
+  url "https://github.com/phpv8/v8js/archive/7c40690ec0bb6df72a2ff7eaa510afc7f0adb8a7.tar.gz"
+  version "2.1.2"
+  sha256 "389cd0810f4330b7e503510892a00902ca3a481dc74423802e06decff966881f"
+  head "https://github.com/phpv8/v8js.git", branch: "php8"
+  license "MIT"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "v8"
+
+  def install
+    args = %W[
+      --with-v8js=#{Formula["v8"].opt_prefix}
+    ]
+    ENV.append "CPPFLAGS", "-DV8_COMPRESS_POINTERS"
+    ENV.append "CPPFLAGS", "-DV8_ENABLE_SANDBOX"
+    ENV.append "CXXFLAGS", "-Wno-c++11-narrowing"
+    ENV.append "LDFLAGS", "-lstdc++"
+    inreplace "config.m4", "$PHP_LIBDIR", "libexec"
+    inreplace "v8js_v8object_class.cc", "static int v8js_v8object_get" \
+                                      , "static zend_result v8js_v8object_get"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
