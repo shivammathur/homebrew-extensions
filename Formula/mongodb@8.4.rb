330bfa53b48ba745503b1ec6f0f6a274956aafb8
Shivam Mathur <shivam_jpr@hotmail.com>
2023-08-30 12:23:43 +0530

    Update mongodb@8.4

---
+++ Formula/mongodb@8.4.rb
@@ -0,0 +1,35 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Mongodb Extension
+class MongodbAT84 < AbstractPhpExtension
+  init
+  desc "Mongodb PHP extension"
+  homepage "https://github.com/mongodb/mongo-php-driver"
+  url "https://pecl.php.net/get/mongodb-1.16.2.tgz"
+  sha256 "d630cf32a73b6e5e05d2806782d35e06d24b7d5c83cfec08239549e6b6a600b2"
+  head "https://github.com/mongodb/mongo-php-driver.git", branch: "master"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "icu4c"
+  depends_on "openssl@3"
+  depends_on "snappy"
+  depends_on "zlib"
+  depends_on "zstd"
+
+  def install
+    Dir.chdir "mongodb-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-mongodb"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
