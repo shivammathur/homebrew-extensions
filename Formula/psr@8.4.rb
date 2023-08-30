3561e9d25ddbd78cb01309137d0d34c3020e7e51
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-30 09:14:20 +0000

    psr@8.4: update 1.2.0 bottle.

--- Formula/psr@8.4.rb
+++ Formula/psr@8.4.rb
@@ -11,6 +11,12 @@ class PsrAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "501090d802d6ea3b3a8f56501dd9398f96a1a6c7530cc6cd888d5d8941dd16f5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "013c2ea97ff8979a3d07043d4a5ca1dc3252db0af5d014dc3894b447b6838e1e"
+    sha256 cellar: :any_skip_relocation, ventura:        "e70bd2601e5d641a0ea3cccd76c8abafc03159e67bd01b6bc2711d68f6d17df5"
+    sha256 cellar: :any_skip_relocation, monterey:       "0ca942bc16886440e3d2b1e3065edb7de2f77f77c1efd1f691eef49bffe1875d"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d304f8dd8157d23f7f19bcf7af471eaf1f6ef6cff61d73cdc02e719489293beb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3eebf70695e556b4154b89a00d16c2563c01766efb5d0c0d0b18d20776874617"
   end
 
   depends_on "pcre"
