c244fc75fd78cd769309fa8768aa4604b173300b
Shivam Mathur <shivam_jpr@hotmail.com>
2023-08-30 17:09:55 +0530

    Update memcached@8.4

---
+++ Formula/memcached@8.4.rb
@@ -0,0 +1,57 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Memcached Extension
+class MemcachedAT84 < AbstractPhpExtension
+  init
+  desc "Memcached PHP extension"
+  homepage "https://github.com/php-memcached-dev/php-memcached"
+  url "https://pecl.php.net/get/memcached-3.2.0.tgz"
+  sha256 "2a41143a7b29f4a962a3805b77aa207a99e4566e2d314ce98a051cd24d6e9636"
+  head "https://github.com/php-memcached-dev/php-memcached.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "libevent"
+  depends_on "libmemcached"
+  depends_on "shivammathur/extensions/igbinary@8.4"
+  depends_on "shivammathur/extensions/msgpack@8.4"
+
+  uses_from_macos "zlib"
+
+  def patch_memcached
+    %w[igbinary msgpack].each do |e|
+      mkdir_p "include/php/ext/#{e}"
+      headers = Dir["#{Formula["#{e}@8.4"].opt_include}/**/*.h"]
+      (buildpath/"memcached-#{version}/include/php/ext/#{e}").install_symlink headers unless headers.empty?
+    end
+  end
+
+  priority "30"
+
+  def install
+    args = %W[
+      --enable-memcached
+      --enable-memcached-igbinary
+      --enable-memcached-json
+      --enable-memcached-msgpack
+      --disable-memcached-sasl
+      --enable-memcached-session
+      --with-libmemcached-dir=#{Formula["libmemcached"].opt_prefix}
+      --with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr
+    ]
+    Dir.chdir "memcached-#{version}"
+    patch_memcached
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
