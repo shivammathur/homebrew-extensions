310faa9d24cdac50843f96b7dbb25b698a9d1fdc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-30 08:03:03 +0000

    expect@8.4: update 0.4.0 bottle.

--- Formula/expect@8.4.rb
+++ Formula/expect@8.4.rb
@@ -15,6 +15,12 @@ class ExpectAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_monterey: "ced30c1390aa35ae253fd242c24673cdeb781927766ab4161bf92087ce76d575"
+    sha256 cellar: :any,                 arm64_big_sur:  "16fd2101ea6eddee53f95ed85dac9b2081e7c4d5557037c00db3ddc8f88eb8c8"
+    sha256 cellar: :any,                 ventura:        "9a3bef6d7425ac3c47b96307b078aa7932c0b0ce59ad55dccaeff7fb2a357cd3"
+    sha256 cellar: :any,                 monterey:       "a633cd32b44f08f3d920bf1980b33c66249bbf92ba0e81022caa237fbeac2024"
+    sha256 cellar: :any,                 big_sur:        "9f1573993794b72d28b134a99913a73ae182e1b4f5b239101d9a12dcc618bd39"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "05ffedbba47ff83721a81ff51746c3c5a592b7e828facbc1fe75d4092ad07368"
   end
 
   depends_on "expect"
