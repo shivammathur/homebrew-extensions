8a9bfca569077dd557311ffe2baea0e8361c5bcf
Shivam Mathur <shivam_jpr@hotmail.com>
2023-08-30 12:40:18 +0530

    Update xlswriter@8.4

---
+++ Formula/xlswriter@8.4.rb
@@ -0,0 +1,33 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Xlswriter Extension
+class XlswriterAT84 < AbstractPhpExtension
+  init
+  desc "Xlswriter PHP extension"
+  homepage "https://github.com/viest/php-ext-xlswriter"
+  url "https://pecl.php.net/get/xlswriter-1.5.5.tgz"
+  sha256 "4b8c8aacf48737c0d741a2401f4c495318ca18879e9bfcef5f698946ef28c671"
+  head "https://github.com/viest/php-ext-xlswriter.git", branch: "master"
+  license "BSD-2-Clause"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    args = %w[
+      --with-xlswriter
+      --enable-reader
+    ]
+    Dir.chdir "xlswriter-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
