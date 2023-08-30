dde1af552dff5fb4f7bf0c1a1a36697787b1d8d4
Shivam Mathur <shivam_jpr@hotmail.com>
2023-08-30 13:42:14 +0530

    Update pdo_sqlsrv@8.4

---
+++ Formula/pdo_sqlsrv@8.4.rb
@@ -0,0 +1,31 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Pdo_sqlsrv Extension
+class PdoSqlsrvAT84 < AbstractPhpExtension
+  init
+  desc "pdo_sqlsrv PHP extension"
+  homepage "https://github.com/Microsoft/msphpsql"
+  url "https://pecl.php.net/get/pdo_sqlsrv-5.11.0.tgz"
+  sha256 "c307d9bcada02bdc21f59b9f099031b90b30727757bca50fd42fd36159812e8a"
+  head "https://github.com/Microsoft/msphpsql.git", branch: "master"
+  license "MIT"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "unixodbc"
+
+  def install
+    Dir.chdir "pdo_sqlsrv-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--with-pdo_sqlsrv"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
