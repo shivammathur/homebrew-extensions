c72ada97e413a33141bf3bdcbb36962dbb31a0ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-30 09:14:40 +0000

    rdkafka@8.4: update 6.0.3 bottle.

--- Formula/rdkafka@8.4.rb
+++ Formula/rdkafka@8.4.rb
@@ -15,6 +15,12 @@ class RdkafkaAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_monterey: "3592efe3795f77635f2e71a72a678ee659ce84793ad3b5e10f6c9ec87ccf9165"
+    sha256 cellar: :any,                 arm64_big_sur:  "5ec1ba5137c3f2edf0948b435370a7bd0d27556dd3aeb59c5f1d87b7566ae214"
+    sha256 cellar: :any,                 ventura:        "d653e4dac30d17ed2a260acd655a7b5042da3cae9f007f5d1f850ffee54e3ff3"
+    sha256 cellar: :any,                 monterey:       "7ca11438f29c21e58bfd70721adcc153ab48f7227edee8ebddd3e58397677594"
+    sha256 cellar: :any,                 big_sur:        "5c310f466157de8d7a0a1fca082c1f98cc199a0b3a054421678e1b7775f9524a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ad0eb26149697a482b96fce2376efb86e9c807f0529cc688dd4f9ddd03e031ec"
   end
 
   depends_on "librdkafka"
