54e48e7a0a23dc987304c8f6bd6c8e31f107ebb5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-08-30 08:03:00 +0000

    couchbase@8.4: update 4.1.5 bottle.

--- Formula/couchbase@8.4.rb
+++ Formula/couchbase@8.4.rb
@@ -15,6 +15,12 @@ class CouchbaseAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 arm64_monterey: "85661d12847ff0b2a6b15bff57e7fdeaf54b1fac1b10f334a5e00696065f3b3f"
+    sha256 arm64_big_sur:  "b0d6a7f72ab6ffd427a4ef26bd00e7e146495f25ad338b5a3d40aef807cb4453"
+    sha256 ventura:        "1194f2eb616ec67e272b57a9e117adda3695efb55d6ef0420b75abb040e03d19"
+    sha256 monterey:       "1826cfe78fb8a2a0c6c96e787f7c7b3a9885a91184bd7eef7a9c28739b6a984b"
+    sha256 big_sur:        "056a399dd44aaad29b51a56a978bf50eb5aea1699d5bdc7692afe93873f3bd09"
+    sha256 x86_64_linux:   "c6436e588d2b1dfb80a3ec3882db6ed9a91ca683a8a84707d7bd937e6ac6dec2"
   end
 
   depends_on "cmake" => :build
