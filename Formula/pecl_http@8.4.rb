ed41d661720d4d23f76411a0bd20f57a50bb0a67
Shivam Mathur <shivam_jpr@hotmail.com>
2023-08-30 17:10:31 +0530

    Update pecl_http@8.4

---
+++ Formula/pecl_http@8.4.rb
@@ -0,0 +1,48 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Pecl_http Extension
+class PeclHttpAT84 < AbstractPhpExtension
+  init
+  desc "Pecl_http PHP extension"
+  homepage "https://github.com/m6w6/ext-http"
+  url "https://pecl.php.net/get/pecl_http-4.2.3.tgz"
+  sha256 "fa2ab558fc8f0928a10f35c0f566f7c4a1d32e727bd3a96579e4c28482ee9d6a"
+  head "https://github.com/m6w6/ext-http.git", branch: "master"
+  license "BSD-2-Clause"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "brotli"
+  depends_on "curl"
+  depends_on "icu4c"
+  depends_on "libevent"
+  depends_on "libidn2"
+  depends_on "shivammathur/extensions/raphf@8.4"
+
+  priority "30"
+
+  def install
+    args = %W[
+      --with-http
+      --with-http-libicu-dir=#{Formula["icu4c"].opt_prefix}
+      --with-http-zlib-dir=#{MacOS.sdk_path_if_needed}/usr
+    ]
+    extra_includes = %W[
+      -I#{Formula["shivammathur/extensions/raphf@8.4"].opt_include}/php
+    ]
+    ENV["EXTRA_INCLUDES"] = extra_includes * " "
+    Dir.chdir "pecl_http-#{version}"
+    inreplace "src/php_http_api.h", "ext/raphf", "ext/raphf@8.4"
+    patch_spl_symbols
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+  end
+end
