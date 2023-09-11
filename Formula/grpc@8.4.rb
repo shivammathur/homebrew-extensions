172f2362ce714926f54e9a19dd0da754b43e826e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-11 09:13:19 +0000

    grpc@8.4: update 1.58.0 bottle.

--- Formula/grpc@8.4.rb
+++ Formula/grpc@8.4.rb
@@ -15,12 +15,13 @@ class GrpcAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3ac3a7ba1a6ee0853ce42f555d1e57edc7690abfb2f4cf2c956f0aa20327716e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "59301979eb6176116933f414b4de187ac09cf503e1797ad457f8c14524167840"
-    sha256 cellar: :any_skip_relocation, ventura:        "2bd0c12da01fe7eb99f3a21b36048acd8586f2b66bb7eb85f2f6073ca606ed07"
-    sha256 cellar: :any_skip_relocation, monterey:       "759854059b91fbdc498cd3d521fcd2e75fa3bc2556bd9a81224336c587aabed8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ea9ae7ab81e88af61bb2909cc62e451740c6bac0c112350b9e757c16f94a53d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f29adebadcc33101b4f383c9c68b0957c0eb24a9c76860307132e6fb272161de"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7b509331cacf803e6974ea38132a33465e880e415fa581804b01c422b5f68c73"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eb7d50cf5e287a7dea74dff6d11974bb864aa54b5d0ef024a0de45077b1ff3cc"
+    sha256 cellar: :any_skip_relocation, ventura:        "7087e5e49609230f9a5b2825f1d12188f9f9e2506ed9d397ab08e6449916f9ce"
+    sha256 cellar: :any_skip_relocation, monterey:       "5ad120b785fd9c0e61681db000186607e3decfe220741cb160f9cd9570d3c6f6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4447006ef96fd5c172cc95f1bac260640a5e013e0e9adb815e17d23656225cfb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4521a01db313654d3ed396186ba6bd22aa5a1ab2df034be5b85a72327044c7c3"
   end
 
   depends_on "grpc"
