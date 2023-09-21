3cc1da140e7d64ebd33f1996582569db77cbf5f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-21 00:45:15 +0000

    vips@7.3: update 1.0.13 bottle.

--- Formula/vips@7.3.rb
+++ Formula/vips@7.3.rb
@@ -16,6 +16,7 @@ class VipsAT73 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 6
+    sha256 cellar: :any,                 arm64_ventura:  "764a8a14f8e68c527166f9457735ef35b26afbdf37a0be44b769a1a247fff666"
     sha256 cellar: :any,                 arm64_monterey: "57b768e85801306f93f05afbfc0d90e8566897541c2865b10eaa6ea21a8d81fa"
     sha256 cellar: :any,                 arm64_big_sur:  "f362e3dd5e49dedc41afae9813dbd595d541aff3b8731639e9dac243827338d3"
     sha256 cellar: :any,                 ventura:        "8575f6d49196b24016e9e62ef7b1c3ae2e4b7403395050c16ea850077484a714"
