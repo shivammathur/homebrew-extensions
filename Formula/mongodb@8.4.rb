11e92775046d233f7dc0deea75afb16cdb03992f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-21 08:28:42 +0000

    mongodb@8.4: update 1.16.2 bottle.

--- Formula/mongodb@8.4.rb
+++ Formula/mongodb@8.4.rb
@@ -16,6 +16,7 @@ class MongodbAT84 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "681474dc3aca53f83067f7f96a8615cc907205c7adaed1a10f4f1540cc0f67cd"
     sha256 cellar: :any,                 arm64_monterey: "b5772814b06c0d6f3a7887a2dbb362f5edf478b2829781029dd28b8ca65d86c6"
     sha256 cellar: :any,                 arm64_big_sur:  "946e99ac22f6d30f07861ce0d12fffdf2bce4dc4e9c1f6d04fb33428d96ae60c"
     sha256 cellar: :any,                 ventura:        "1f723bc9fdd1f98446bf10d2b209415094b5537d7898cfc2cd32aecec7c573c6"
