3c10041f15c7a917ff7b213a53216ec100c35608
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-21 04:49:13 +0000

    sqlsrv@7.2: update 5.9.0 bottle.

--- Formula/sqlsrv@7.2.rb
+++ Formula/sqlsrv@7.2.rb
@@ -16,6 +16,7 @@ class SqlsrvAT72 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "c01c3290a479ef533bd3ffd4478a501d93e7d41f84dea7f674e89a2de8f6a2be"
     sha256 cellar: :any,                 arm64_monterey: "7cd2bd68dc8c6ec66ba2eea8f11b909735c9541df901ea40228342a5bcc94ca5"
     sha256 cellar: :any,                 arm64_big_sur:  "e8b07d6500f01138ae7eb653dd4090a671dd7824d3cb8dc2c1ef0d9ae3489770"
     sha256 cellar: :any,                 ventura:        "29a13ef06d117d7c15aa9a2fafd89b49bf8a97f81ab0c6a10a8391bf1b28ae05"
