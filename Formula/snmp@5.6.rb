42d769eb6db113b9247b9f304568a2a347e65470
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-21 01:57:24 +0000

    snmp@5.6: update 5.6.40_1 bottle.

--- Formula/snmp@5.6.rb
+++ Formula/snmp@5.6.rb
@@ -18,6 +18,7 @@ class SnmpAT56 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 2
+    sha256 cellar: :any,                 arm64_ventura:  "a0d20efe3fcd3c2ca0779b049058f376aa337653685e3146e604ba0dd58e1c58"
     sha256 cellar: :any,                 arm64_monterey: "afb89c82af2db01b0c289d8e729731ad92e2c89e796f9dc4c81d0e7372e6b5b7"
     sha256 cellar: :any,                 arm64_big_sur:  "caee560c53e212ee6a1670078d05225e6ca323b3827fff7831f15c786783af68"
     sha256 cellar: :any,                 ventura:        "f1ba3ca25d7b7945329f02dd116e09a643063de9020955239f9855764bf5dae8"
