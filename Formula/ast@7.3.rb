bff9a6d4ef15801abfaf55f4f06b5fb1449984ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-21 02:01:36 +0000

    ast@7.3: update 1.1.0 bottle.

--- Formula/ast@7.3.rb
+++ Formula/ast@7.3.rb
@@ -16,6 +16,7 @@ class AstAT73 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "07f5e4ae3704d7622c1ae7ef102dce9aff5bcbfe98a901bb88543b47b3cf5a35"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "aece6ea0a56af00804846e61f2f86eb2873c01a2b2af2c9a9198f71c3fef812a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cf9257a05a51025e92282c54b63586f1adeb4ec9fba35e43aae4f85de6ce64e3"
     sha256 cellar: :any_skip_relocation, ventura:        "78163d4517aa1427febdf9c5a0ea3e8278cb52a0c2e0825e6a28bae0a02642ca"
