85a0e8cad346eba2a8acc9e0b48a86ea8bc3cd3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-22 01:35:48 +0000

    yaml@7.0: update 2.0.4 bottle.

--- Formula/yaml@7.0.rb
+++ Formula/yaml@7.0.rb
@@ -16,12 +16,13 @@ class YamlAT70 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 1
-    sha256 cellar: :any,                 arm64_ventura: "8d82351ea87d7c5986398e9bce39a52695095f2c6345abf9224a281123b56d20"
-    sha256 cellar: :any,                 arm64_big_sur: "7dc63852d7b05ccecd1b7421c29114af4b39ccdbdbc015b960c3148e03590ba9"
-    sha256 cellar: :any,                 ventura:       "bac5e501e58dab8aec544791874906e4e954943485afe529e85f4648dda589f1"
-    sha256 cellar: :any,                 big_sur:       "b73ef6d2a0f3d1095c5de474d07eec9347dadf3dba79126b25f0b18e47b66664"
-    sha256 cellar: :any,                 catalina:      "57df8f5421356bf1cd4750e5d771da933807d49a160fdc4085db41b73c36461e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3f42d9b2f483ae926e80d118ba80746fef1133eabc76e4bb2984b5bd3545d6c1"
+    sha256 cellar: :any,                 arm64_ventura:  "8d82351ea87d7c5986398e9bce39a52695095f2c6345abf9224a281123b56d20"
+    sha256 cellar: :any,                 arm64_monterey: "e9215339046ecb30bb185cf14e05bdca9f497538e59bd69a05f071a25aa2ab17"
+    sha256 cellar: :any,                 arm64_big_sur:  "7dc63852d7b05ccecd1b7421c29114af4b39ccdbdbc015b960c3148e03590ba9"
+    sha256 cellar: :any,                 ventura:        "bac5e501e58dab8aec544791874906e4e954943485afe529e85f4648dda589f1"
+    sha256 cellar: :any,                 big_sur:        "b73ef6d2a0f3d1095c5de474d07eec9347dadf3dba79126b25f0b18e47b66664"
+    sha256 cellar: :any,                 catalina:       "57df8f5421356bf1cd4750e5d771da933807d49a160fdc4085db41b73c36461e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3f42d9b2f483ae926e80d118ba80746fef1133eabc76e4bb2984b5bd3545d6c1"
   end
 
   depends_on "libyaml"
