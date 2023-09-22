31ceebaa0f63eac25fea29f4da4f5f15506b5111
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-22 01:00:14 +0000

    pcov@7.1: update 1.0.11 bottle.

--- Formula/pcov@7.1.rb
+++ Formula/pcov@7.1.rb
@@ -15,12 +15,13 @@ class PcovAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "c38c7762fba37e3dd63c3bffcbc5bec933b12c87444f1376ec27ed55304fd0fc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9109cc64a367c4f84c80ecc07c29529e80baf64bccbccc21988a45436d1c9554"
-    sha256 cellar: :any_skip_relocation, ventura:       "08e2d2c27cba0f0c2a9b0a68ef79b0e22fd4d01cbb8d77cb3d880e6647c180ff"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4d5b61e736703094cf752725173600f518761ee00567a7d3108daf0c6afe0f4c"
-    sha256 cellar: :any_skip_relocation, catalina:      "51899184179d8466fcad7cb6a7ca7c04445ad18397a7e9c235572782c39b4dfb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f819e206180c2a685e557e7bb03eacd3cae8470fba4d26db48f3775cd60bacf1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c38c7762fba37e3dd63c3bffcbc5bec933b12c87444f1376ec27ed55304fd0fc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8b3599c52702b9a9e4a06802be76fcea26dd2fe372f837205060cb83406b6e89"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9109cc64a367c4f84c80ecc07c29529e80baf64bccbccc21988a45436d1c9554"
+    sha256 cellar: :any_skip_relocation, ventura:        "08e2d2c27cba0f0c2a9b0a68ef79b0e22fd4d01cbb8d77cb3d880e6647c180ff"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4d5b61e736703094cf752725173600f518761ee00567a7d3108daf0c6afe0f4c"
+    sha256 cellar: :any_skip_relocation, catalina:       "51899184179d8466fcad7cb6a7ca7c04445ad18397a7e9c235572782c39b4dfb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f819e206180c2a685e557e7bb03eacd3cae8470fba4d26db48f3775cd60bacf1"
   end
 
   def install
