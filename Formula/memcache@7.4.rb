72202f9fb7b486ae1ca231ab8e2b61adb3586b21
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-22 01:02:01 +0000

    memcache@7.4: update 4.0.5.2 bottle.

--- Formula/memcache@7.4.rb
+++ Formula/memcache@7.4.rb
@@ -15,12 +15,13 @@ class MemcacheAT74 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "4a18bd4a463eda9ac19773640c4b3fc3d1f2cba9ceecb0d46812073792f14868"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2b55a03fbdeba04847b21423086ee636cfd9fd6f83dbd469a8ae653beddc3583"
-    sha256 cellar: :any_skip_relocation, ventura:       "0f2408cb31542daa8e472ef875e53f871c526f9eff8e40ab25785745903cf67b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "81a021474dd7fa22160051997b8835b941a9ca9593f46592c3aac1bcbf8f5a74"
-    sha256 cellar: :any_skip_relocation, catalina:      "d2c52b467943db32aa82a31fa466e60623e47c4eed0ea3866aeca48e8f2953d9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d2eef635c4c163a0032880461465e66469899d493c7bc3c5d16eb67873e3a6a2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4a18bd4a463eda9ac19773640c4b3fc3d1f2cba9ceecb0d46812073792f14868"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a81ba64d3c0e6290129dca681d63c5060b3ef16b8b09c468f76e5b2c808e1c0e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b55a03fbdeba04847b21423086ee636cfd9fd6f83dbd469a8ae653beddc3583"
+    sha256 cellar: :any_skip_relocation, ventura:        "0f2408cb31542daa8e472ef875e53f871c526f9eff8e40ab25785745903cf67b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "81a021474dd7fa22160051997b8835b941a9ca9593f46592c3aac1bcbf8f5a74"
+    sha256 cellar: :any_skip_relocation, catalina:       "d2c52b467943db32aa82a31fa466e60623e47c4eed0ea3866aeca48e8f2953d9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d2eef635c4c163a0032880461465e66469899d493c7bc3c5d16eb67873e3a6a2"
   end
 
   def install
