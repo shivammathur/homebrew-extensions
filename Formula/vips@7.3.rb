ab6d93c4c8282c7dffea8e060913279b88b234ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-22 09:50:34 +0000

    vips@7.3: update 1.0.13 bottle.

--- Formula/vips@7.3.rb
+++ Formula/vips@7.3.rb
@@ -15,14 +15,14 @@ class VipsAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 6
-    sha256 cellar: :any,                 arm64_ventura:  "764a8a14f8e68c527166f9457735ef35b26afbdf37a0be44b769a1a247fff666"
-    sha256 cellar: :any,                 arm64_monterey: "57b768e85801306f93f05afbfc0d90e8566897541c2865b10eaa6ea21a8d81fa"
-    sha256 cellar: :any,                 arm64_big_sur:  "f362e3dd5e49dedc41afae9813dbd595d541aff3b8731639e9dac243827338d3"
-    sha256 cellar: :any,                 ventura:        "8575f6d49196b24016e9e62ef7b1c3ae2e4b7403395050c16ea850077484a714"
-    sha256 cellar: :any,                 monterey:       "b046a280401286a2835bd96531898d49c27e3ff694f27ec227904b567ab67f05"
-    sha256 cellar: :any,                 big_sur:        "bdcf6d65d085fa89c90ff99e2061b0a888d463035111972fba6da3535b887f41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1726dc1c32fcdd95899e768f195d85487c4fab17be81afc1d89680eb1b040729"
+    rebuild 7
+    sha256 cellar: :any,                 arm64_ventura:  "91c3349396565107609c0384fbf2176dfaf9af7f23534ec546d974fe64c01122"
+    sha256 cellar: :any,                 arm64_monterey: "0fbdb1ee64f7eb8c9de7c111d449c5191d6cd5c72657f8818bf131ebf2f91363"
+    sha256 cellar: :any,                 arm64_big_sur:  "fa1c3c1ed758f2d51dae96f15d10a4a5e6b5c698371709f24c31ee6d108e33c8"
+    sha256 cellar: :any,                 ventura:        "2b5e69762578332e23eacbd3efd6e29c016c6ed0124fda787bca01c16d92cca3"
+    sha256 cellar: :any,                 monterey:       "a7bd09eb0bc35a294f0a5a40308d6ec89ceba88dc27d896e1df6f7dba0f0f867"
+    sha256 cellar: :any,                 big_sur:        "a27b14f10b92ee45e87cca6d228e04a56105225778bfe15b8259dad5223cc9c4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eab53fd310ee453031053bb8af6556c2a2224c9a35e398d1cbe59349db2fe460"
   end
 
   depends_on "vips"
