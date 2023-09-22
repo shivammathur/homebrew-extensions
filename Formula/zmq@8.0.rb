e1df3d5093433d1d637988c6141087fa5712ac29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-22 01:53:09 +0000

    zmq@8.0: update 1.1.3 bottle.

--- Formula/zmq@8.0.rb
+++ Formula/zmq@8.0.rb
@@ -16,12 +16,13 @@ class ZmqAT80 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_ventura: "63d541094a9c58ea273e3faaeedd50a9b15f75023242ef7e48139706dd30a889"
-    sha256                               arm64_big_sur: "7f936bb49e9df8b6fb2dc41a2a685802c1ceecac16414830630aba5cae7e70a8"
-    sha256 cellar: :any,                 ventura:       "0b627fe62ecdde083e3400f83a0479c3b1cbb3e38ab65cfd1ea06611e7e498bc"
-    sha256                               big_sur:       "ba40b5d1f4c99e227c1b3a5ce57b1aa3c3cdb01a675a6d905eb1f88bb3f9d2df"
-    sha256                               catalina:      "a57f360bb467153135f94ec9935335260178277f108fb80777a5566e362757eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "10c1ec0577c20f1327da63aad7df97ca3af6b381ca515edf0197ebdf2de5193e"
+    sha256 cellar: :any,                 arm64_ventura:  "63d541094a9c58ea273e3faaeedd50a9b15f75023242ef7e48139706dd30a889"
+    sha256 cellar: :any,                 arm64_monterey: "4e146b682a0a574751d0aaade42fc93df6edcfb8405f42251dcd7e076249ee19"
+    sha256                               arm64_big_sur:  "7f936bb49e9df8b6fb2dc41a2a685802c1ceecac16414830630aba5cae7e70a8"
+    sha256 cellar: :any,                 ventura:        "0b627fe62ecdde083e3400f83a0479c3b1cbb3e38ab65cfd1ea06611e7e498bc"
+    sha256                               big_sur:        "ba40b5d1f4c99e227c1b3a5ce57b1aa3c3cdb01a675a6d905eb1f88bb3f9d2df"
+    sha256                               catalina:       "a57f360bb467153135f94ec9935335260178277f108fb80777a5566e362757eb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "10c1ec0577c20f1327da63aad7df97ca3af6b381ca515edf0197ebdf2de5193e"
   end
 
   depends_on "zeromq"
