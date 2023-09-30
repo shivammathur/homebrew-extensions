37ad451b42633f93ffb492a06f3c8004a80bc5f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-09-30 10:06:34 +0000

    Update extension dependencies

--- .github/deps/vips/vips.rb
+++ .github/deps/vips/vips.rb
@@ -11,9 +11,11 @@ class Vips < Formula
   end
 
   bottle do
+    sha256 arm64_sonoma:   "b69261616c15896f17a5e273576dee542a064956c68bb4c08a1bcaf5ce75f6d1"
     sha256 arm64_ventura:  "9f1d7309268ed8f6ecc1cadcfc39cd3e7ba1275b3417d429ab79ad141b529b91"
     sha256 arm64_monterey: "098f6ab0a43ae8700d56ffe204fdbfe47469178830e60257cec66f9d45910933"
     sha256 arm64_big_sur:  "6ed403aa32c9685527cba0c08f9e2e5e40688e3009eea578911ef1a23bcbf65d"
+    sha256 sonoma:         "fde7711e863c9e8b01be5f1f4d0c5b9d6b76a69fd631e0b438807cacf07d20dd"
     sha256 ventura:        "4cf77fb01d2eadd23de82cb9aa5c2c3bee891c8f4388df4b19beb4ab9bbbe6ff"
     sha256 monterey:       "abb6aca9d89e67f1c0c056dd107a7ff6096f70c624884875d14ef18ea87f94a8"
     sha256 big_sur:        "590174408c0ab938d9267bb00508ff32a94d7f00b58246b990ddf05131b99702"
