dd3671c7fc1f46214ae0cecdf693659ea0bd1d56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-12 04:02:23 +0000

    Update couchbase@8.0

--- Formula/couchbase@8.0.rb
+++ Formula/couchbase@8.0.rb
@@ -8,8 +8,8 @@ class CouchbaseAT80 < AbstractPhpExtension
   init
   desc "Couchbase PHP extension"
   homepage "https://github.com/couchbase/couchbase-php-client"
-  url "https://pecl.php.net/get/couchbase-4.1.5.tgz"
-  sha256 "e1335d70e10687e969ec73b546995d1fe8937ebe511d97c71bbaf2d66056d9f5"
+  url "https://pecl.php.net/get/couchbase-4.1.6.tgz"
+  sha256 "43da457eceb494a8fee95cbb7ff9383efe66899f525cf530760507257080597f"
   head "https://github.com/couchbase/couchbase-php-client.git"
   license "Apache-2.0"
 
