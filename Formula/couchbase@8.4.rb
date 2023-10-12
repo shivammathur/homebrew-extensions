d45da2e192398e4015b0ae95f25e37814f83e702
Shivam Mathur <shivam_jpr@hotmail.com>
2023-10-12 08:55:20 +0000

    Update couchbase@8.4

--- Formula/couchbase@8.4.rb
+++ Formula/couchbase@8.4.rb
@@ -8,8 +8,8 @@ class CouchbaseAT84 < AbstractPhpExtension
   init
   desc "Couchbase PHP extension"
   homepage "https://github.com/couchbase/couchbase-php-client"
-  url "https://pecl.php.net/get/couchbase-4.1.5.tgz"
-  sha256 "e1335d70e10687e969ec73b546995d1fe8937ebe511d97c71bbaf2d66056d9f5"
+  url "https://pecl.php.net/get/couchbase-4.1.6.tgz"
+  sha256 "43da457eceb494a8fee95cbb7ff9383efe66899f525cf530760507257080597f"
   head "https://github.com/couchbase/couchbase-php-client.git", branch: "main"
   license "Apache-2.0"
 
@@ -38,7 +38,6 @@ class CouchbaseAT84 < AbstractPhpExtension
   def install
     ENV["OPENSSL_ROOT_DIR"] = "#{Formula["openssl@3"]}.opt_prefix"
     Dir.chdir "couchbase-#{version}"
-    inreplace "src/wrapper/common.hxx", "zend_bool", "bool"
     safe_phpize
     inreplace "configure",
       "EXTENSION_DIR=`$PHP_CONFIG --extension-dir 2>/dev/null`",
