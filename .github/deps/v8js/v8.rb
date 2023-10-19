9d38226c3884115be1f2f98911f0cbb9fdb82aa0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-19 10:06:35 +0000

    Update extension dependencies

--- .github/deps/v8js/v8.rb
+++ .github/deps/v8js/v8.rb
@@ -1,6 +1,6 @@
 class V8 < Formula
   desc "Google's JavaScript engine"
-  homepage "https://github.com/v8/v8/wiki"
+  homepage "https://v8.dev/docs"
   # Track V8 version from Chrome stable: https://chromiumdash.appspot.com/releases?platform=Mac
   url "https://github.com/v8/v8/archive/11.7.439.16.tar.gz"
   sha256 "df073b032cafab5124a166771b5d66f6afaa8412c4d5863d2ba8d52c253ced33"
