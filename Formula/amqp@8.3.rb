6400c4004eab2d457d2518563760dc74f08e3ef1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-10-19 06:05:41 +0000

    amqp@8.3: update 2.1.1 bottle.

--- Formula/amqp@8.3.rb
+++ Formula/amqp@8.3.rb
@@ -15,13 +15,12 @@ class AmqpAT83 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_ventura:  "e71a332340336848d7219f31cc6917f52bc9fb3dc303a9b8bf7c2feee8ac9a11"
-    sha256 cellar: :any,                 arm64_monterey: "41c21cf67ebfa6b7f95c3c2547c9abfd47bde6bd9a6207efe6f8472a26623080"
-    sha256 cellar: :any,                 arm64_big_sur:  "5bc25ca3c0b6544c407025ca1d780a9391e754f415080400cf01a120302f2dd4"
-    sha256 cellar: :any,                 ventura:        "c3aded3af56e972ede25c7382b609e051e9614623748c329e81553b94092e36d"
-    sha256 cellar: :any,                 monterey:       "1086cff91c272177bed599f7615241fc686266e6d86ac3d909f9511408284440"
-    sha256 cellar: :any,                 big_sur:        "c3f5b7d853e2f5e80881eeb7614fa1fc7773d83c766e5e21a90913cc552b2188"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9ddaba032dfd4dfb8e00c31f2e53161a213e90c3dab0324e6c5ca7b17ddcd2c"
+    sha256 cellar: :any,                 arm64_sonoma:   "f01a17096ac0c31e4e622b75a952e62cca85497517694e78e219bd758597124e"
+    sha256 cellar: :any,                 arm64_ventura:  "3214588d0fc8c8fbf7c46e642c3672a817d097c4b07e567e02734df8c524cf13"
+    sha256 cellar: :any,                 arm64_monterey: "354e7945d5027a51d543bcd15359b7159e015491d42b3130ac5d1c25d79ad78b"
+    sha256 cellar: :any,                 ventura:        "7936ac3968166762a7ea3928d88cca272ecad1337819e0c45149ad897e8c923a"
+    sha256 cellar: :any,                 monterey:       "649618bd7a94f889a307317ca7337285aeea8b8debbd85bc13868640ed6813bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e9b60a296f6c44fc81ebcd7422322514e84662c936663e3fbfaf560e78502909"
   end
 
   depends_on "rabbitmq-c"
