b254ae72195f298d6e9d0edfb8149eaaa94141a3
Shivam Mathur <shivam_jpr@hotmail.com>
2023-11-16 04:50:21 +0000

    Fix updating mongodb [skip ci]

--- .github/scripts/edit.sh
+++ .github/scripts/edit.sh
@@ -2,13 +2,13 @@ fetch() {
   REPO="$(grep '^  homepage' < ./Formula/"$VERSION".rb | cut -d\" -f 2)"
   sudo cp "Formula/$VERSION.rb" "/tmp/$VERSION.rb"
   if [[ "$EXTENSION" =~ event|expect|gnupg|imagick|imap|mcrypt|pcov|snmp ]] ||
-     [[ "$VERSION" =~ (amqp)@(7.4|8.[0-4]) ]] ||
+     [[ "$VERSION" =~ (amqp|mongodb)@(7.4|8.[0-4]) ]] ||
      [[ "$VERSION" =~ (phalcon5)@(7.4|8.[0-2]) ]] ||
      [[ "$VERSION" =~ (propro)@7.[0-4] ]] ||
      [[ "$VERSION" =~ (msgpack)@(7.[0-4]|8.0) ]] ||
      [[ "$VERSION" =~ (apcu|grpc|igbinary|gearman|lua|memcached|pecl_http|raphf|rdkafka|ssh2|uuid|vips|xlswriter)@(7.[0-4]|8.[0-4]) ]] ||
      [[ "$VERSION" =~ (yaml)@(7.[1-4]|8.[0-4]) ]] ||
-     [[ "$VERSION" =~ (ast|mcrypt|mongodb|redis)@(7.[2-4]|8.[0-4]) ]] ||
+     [[ "$VERSION" =~ (ast|mcrypt|redis)@(7.[2-4]|8.[0-4]) ]] ||
      [[ "$VERSION" =~ (ds|mailparse|psr)@(7.[3-4]|8.[0-4]) ]] ||
      [[ "$VERSION" =~ (couchbase|memcache|protobuf|swoole|sqlsrv|pdo_sqlsrv|xdebug)@(8.[0-4]) ]]; then
     sudo chmod a+x .github/scripts/update.sh && bash .github/scripts/update.sh "$EXTENSION" "$VERSION" "$REPO"
