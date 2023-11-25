b0e9f48661fd3447285ec1e0b79ade20fd0c80fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-11-25 08:10:29 +0000

    swoole@8.0: update 5.1.0 bottle.

--- Formula/swoole@8.0.rb
+++ Formula/swoole@8.0.rb
@@ -15,12 +15,13 @@ class SwooleAT80 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_sonoma:   "55275f4435125cfeba245afa19b9e006579e14cbcf66c7359381eab79f9180c9"
-    sha256 cellar: :any,                 arm64_ventura:  "313919f22adcf4ec4612573cf3d4716a152f7e14ac58cdad9a9299d3807d08ed"
-    sha256 cellar: :any,                 arm64_monterey: "f010cebe470e9f20f136cf55fea7dc3b0236f100b6275dff073d221ecce7ea45"
-    sha256 cellar: :any,                 ventura:        "c3d04045c9dccfa17bbb0af0889f7c0371d47ff1a79feda9bdf340b6ebb0094f"
-    sha256 cellar: :any,                 monterey:       "45814cb9367b676f95e7a8b8fbc361b9af6b2bf5860d0c6a444d17a41921bc98"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6e4a80c7b8d6c76b8a9192163d5db97667024ee1509ac85fe51c836bbbc53fd9"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_sonoma:   "b2da696dad864d019847ed70e73f5aec331855e2c4ef48507fc96928779ad278"
+    sha256 cellar: :any,                 arm64_ventura:  "4aa82bb6693dd3a10d363cfec22b906f21c6a90a82f0c3fc4ce59fde5f9afb8c"
+    sha256 cellar: :any,                 arm64_monterey: "0fab090d35fe21bd68037f7864f5706afe69f0fadd9f9607d454cf8afddca361"
+    sha256 cellar: :any,                 ventura:        "b4868c5b8c18816e4f1ffc88fbe059711e8c2ad060aeb5c2e34e6b2bb44f08f6"
+    sha256 cellar: :any,                 monterey:       "cba171e7330ae561504c6e27bfe84e9e42db18a9ff645db585160c8f659502cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0ab1be59f7a107949fc6e6f25a17928096725a3c0c9f1b796bd3d4fd670d5a1c"
   end
 
   depends_on "brotli"
