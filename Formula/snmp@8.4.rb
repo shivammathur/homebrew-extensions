82c35fb4bf5104436ead05fd419c5f19ebcba2d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-07 06:51:23 +0000

    snmp@8.4: update 8.4.0 bottle.

--- Formula/snmp@8.4.rb
+++ Formula/snmp@8.4.rb
@@ -16,13 +16,13 @@ class SnmpAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 17
-    sha256 cellar: :any,                 arm64_sonoma:   "fdf81ff1f8b6d7f0159fc7f90dc2d95df1ed4c4e7226fc3af08efeda230651fc"
-    sha256 cellar: :any,                 arm64_ventura:  "22b590c022caa3420731efb6febd79f225fc4f938c6875236d543a4df19fbcab"
-    sha256 cellar: :any,                 arm64_monterey: "a6c7386449b5928d75c7b74fee93c4daccb1812911c393e59a75df122909e98d"
-    sha256 cellar: :any,                 ventura:        "17740053a4d6f92774198568ab2b33a248f7cef77e9fac5eb5ce754745c5dca6"
-    sha256 cellar: :any,                 monterey:       "c10b36d434c8267ab7ccb16a98c63f358d262c626e8c28d56b8d2475c83e05aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "330f4d5712cb4b07f98025c6f0feb11feec6ac5ae2a69086b7b8c2a16176b49c"
+    rebuild 18
+    sha256 cellar: :any,                 arm64_sonoma:   "958c86f4f0e15d6e27373002eb5fb5267edea463aabc51f061f8900c3f9d3453"
+    sha256 cellar: :any,                 arm64_ventura:  "4acef779230f1d1ce65ac0230122c0bb403b9d0a42373316933910513573cb0f"
+    sha256 cellar: :any,                 arm64_monterey: "7e87e030a6cd547b3bc0de43b4360a2867ce3071fcd7e57466b26a76dfe79d47"
+    sha256 cellar: :any,                 ventura:        "5102343be94b30b4342233e4626f1ed2c485b32971c175f1b385d878f2ac9c74"
+    sha256 cellar: :any,                 monterey:       "86283a3753c8901a9ba6ce21e862a578f16f6b90c2672b8c367df698c3b77401"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "944f4c6770f63cb4325fdcc17603a152896112cc286dc50bb520316988e20d2c"
   end
 
   depends_on "net-snmp"
