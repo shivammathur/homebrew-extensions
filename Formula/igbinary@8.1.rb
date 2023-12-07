3c648709a294b8417fec32a86832577fe798931b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-07 06:05:01 +0000

    igbinary@8.1: update 3.2.15 bottle.

--- Formula/igbinary@8.1.rb
+++ Formula/igbinary@8.1.rb
@@ -15,14 +15,12 @@ class IgbinaryAT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5f1e793f00995bd7b6a68a2c9524931d4ef881abf3ae1dcdf8e0295f04553297"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "94d1be659860b4534f455f811d33c7aa01e371a9db9a0a695d8c222f766e3e2b"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4327221d34d5782073fd238c34d2f8d5e1eff7f8cf9bde2aa4683105ed690511"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a62a47933ecc05d4298fde719597d18b7c2af1b79151f17dd0da8a99fbca19fa"
-    sha256 cellar: :any_skip_relocation, ventura:        "da25f35f076755aae25be6d7f44d9b779765e5c1b246c675fa49fdd5d39a9c21"
-    sha256 cellar: :any_skip_relocation, monterey:       "2e426381ebbbece305254762538ed12b373ba92f86a1c1ec37e822fc757b3ba9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "24f6ea583d489d686084430b4774b5c2058596a71764e40cc9c1a6183182b238"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "93f71f2d175b99cf0c583d9a6c0d23eb43fcf65180058c00204ce760320d0443"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d2904ed12df7c7be487c452ff53ba88fca270b9293eec7ac381ee8c0a5f8c85d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3ad81cdb21a04635acf310b9cba6230ab7d0e548c318320f8534fbe20c73cd74"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "213202f4cced05422d31591901e3ba0c4c478750c6a2ab0e8ee6b8481afd697e"
+    sha256 cellar: :any_skip_relocation, ventura:        "35c343c2e838f9717784fecf63373b1647dd93d46ad5d753b2c734072c2b87c4"
+    sha256 cellar: :any_skip_relocation, monterey:       "36417bf7537029fdcbbd5e80ed23763930acdf02eb4c5434a60674d598270f13"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f03e22bb571a678ac29e39633b06ea1fb8f3f8d31a315404e9ebf854097284dd"
   end
 
   def install
