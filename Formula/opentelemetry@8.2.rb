5de63a4e03f95640917f2c999c5d5af83fea6fe9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-10 06:37:45 +0000

    opentelemetry@8.2: update 1.0.0 bottle.

--- Formula/opentelemetry@8.2.rb
+++ Formula/opentelemetry@8.2.rb
@@ -15,6 +15,12 @@ class OpentelemetryAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "14a3cf18c9449bb11c26876ed79b6eb816559c637a576fc010fff2775db5c254"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4de59359fbedeaec121ac80d67b5197735c5a479e4bd57052463e224666f3e78"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "668df3bed3bfcf73224dc38f5c3d0e8282f9d5f209dba90401980c5f0fca4e33"
+    sha256 cellar: :any_skip_relocation, ventura:        "0e94cd5a5d5fdb7cab86a486cc11ea008f0fa2c1cdbb8da8a8b540e1bb561a15"
+    sha256 cellar: :any_skip_relocation, monterey:       "213c850daef19d6c450c7375ca66cda6a970b1e0d7e1bcf5318474cce8e72675"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c45d1e4ed5f6fb27f4fbdc61e4212f33bbf10bbdbd5251f0276c4d979b6e2d4c"
   end
 
   def install
