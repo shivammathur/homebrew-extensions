8d3028bd30659fdf235cc43255691ec2ba5dec0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-18 10:06:34 +0000

    Update extension dependencies

--- .github/deps/grpc/c/c-ares.rb
+++ .github/deps/grpc/c/c-ares.rb
@@ -1,11 +1,11 @@
 class CAres < Formula
   desc "Asynchronous DNS library"
   homepage "https://c-ares.org/"
-  url "https://c-ares.org/download/c-ares-1.23.0.tar.gz"
-  mirror "https://github.com/c-ares/c-ares/releases/download/cares-1_17_2/c-ares-1.23.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/dns/c-ares-1.23.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/dns/legacy/c-ares-1.23.0.tar.gz"
-  sha256 "cb614ecf78b477d35963ebffcf486fc9d55cc3d3216f00700e71b7d4868f79f5"
+  url "https://c-ares.org/download/c-ares-1.24.0.tar.gz"
+  mirror "https://github.com/c-ares/c-ares/releases/download/cares-1_17_2/c-ares-1.24.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/dns/c-ares-1.24.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/dns/legacy/c-ares-1.24.0.tar.gz"
+  sha256 "c517de6d5ac9cd55a9b72c1541c3e25b84588421817b5f092850ac09a8df5103"
   license "MIT"
   head "https://github.com/c-ares/c-ares.git", branch: "main"
 
@@ -15,14 +15,13 @@ class CAres < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sonoma:   "a2659a49b749fa30972d68042f23ae8500eb2ef470b39d4d7c1d006b0d631192"
-    sha256 cellar: :any,                 arm64_ventura:  "6afea5fb4e5ff029cc3f68c6523458f7f4d800d1e20e1f7617d776c77c203e22"
-    sha256 cellar: :any,                 arm64_monterey: "52797daa8054095bf4d4c8d0f3bdce6b41a5ae4d5f6e34ac8c2d09851ca6c187"
-    sha256 cellar: :any,                 sonoma:         "eb6e1e08bc53b05cdbdcb64b6b494646b93cec6650e58182b80a22dfcee72c7d"
-    sha256 cellar: :any,                 ventura:        "7a5d3e35412af7e9f924ae2314579bb47874f5c1183a43cdcd955c92ac3065aa"
-    sha256 cellar: :any,                 monterey:       "5d56fbdc7854b40681bc7d76cbeb463d2d15524b0ea646d4ea169cd27e8a99b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "59015edadad722031a2b46a0791c728b3453015c1c217175cf22c41ea12ddf5c"
+    sha256 cellar: :any,                 arm64_sonoma:   "f9345a17bd9555ceb70b9c2ec186042a06ec4269fe7ddd07eda1bfd0a048cc8d"
+    sha256 cellar: :any,                 arm64_ventura:  "2e099330f8027783e7556853a29db75b01e2c01d9ed03287d18225c2033f625b"
+    sha256 cellar: :any,                 arm64_monterey: "b14712164c85ab07dcae91a19dc4b2914635ea633ccfd837d419a6610ad1cebe"
+    sha256 cellar: :any,                 sonoma:         "08c89c93f07e2856aace4fa1a851bf52b5b4200bb8993559ad04121164714cb4"
+    sha256 cellar: :any,                 ventura:        "4e3f29d9e53f60402920af4d00091d1294f1443fc0b76c4b5bdee21011526005"
+    sha256 cellar: :any,                 monterey:       "9b1db321f720acdaa970a629ef6b086bd5e75a88fcaae8af80d670338a8b62f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0247b5008e5f2415cff30024616f32a99ade43fa3f58477e0893dfaa26210d9a"
   end
 
   depends_on "cmake" => :build
