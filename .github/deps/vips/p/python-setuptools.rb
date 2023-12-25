857f458d7ebcc09d2cc72ae802062cb564197493
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-25 10:06:10 +0000

    Update extension dependencies

--- .github/deps/vips/p/python-setuptools.rb
+++ .github/deps/vips/p/python-setuptools.rb
@@ -1,18 +1,18 @@
 class PythonSetuptools < Formula
   desc "Easily download, build, install, upgrade, and uninstall Python packages"
   homepage "https://setuptools.pypa.io/"
-  url "https://files.pythonhosted.org/packages/4b/d9/d0cf66484b7e28a9c42db7e3929caed46f8b80478cd8c9bd38b7be059150/setuptools-69.0.2.tar.gz"
-  sha256 "735896e78a4742605974de002ac60562d286fa8051a7e2299445e8e8fbb01aa6"
+  url "https://files.pythonhosted.org/packages/fc/c9/b146ca195403e0182a374e0ea4dbc69136bad3cd55bc293df496d625d0f7/setuptools-69.0.3.tar.gz"
+  sha256 "be1af57fc409f93647f2e8e4573a142ed38724b8cdd389706a867bb4efcf1e78"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "422411b81400ef2ccabaf444da4e08d8c7e4cf2b15300c1840e0c362b36ffdb9"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d48d89585de306ccf804d60bee0d968a3a39cc68957cfe7b9cb6d0a4e11b37ff"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4e02ab02e6ae808b561085b2149b8eb037086179b32ebf1c7ba099fab282c481"
-    sha256 cellar: :any_skip_relocation, sonoma:         "57c4023c78823c72db7e4701c2f942ea380e0ade386eb84c5c4bc6e15b2adc8b"
-    sha256 cellar: :any_skip_relocation, ventura:        "d2771b32e2303a373e949c92aac3f0449634aecd27ce1cd97a1bef2830fe3da9"
-    sha256 cellar: :any_skip_relocation, monterey:       "f5305c297ebbe7d09431df85006440c7a87b693edad36767002a47cb25e468ef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "07a6bd6690adcdb4c9fe8102ae5955cf1f5354002f6d53da4521c69929ddb6a0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "7401711f1383d56eafe4ce69a9f6c80527627a63c6dbe26945eaf778cd14b836"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "618fc870d2b418f33b855bf3292e63a20217f3d46b9374213569e02469ff95d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "83abdbe36f879939e74437be475abab07502d6eed57924da2db723d61d3918b0"
+    sha256 cellar: :any_skip_relocation, sonoma:         "de72395d2db740d32a41bba965d66d69616369104fbb02c2971ad3a36a1717fd"
+    sha256 cellar: :any_skip_relocation, ventura:        "c9804924c407eb0eca15bcb7080eaa05fe192ce0cb2dd6d29d77a56e8987fcf8"
+    sha256 cellar: :any_skip_relocation, monterey:       "a25631d40983d9fc23163821bbe0025d61d425666804f955f66cef9d6e2e6f67"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e7cc325af6ffb81ca8a869984ddf5ace890f841db607126a68c906024499bc05"
   end
 
   depends_on "python@3.12" => [:build, :test]
