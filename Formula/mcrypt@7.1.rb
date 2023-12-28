2682b21268e3c4e53653d347284240c5fdd8e6b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-28 12:54:57 +0000

    mcrypt@7.1: update 7.1.33 bottle.

--- Formula/mcrypt@7.1.rb
+++ Formula/mcrypt@7.1.rb
@@ -15,14 +15,13 @@ class McryptAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 10
-    sha256 cellar: :any,                 arm64_ventura:  "af74e84337d40f7b116dee3abf6ee8416f693b4a91fd6fb221f8382ca69bdcdf"
-    sha256 cellar: :any,                 arm64_monterey: "7f41272ba6384478cb98417633570a3f725d83f6fbdc6ad8505fad2bec7ec09f"
-    sha256 cellar: :any,                 arm64_big_sur:  "1e57e355166a230d1d48455f620cb2ba25598b270d3467ae8c5e639493446229"
-    sha256 cellar: :any,                 ventura:        "c2f28cb2404c53e2c819489e3ff8ce8278212dc037670fea2a0a2680ae023622"
-    sha256 cellar: :any,                 monterey:       "39bc7f84a0aecd374fe783b28196b9d98379cd29f73d89e903cbad95a28516e8"
-    sha256 cellar: :any,                 big_sur:        "e10b334db4011b5c14def1e9065db61b8d8303e5f4e0c3945746c187dea15096"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0223d30dfaa260cf7f53921fd7af28249b1f40bc03fb47299b3e032e909b91cc"
+    rebuild 11
+    sha256 cellar: :any,                 arm64_sonoma:   "2edd33014b47387c8fe591331a37350bd13457909b431a2b59cb77abb6b0a28c"
+    sha256 cellar: :any,                 arm64_ventura:  "47b47d68d6b7a706143bb5e6b76bfa4cb5133cbaad7931eda5a6f811ac02bf54"
+    sha256 cellar: :any,                 arm64_monterey: "2acc2e820a0d2b13ada934645941e4428fc2f584d32b100951e0e4405f801b53"
+    sha256 cellar: :any,                 ventura:        "e209432155e06f9ee550501d14ed8618443ae34cf5f91afb5eb6185bed6c7837"
+    sha256 cellar: :any,                 monterey:       "8b59711b2e22917f56958d8aca1d3b9aea62585393bdbcd096402914e5ba308e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48e34ffa879ff68386bd6fc59ca7285c5eef25f111ba395874d84bbeaf5410d3"
   end
 
   depends_on "automake" => :build
