4a02d6ccd6d140e4fc38316ee86090421af20041
wielorzeczownik <wielorzeczownik@furryunicorn.com>
2023-12-29 20:56:29 +0100

    chore: Add phalcon5@8.3

---
+++ Formula/phalcon5@8.3.rb
@@ -0,0 +1,31 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Phalcon5 Extension
+class Phalcon5AT83 < AbstractPhpExtension
+  init
+  desc "Phalcon5 PHP extension"
+  homepage "https://github.com/phalcon/cphalcon"
+  url "https://pecl.php.net/get/phalcon-5.5.0.tgz"
+  sha256 "cb68e8c4d082b0e3c4d0ee3d108d68dbc93880a7a581c4c492070a345f2226c3"
+  head "https://github.com/phalcon/cphalcon.git", branch: "master"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "pcre"
+
+  def install
+    Dir.chdir "phalcon-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-phalcon"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
