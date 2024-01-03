578f66d6573854908fe037dd04b9de083aec5d63
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-03 10:06:29 +0000

    Update extension dependencies

--- .github/deps/vips/l/little-cms2.rb
+++ .github/deps/vips/l/little-cms2.rb
@@ -3,8 +3,8 @@ class LittleCms2 < Formula
   homepage "https://www.littlecms.com/"
   # Ensure release is announced at https://www.littlecms.com/categories/releases/
   # (or https://www.littlecms.com/blog/)
-  url "https://downloads.sourceforge.net/project/lcms/lcms/2.15/lcms2-2.15.tar.gz"
-  sha256 "b20cbcbd0f503433be2a4e81462106fa61050a35074dc24a4e356792d971ab39"
+  url "https://downloads.sourceforge.net/project/lcms/lcms/2.16/lcms2-2.16.tar.gz"
+  sha256 "d873d34ad8b9b4cea010631f1a6228d2087475e4dc5e763eb81acc23d9d45a51"
   license "MIT"
   version_scheme 1
 
@@ -18,15 +18,13 @@ class LittleCms2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "6423981030e9951b0daa62678e90d1bfcd723d13bea30cada62844b5b1d5a4cc"
-    sha256 cellar: :any,                 arm64_ventura:  "ec43c4b1d15b75200740331b92656b624be01bd40cb993f862f41aca60ae670a"
-    sha256 cellar: :any,                 arm64_monterey: "de857ee35cde49fb0c675a9620796d9ee8f3d71a995351c8ff7a86ed23bda8e1"
-    sha256 cellar: :any,                 arm64_big_sur:  "64597b6b3dc07b07e06aab5280eea44493166bfcf21800cc8d16b3353ce7a37d"
-    sha256 cellar: :any,                 sonoma:         "d6e6462049c343d51bfd477be36c9f31414db64f44a0205f7cc7f555b9de8dbc"
-    sha256 cellar: :any,                 ventura:        "c0bcd5e7befef41984d5d55ee139c807e47cb43837d39a9bf4598b251e552371"
-    sha256 cellar: :any,                 monterey:       "4476622668a4a2290fe41470f656ff8d2c4f5b55122419020d99e0b54d847103"
-    sha256 cellar: :any,                 big_sur:        "c7cb39e28b14011c8ccf73c5de99c77328b6e41626bdfa400265e83911dd2070"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d1b4f53e85030cd1f5e52c997cbea72e77f68472f8bff18a8c18f0d08f751b1d"
+    sha256 cellar: :any,                 arm64_sonoma:   "d1ed5796de3f00d4b1301e9062cb54b2337c8e8dcf2ae9be8e03f3ed7af791e0"
+    sha256 cellar: :any,                 arm64_ventura:  "c7eee75a83a2be3f19aa53ad043da4a316af7bc7f57e6aaf8f311f9e6e354be8"
+    sha256 cellar: :any,                 arm64_monterey: "2f155dd797f8d008616c61677d7b7ebc49b349feb06a1c86239d49e1b9a5118d"
+    sha256 cellar: :any,                 sonoma:         "46dd0d6ba9293999feaeb701a4c614440250a51daf0949478fbd486650a637bc"
+    sha256 cellar: :any,                 ventura:        "1c2e7ed787c6ff05b52bf3d1f6e27047e390f609af8a2dcfbb5904cdee8a0fff"
+    sha256 cellar: :any,                 monterey:       "c3add26aa2b85ad6bda455cb9ad2f14a3e0055f458722757162dcfe91a992044"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "79268ac2afeaaa7bce8af516f329f808443c3bbad64e705f8a39e4f0ccc112e2"
   end
 
   depends_on "jpeg-turbo"
