b0c83cf75b80eb7a5e808d21589f379e5f80c807
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-15 10:06:56 +0000

    Update extension dependencies

--- .github/deps/vips/lib/libidn.rb
+++ .github/deps/vips/lib/libidn.rb
@@ -1,22 +1,19 @@
 class Libidn < Formula
   desc "International domain name library"
   homepage "https://www.gnu.org/software/libidn/"
-  url "https://ftp.gnu.org/gnu/libidn/libidn-1.41.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libidn/libidn-1.41.tar.gz"
-  sha256 "884d706364b81abdd17bee9686d8ff2ae7431c5a14651047c68adf8b31fd8945"
+  url "https://ftp.gnu.org/gnu/libidn/libidn-1.42.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libidn/libidn-1.42.tar.gz"
+  sha256 "d6c199dcd806e4fe279360cb4b08349a0d39560ed548ffd1ccadda8cdecb4723"
   license any_of: ["GPL-2.0-or-later", "LGPL-3.0-or-later"]
 
   bottle do
-    sha256 cellar: :any, arm64_sonoma:   "cfcf1220a9901675495f8d64a8914283b6e9d2ced826b631aaa0b528bab5251f"
-    sha256 cellar: :any, arm64_ventura:  "9b6d7f6616a31a56829e53ed6ff8c7491f0a076afdf9ec25d3cccd3701b83b0a"
-    sha256 cellar: :any, arm64_monterey: "c4a1c32197a17b25f61adecc077503661b595efa986d9c418e9a736354eee3e1"
-    sha256 cellar: :any, arm64_big_sur:  "2c1ec1cc41cb3ca506930d50367fcdcabee872156c8c4ae44ac68ae022cd5d41"
-    sha256 cellar: :any, sonoma:         "faef44af83feb8aafe6ee0d3ce676422bb2d124eefae831ffb69364f4d4c8a12"
-    sha256 cellar: :any, ventura:        "d1eac4ba5398dc47ffb3ba9931f71590a487b1847f146cdd615026a184304ca6"
-    sha256 cellar: :any, monterey:       "ad84e01ad371a8bd47a15a2b2da2acef55293cc6fc72ef0e5130986ddfd119c7"
-    sha256 cellar: :any, big_sur:        "464812fe81d7bafe7c25fe5d4e7348b603e5ded35410ff52b9933db76e6e5724"
-    sha256 cellar: :any, catalina:       "9e6ed6c2ea5ad341d3e0627ee67861001bb8104f441298b456b983e935d5aa55"
-    sha256               x86_64_linux:   "fe9a04089d251cc404385029ab855630c509961937930cb703ff85b236503751"
+    sha256 cellar: :any, arm64_sonoma:   "d342445da0046d7c00011f2ae04869066359318d77d24a9eeca8117436c0436c"
+    sha256 cellar: :any, arm64_ventura:  "028793a845059cb3db2abe7de82e257c080df9d3be18457a1b765bd70e0918d3"
+    sha256 cellar: :any, arm64_monterey: "0653633442ed419791ec797ba4856f4f11c1d56253332325624a7457f137d1fb"
+    sha256 cellar: :any, sonoma:         "93f6802767b31f0f6b8baeafc8cbc07df4312879cf2416711bc22d66cda14690"
+    sha256 cellar: :any, ventura:        "367c1d3e45212508a0a28291d4d750c6abdf6544ca9bd551c9f8367f176c0029"
+    sha256 cellar: :any, monterey:       "f8600f13a5956e11500c5bb96435b2e41ac9ac0870839b630f42b6425a6c58e3"
+    sha256               x86_64_linux:   "3cc66b8450c436a5047d8075a004be087d3f21b5322834ef741165487fad262b"
   end
 
   depends_on "pkg-config" => :build
