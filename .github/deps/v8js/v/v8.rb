0cc3d7a300685f87fcb96f7782a48a4444fe3517
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-23 10:07:00 +0000

    Update extension dependencies

--- .github/deps/v8js/v/v8.rb
+++ .github/deps/v8js/v/v8.rb
@@ -2,8 +2,8 @@ class V8 < Formula
   desc "Google's JavaScript engine"
   homepage "https://v8.dev/docs"
   # Track V8 version from Chrome stable: https://chromiumdash.appspot.com/releases?platform=Mac
-  url "https://github.com/v8/v8/archive/refs/tags/12.0.267.10.tar.gz"
-  sha256 "63c849f6986c99f979a071d1ef1359d9e0908e183f265d3138cc16ddce1aeeb0"
+  url "https://github.com/v8/v8/archive/refs/tags/12.1.285.24.tar.gz"
+  sha256 "30942df4ec837b212b697895b9453459b2cdce118eed2e07656dcefea465da72"
   license "BSD-3-Clause"
 
   livecheck do
@@ -23,13 +23,13 @@ class V8 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "d340fd21f54e5688d13be3f0d7663971b686d0456f77497cab5e59a1a2a46667"
-    sha256 cellar: :any,                 arm64_ventura:  "c5e164f26fd809f4d3a3c9f88b01816154b29aaef164192083fb784205e801f9"
-    sha256 cellar: :any,                 arm64_monterey: "09d9c6f9c987cfa98a9712c00c663ebfcfdcb961955f05b5dfd9e2f732764a4a"
-    sha256 cellar: :any,                 sonoma:         "3683f4d4561c74150d124b394c17e62d3e899188509c6bf10c65ce6134a9eb14"
-    sha256 cellar: :any,                 ventura:        "a0388fd2c369e209433407c30741028db7231118e7cc58d0c6c4dfc9f5e10cb9"
-    sha256 cellar: :any,                 monterey:       "8c2965859dce4434085fa1bfa56d1c056942b5f98df5fae4d0569398bdf3938c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "448d382cef529987cc27e4e7f74cad05d2ccb3388e3246192fbfe2decc3574c0"
+    sha256 cellar: :any,                 arm64_sonoma:   "f32a5a022378b428cea1b649a34fd55d8c7edc443e1526a701314a993eca4f08"
+    sha256 cellar: :any,                 arm64_ventura:  "fc52a73f061198c928bc31a5561ad8460e948a8ea20ae45218e4f360d3c52fa0"
+    sha256 cellar: :any,                 arm64_monterey: "4cd19a160f5ad942a30968f582ffaeb9485aad034e122c14910a4101e68aac35"
+    sha256 cellar: :any,                 sonoma:         "ca514e9dd9466c58c3fd466a11796655d8131985602b44d0c7a02d3788fb3841"
+    sha256 cellar: :any,                 ventura:        "2de6daceddeae9d5c08d0842bdf08b9eb78a85bc3a869eae6629e4a5b883bcae"
+    sha256 cellar: :any,                 monterey:       "9596dd1d33272babe4959de7869f569a00de8e62a8df7711e60b172a6a3f0c43"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c109bd1d47b502257784e1e28acbfdf68292f2f839feaab396d7fe71cc5a063e"
   end
 
   depends_on "ninja" => :build
@@ -51,10 +51,10 @@ class V8 < Formula
   fails_with gcc: "5"
 
   # Look up the correct resource revisions in the DEP file of the specific releases tag
-  # e.g. for CIPD dependency gn: https://chromium.googlesource.com/v8/v8.git/+/refs/tags/<version>/DEPS#64
+  # e.g. for CIPD dependency gn: https://chromium.googlesource.com/v8/v8.git/+/refs/tags/<version>/DEPS#74
   resource "gn" do
     url "https://gn.googlesource.com/gn.git",
-        revision: "e4702d7409069c4f12d45ea7b7f0890717ca3f4b"
+        revision: "7367b0df0a0aa25440303998d54045bda73935a5"
   end
 
   resource "v8/base/trace_event/common" do
@@ -64,7 +64,7 @@ class V8 < Formula
 
   resource "v8/build" do
     url "https://chromium.googlesource.com/chromium/src/build.git",
-        revision: "a21fc6065131d0442e8a54c3ca2638e393b69438"
+        revision: "9b8bc79c291c01ddcdb0383ace9316e3dcccce2f"
 
     # Use Arch Linux Chromium package patch to remove some LLVM/Clang 18 flags
     patch :p2 do
@@ -95,12 +95,12 @@ class V8 < Formula
 
   resource "v8/third_party/zlib" do
     url "https://chromium.googlesource.com/chromium/src/third_party/zlib.git",
-        revision: "dfc48fc4de8e80416606e2aab42f430ced2a524e"
+        revision: "dd5fc1316c9bfe87091c4f418e427633590a84a4"
   end
 
   resource "v8/third_party/abseil-cpp" do
     url "https://chromium.googlesource.com/chromium/src/third_party/abseil-cpp.git",
-        revision: "16ed8d7d56105c49a0bbc04a428bf00dc7fadaf6"
+        revision: "0764ad493e54a79c7e3e02fc3412ef55b4835b9e"
   end
 
   def install
