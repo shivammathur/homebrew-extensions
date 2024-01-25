c71badfe93a9157cabbea630c4f8a9cd8bbc1ed0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-25 06:03:14 +0000

    event@7.2: update 3.1.2 bottle.

--- Formula/event@7.2.rb
+++ Formula/event@7.2.rb
@@ -15,12 +15,12 @@ class EventAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_sonoma:   "568895f305656b886b71b642205444aab0457e7390eefc8cbb03671e2c8bdefe"
-    sha256 cellar: :any,                 arm64_ventura:  "39cdff52d074379203bd9d6d300182cb5220f7e8918c3f3425a38908f3255161"
-    sha256 cellar: :any,                 arm64_monterey: "c86627b1cc6500c52381937089a1ab811767ef46072ec6c720a5916eeaeaa6c3"
-    sha256 cellar: :any,                 ventura:        "a64e47d84ad6d33ae2659a9e409b371872ce35a9c4baaa603d894bb96fbd768e"
-    sha256 cellar: :any,                 monterey:       "9946b76b94b376e7dc9bf97eedd0159c732fa51895972f40939f536f44a82898"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7f702fd8c7a0930bca2ae51b04393551d21603972541d5cb8bcaef9c8527459c"
+    sha256 cellar: :any,                 arm64_sonoma:   "4e9b58d09cbb5a44522462facd397052c87b3a53d461b3c32f7e68b390611c19"
+    sha256 cellar: :any,                 arm64_ventura:  "75929bfd5e01adb82ab02ea0194ec49ba2f3e885b67f6c07c45bfc1d9c4a317f"
+    sha256 cellar: :any,                 arm64_monterey: "8fa617ab2556e673890766b6227882dca851da7e717cb8e10f0b2cfe0fb5a317"
+    sha256 cellar: :any,                 ventura:        "70311433abff18029a12dbcb669411010b5d5dbe7db945f79c4afb3a790b0a89"
+    sha256 cellar: :any,                 monterey:       "5b155cce3459bf226c9b3ce52ddcd7730fb6377cb88fae7530ce7aa1b1e52dcc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e5b4ba03e045fc59c3a5d711ad00fa236df4f71462e90950c5363946df24e102"
   end
 
   depends_on "libevent"
