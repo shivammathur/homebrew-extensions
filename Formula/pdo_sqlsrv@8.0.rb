548bdd89ef6e9e774c5fbef87ed0df3058105f08
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-01 04:01:50 +0000

    Update pdo_sqlsrv@8.0

--- Formula/pdo_sqlsrv@8.0.rb
+++ Formula/pdo_sqlsrv@8.0.rb
@@ -8,8 +8,8 @@ class PdoSqlsrvAT80 < AbstractPhpExtension
   init
   desc "pdo_sqlsrv PHP extension"
   homepage "https://github.com/Microsoft/msphpsql"
-  url "https://pecl.php.net/get/pdo_sqlsrv-5.11.1.tgz"
-  sha256 "549855a992a1363e4edef7b31be6ab0f9cd6dd9cc446657857750065eae6af89"
+  url "https://pecl.php.net/get/pdo_sqlsrv-5.12.0.tgz"
+  sha256 "22f0cb17b45f0deccd0bba072ee0085ff4094cd6ee2acc26f7f924975ef652c6"
   head "https://github.com/Microsoft/msphpsql.git"
   license "MIT"
 
