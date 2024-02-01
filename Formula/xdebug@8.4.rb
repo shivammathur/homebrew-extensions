a88dc92d288f4e84557a8d08a245cdcd58cffb41
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-01 04:52:25 +0000

    xdebug@8.4: update 3.3.0 bottle.

--- Formula/xdebug@8.4.rb
+++ Formula/xdebug@8.4.rb
@@ -16,13 +16,13 @@ class XdebugAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 5
-    sha256 arm64_sonoma:   "19908c3c671b5ffd1669a2bdfdfca2996d3d61a9a82740b92436a84ab92b5b19"
-    sha256 arm64_ventura:  "d73e69c5c7deeae817d27507b81ceead6abba828a5fd8db78a6349e17c1f3fbe"
-    sha256 arm64_monterey: "d1e556891438f1fdb47d625f9fc91331abf3cd51544f693a4b5baf557068c1f4"
-    sha256 ventura:        "811d228f8d7b48ed6905335d4f8f456fbae2fc18f0cbaed27aa86f1a66a00280"
-    sha256 monterey:       "60fcc484dbebb9143556b277cd5acf04732b39d31f01aa754b409dff6299c9e7"
-    sha256 x86_64_linux:   "1522779453adb4da8f9e13cd7f5d4072fbdfbf16e57886cb1ca80d051d4e6622"
+    rebuild 6
+    sha256 arm64_sonoma:   "777f17135044406c660b2d1a4b3100d0b33fd2d2a40b1240a0d37561634d2f33"
+    sha256 arm64_ventura:  "6ae7a3f2953c6098cd2dfa5d714cb7b1e9336f610d344cc0cea815ca84fa2041"
+    sha256 arm64_monterey: "5e39e6b10efe95d9b7daba75fa832873a9f5103b1bbc762032cd919524b56f45"
+    sha256 ventura:        "954e6bc66827b4a18da1de47c2b04d07684135c470263027fc8e20d8dd538514"
+    sha256 monterey:       "89c2f556e84e7ae063a5869377c4013c8dd46631b03e51de1d6f528794130ff6"
+    sha256 x86_64_linux:   "1302310cd9d896b80b3190c43df3a61eea51cd8912722812bb32ec37e04bd8f7"
   end
 
   uses_from_macos "zlib"
