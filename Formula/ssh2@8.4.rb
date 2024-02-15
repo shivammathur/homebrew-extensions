b2bfea8f2eab52aed5e617fbb393bac5908ef488
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-15 08:05:00 +0000

    ssh2@8.4: update 1.4.1 bottle.

--- Formula/ssh2@8.4.rb
+++ Formula/ssh2@8.4.rb
@@ -15,13 +15,12 @@ class Ssh2AT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 2
-    sha256 cellar: :any,                 arm64_sonoma:   "c95a0b0ff802d7c7c06bb4bca14eaa37dbcd9f47279b2e10cf0628f5064fedef"
-    sha256 cellar: :any,                 arm64_ventura:  "cbf5d81f7f5ee89954624dd9938cceec4610d91355fbcf47e6d93678e5635db7"
-    sha256 cellar: :any,                 arm64_monterey: "62c07ed73c5cd85130f04948a83c0be1b0242e4f7f14895950d3652b87723a6d"
-    sha256 cellar: :any,                 ventura:        "5b45ea2ee49519ad6e28cd45ae5f85b8d5dfcecb7a633fadbb8143af94f30a96"
-    sha256 cellar: :any,                 monterey:       "60c03aaf5e1298c098c83997be67298cb6b389cf719a127a688885d386ae2352"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eb167a6240c661f68446dcf423cb3215e90c27c4dd91e36ef268894cba19ff55"
+    sha256 cellar: :any,                 arm64_sonoma:   "8df86d0808d0f100442658aea960f19e375ba6f350c9944e2a37e9e71ceb0e22"
+    sha256 cellar: :any,                 arm64_ventura:  "aa3bac9f1a1ba582e2a2e66ec132473aba08951bc2e136ab9bdb2339c8380ec5"
+    sha256 cellar: :any,                 arm64_monterey: "987f958edcd0d1c994ed30c0726398af12dc996322e259561ed57322ca61744f"
+    sha256 cellar: :any,                 ventura:        "0250ab6fe0d3fb8d51fc5f65e809d89b4bcc5303c1cb4a4352cbdaa66ba570be"
+    sha256 cellar: :any,                 monterey:       "b09cb71b00890f4ad9d87c70dcc17b40088d3cac2372ef0433633e718d7bfecb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f49ab6bcbbe7779231aae889c2e60a3bdfc12d16adc6f2c6c91055f7dbc32a7e"
   end
 
   depends_on "libssh2"
