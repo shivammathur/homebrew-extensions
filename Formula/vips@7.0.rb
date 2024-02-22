59874e9544a95e296f03272c0f667da00fcbb3db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-22 06:44:13 +0000

    vips@7.0: update 1.0.13 bottle.

--- Formula/vips@7.0.rb
+++ Formula/vips@7.0.rb
@@ -15,13 +15,13 @@ class VipsAT70 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 8
-    sha256 cellar: :any,                 arm64_sonoma:   "8e88b30ed46dc12e8ce5b04fbe0e73a1ac52a9ff3f6ef0af7645cbde956a444a"
-    sha256 cellar: :any,                 arm64_ventura:  "46525e104de894e2cb8c3ebfd0c4e2dad89e485aa7160e81b46a8a9379b019f3"
-    sha256 cellar: :any,                 arm64_monterey: "b0c2f2b3315b24a05903902611cc8b2499c4fe3a5778c4de0d9d12a04b2ed522"
-    sha256 cellar: :any,                 ventura:        "2891a12736f2a9b5e3d0b9c20d414f6e0d9e574ab3e6835494b288a85cb2ffbb"
-    sha256 cellar: :any,                 monterey:       "2a70c0f73cc1b642f9adcaf1898e3ad28575ca9b21ca16eddbcbb2f36e0719de"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "60530a19889cbb64f35edf15af0b8631fd63dabde3e39f987cb0e1e914255adf"
+    rebuild 9
+    sha256 cellar: :any,                 arm64_sonoma:   "f803ad9adfa66ad9872861e9cd2a29c5abc0a10a9abaa36ac762eccd636b6a23"
+    sha256 cellar: :any,                 arm64_ventura:  "e03babc63232252975d8cdf918299cf174b2d6cba83a9d4d808af44c68790326"
+    sha256 cellar: :any,                 arm64_monterey: "d84947d29e484cf60c71d4bc5219605903e17ec7c92e8b04d2d621844ca30986"
+    sha256 cellar: :any,                 ventura:        "e2882293f4f47b309c4af24886d660963964145f476904f5411ec6ba0818efc5"
+    sha256 cellar: :any,                 monterey:       "89f002fad5ae4b3271c481ce58238cc8c705da0dce8d45091147d083d6abb719"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f9f539523eb1749da78a9f61fd3c5eded2dbb35b8118bcc4b4e37c5f17339159"
   end
 
   depends_on "vips"
