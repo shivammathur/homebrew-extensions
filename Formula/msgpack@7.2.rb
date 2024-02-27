ac63519be89106be9ccb4a2c8d6c3f88b09dc957
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-27 14:36:11 +0000

    msgpack@7.2: update 2.2.0 bottle.

--- Formula/msgpack@7.2.rb
+++ Formula/msgpack@7.2.rb
@@ -15,14 +15,13 @@ class MsgpackAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c8d91472aae67191fa24fedf104c13a87ec8a16e8fc8bc5e0273aa35101a38fc"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "adfe1e519d5cdbebde4d9c957d6cb3c7b43fb55be1370f4b73aa03f0f3a8d7c0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "200bd16c96f590c0b28a1ae1606ba8aa78042c0c9f7780227bfc6cbeb3119770"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3c77365217061065395d65a4ef147f031efb05a24a8de878cfc646768e1bf369"
-    sha256 cellar: :any_skip_relocation, ventura:        "c6ab235868e298132bd54f0c96dea843ae33c681b644e2c719897dcb70d05686"
-    sha256 cellar: :any_skip_relocation, monterey:       "ade57de102d73aaba97ae75761d114d721a52e632e706249ef46668bfc5e3c73"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3d46746816aa9436fcd94d3a34226c8a2b75033d1f21738fccec7bb38f13ef96"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "14a4f5c0ebd9b91948e1c0c480e276ec18e76acdd10949012da9d8d7b197958c"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9046c27ebb8a09dc8e43ae4abebc4ca4fca3b45ba3767f1e98188fd3df245671"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f5ceb720e7545ea53f9f324a811de141ecd98be2a5360885c4a2ae93a64dc78f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "29e11b8ceea42c632cda988ce3987010d1c07109ffb0602c01844790e6e3072a"
+    sha256 cellar: :any_skip_relocation, ventura:        "bb96e3e4d79d4b6fead719a4086569cf9763c350803430cb00b4f648f7fa6274"
+    sha256 cellar: :any_skip_relocation, monterey:       "e66a4380118c73ff9d790c4f5ec8de6ff532ca8a1888e5a43ce0cf555a1dbb23"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "279457c58243998934973b9dd4eb1a8011f5848cca1252d37447a3fc9ab056c0"
   end
 
   def install
