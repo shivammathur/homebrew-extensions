17a1ae440f320f7fb021152e0afb8baa5fb9abaf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-27 16:03:58 +0000

    raphf@8.1: update 2.0.1_1 bottle.

--- Formula/raphf@8.1.rb
+++ Formula/raphf@8.1.rb
@@ -16,15 +16,13 @@ class RaphfAT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8c8460f68c397c9d3b382a7275cc734f946677c3c34ea6f8999f467e74d42bb5"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "977f7c055e479062bdd94dfc238ad1f304ad9fa63acd3e2a627b5ff7c9a1049d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8b35713dd47fcf7fef50f9e33e67d4e38ef62e593bb2e29157994d6c16de1b2b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b3a0c73ffc70c6af5d992c76ef4453c1e2275efb0a98a0bb1f0a88489cd9127f"
-    sha256 cellar: :any_skip_relocation, ventura:        "1593cf5f1bdc18855f633a4306ae06751a3a2925b786715353982e1945e343ea"
-    sha256 cellar: :any_skip_relocation, monterey:       "2258d2ca2345b1dc3a97c5b199e95cef4cd18cf29eaf8a0b999306a98df8aa6b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b345122c2ff0c6376dde10aaf89fff957f8c9fa6851d84a3948621ada774ff4b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c6712fd6b530781b1494c19504a0a677ff9a5df95c68f4d27d69d50210debab"
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5fef47606d7649f201b3e7660e697b9e8e5ff373d4f6675eb9eae54543154b0a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a3c8a5fda590738cf97e8e6b9dc672b549f59e1a0718e6ca12fd0a2530e8c2f6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b2f5cd0239b0a9163b1cecf589f1505ebe4a50605cbd0a070f14d0c336c44b14"
+    sha256 cellar: :any_skip_relocation, ventura:        "04f247873bcc877eddd01780d696c88f44f9871a9c492cc77b52005acd0d40cf"
+    sha256 cellar: :any_skip_relocation, monterey:       "37ae16098912c57d8d1782f6da535a92173d7ad36e634261b08a02b22d141909"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f9cd4506817a42a0421096228a88b8153f0d308497cfd31ee8dd390c10b190e8"
   end
 
   def install
