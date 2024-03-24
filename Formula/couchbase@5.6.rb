10e99ec686fcfd453717c0a023dc55d738867884
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-24 23:37:42 +0000

    couchbase@5.6: update 2.6.2 bottle.

--- Formula/couchbase@5.6.rb
+++ Formula/couchbase@5.6.rb
@@ -15,15 +15,13 @@ class CouchbaseAT56 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "53812b94d2d4d3428b6664c660e7efea120e72b270cbecd22f2ab22a1fd56d17"
-    sha256 cellar: :any,                 arm64_monterey: "7d31fdb0939fd7a40da327673f9303ac776b356f3491d65a4231712bd1ba581b"
-    sha256 cellar: :any,                 arm64_big_sur:  "adf794bb8c37455c5e065af50a5dd11b9df304b2ce2e0ebfe933c93237471856"
-    sha256 cellar: :any,                 ventura:        "eb45482f5142685b80c285dfe171c2df733a42ee28d50b9ca0e103be6bda22d3"
-    sha256 cellar: :any,                 monterey:       "55c096a7617b023d6fbe8015c6325d9ac5bd0765c11f8433eaf42855744d5e73"
-    sha256 cellar: :any,                 big_sur:        "94deb13d7d68c6bcd3a26b32df282cf198f5e3481863c74a80c05aad9437b2ca"
-    sha256 cellar: :any,                 catalina:       "62259ee50487083700422d5b08f6c5c27fc1327b8feed6a68615f20a7a8e09fa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa11450b668fc666cadacaa75003fb4d02a77888409ceedbc3d9782b7e2d6ba2"
+    rebuild 2
+    sha256 cellar: :any,                 arm64_sonoma:   "c1e6b4dd7bdc994f07241d9e2b9b38ab4c8aaf106c8a36694ab86275bf8c39d6"
+    sha256 cellar: :any,                 arm64_ventura:  "f81c4d9b9580aaa6e1aeaf7b59759e3dbff0c5e98c00b7ae231e37f2b9379d78"
+    sha256 cellar: :any,                 arm64_monterey: "a2ee8cc72279d5272e62f9310662ab8c43fdcb38ad93ef4c4737d1b4f8fb7ea0"
+    sha256 cellar: :any,                 ventura:        "cd7ae60d0200d590232745b2277f20264fd89f1de98c6df23c66791194bf9937"
+    sha256 cellar: :any,                 monterey:       "b96a303252cbcdc0a69f8aba8f317bb17b7248fad553718f5adc049503fd8cea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "258d370a714338fd4bd339bbe18490de6e338a0dfea33c4afced03c01555511d"
   end
 
   depends_on "shivammathur/extensions/libcouchbase@2"
