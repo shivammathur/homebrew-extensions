c14d0c8e37e40acd068283dd0c852dab55f990b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-26 11:48:56 +0000

    vld@8.3: update 0.18.0 bottle.

--- Formula/vld@8.3.rb
+++ Formula/vld@8.3.rb
@@ -15,6 +15,12 @@ class VldAT83 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "104eb44be267c90d722f0a7f74d57aab166f3d84b5929ba731e0d134240e9be5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "54b1704ed4effc44e32deaee65c001ec83724ae6461cbd278af8e19489d0e439"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2ad0af1852211d3eedf8bac67c22a788070db9fc346aea6a464d5f278eedb541"
+    sha256 cellar: :any_skip_relocation, ventura:        "cd9a0c5a75670ed80ede95d6c174b98664f9dcdb7b2f83e673a92fdf22d9e340"
+    sha256 cellar: :any_skip_relocation, monterey:       "bd86d1cb217df4c6d41c42e1bd2c2e907ac51905e565c52bb61bd226cf18d7df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4277294ab5905fbb5d79176dc6731c1785f0db1faee3589d8cc568162ec524e5"
   end
 
   def install
