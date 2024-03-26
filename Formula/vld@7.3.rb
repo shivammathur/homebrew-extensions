834b791d4803e737dc38e9db9f5624a7e54d7894
Shivam Mathur <shivam_jpr@hotmail.com>
2024-03-26 16:41:31 +0530

    Add vld extension

---
+++ Formula/vld@7.3.rb
@@ -0,0 +1,28 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Vld Extension
+class VldAT73 < AbstractPhpExtension
+  init
+  desc "Vld PHP extension"
+  homepage "https://github.com/derickr/vld"
+  url "https://github.com/derickr/vld/archive/0.18.0.tar.gz"
+  sha256 "b6cd6165014bd8c1ddd8b473fc2e232a722c88226a52368c32555cc9cbfa71ed"
+  head "https://github.com/derickr/vld.git", branch: "master"
+  license "BSD-2-Clause"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-vld"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
