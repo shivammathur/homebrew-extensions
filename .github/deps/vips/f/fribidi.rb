34af9161eb1d0467c49d7fe970cb6be81e71130b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-26 10:05:30 +0000

    Update extension dependencies

--- .github/deps/vips/f/fribidi.rb
+++ .github/deps/vips/f/fribidi.rb
@@ -1,20 +1,18 @@
 class Fribidi < Formula
   desc "Implementation of the Unicode BiDi algorithm"
   homepage "https://github.com/fribidi/fribidi"
-  url "https://github.com/fribidi/fribidi/releases/download/v1.0.13/fribidi-1.0.13.tar.xz"
-  sha256 "7fa16c80c81bd622f7b198d31356da139cc318a63fc7761217af4130903f54a2"
+  url "https://github.com/fribidi/fribidi/releases/download/v1.0.14/fribidi-1.0.14.tar.xz"
+  sha256 "76ae204a7027652ac3981b9fa5817c083ba23114340284c58e756b259cd2259a"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "bc022e5da0135b6f991274ffd688eed3a3ed5332b1e9a1a26fffcef792e6bd87"
-    sha256 cellar: :any,                 arm64_ventura:  "0272f179bac6809467c56eb0b1fac7f1de88b4c012fd36e77411aec39e5f9b4f"
-    sha256 cellar: :any,                 arm64_monterey: "ad4a3ff427cd169933fcb3266e0b85c841d25cfa3d7883759bcf84f9655c59eb"
-    sha256 cellar: :any,                 arm64_big_sur:  "d7c97dc2da426a3586d0c428f57d63300951ae23784769ebfdf3a4af0ab225f0"
-    sha256 cellar: :any,                 sonoma:         "4f38b08fceaf4b1707a7931953d55ed00e556c08abd61449aef1dc9f1ecd2ee1"
-    sha256 cellar: :any,                 ventura:        "df4ec1000cdaac83d1b712c838e2eb32a93c8ec7c7d7d47e2d199fe96501a435"
-    sha256 cellar: :any,                 monterey:       "d2642675b0e7340ef0cc954a694b0d80e80f2b0b8b4e04f2f7ffe7b82aed1cd2"
-    sha256 cellar: :any,                 big_sur:        "8ee3767e307b336a952f55d2c3c1a043bd2a8a88fc10889a7888e3c69207b105"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4771f18b9338c897f39328998452a0aa4d03b7a13021e5303a12376170f3fb7d"
+    sha256 cellar: :any,                 arm64_sonoma:   "5aea13f26a125562f2564cfc3c4051bd3bd6c65780b8349f5bbca3afbc3f2bf5"
+    sha256 cellar: :any,                 arm64_ventura:  "e4bfebfee694b26afb1a95460a627eea98c762d1cab9b2d759c9206a3d008fb3"
+    sha256 cellar: :any,                 arm64_monterey: "04f12760ee7c9e0a000d2d58985b57b2984378fee41c09174324fe47b88e81d1"
+    sha256 cellar: :any,                 sonoma:         "4e5ad7bbd7039debb0f7a2b8ab8774e4652e698aa7d28a71ce62667c529c7c27"
+    sha256 cellar: :any,                 ventura:        "c8b57d7acc7618677023c3849095b7f89e6440b747ae589704d30a607e0c6d56"
+    sha256 cellar: :any,                 monterey:       "2f9cdc1f4b36b0fbc823aa4346775e739947033b9915ae59e7fb87c9ba0fbb58"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4ff015a79f1a78045a857c867794697ed48fbab97ae36ad785c62049074210d5"
   end
 
   def install
--- .github/deps/vips/lib/libgpg-error.rb
+++ .github/deps/vips/lib/libgpg-error.rb
@@ -1,8 +1,8 @@
 class LibgpgError < Formula
   desc "Common error values for all GnuPG components"
   homepage "https://www.gnupg.org/related_software/libgpg-error/"
-  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.48.tar.bz2"
-  sha256 "89ce1ae893e122924b858de84dc4f67aae29ffa610ebf668d5aa539045663d6f"
+  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.49.tar.bz2"
+  sha256 "8b79d54639dbf4abc08b5406fb2f37e669a2dec091dd024fb87dd367131c63a9"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -11,13 +11,13 @@ class LibgpgError < Formula
   end
 
   bottle do
-    sha256 arm64_sonoma:   "a0ba26a324b2b0ecd2346f1c1075a7cec1b73029ae92321b2271de1a254ceb42"
-    sha256 arm64_ventura:  "84627e30a47510d90f048ce09b41a4c85950444d93f59b586cbfa12b86663670"
-    sha256 arm64_monterey: "46968ed0dc58494cfbd33ebea402c0ad15d8515aa9d6b49655e32a513889c4d8"
-    sha256 sonoma:         "5fbd068a04c0aba00404f7b69c5fb2b0bed32eb31eff35981583968dba072485"
-    sha256 ventura:        "9333a88dbebd98b41a07f3d68fc23099c686a53498a86cc74355f74ee7b6eb8f"
-    sha256 monterey:       "e1a2e37e62d528b0d512524248d8682180947eb043437f37b99af9a361c829dd"
-    sha256 x86_64_linux:   "5669f61dcc25f047eab9eadd84fe279af4cba2eff2e726043d84ee1fdf3a53e7"
+    sha256 arm64_sonoma:   "7473b0f8035b06d01bd0770e830f194d9b96eb0b42071fde9fd8275a6c62439b"
+    sha256 arm64_ventura:  "bfc628bab49e64ecf7680ae451d49785063dc366f1d171414d28a102c68c313e"
+    sha256 arm64_monterey: "c250e817825c6b7e02cde2adac399397b805ea2cd7eadbcfe8271adc6dede6a0"
+    sha256 sonoma:         "2a0b65d124ca8a17b753429d1440062c3661c7a8db0d4aeb2974eaa3282d3879"
+    sha256 ventura:        "24b0a78e668d215395a3cef954988d842ff8d7991d9aa8bcd8ea26cc8115575a"
+    sha256 monterey:       "1feff61a050b82d59cb3d72355cd246e2f938713944ffdd44fff2651b4e77352"
+    sha256 x86_64_linux:   "225d361c850575dcf123790dfe952d005ceb67b57a33e23a1eccfb086aab81e2"
   end
 
   on_macos do
