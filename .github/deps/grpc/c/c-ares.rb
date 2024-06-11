9a0d166c74a229ef3ed9da71de31c6c9a7482776
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-11 10:05:37 +0000

    Update extension dependencies

--- .github/deps/grpc/c/c-ares.rb
+++ .github/deps/grpc/c/c-ares.rb
@@ -1,24 +1,10 @@
 class CAres < Formula
   desc "Asynchronous DNS library"
   homepage "https://c-ares.org/"
+  url "https://github.com/c-ares/c-ares/releases/download/v1.30.0/c-ares-1.30.0.tar.gz"
+  sha256 "4fea312112021bcef081203b1ea020109842feb58cd8a36a3d3f7e0d8bc1138c"
   license "MIT"
   head "https://github.com/c-ares/c-ares.git", branch: "main"
-
-  # Remove `stable` block when `dnsinfo.h` resource is no longer needed.
-  stable do
-    # Don't forget to change both instances of the version in the first mirror. (e.g. `cares-1_xy_z`)
-    url "https://c-ares.org/download/c-ares-1.29.0.tar.gz"
-    mirror "https://github.com/c-ares/c-ares/releases/download/cares-1_29_0/c-ares-1.29.0.tar.gz"
-    mirror "http://fresh-center.net/linux/misc/dns/c-ares-1.29.0.tar.gz"
-    mirror "http://fresh-center.net/linux/misc/dns/legacy/c-ares-1.29.0.tar.gz"
-    sha256 "0b89fa425b825c4c7bc708494f374ae69340e4d1fdc64523bdbb2750bfc02ea7"
-
-    # TODO: Remove at next release.
-    resource "dnsinfo.h" do
-      url "https://raw.githubusercontent.com/c-ares/c-ares/6bbdcf766eeab31b3c8f3e471fb6beceb18ff351/src/lib/thirdparty/apple/dnsinfo.h"
-      sha256 "9b8b3c820a6708f0add91887e8332e2700f7dfab6d9c2cf74866d4c2f26d58ea"
-    end
-  end
 
   livecheck do
     url :homepage
@@ -26,23 +12,18 @@ class CAres < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "50b8ff29e104224eb64084cea5119fba638e1455d4300d28febc731c83d6e540"
-    sha256 cellar: :any,                 arm64_ventura:  "02ceb19766f82e841a09f9ab43877b5a1cda9297f22e301625a7388b42c85be8"
-    sha256 cellar: :any,                 arm64_monterey: "809e8f0274735468d9cb268b9cf14bc1614212556571f86d8e8e65c6ced022a0"
-    sha256 cellar: :any,                 sonoma:         "c4b105e475ab425140af36ad950a6c6cabe472a16889023728f6c42bc5f22f0b"
-    sha256 cellar: :any,                 ventura:        "e36d47a8a81e36b4c6da725e909b91743dd52e57103891506de44f9764370c65"
-    sha256 cellar: :any,                 monterey:       "698e01cd2e616a944147ce7a1f9173efb0e1e869350589527dcedee06ba9bc29"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "77488d72926f8ea72ae6a5383f01389b9f25d7fac1297be30fc96cd20f81e6bb"
+    sha256 cellar: :any,                 arm64_sonoma:   "e304ec5f5386007c2554dbd3a92a1a340ca8d8fe84a0396dfc006694e6306ae8"
+    sha256 cellar: :any,                 arm64_ventura:  "2afb25bfdcc6a91c1fc6fe7b576692ec67e12dc96ba561385e287861aee99a0b"
+    sha256 cellar: :any,                 arm64_monterey: "f538ea0a8392e20cff0f3a3d9f7f56ba562b0ad57f68e4e9546ed8f3fd6dcc72"
+    sha256 cellar: :any,                 sonoma:         "5a6aa951ec2048cea52b1b070d45f06cb831de561c8833e9dc20888afe7d8045"
+    sha256 cellar: :any,                 ventura:        "e22da8c0e8dcf5b2c114b5d2be32b1a30ab632aeadc59fb3fc7ba4d1f3e90c15"
+    sha256 cellar: :any,                 monterey:       "87b091b809c673b125927e4e318d4e6749bb0e558269ab5d1dfcb2385c50bbdf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "999cd2bf4b15f8f00ec70bbaae381b9b89d5cd338de46d3150fdea1197c3ba36"
   end
 
   depends_on "cmake" => :build
 
   def install
-    if build.stable?
-      (buildpath/"src/lib/thirdparty/apple").install resource("dnsinfo.h")
-      odie "The `dnsinfo.h` resource should be removed!" if version > "1.29.0"
-    end
-
     args = %W[
       -DCARES_STATIC=ON
       -DCARES_SHARED=ON
@@ -69,6 +50,6 @@ class CAres < Formula
     system ENV.cc, "test.c", "-L#{lib}", "-lcares", "-o", "test"
     system "./test"
 
-    system "#{bin}/ahost", "127.0.0.1"
+    system bin/"ahost", "127.0.0.1"
   end
 end
