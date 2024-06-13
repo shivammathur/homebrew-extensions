9242ca9243e3b68535603a53be70b9833d0d6e41
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-13 04:03:28 +0000

    Update snmp@8.3

--- Formula/snmp@8.3.rb
+++ Formula/snmp@8.3.rb
@@ -8,8 +8,8 @@ class SnmpAT83 < AbstractPhpExtension
   init
   desc "Snmp PHP extension"
   homepage "https://github.com/php/php-src"
-  url "https://www.php.net/distributions/php-8.3.7.tar.xz"
-  sha256 "d53433c1ca6b2c8741afa7c524272e6806c1e895e5912a058494fea89988570a"
+  url "https://www.php.net/distributions/php-8.3.8.tar.xz"
+  sha256 "aea358b56186f943c2bbd350c9005b9359133d47e954cfc561385319ae5bb8d7"
   head "https://github.com/php/php-src.git", branch: "master"
   license "PHP-3.01"
 
