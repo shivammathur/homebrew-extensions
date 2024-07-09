e2ef39d99f65da03ddb886868be080e2abf1d549
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-09 10:05:30 +0000

    Update extension dependencies

--- .github/deps/vips/p/p11-kit.rb
+++ .github/deps/vips/p/p11-kit.rb
@@ -1,18 +1,18 @@
 class P11Kit < Formula
   desc "Library to load and enumerate PKCS#11 modules"
   homepage "https://p11-glue.freedesktop.org"
-  url "https://github.com/p11-glue/p11-kit/releases/download/0.25.3/p11-kit-0.25.3.tar.xz"
-  sha256 "d8ddce1bb7e898986f9d250ccae7c09ce14d82f1009046d202a0eb1b428b2adc"
+  url "https://github.com/p11-glue/p11-kit/releases/download/0.25.4/p11-kit-0.25.4.tar.xz"
+  sha256 "4c4153f81167444ff6d5e7ca118472ae607bd25c0cf6346fcc5dcc30451e97ce"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 arm64_sonoma:   "cd5ef135c54d2a312c17af15e9f3c807b3b37a65388b64a35a4d215b54745789"
-    sha256 arm64_ventura:  "f965f464d9c3b641003d924bcea428586ec8572dd0ed54f41b879ef727b4b4e9"
-    sha256 arm64_monterey: "3bc4bc733ac93bdb69cad61da77152e17758613736eddbd2b1518145a24efa21"
-    sha256 sonoma:         "c09253484c1237e942e0c91586422abae0b3af1c026bb5cce3bcd5900ad690cc"
-    sha256 ventura:        "c4b2c1001b5add01313ec51f2786b2744d13eb86bf13ef88f1fa4a581ef69bdb"
-    sha256 monterey:       "37d1d22a9b656be0423b9a410701dcc2d5ab12ec4b33bc0806a608a1e3680dbe"
-    sha256 x86_64_linux:   "4f7ca2105451e0561951b327254cb179505798e8b5c491e9e4ee9124b0855397"
+    sha256 arm64_sonoma:   "e901220d8fb3824f261dc9773131249cdab2ec10119c758ed70ab3f3d843d790"
+    sha256 arm64_ventura:  "d29d433e272014c612aba22210c730907b79302c3bb5864cffb094a760644746"
+    sha256 arm64_monterey: "ab751f6e400df153f1ecc7b91a250dce68eb94aa602a190184426d3fec7e7442"
+    sha256 sonoma:         "001cf44d0044a0d06e8bc18c06a3b658bf70eef0c5cbf738b4366b355090bbc4"
+    sha256 ventura:        "56ad10f3931ff9d4a2fd81b0a201dc3304ac2871fd167aee12a6aaa2ab5416e6"
+    sha256 monterey:       "96252329231c5a9ec2f46ae8cbd63674a9bd035028b7e7afa0dd8053994aeb97"
+    sha256 x86_64_linux:   "5c64614d0aabea975032407ff48bf5059130e039089fab4f9e32f78a939cb7e7"
   end
 
   head do
