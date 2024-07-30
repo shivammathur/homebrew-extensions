cb3613617f380d1f93970abdb6c3d74f32e0bfa7
DracoBlue <JanS@DracoBlue.de>
2024-07-30 19:31:21 +0200

    Create newrelic@8.2.rb

---
+++ Formula/newrelic@8.2.rb
@@ -0,0 +1,54 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Newrelic Extension
+class NewrelicAT82 < AbstractPhpExtension
+  init
+  desc "Newrelic PHP extension"
+  homepage "https://github.com/newrelic/newrelic-php-agent"
+  url "https://github.com/newrelic/newrelic-php-agent/archive/refs/tags/v10.15.0.4.tar.gz"
+  version "v10.15.0.4"
+  sha256 "6c27106e8fcd428b1f018fdf7c43513e2a4eb490"
+  head "https://github.com/newrelic/newrelic-php-agent.git", branch: "main"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  # for pcre_compile
+  depends_on "pcre"
+
+  # for aclocal + glibtoolize
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  # for the daemon
+  depends_on "go" => :build
+
+  def config_file_content
+    <<~EOS
+      [#{extension}]
+      #{extension_type}="#{module_path}"
+      newrelic.daemon.location="#{prefix}/daemon"
+      newrelic.daemon.address="/tmp/.newrelic82.sock"
+      newrelic.daemon.port="/tmp/.newrelic82.sock"
+      newrelic.logfile="/var/log/newrelic_php_agent.log"
+      newrelic.daemon.logfile="/var/log/newrelic_daemon.log"
+    EOS
+  rescue error
+    raise error
+  end
+
+  def install
+    inreplace "agent/php_autorum.c", "return (char*)emalloc(len);", "return (char*)emalloc((unsigned)len);"
+    inreplace "axiom/Makefile", "AXIOM_CFLAGS += -Wimplicit-fallthrough", "#AXIOM_CFLAGS += -Wimplicit-fallthrough"
+    system "make", "daemon"
+    system "make", "agent"
+    prefix.install "agent/modules/#{extension}.so"
+    prefix.install "bin/daemon"
+    write_config_file
+  end
+end
