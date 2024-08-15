e4a6c1958cadbc4de6b11353bd939cf4ff30cf6e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-15 08:09:27 +0000

    ast@8.4: update 1.1.2 bottle.

--- Formula/ast@8.4.rb
+++ Formula/ast@8.4.rb
@@ -15,13 +15,12 @@ class AstAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e89920f1f6313da5ac33938c79ca181cf21036d498d2316e6f3617b2773f353a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7be0349d0d7722dd640fc77033f28ff371e4ec07073c4a21663782967661d031"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4446fc33e684fc1798e0ed60e3c8dcb7bdd0bb7337148cf6d0d22d6e1eb56f85"
-    sha256 cellar: :any_skip_relocation, ventura:        "2a2ad9208ad1fae51ebf5eeb4144f0f52baabe581bf21ef23b746b5bbbde8080"
-    sha256 cellar: :any_skip_relocation, monterey:       "6b1d18dc99818161880eccf380ffd1aa2ccfd0a4b06ff29a7a6d3abe0102ec75"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b7ebee1d81147805146d51a8183a35207713f1ecd75459b7aa466d9d9d6b10c4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "95cc1b2c054b84be1025d237aa9e0c9682d38f507cf886b505868aa065760d94"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5dd00494f48175d679f212f8ceaeeba3b443fb16a3a862ba3b829de250062bea"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bad9cd28d84ec6b99569b732030e5afe1483b9a7fcdf78407d5179b6d234fad2"
+    sha256 cellar: :any_skip_relocation, ventura:        "28b22e574013f46c1cbd0fbb9ecdf2269dec3b1591064197fc3571ef2576d5f8"
+    sha256 cellar: :any_skip_relocation, monterey:       "ac6790198ddcf7b9715273bb2b067ad38308068f640f598eb681c7c8e3d2d797"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7760e6627ab2c26713302fdc22935a742f8a1e97052c7ca44866ca62640d695a"
   end
 
   def install
