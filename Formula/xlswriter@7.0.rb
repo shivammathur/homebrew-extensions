59d2dd356dece3073803605599418eb26a2cb68f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-29 04:01:59 +0000

    Update xlswriter@7.0

--- Formula/xlswriter@7.0.rb
+++ Formula/xlswriter@7.0.rb
@@ -8,8 +8,8 @@ class XlswriterAT70 < AbstractPhpExtension
   init
   desc "Xlswriter PHP extension"
   homepage "https://github.com/viest/php-ext-xlswriter"
-  url "https://pecl.php.net/get/xlswriter-1.5.5.tgz"
-  sha256 "4b8c8aacf48737c0d741a2401f4c495318ca18879e9bfcef5f698946ef28c671"
+  url "https://pecl.php.net/get/xlswriter-1.5.6.tgz"
+  sha256 "b05b58803ea4a45f51f8344e8b99b15aff6adb76e8ab4c0653b6bf188d3b315f"
   head "https://github.com/viest/php-ext-xlswriter.git"
   license "BSD-2-Clause"
 
