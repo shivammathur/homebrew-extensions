14d66efd6b225a88001e4bf5222bd1b0b968f8f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-15 22:20:34 +0000

    vld@8.2: update 0.18.0 bottle.

--- Formula/vld@8.2.rb
+++ Formula/vld@8.2.rb
@@ -15,6 +15,7 @@ class VldAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "a9b809251c0531e1d505f6d75bf4cb5ddc5631a549719d44c2b02f2821fcdcd8"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6b16639b64427a4baa80e815aa9909e924d6bfaec1cf3871ea720d3e86c9fe50"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "42bee283b517b2805318fc2b66073209157f6acc0c60f1363c9a36239c326c5d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2ce591f18eccfa6bd4a1f5540d23dc291affd1dacb7df3996797a620399d23aa"
