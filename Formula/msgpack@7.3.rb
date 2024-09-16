5373fc4c8a69472c530765ac3b631674ec6b7f5d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-16 03:33:50 +0000

    msgpack@7.3: update 2.2.0 bottle.

--- Formula/msgpack@7.3.rb
+++ Formula/msgpack@7.3.rb
@@ -16,6 +16,7 @@ class MsgpackAT73 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "fdc18c36ef7e214cf1c9169f13503b3720b2390eb659f431ec654565df6a52b6"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "f269f92bc0d8ca8612ea06f7de554978dab7694970c605bfb27559769ed7452f"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "e257ad913ecd03d149c99cf2f6f75b70865a9c152ac92cb5d471293a28867f05"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ff144079e62532b37fc368c6a0424901c2591d605cfd3d4d9d03d74d6fb6ef83"
