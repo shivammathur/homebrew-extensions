8f46bdf39ec90c9e297394d52958d131145494f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-16 03:33:48 +0000

    opentelemetry@8.1: update 1.0.0 bottle.

--- Formula/opentelemetry@8.1.rb
+++ Formula/opentelemetry@8.1.rb
@@ -15,6 +15,7 @@ class OpentelemetryAT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "dc13654f465a8c806b868f8a75a0b6e77e2c29cddba7157659773ff9296664f9"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "afb5edfd7ee380ca7a661bfd03c7fa897aafced5486245ce24b98fd7741a755c"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "626ad59e90e31c154cbe350149fb16d794c279b789997c0998c4cf076093542d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "679c3f2cd71101930a7876876975b66f2aa30cccc16b7ab35b42d520ad09c2b7"
