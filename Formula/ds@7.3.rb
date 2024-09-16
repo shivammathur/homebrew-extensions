f5a9c395dac93b16bfba15c64ca38c2e23d65ecb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-16 03:02:18 +0000

    ds@7.3: update 1.5.0 bottle.

--- Formula/ds@7.3.rb
+++ Formula/ds@7.3.rb
@@ -15,6 +15,7 @@ class DsAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "ef23cd70c3fef99ae0d8b7317a38f31b062becac65fc5265722c99099b10f468"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "69610d278f53e05fb2f3adf2661fae2d9c85be241298b87ac6ea2b6f8487a219"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "4c2793a888ee18bfb9673d798d97756e9b7baae69312b3e6ec2da42d9a254ec8"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4d2cebeda5979a0fea7088f73cca3d79d37b986a82810508b6e6c9c1a4cf8454"
