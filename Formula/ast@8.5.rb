4cc1f58071d3d7e82f861f0f793895218c5d5788
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:16:13 +0000

    Add ast@8.5

---
+++ Formula/ast@8.5.rb
@@ -0,0 +1,29 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Ast Extension
+class AstAT85 < AbstractPhpExtension
+  init
+  desc "Ast PHP extension"
+  homepage "https://github.com/nikic/php-ast"
+  url "https://pecl.php.net/get/ast-1.1.2.tgz"
+  sha256 "8742427ff7c07ba93f940968f7363972ea040d97d847da3b79b4283c2a369dea"
+  head "https://github.com/nikic/php-ast.git", branch: "master"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "ast-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-ast"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
