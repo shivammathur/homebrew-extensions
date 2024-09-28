41096d3d37a77e236c34ad43d351ab11d3170c64
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:17:31 +0000

    Add newrelic@8.5

---
+++ Formula/newrelic@8.5.rb
@@ -0,0 +1,54 @@
+# typed: false
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Newrelic Extension
+class NewrelicAT85 < AbstractPhpExtension
+  init
+  desc "Newrelic PHP extension"
+  homepage "https://github.com/newrelic/newrelic-php-agent"
+  url "https://github.com/newrelic/newrelic-php-agent/archive/refs/tags/v11.0.0.13.tar.gz"
+  version "v11.0.0.13"
+  sha256 "69f72541acf03e63a4d7d4a1053857b009279d1c526c9b68ed1338670a9e2cc0"
+  head "https://github.com/newrelic/newrelic-php-agent.git", branch: "main"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  # for pcre_compile
+  depends_on "pcre"
+
+  # for aclocal + glibtoolize
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  # for the daemon
+  depends_on "go" => :build
+
+  def config_file_content
+    <<~EOS
+      [#{extension}]
+      #{extension_type}="#{module_path}"
+      newrelic.daemon.location="#{prefix}/daemon"
+      newrelic.daemon.address="/tmp/.newrelic84.sock"
+      newrelic.daemon.port="/tmp/.newrelic84.sock"
+      newrelic.logfile="/var/log/newrelic_php_agent.log"
+      newrelic.daemon.logfile="/var/log/newrelic_daemon.log"
+    EOS
+  rescue error
+    raise error
+  end
+
+  def install
+    inreplace "agent/config.m4", "-l:libprotobuf-c.a", "-lprotobuf-c"
+    inreplace "axiom/Makefile", "AXIOM_CFLAGS += -Wimplicit-fallthrough", "#AXIOM_CFLAGS += -Wimplicit-fallthrough"
+    system "make", "daemon"
+    system "make", "agent"
+    prefix.install "agent/modules/#{extension}.so"
+    prefix.install "bin/daemon"
+    write_config_file
+  end
+end
