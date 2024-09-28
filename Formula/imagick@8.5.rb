50f0e4ddc29cd0ece1f3a9f4015d8dc63c05ebba
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:16:56 +0000

    Add imagick@8.5

---
+++ Formula/imagick@8.5.rb
@@ -0,0 +1,31 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Imagick Extension
+class ImagickAT85 < AbstractPhpExtension
+  init
+  desc "Imagick PHP extension"
+  homepage "https://github.com/Imagick/imagick"
+  url "https://pecl.php.net/get/imagick-3.7.0.tgz"
+  sha256 "5a364354109029d224bcbb2e82e15b248be9b641227f45e63425c06531792d3e"
+  head "https://github.com/Imagick/imagick.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "imagemagick"
+
+  def install
+    ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
+    Dir.chdir "imagick-#{version}"
+    safe_phpize
+    system "./configure"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+  end
+end
