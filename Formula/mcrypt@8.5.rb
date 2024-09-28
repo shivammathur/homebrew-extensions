f8c02b6f260a25df2d3f9f026ef62e0d4dbe12cf
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:17:11 +0000

    Add mcrypt@8.5

---
+++ Formula/mcrypt@8.5.rb
@@ -0,0 +1,56 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Mcrypt Extension
+class McryptAT85 < AbstractPhpExtension
+  init
+  desc "Mcrypt PHP extension"
+  homepage "https://github.com/php/pecl-encryption-mcrypt"
+  url "https://pecl.php.net/get/mcrypt-1.0.7.tgz"
+  sha256 "12ea2fbbf2e2efbe790a12121f77bf096c8b84cef81d0216bec00d56e5badef4"
+  head "https://github.com/php/pecl-encryption-mcrypt.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "automake" => :build
+  depends_on "libtool"
+
+  resource "libmcrypt" do
+    url "https://downloads.sourceforge.net/project/mcrypt/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz"
+    sha256 "e4eb6c074bbab168ac47b947c195ff8cef9d51a211cdd18ca9c9ef34d27a373e"
+  end
+
+  def install
+    # Work around configure issues with Xcode 12
+    ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
+    ENV.append "CFLAGS", "-Wno-implicit-int"
+
+    resource("libmcrypt").stage do
+      # Workaround for ancient config files not recognising aarch64 macos.
+      %w[config.guess config.sub].each do |fn|
+        cp "#{Formula["automake"].opt_prefix}/share/automake-#{Formula["automake"].version.major_minor}/#{fn}", fn
+      end
+
+      # Avoid flat_namespace usage on macOS
+      inreplace "./configure", "${wl}-flat_namespace ${wl}-undefined ${wl}suppress", "" if OS.mac?
+
+      system "./configure", "--prefix=#{prefix}",
+                            "--mandir=#{man}"
+      system "make", "install"
+    end
+
+    Dir.chdir "mcrypt-#{version}"
+    inreplace "mcrypt.c", "ext/standard/php_rand.h", "ext/random/php_random.h"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--with-mcrypt=#{prefix}"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
