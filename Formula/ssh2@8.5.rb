b71e8d356ab0d2b872c4235708a2ecd9cba4b2c1
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:18:22 +0000

    Add ssh2@8.5

---
+++ Formula/ssh2@8.5.rb
@@ -0,0 +1,34 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Ssh2 Extension
+class Ssh2AT85 < AbstractPhpExtension
+  init
+  desc "Ssh2 PHP extension"
+  homepage "https://github.com/php/pecl-networking-ssh2"
+  url "https://pecl.php.net/get/ssh2-1.4.1.tgz"
+  sha256 "7bca5b23f731db9d8ed0aea5db9bb15da8ff133b0fbba96102b82e95da4d8764"
+  head "https://github.com/php/pecl-networking-ssh2.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "libssh2"
+
+  def install
+    args = %W[
+      --with-ssh2=shared,#{Formula["libssh2"].opt_prefix}
+    ]
+    Dir.chdir "ssh2-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
