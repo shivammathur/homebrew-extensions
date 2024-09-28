36f99ac1402074aa46751a0d7057b6a2d8dcc923
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 11:59:36 +0000

    Add raphf@8.5

---
+++ Formula/raphf@8.5.rb
@@ -0,0 +1,31 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Raphf Extension
+class RaphfAT85 < AbstractPhpExtension
+  init
+  desc "Raphf PHP extension"
+  homepage "https://github.com/m6w6/ext-raphf"
+  url "https://pecl.php.net/get/raphf-2.0.1.tgz"
+  sha256 "da3566db17422e5ef08b7ff144162952aabc14cb22407cc6b1d2a2d095812bd0"
+  head "https://github.com/m6w6/ext-raphf.git", branch: "master"
+  license "BSD-2-Clause"
+  revision 1
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "raphf-#{version}"
+    patch_spl_symbols
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-raphf"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
