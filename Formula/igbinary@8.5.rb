f414691a020f594c7dc32dce60404551811f0e05
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 11:59:25 +0000

    Add igbinary@8.5

---
+++ Formula/igbinary@8.5.rb
@@ -0,0 +1,30 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Igbinary Extension
+class IgbinaryAT85 < AbstractPhpExtension
+  init
+  desc "Igbinary PHP extension"
+  homepage "https://github.com/igbinary/igbinary"
+  url "https://github.com/igbinary/igbinary/archive/3.2.16.tar.gz"
+  sha256 "941f1cf2ccbecdc1c221dbfae9213439d334be5d490a2f3da2be31e8a00b0cdb"
+  head "https://github.com/igbinary/igbinary.git", branch: "master"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    patch_spl_symbols
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-igbinary"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    Dir.chdir "src/php7"
+    add_include_files
+  end
+end
