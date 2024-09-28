7b9fd353a92d75630394745e2e8e7ffa2f20e934
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-28 14:04:57 +0000

    ast@8.5: update 1.1.2 bottle.

--- Formula/ast@8.5.rb
+++ Formula/ast@8.5.rb
@@ -15,6 +15,11 @@ class AstAT85 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "840f6c02d64cc00be7109229c21b8da6b4e8fe74b7b208374e2ea8656356e94f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "328be49cf3d77204f73fc8412ec5329f3b28e397dda0d76d5cbc1ddd20c2a860"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "daeaa7a04a721e3b313cedc2b34679a59d6a3cfac85d57b652fc9285887f95ba"
+    sha256 cellar: :any_skip_relocation, ventura:       "9e4c2d241a804e15acbac6dcaa543d419187d6ba30437be41cd66d6cfb7edc14"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3e534f9c59a93b897fecc78fa321d737895552ba17e9ac1dd2e590865de9faab"
   end
 
   def install
