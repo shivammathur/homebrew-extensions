2012386db2c3d668d5589b9b54822c7600de2c34
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-28 13:27:32 +0000

    mailparse@8.5: update 3.1.6 bottle.

--- Formula/mailparse@8.5.rb
+++ Formula/mailparse@8.5.rb
@@ -15,6 +15,11 @@ class MailparseAT85 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ca75bc7ba5b28d416f73a491cf1cdff49dc1bb824e8b5fa0a55d8c553299dcb3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "998036a3c74b8c9dfa69f346cf1dc0943564fdbf403ad66949d541f2ee696e06"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "61fc5a68f4912e9f9b6acaa2ac225dc2891060609fab9b03a456461cd30a407f"
+    sha256 cellar: :any_skip_relocation, ventura:       "a1f9bd93b974f2765f27b167dff54ff83e6a8a99fbb1ca3bcbf06771066eeeab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9f06c0084a0093aa885252456db8439df920b6b2f2da590d69d263e0e57f8b0"
   end
 
   def install
