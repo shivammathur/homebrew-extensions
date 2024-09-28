7eb68bf35c3a514b16e6e537d08ce823c232762c
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:16:46 +0000

    Add gnupg@8.5

---
+++ Formula/gnupg@8.5.rb
@@ -0,0 +1,34 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Gnupg Extension
+class GnupgAT85 < AbstractPhpExtension
+  init
+  desc "Gnupg PHP extension"
+  homepage "https://github.com/php-gnupg/php-gnupg"
+  url "https://pecl.php.net/get/gnupg-1.5.1.tgz"
+  sha256 "a9906f465ab2343cb2f3127ee209c72760238745c34878d9bbc1576486219252"
+  head "https://github.com/php-gnupg/php-gnupg.git", branch: "master"
+  license "BSD-2-Clause"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "gpgme"
+
+  def install
+    args = %W[
+      --with-gnupg=#{Formula["gpgme"].opt_prefix}
+    ]
+    Dir.chdir "gnupg-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
