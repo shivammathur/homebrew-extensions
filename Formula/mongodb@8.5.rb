fc5b92e0e8affdad8bb730c09b9b86474558ae4d
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:17:26 +0000

    Add mongodb@8.5

---
+++ Formula/mongodb@8.5.rb
@@ -0,0 +1,35 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Mongodb Extension
+class MongodbAT85 < AbstractPhpExtension
+  init
+  desc "Mongodb PHP extension"
+  homepage "https://github.com/mongodb/mongo-php-driver"
+  url "https://pecl.php.net/get/mongodb-1.20.0.tgz"
+  sha256 "01e87973fe7e54aac52054ec4a99cdd439ed5c01f7e5b8ea0a57031850d8e75a"
+  head "https://github.com/mongodb/mongo-php-driver.git", branch: "v1.x"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "icu4c"
+  depends_on "openssl@3"
+  depends_on "snappy"
+  depends_on "zlib"
+  depends_on "zstd"
+
+  def install
+    Dir.chdir "mongodb-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-mongodb"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
