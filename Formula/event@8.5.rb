bd1929f07712dc78b6232106079018547cff3903
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:16:30 +0000

    Add event@8.5

---
+++ Formula/event@8.5.rb
@@ -0,0 +1,40 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Event Extension
+class EventAT85 < AbstractPhpExtension
+  init
+  desc "Event PHP extension"
+  homepage "https://bitbucket.org/osmanov/pecl-event"
+  url "https://pecl.php.net/get/event-3.1.4.tgz"
+  sha256 "5c4caa73bc2dceee31092ff9192139df28e9a80f1147ade0dfe869db2e4ddfd3"
+  head "https://bitbucket.org/osmanov/pecl-event.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "libevent"
+  depends_on "openssl@3"
+
+  def install
+    args = %W[
+      --with-event-core
+      --with-event-extra
+      --with-event-openssl
+      --enable-event-sockets
+      --with-openssl-dir=#{Formula["openssl@3"].opt_prefix}
+      --with-event-libevent-dir=#{Formula["libevent"].opt_prefix}
+    ]
+    Dir.chdir "event-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
