88cb76c3919bb6776da3b3535ec1cbd91de26be3
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:18:52 +0000

    Add zmq@8.5

---
+++ Formula/zmq@8.5.rb
@@ -0,0 +1,47 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Zmq Extension
+class ZmqAT85 < AbstractPhpExtension
+  init
+  desc "Zmq PHP extension"
+  homepage "https://github.com/zeromq/php-zmq"
+  url "https://github.com/zeromq/php-zmq/archive/43464c42a6a47efdf8b7cab03c62f1622fb5d3c6.tar.gz"
+  sha256 "cbf1d005cea35b9215e2830a0e673b2edd8b526203f731de7a7bf8f590a60298"
+  version "1.1.3"
+  head "https://github.com/zeromq/php-zmq.git", branch: "master"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "zeromq"
+
+  on_macos do
+    depends_on "czmq"
+  end
+
+  def install
+    ENV["PKG_CONFIG"] = "#{HOMEBREW_PREFIX}/bin/pkg-config"
+    ENV.append "PKG_CONFIG_PATH", "#{Formula["libsodium"].opt_prefix}/lib/pkgconfig"
+    args = %W[
+      prefix=#{prefix}
+    ]
+    on_macos do
+      args << "--with-czmq=#{Formula["czmq"].opt_prefix}"
+    end
+    inreplace "package.xml", "@PACKAGE_VERSION@", version.to_s
+    inreplace "php-zmq.spec", "@PACKAGE_VERSION@", version.to_s
+    inreplace "php_zmq.h", "@PACKAGE_VERSION@", version.to_s
+    patch_spl_symbols
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
