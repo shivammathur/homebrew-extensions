d578d4528909f29cbeda3836c7b212f017b15893
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:18:12 +0000

    Add snmp@8.5

---
+++ Formula/snmp@8.5.rb
@@ -0,0 +1,37 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Snmp Extension
+class SnmpAT85 < AbstractPhpExtension
+  init
+  desc "Snmp PHP extension"
+  homepage "https://github.com/php/php-src"
+  url "https://github.com/php/php-src/archive/db545767e57d74e7ec0e1817a208d0d44e0932e6.tar.gz?commit=db545767e57d74e7ec0e1817a208d0d44e0932e6"
+  version "8.4.0"
+  sha256 "7f791c3bff47dd3e9254b741444fbdb29085220e20466a2ee61ae9e87c87a3c6"
+  head "https://github.com/php/php-src.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "net-snmp"
+  depends_on "openssl@3"
+
+  def install
+    args = %W[
+      --with-snmp=#{Formula["net-snmp"].opt_prefix}
+      --with-openssl-dir=#{Formula["openssl@3"].opt_prefix}
+    ]
+    Dir.chdir "ext/#{extension}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
