d34d13909cefb36416b0cefea2a729d62bdda512
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:16:20 +0000

    Add couchbase@8.5

---
+++ Formula/couchbase@8.5.rb
@@ -0,0 +1,43 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Couchbase Extension
+class CouchbaseAT85 < AbstractPhpExtension
+  init
+  desc "Couchbase PHP extension"
+  homepage "https://github.com/couchbase/couchbase-php-client"
+  url "https://pecl.php.net/get/couchbase-4.2.3.tgz"
+  sha256 "17cc469d0eb509324cc43455fd84bf2d9ed9615b75bff2b152d67d77a50368e8"
+  head "https://github.com/couchbase/couchbase-php-client.git", branch: "main"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "openssl@3"
+  depends_on "zlib"
+
+  on_linux do
+    depends_on "gcc" # C++17
+  end
+
+  fails_with gcc: "7"
+
+  def install
+    ENV["OPENSSL_ROOT_DIR"] = "#{Formula["openssl@3"]}.opt_prefix"
+    ENV["CURL_SSL_BACKEND"] = "SecureTransport"
+    Dir.chdir "couchbase-#{version}"
+    safe_phpize
+    inreplace "configure",
+      "EXTENSION_DIR=$($PHP_CONFIG --extension-dir 2>/dev/null)",
+      "EXTENSION_DIR=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-couchbase"
+    system "make"
+    system "make", "phpincludedir=#{include}/php", "install"
+    write_config_file
+  end
+end
