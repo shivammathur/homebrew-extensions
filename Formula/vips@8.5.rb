ea6dc17609b13c42fc4b913d5f1e4652aa2565a7
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:18:32 +0000

    Add vips@8.5

---
+++ Formula/vips@8.5.rb
@@ -0,0 +1,34 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Vips Extension
+class VipsAT85 < AbstractPhpExtension
+  init
+  desc "Vips PHP extension"
+  homepage "https://github.com/libvips/php-vips-ext"
+  url "https://pecl.php.net/get/vips-1.0.13.tgz"
+  sha256 "4e655843e5ee8150c927c10853dfa0d2a3b924bc2453ed8fb5e5a2a90e686f8f"
+  head "https://github.com/libvips/php-vips-ext.git", branch: "master"
+  license "MIT"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "vips"
+
+  def install
+    args = %W[
+      --with-vips=#{Formula["vips"].opt_prefix}
+    ]
+    Dir.chdir "vips-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
