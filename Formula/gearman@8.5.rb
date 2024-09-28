c0bd92ad54bafed59df05f316821f9a10b9af3a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-28 13:27:30 +0000

    gearman@8.5: update 2.1.2 bottle.

--- Formula/gearman@8.5.rb
+++ Formula/gearman@8.5.rb
@@ -15,6 +15,11 @@ class GearmanAT85 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_sequoia: "06119717494d2b5b442d4b38a50559deb506ea01ffa6fcf2d52c95c37568d484"
+    sha256 cellar: :any,                 arm64_sonoma:  "d53a3c2e13f2cdc2da27fe1158f3517a1fdabbb9fc97c9073e76d95face767d4"
+    sha256 cellar: :any,                 arm64_ventura: "db17a9c8630b1ed9bf340a22054c3411fe1a5ebded141d39e534c9ab968facd7"
+    sha256 cellar: :any,                 ventura:       "ce08101678698ae072e7f03d8a498be1c9ec4098f54447dd3a9510188ad3ffbb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a3caacd75cd1bba002e05a6065148e0eb32901fb2017aeb9f7505dc8f7d62b5"
   end
 
   depends_on "gearman"
