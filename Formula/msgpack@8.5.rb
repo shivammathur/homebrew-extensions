04b23207984ded5e72fede4c23a7b42c7a6a501e
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 11:59:31 +0000

    Add msgpack@8.5

---
+++ Formula/msgpack@8.5.rb
@@ -0,0 +1,29 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Msgpack Extension
+class MsgpackAT85 < AbstractPhpExtension
+  init
+  desc "Msgpack PHP extension"
+  homepage "https://github.com/msgpack/msgpack-php"
+  url "https://pecl.php.net/get/msgpack-2.2.0RC1.tgz"
+  sha256 "55ad876ba2340233450fc246978229772078f16d83edbab0f6ccb2cc4b7ca63f"
+  head "https://github.com/msgpack/msgpack-php.git", branch: "master"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "msgpack-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--with-msgpack"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
