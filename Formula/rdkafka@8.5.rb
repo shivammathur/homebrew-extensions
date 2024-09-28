47b58822e1eb94fde661a28b11c11cd7e2b522a7
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 12:18:01 +0000

    Add rdkafka@8.5

---
+++ Formula/rdkafka@8.5.rb
@@ -0,0 +1,32 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Rdkafka Extension
+class RdkafkaAT85 < AbstractPhpExtension
+  init
+  desc "Rdkafka PHP extension"
+  homepage "https://github.com/arnaud-lb/php-rdkafka"
+  url "https://pecl.php.net/get/rdkafka-6.0.3.tgz"
+  sha256 "12eaab976d49697e31f1638b47889eb5ec61adb758708941112b157f8ec7dd48"
+  head "https://github.com/arnaud-lb/php-rdkafka.git", branch: "6.x"
+  license "MIT"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "librdkafka"
+
+  def install
+    Dir.chdir "rdkafka-#{version}"
+    patch_spl_symbols
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--with-rdkafka=#{Formula["librdkafka"].opt_prefix}"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
