193ee36c6a679a20229ccb0df3f8809eb741fa06
Shivam Mathur <shivam_jpr@hotmail.com>
2024-09-28 11:59:42 +0000

    Add psr@8.5

---
+++ Formula/psr@8.5.rb
@@ -0,0 +1,29 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+class PsrAT85 < AbstractPhpExtension
+  init
+  desc "PHP extension providing the accepted PSR interfaces "
+  homepage "https://github.com/jbboehr/php-psr"
+  url "https://pecl.php.net/get/psr-1.2.0.tgz"
+  sha256 "9c3d2a0d9770916d86e2bc18dfe6513ad9b2bfe00f0d03c1531ef403bee38ebe"
+  head "https://github.com/jbboehr/php-psr.git", branch: "master"
+  license "BSD-2-Clause"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "pcre"
+
+  def install
+    Dir.chdir "psr-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-psr"
+    system "make"
+    prefix.install "modules/psr.so"
+    write_config_file
+  end
+end
