0ba7dac2338241c9ec21050299de0f721b213b3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-04 14:39:07 +0000

    zstd@8.4: add 0.13.3 bottle.

--- Formula/zstd@8.4.rb
+++ Formula/zstd@8.4.rb
@@ -13,6 +13,15 @@ class ZstdAT84 < AbstractPhpExtension
   head "https://github.com/kjdev/php-ext-zstd.git", branch: "master"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_sequoia: "99c7a791e4b913772751651c7c984bffc5ef3e1723496d37c2258c37ef15d1c1"
+    sha256 cellar: :any,                 arm64_sonoma:  "ae00677ab44d1da81f9ee32425e9c0f23f9a41a0873b38d9f1e3fc41c48ae6ef"
+    sha256 cellar: :any,                 arm64_ventura: "c17e7fdda31cf29805faf388bb45b4793468a56361c711f9cd986de56fe41566"
+    sha256 cellar: :any,                 ventura:       "775bf939cc00ead8d5932333c298bf10c16c7eebb7dd800558ae3077e7d0bb27"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "02bb8e1ea179622e2af54570f8ca2f09ee86eab9bc04ccc78f8dc91d8194428f"
+  end
+
   depends_on "zstd"
 
   def install
