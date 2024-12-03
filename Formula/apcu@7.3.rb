af5f43eaf54ef28f76dad2a1868ac619712bc250
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-03 05:26:11 +0000

    apcu@7.3: update 5.1.24 bottle.

--- Formula/apcu@7.3.rb
+++ Formula/apcu@7.3.rb
@@ -15,13 +15,11 @@ class ApcuAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "ab3ae5643272754a0dfe511f7ab387d1806532f629f32ad88fd2122aa16d67cd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "7f8ebd05c7b90d07f5079c1cfe750ee1d41ec064098c873089ad1fe79c57c7e4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4f2de89f852372b524c747ab77d1295ce21d23f2b421fac3cb9c1f1b9b92a284"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c3ad9e9ad801ad88734cfc62f1ba258a6cb0726ba9792123c1ffc2253d1b1c82"
-    sha256 cellar: :any_skip_relocation, ventura:        "17c89f9249594a0fb6750fb5981848ced72fab6f6333c27760891476487c8823"
-    sha256 cellar: :any_skip_relocation, monterey:       "3e1af74be6956aa68c090d08b7a0c81db8148887c0bf677816847c29e6472ec0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9e09281795549560f94a5e0e23a12754c48b6babb81eba5ab76dd01422455d66"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "54600d37d9c774b07856fdfbc91530c425c47c37c79d9df6d94abe489acb7d83"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f6a8773c42202f592e2bedec96de68fba1b2bb35a6a101f2c90904f9e2792e19"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "bddb75de09f4ff322508a41ece916af7050485e52a2a5ffea220008f749783be"
+    sha256 cellar: :any_skip_relocation, ventura:       "dccd2fdb430afc61a508a5b8172984fbf778551bbc196001a558d204dfbd4b50"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8b1629500bb63e69d38c9df789111fbf8ed1a2bb6ffd91f4da1f29ac31319f7c"
   end
 
   def install
