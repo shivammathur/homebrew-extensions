b305f746e1f8b1e8b78e1ccaac4610ba0a511c5a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-03 11:53:11 +0000

    uuid@8.4: update 1.2.1 bottle.

--- Formula/uuid@8.4.rb
+++ Formula/uuid@8.4.rb
@@ -15,12 +15,11 @@ class UuidAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 4
-    sha256 cellar: :any,                 arm64_sequoia: "afc8657037ce601f9f40fb44f7678ecd34d39d45fafef94cd10a0806d594e58a"
-    sha256 cellar: :any,                 arm64_sonoma:  "3684106b3f9f388058e01317ffbff3264e4b871e8f9e85578fc81ba5941ce83c"
-    sha256 cellar: :any,                 arm64_ventura: "d3ab221d345dc260dcf88915070213df87f58b75ec9bf2b0c4365e71d50df0a0"
-    sha256 cellar: :any,                 ventura:       "dcc3e9945ebe665aaf497698f34f8ad7d960be14b696acc3c15f4fbe0b840273"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "515ecf7198587685f2ccce1ffbc69f4d2e2904c6d9f0c12478191fda3d8723dd"
+    sha256 cellar: :any,                 arm64_sequoia: "55e3b7771326349e3f34c008767e272e1a77310b14785d8e37a6ead2edf977be"
+    sha256 cellar: :any,                 arm64_sonoma:  "055d50642403a53c32e79e1863c859696b2e004d2724c32fb1c550d13510cf36"
+    sha256 cellar: :any,                 arm64_ventura: "fe6c17f93195d4a5b554c942a74169786a887fb7e9f31ed63561751bb9843821"
+    sha256 cellar: :any,                 ventura:       "1aa5f0721899a535fb755c60da82f0ac2d5bbdd59efbf03c542e5366a31628a6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "19904d963dfca6dfdd8938d64958136093f9775ce78995e85aefc1a29921e31c"
   end
 
   def uuid_dependency
