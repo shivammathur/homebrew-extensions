fbcf4c0aeca3fbc8191c226e1b6665e26924d874
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-03 08:12:35 +0000

    rdkafka@7.4: update 6.0.5 bottle.

--- Formula/rdkafka@7.4.rb
+++ Formula/rdkafka@7.4.rb
@@ -15,15 +15,11 @@ class RdkafkaAT74 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 2
-    sha256 cellar: :any,                 arm64_sequoia:  "2fb0f5644ab52ca7050f8e0f7633b4cefc16776c77bb641373a8da0034cee1da"
-    sha256 cellar: :any,                 arm64_ventura:  "2c4032ca5e1b8bacd9f75030f201fe3737bca587ef59ac3a34f6da8138ba4b5c"
-    sha256 cellar: :any,                 arm64_monterey: "3ad7447c7aee50517a652e4e886ffda48a876eef3e9976af6b5c9f4d1c7c6d70"
-    sha256 cellar: :any,                 arm64_big_sur:  "cfde739ffc7adf1cb64c857e9b1ad8aaebfff2cccd724129310d9a918d6c892a"
-    sha256 cellar: :any,                 ventura:        "1d015d43787019caf70695e14e3e38a2a47d420531a55492e37d51ea4e3fe6ea"
-    sha256 cellar: :any,                 monterey:       "3f4e3f312e8139e271e50e534afd4e0c78ba37f0d24d4b2179371ffc0d9f9517"
-    sha256 cellar: :any,                 big_sur:        "fd5b631496990f6603b1cea5c1ff2456961db91fb03e72e5907f67b2920c7f50"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4e8d1850f7513083bc240f2867779238d6d3285de94d1998cbc1da74c6d8a3b5"
+    sha256 cellar: :any,                 arm64_sequoia: "8c356d703627baed3be62828d7b9fd1e2eafd5992013feb2ecb27828991ed5e4"
+    sha256 cellar: :any,                 arm64_sonoma:  "76aafba2831ac31d3384b5ff12d438b091be137396baca05e107bbbae708ef83"
+    sha256 cellar: :any,                 arm64_ventura: "bf08fe7125091c5009acf977b1de7389116799ebfa174b006bdb2e91f533751b"
+    sha256 cellar: :any,                 ventura:       "0de531e8b3b7f43788a9b57e86e44528bf8c0b2de5b1f89170536d7ecbe83314"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b24068e6a1a4391bbb0fc132a2362dac84c68ea22d84a2f22d92c846d0f0bb9"
   end
 
   depends_on "librdkafka"
