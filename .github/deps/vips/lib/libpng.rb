885bb31ce88d3e038d8ff0be6bba711fc83a558a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-09 10:06:35 +0000

    Update extension dependencies

--- .github/deps/vips/lib/libpng.rb
+++ .github/deps/vips/lib/libpng.rb
@@ -1,9 +1,9 @@
 class Libpng < Formula
   desc "Library for manipulating PNG images"
   homepage "http://www.libpng.org/pub/png/libpng.html"
-  url "https://downloads.sourceforge.net/project/libpng/libpng16/1.6.44/libpng-1.6.44.tar.xz"
-  mirror "https://sourceforge.mirrorservice.org/l/li/libpng/libpng16/1.6.44/libpng-1.6.44.tar.xz"
-  sha256 "60c4da1d5b7f0aa8d158da48e8f8afa9773c1c8baa5d21974df61f1886b8ce8e"
+  url "https://downloads.sourceforge.net/project/libpng/libpng16/1.6.45/libpng-1.6.45.tar.xz"
+  mirror "https://sourceforge.mirrorservice.org/l/li/libpng/libpng16/1.6.45/libpng-1.6.45.tar.xz"
+  sha256 "926485350139ffb51ef69760db35f78846c805fef3d59bfdcb2fba704663f370"
   license "libpng-2.0"
 
   livecheck do
@@ -12,12 +12,12 @@ class Libpng < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "97e7780fa459489f57043430f53660e81c4bab4c0bd30b3c021ff8e1d1ae27ea"
-    sha256 cellar: :any,                 arm64_sonoma:  "04c7ef0bb97718b8388e93e0e0f6a3e361da422b7f63f2a1f2bbb8ddb92da119"
-    sha256 cellar: :any,                 arm64_ventura: "f17614f1c903db5ce208c3f78c3181377f121801fbeec50a53920591f0de4d34"
-    sha256 cellar: :any,                 sonoma:        "3867ac12f43852e8e4b028b7cfbae1c7bbc2a4e3c36e7d7a7b1690c40392fd6d"
-    sha256 cellar: :any,                 ventura:       "68f13a9e3ee84c0c50cb7d6d9d06a65287b3dcbc3baeb42ee7d2bcc1884a73d9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf958f9647a35c402c463e78273a1bcd6e0dac715c1e949b03e876f2224d51e4"
+    sha256 cellar: :any,                 arm64_sequoia: "2d9fddec7ff85ea205bf37bcba29116f37b581da9eb90c5bb8beda489f7d6105"
+    sha256 cellar: :any,                 arm64_sonoma:  "ceae05fe7def37733cece3f2e459a06ca2dd11b13a70fbd0cc024e3839f743a4"
+    sha256 cellar: :any,                 arm64_ventura: "b5d796dfbbc23b292221eb2d5b561bb916d1994c90f788e9049ffe32ef5d27f9"
+    sha256 cellar: :any,                 sonoma:        "c296b3b866794ded0c5f87964ebe637ad8904d04c45972f1a1867afd4b37e289"
+    sha256 cellar: :any,                 ventura:       "0a59a94ee138a9903a2cf0f7a06b448d267647f245dec672ae56c92d27fd6951"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7d313004e4148066ac5c5f78cd7aafa15737103d8ec59297e8fed2c5c417755d"
   end
 
   head do
--- .github/deps/vips/n/nettle.rb
+++ .github/deps/vips/n/nettle.rb
@@ -1,20 +1,18 @@
 class Nettle < Formula
   desc "Low-level cryptographic library"
   homepage "https://www.lysator.liu.se/~nisse/nettle/"
-  url "https://ftp.gnu.org/gnu/nettle/nettle-3.10.tar.gz"
-  mirror "https://ftpmirror.gnu.org/nettle/nettle-3.10.tar.gz"
-  sha256 "b4c518adb174e484cb4acea54118f02380c7133771e7e9beb98a0787194ee47c"
+  url "https://ftp.gnu.org/gnu/nettle/nettle-3.10.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/nettle/nettle-3.10.1.tar.gz"
+  sha256 "b0fcdd7fc0cdea6e80dcf1dd85ba794af0d5b4a57e26397eee3bc193272d9132"
   license any_of: ["GPL-2.0-or-later", "LGPL-3.0-or-later"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia:  "8dd077a363c69d5d31968c7cfbc79dd5ec856b58957bc081f6f38632d59029cf"
-    sha256 cellar: :any,                 arm64_sonoma:   "79f26a53dfc0202c437153961d441a8793fb651d1eb61930f1558d139cdfd99a"
-    sha256 cellar: :any,                 arm64_ventura:  "cc13fe4481bb5c606b811561da643eace9e0da7f30de62ced74811659b8bb41d"
-    sha256 cellar: :any,                 arm64_monterey: "8be68cdd3cc9b2df465840fd843b50e6bb10fda11c5711e50f002bb31267cb1f"
-    sha256 cellar: :any,                 sonoma:         "2638a7bfcca9dbaa6a935f7ae80aed9d3f1064b64189a0f3364f4b20f4a9d341"
-    sha256 cellar: :any,                 ventura:        "372ad197aa7857e6ac863471ae301cdc13f313b34397309b7e1a27ca5b104ba0"
-    sha256 cellar: :any,                 monterey:       "c04364b7c2beb64a5c7d2ba645749e07e256b7c5d30968cfa2722c289be24542"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "05101c1f60cf7b267464af60a6814404e32691af7630e4ea23ed5b85ae1257a7"
+    sha256 cellar: :any,                 arm64_sequoia: "df8b22b1a4deeafeae7f4afe13cd7403a3700c7779715916c43a45fad7ca5aaa"
+    sha256 cellar: :any,                 arm64_sonoma:  "87c602d6924a588d6fb7164f714cc3f29a63c6edd83ffb6b437faccc263133b8"
+    sha256 cellar: :any,                 arm64_ventura: "10243a9374699001a49008a6856904821a75e9a644ab49b9c2ac66c22ac31aa5"
+    sha256 cellar: :any,                 sonoma:        "6e502038a53a443864edeb85e3423986ec5c383c0fd8c3061ba95af71d369744"
+    sha256 cellar: :any,                 ventura:       "7d924af8b68729fbd75a279173b03da71219ee53c8c660198360dd4c8c0cde59"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8aeaad58a40d92dc7e23b80d7b1663ece6d7a46d9b919a52c4553cbed9a9e214"
   end
 
   depends_on "gmp"
