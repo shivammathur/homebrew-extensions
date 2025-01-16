f6668b7358ed8b3dbd948b2d4d285985b28fa735
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-16 10:05:56 +0000

    Update extension dependencies

--- .github/deps/couchbase/lib/libuv.rb
+++ .github/deps/couchbase/lib/libuv.rb
@@ -1,8 +1,8 @@
 class Libuv < Formula
   desc "Multi-platform support library with a focus on asynchronous I/O"
   homepage "https://libuv.org"
-  url "https://github.com/libuv/libuv/archive/refs/tags/v1.49.2.tar.gz"
-  sha256 "388ffcf3370d4cf7c4b3a3205504eea06c4be5f9e80d2ab32d19f8235accc1cf"
+  url "https://github.com/libuv/libuv/archive/refs/tags/v1.50.0.tar.gz"
+  sha256 "b1ec56444ee3f1e10c8bd3eed16ba47016ed0b94fe42137435aaf2e0bd574579"
   license "MIT"
   head "https://github.com/libuv/libuv.git", branch: "v1.x"
 
@@ -12,12 +12,12 @@ class Libuv < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "0b28a00add187590626c395c52a9295857fc1320e9ff0b8dd9fe726f4614ebcf"
-    sha256 cellar: :any,                 arm64_sonoma:  "bf7e4fc58b402b235dabb022fcba6e7960c8e1afb28e482a35771345de91d117"
-    sha256 cellar: :any,                 arm64_ventura: "f7f9df3d16699a31cd90bd8e5a8172d590c3f18bef7c03f6cd0bdf32fa4b968e"
-    sha256 cellar: :any,                 sonoma:        "1c30f58448ce9c0d30cef601a475a1986d1cf072188b110adf783c7002a9b559"
-    sha256 cellar: :any,                 ventura:       "219fe43b746aaaa223c27cedf8634da8e0977a4e03bf970c254dc050e300da29"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a6d13b73eee595b1bf4157d8c047bcdbc46f61b8a3aa2d7225d80a9183849396"
+    sha256 cellar: :any,                 arm64_sequoia: "9a70ed97116c4960f0484159c07145df8e768b1a62be68c071070869ba4c3644"
+    sha256 cellar: :any,                 arm64_sonoma:  "b39ee9c307a07d8422f5d604b36eb61879bc1f8563bfa964bfab96fc79aaa5ce"
+    sha256 cellar: :any,                 arm64_ventura: "7675ba03df1cef221fc81024c4f2efe38cc08b23903e60ee60ecb99f917323b6"
+    sha256 cellar: :any,                 sonoma:        "50124229722199f08735d93a315c1a8678e19635eb31099331e608856870dd54"
+    sha256 cellar: :any,                 ventura:       "112d27659fbb30ed6b607846c4ff7754526f39fe9b227a851738347f113aa65b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "06e43cd87e374d2056468b681e66243b6da9088013ab49f9838e4cd44ddfc96d"
   end
 
   depends_on "autoconf" => :build
--- .github/deps/imagick/s/sqlite.rb
+++ .github/deps/imagick/s/sqlite.rb
@@ -1,9 +1,9 @@
 class Sqlite < Formula
   desc "Command-line interface for SQLite"
   homepage "https://sqlite.org/index.html"
-  url "https://www.sqlite.org/2024/sqlite-autoconf-3470200.tar.gz"
-  version "3.47.2"
-  sha256 "f1b2ee412c28d7472bc95ba996368d6f0cdcf00362affdadb27ed286c179540b"
+  url "https://www.sqlite.org/2025/sqlite-autoconf-3480000.tar.gz"
+  version "3.48.0"
+  sha256 "ac992f7fca3989de7ed1fe99c16363f848794c8c32a158dafd4eb927a2e02fd5"
   license "blessing"
 
   livecheck do
@@ -15,12 +15,12 @@ class Sqlite < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "7eca69ad4697874a907a2eb2ae60ceec930d93e92796b3433b65fc7c65a8dbaa"
-    sha256 cellar: :any,                 arm64_sonoma:  "725168435181011bdd06d7d1f18dad719529aec4ae0176392b0a16a725612b37"
-    sha256 cellar: :any,                 arm64_ventura: "3ab78c5df09c610070f4a64a8fc55e4ef5037de719e9361b8e30dd00590bd510"
-    sha256 cellar: :any,                 sonoma:        "03bd52415c045f9ee4cc5c9e4711aee2872ca186cca60c8c7f6296fec8d3e87c"
-    sha256 cellar: :any,                 ventura:       "0f2d1a76178f1da4bfbc684b8e4a0393bb18ff6ed4a24261aa66b3d2e2011f04"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "082aa7c1c531ca9f74fbc8833fcf8459f985c11bdf5164e2ea4a820d479497ae"
+    sha256 cellar: :any,                 arm64_sequoia: "56459499fcb30c412b34069af462affba6150a684684b747a7696fb96bd6afe5"
+    sha256 cellar: :any,                 arm64_sonoma:  "88f8ceb923134e0f733355ddd5d8aeca284a6749b47bf42dab86de23854b8255"
+    sha256 cellar: :any,                 arm64_ventura: "b7aa1674e769996a9fff7a79b81eae73dfcb7c91dd2b746bd2f0c8c9f05ddb4e"
+    sha256 cellar: :any,                 sonoma:        "7ca5392892461703f4aaad060a0c33849d95a523e16e7b9e86ec97b94ccd1082"
+    sha256 cellar: :any,                 ventura:       "3046ba527c7add5e0406dfb8cdce75a76fa6ba273a3bf0a64041adc2753ef8dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e8bc3f02f117c74ccf0f315354ccc1ecf10a013c7b2cbc6b7137617f1422a30e"
   end
 
   keg_only :provided_by_macos
