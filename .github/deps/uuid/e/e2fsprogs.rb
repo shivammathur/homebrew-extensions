6ee590cca2aa8b716c10f51b572bd27bd9caa5c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-29 10:06:26 +0000

    Update extension dependencies

--- .github/deps/uuid/e/e2fsprogs.rb
+++ .github/deps/uuid/e/e2fsprogs.rb
@@ -1,8 +1,8 @@
 class E2fsprogs < Formula
   desc "Utilities for the ext2, ext3, and ext4 file systems"
   homepage "https://e2fsprogs.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.47.1/e2fsprogs-1.47.1.tar.gz"
-  sha256 "9afcd201f39429d2db2492aeb13dba5e75d6cc50682b732dca35643bd5f092e3"
+  url "https://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.47.2/e2fsprogs-1.47.2.tar.gz"
+  sha256 "6dcd67ff9d8b13274ba3f088e4318be4f5b71412cd863524423fc49d39a6371f"
   license all_of: [
     "GPL-2.0-or-later",
     "LGPL-2.0-or-later", # lib/ex2fs
@@ -18,14 +18,12 @@ class E2fsprogs < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia:  "fe6514a7b9690195806c16b1eebeaf0a991f0944c6cfe76681a4c57e07e8a660"
-    sha256 arm64_sonoma:   "98368d23728fb485ff67766148c8aee4bbeee3c542ba0d5c200cb608aedb8e1a"
-    sha256 arm64_ventura:  "2226708887d3d43427d39d355a75b2367d7ecf5145621de5dae2749dd4f130e4"
-    sha256 arm64_monterey: "942b459a0a9bc92157d4afa5e0f71d3f14d5ddccb0b8d652c4e70416d6d32cc4"
-    sha256 sonoma:         "248320615070b5b3f32ee4c581c46a4e9e74e51c4765624d253aa74a2cc44469"
-    sha256 ventura:        "85f74a6d50a82f4e2f2c29e0569cd102034a127e81d0fdef83c15411aa240042"
-    sha256 monterey:       "07e61dc2625c27caa877ce698f5c3f5cd2f7aaee3cba6da22d865db7224105ae"
-    sha256 x86_64_linux:   "eb63ea295700e11246a09b546f36a360db3c56c227deb4a30e12450e2c76dbab"
+    sha256 arm64_sequoia: "a6ca9c2d59683ed1f7d97cb2fbf2abae9bcaf4cb3af2cc451c42fb25f2d0931b"
+    sha256 arm64_sonoma:  "75fda5c12d0363d8bd5fb2721bff367361369abdd16a7fd9dae612c5264fce45"
+    sha256 arm64_ventura: "70e8e92b42117b3161e431f6c9de55f754d7e358b4cd74a0a281807828a98004"
+    sha256 sonoma:        "129f5d0b2dc865c6c2d7a2959a71667236b10cbd8a8abbbc5424df73d788ee58"
+    sha256 ventura:       "aaa928d296c1b82a64b89183d5005d3c98bbbc51cb17aa1151b5850519c9b947"
+    sha256 x86_64_linux:  "42721911833f1c21f50d2ed01327f86b0470995cdcfc8d48dcc7af5c7b0cc8a6"
   end
 
   keg_only :shadowed_by_macos
