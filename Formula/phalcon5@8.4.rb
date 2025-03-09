f851ff2e71652e2b646ecc03329a31ad16677606
Shivam Mathur <shivam_jpr@hotmail.com>
2025-03-09 02:19:55 +0000

    Add phalcon5@8.4

---
+++ Formula/phalcon5@8.4.rb
@@ -0,0 +1,36 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Phalcon5 Extension
+class Phalcon5AT84 < AbstractPhpExtension
+  init
+  desc "Phalcon5 PHP extension"
+  homepage "https://github.com/phalcon/cphalcon"
+  url "https://pecl.php.net/get/phalcon-5.9.0.tgz"
+  sha256 "7b8622bf7df5385e159dde3f41ed91bc6798726d8d725a46db8ba884651664d0"
+  head "https://github.com/phalcon/cphalcon.git", branch: "master"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/phalcon/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "pcre"
+
+  def install
+    Dir.chdir "phalcon-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-phalcon"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
