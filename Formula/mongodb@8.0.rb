f62fce49335bd2794876267961de5f2c0a4116d8
Shivam Mathur <shivam_jpr@hotmail.com>
2025-03-21 14:28:11 +0530

    mongodb@8.0: revision bump to migrate to icu4c@77

--- Formula/mongodb@8.0.rb
+++ Formula/mongodb@8.0.rb
@@ -10,6 +10,7 @@ class MongodbAT80 < AbstractPhpExtension
   homepage "https://github.com/mongodb/mongo-php-driver"
   url "https://pecl.php.net/get/mongodb-1.20.1.tgz"
   sha256 "614e57594918feb621f525e6516d59ce09b78f5172355ba8afb6c2207c1ce900"
+  revision 1
   head "https://github.com/mongodb/mongo-php-driver.git", branch: "v1.20"
   license "Apache-2.0"
 
@@ -22,7 +23,7 @@ class MongodbAT80 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "db65bc4cfda5b6d30369898873d1fe4948356a3197b0641798d5b4fff2e63946"
   end
 
-  depends_on "icu4c@76"
+  depends_on "icu4c@77"
   depends_on "openssl@3"
   depends_on "snappy"
   depends_on "zlib"
