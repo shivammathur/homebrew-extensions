c7e6e735d329f1bbf64bcdb40e8374317b86272c
Shivam Mathur <shivam_jpr@hotmail.com>
2025-03-21 14:29:17 +0530

    pecl_http@8.1: revision bump to migrate to icu4c@77

--- Formula/pecl_http@8.1.rb
+++ Formula/pecl_http@8.1.rb
@@ -10,6 +10,7 @@ class PeclHttpAT81 < AbstractPhpExtension
   homepage "https://github.com/m6w6/ext-http"
   url "https://pecl.php.net/get/pecl_http-4.2.6.tgz"
   sha256 "cd33230050b3f7c5ddb6f4383ce2a81f0bcdb934432029eec72ebf0f942b876d"
+  revision 1
   head "https://github.com/m6w6/ext-http.git"
   license "BSD-2-Clause"
 
@@ -29,7 +30,7 @@ class PeclHttpAT81 < AbstractPhpExtension
 
   depends_on "brotli"
   depends_on "curl"
-  depends_on "icu4c@76"
+  depends_on "icu4c@77"
   depends_on "libevent"
   depends_on "libidn2"
   depends_on "shivammathur/extensions/raphf@8.1"
