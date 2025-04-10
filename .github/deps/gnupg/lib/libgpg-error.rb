6bb966eecc8bbfaf1ed72d52b3b11f77889e1c6a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-10 10:06:28 +0000

    Update extension dependencies

--- .github/deps/gnupg/lib/libgpg-error.rb
+++ .github/deps/gnupg/lib/libgpg-error.rb
@@ -1,8 +1,8 @@
 class LibgpgError < Formula
   desc "Common error values for all GnuPG components"
   homepage "https://www.gnupg.org/related_software/libgpg-error/"
-  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.51.tar.bz2"
-  sha256 "be0f1b2db6b93eed55369cdf79f19f72750c8c7c39fc20b577e724545427e6b2"
+  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.53.tar.bz2"
+  sha256 "6a0721b52027415f53abcbf63b5c37776a0f774d9126d560a3ce76c0eb42903f"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -11,13 +11,13 @@ class LibgpgError < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia: "7db996510272893e4db9cd536e65e7fa6d0f20c4dbef13c55e95a3ebab67d103"
-    sha256 arm64_sonoma:  "92893cbf03a9de9c16bc05f67e2bb6ab257ad2108aa61bdf1cea6178ac2dcac7"
-    sha256 arm64_ventura: "88f92f8777d3dbe2e163ea5736727b804a843ed707fcf270991a853943df686a"
-    sha256 sonoma:        "cb513c9b9bc05125027e8dee2b2f6ae5a7288ca27ee28aed032086f6f1a9629a"
-    sha256 ventura:       "a184d3d66348ce9885b39561a7c6f98bdaae83791400c018f2634720889cd10d"
-    sha256 arm64_linux:   "b2990797e65fac537e069c38ebd5ef362bc883aef402591565914546fc1c98cb"
-    sha256 x86_64_linux:  "9b65876e4b4b45e6111dd0047e8a3ebb46df26a42d75fc2af41e91beca83ee7d"
+    sha256 arm64_sequoia: "220358478d50a3f9f62659ae5da02cce0ff62a138c1f4a05f17bc222485e11f8"
+    sha256 arm64_sonoma:  "906bb05c2f545c05d01bad4b2586062eb1b20ad8bad282764c0f7a92854bd8d7"
+    sha256 arm64_ventura: "33a578ee475d93df011511e55401bcbc0ab088be992adfacadc15b0a52d243c7"
+    sha256 sonoma:        "3754ae61604fdf599d180ecafbaacb9556c20ece3648517fc073c34347fabe8c"
+    sha256 ventura:       "bfc24629df2761c9f36568977ab0e014e0b4ca15fad604c4bb2e7c3649d36433"
+    sha256 arm64_linux:   "68202f26b1e5f3f2e4078dcc5e023c6bf3085104fbf7a2a130db248bc44a8400"
+    sha256 x86_64_linux:  "eeb0cacef3dfec5b0d843289cf7fa34b96d08f569e5ac820a796c9109ed3c12a"
   end
 
   on_macos do
--- .github/deps/vips/lib/libdicom.rb
+++ .github/deps/vips/lib/libdicom.rb
@@ -1,20 +1,18 @@
 class Libdicom < Formula
   desc "DICOM WSI read library"
   homepage "https://github.com/ImagingDataCommons/libdicom"
-  url "https://github.com/ImagingDataCommons/libdicom/releases/download/v1.1.0/libdicom-1.1.0.tar.xz"
-  sha256 "058bfaa7653c60a70798e021001d765e3f91ca4df5a8824b7604eaa57376449b"
+  url "https://github.com/ImagingDataCommons/libdicom/releases/download/v1.2.0/libdicom-1.2.0.tar.xz"
+  sha256 "3b8c05ceb6bf667fed997f23b476dd32c3dc6380eee1998185c211d86a7b4918"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia:  "ed6b288f8c8cf6b1574f5027701922fc33d96dd42f4a66568908eaf3f78f727c"
-    sha256 cellar: :any,                 arm64_sonoma:   "e04c4b953a01ad22ad1b0a275e125572ad56724f72b1e0722c995889f2100128"
-    sha256 cellar: :any,                 arm64_ventura:  "4d7c3ec3e97cf15b3e880f105d2abc5293ab53df9987b572c10c1f223d920fef"
-    sha256 cellar: :any,                 arm64_monterey: "5da10c0fbf8863b894a4991e19e126dead1746a2fc364fb30916f8c256a12de0"
-    sha256 cellar: :any,                 sonoma:         "9a0b6ddb37db98584843c42e5542f24b9d8e8ec6cbb431cc9c972578b4acb2be"
-    sha256 cellar: :any,                 ventura:        "211edda989f53885f744b7dfdd930277a2197819ceca052a3f03a281c10d7f55"
-    sha256 cellar: :any,                 monterey:       "7bf20b6f29eff5ba9c9cf2dc33a447775af8d84eedb1a6405a856b882f7099d1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "f3a4c504aa7eacab5ff0aecbb563b651776584e37222ca7199d6618a104541c3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "12aeb51a6477d922ae32934b31abfd489853b8a40833f78ba2bf9c00d82b631c"
+    sha256 cellar: :any,                 arm64_sequoia: "a0ebd3a3367d71d0334a7891bcfd592e2b3afb155ee06ef53147132dfcefd60e"
+    sha256 cellar: :any,                 arm64_sonoma:  "f375601773df0c5094df266775394c93aa72f5b48fb05cf1fc05b5af5cc9c3af"
+    sha256 cellar: :any,                 arm64_ventura: "878c5957cfd9c3a44b204f9917c2dcdde618abbab7038a7b60052d4c9a5ac504"
+    sha256 cellar: :any,                 sonoma:        "070a0a036117b34c98f56cd86327d70572bbb0131df4b2b0c31d3c32d585009e"
+    sha256 cellar: :any,                 ventura:       "24ecd6d69b3020110e55b0a6d70737dec2df074a9568b65b1c7bf5a5f7e5d381"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c2123321b416caf06cb685ed6307a53ada4d8eace6429595d99a1249caf171f7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a12c2177e7519f1cd98ed1d88e227eeabdee7236532fa270b1a780b16a068020"
   end
 
   depends_on "meson" => :build
