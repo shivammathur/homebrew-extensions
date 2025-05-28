36adef2b56c45b12a4a6497231d7d32caab3fbc1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 09:36:17 +0000

    uuid@8.5: update 1.3.0 bottle.

--- Formula/uuid@8.5.rb
+++ Formula/uuid@8.5.rb
@@ -24,6 +24,7 @@ class UuidAT85 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sonoma:  "171da75660ed8dc61e134aad266eb9f4ee68e8b383c075239ca328a4ad279d43"
     sha256 cellar: :any,                 arm64_ventura: "5651656428cca96007f5ea7abdfe4490001bb18e310fdcb44d80f7ce93e4ba3d"
     sha256 cellar: :any,                 ventura:       "370244277cab806be64397f5a5c9d6d5fcdbfa9bca55d953db84ed1c9618f227"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "692b8eb743fb56e3f1dad96edd65617ae8540b284a108a3623cf9c3879c03177"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "a003625d61510146e144bbbf7f4b57b566ba2cb4f844b7cb30c18bff63952e64"
   end
 
