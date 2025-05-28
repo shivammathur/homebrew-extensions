ff503fec3d35c16dfa7aa33c614616294e52209b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 10:53:54 +0000

    xdebug@7.0: update 2.9.0 bottle.

--- Formula/xdebug@7.0.rb
+++ Formula/xdebug@7.0.rb
@@ -22,6 +22,7 @@ class XdebugAT70 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1138c70d767e83766fa2dd3228435be4eaa4a16aee724d0d87bfc6ddfc16c564"
     sha256 cellar: :any_skip_relocation, ventura:        "775a21714729a2631e2e6a87066b7ab43be00b05fd814fb54abfc5591de7725e"
     sha256 cellar: :any_skip_relocation, monterey:       "9a6a6c1542c7b2b1c51861c2f49ba0d9b3de28b0b2f4058b61e10542282d1b6c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:    "b64556b29bc1d6a48c998bda44ebbbc5f4a869dfeae6759a3f3669630b1db4ff"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "17afb2eb312fdaa4733b22d4bbb9f3ed79701ca4304b7ce393e8e60ae532c9ea"
   end
 
