9f29221ca147038d6367f859039e060c4a19eda9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 19:06:43 +0000

    pecl_http@8.3: update 4.2.6_1 bottle.

--- Formula/pecl_http@8.3.rb
+++ Formula/pecl_http@8.3.rb
@@ -25,6 +25,7 @@ class PeclHttpAT83 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sonoma:  "9716935fca7c47ac39560aa26e9534df79286da275ae27dc877d058b43da1e3d"
     sha256 cellar: :any,                 arm64_ventura: "1a6b87a2c157dba0119c4d58c74ec854cba64be8eac0dc6a27f6bbcee62aea1c"
     sha256 cellar: :any,                 ventura:       "50455908dbbf95ff3fc9270b6725dac9217e14f094e876ec907162b585147276"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a10effa509f194175c4e7b019e94b4ef4163951c713a7769839c2a29d661b66b"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ff2bfe863784aa0a893359084b0c156b639d312fa3afdb597e6c82308f6f27b"
   end
 
