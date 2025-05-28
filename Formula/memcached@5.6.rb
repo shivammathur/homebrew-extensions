7c70173b1bca7a126db3161bda55238977137824
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 19:09:05 +0000

    memcached@5.6: update 2.2.0 bottle.

--- Formula/memcached@5.6.rb
+++ Formula/memcached@5.6.rb
@@ -24,6 +24,7 @@ class MemcachedAT56 < AbstractPhpExtension
     sha256 cellar: :any,                 monterey:       "f8a39e14fd96a6a6606f236b192411ba31d8f2487d3e65d72986879a130fe5e7"
     sha256 cellar: :any,                 big_sur:        "c4903ca3df34051904e2b1100012cb082220daa4db7697367a557674d930c665"
     sha256 cellar: :any,                 catalina:       "c29a461b838dc550d4d5c46eb54055b31f409c2c10168f3cb3fd2d19fb1fd20a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:    "9b731b4ec68eb2701768b18a99b8c48749a0c6e5bf4658a6f8372c777f7dddfd"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "5f417b6abf47546302f3efecb0eb00c4540f5382c5dfcc50e73878a23105e7ac"
   end
 
