eb48efc21ff4926c20e1ffc1da36b5fd6f7b83e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 09:44:41 +0000

    gnupg@8.0: update 1.5.2 bottle.

--- Formula/gnupg@8.0.rb
+++ Formula/gnupg@8.0.rb
@@ -24,6 +24,7 @@ class GnupgAT80 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sonoma:  "f96b03bf6901780b2c117478b413e12868d15fe09066e4a3ab709d992236db34"
     sha256 cellar: :any,                 arm64_ventura: "ecb63827ec7cea6a31b91bdbd1b1ad6328cc3a56d6129f0a12f5f9637840886f"
     sha256 cellar: :any,                 ventura:       "eae9462ed881eed6870bad7189f9ef56c403e398c2162d26e56cccaaf4bd97b8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f147531fd31dbc5db05fee5f952f4ce88c1a0f192b031b1b7a478dda3cd9aa3e"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "087cbd39cf6f7e51443fd03c5a95ad2eb30259c1fc92529f5ecddab7207f9a78"
   end
 
