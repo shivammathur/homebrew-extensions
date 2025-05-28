fe5a64df7d24b4bac17e4e23f60b64c366900256
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 10:51:43 +0000

    event@8.5: update 3.1.4 bottle.

--- Formula/event@8.5.rb
+++ Formula/event@8.5.rb
@@ -25,6 +25,7 @@ class EventAT85 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sonoma:  "248e161ddf9a50d31d624a36d31e19f96c24412612740b60e3cdd31812af4605"
     sha256 cellar: :any,                 arm64_ventura: "79296a8ecf7372d0b1533a80e12d00dfdc18d7931fe03002c7c4c0874f406f4e"
     sha256 cellar: :any,                 ventura:       "2ab49628da9bb960194ea4c39ed3fa298470b04d25af225d490267fc192ad966"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "eea71c918386ace8ccb4c4c758f9bb8b9d13bb7cabd79ca47b7adaa6710765ef"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e27aefde679f462ae21fff0d4eadc9005cfa5b6c7e3765d15d9a5427866ea5e"
   end
 
