83d22e11579df943a46da54f686fbefc094057d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 10:51:56 +0000

    apcu@8.1: update 5.1.24 bottle.

--- Formula/apcu@8.1.rb
+++ Formula/apcu@8.1.rb
@@ -24,6 +24,7 @@ class ApcuAT81 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "522d5d02c3e56f56688e6e7793a1f54ea627e4c0949b34b646a32d3508f7c0d0"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "726c95f9809ad874000d7e8902b3cd0807d4d68d0e2ed83ff8e8c792ebb2170b"
     sha256 cellar: :any_skip_relocation, ventura:       "c6baef6372e3caf1367f024d307029a647c65916f9e53a56a09a8e159245c26d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "106019cf15fc1c43cbb4ccce65f2fa5404585425960ee64800a7a158ee42e553"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "0f603d52622605e137148c209645646ee5395f4be1fa90f8a04d6216ef09f214"
   end
 
