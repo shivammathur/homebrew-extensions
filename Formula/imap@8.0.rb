5ad0a42459192c5f006c82940f5e234837be9ba1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 10:59:46 +0000

    imap@8.0: update 8.0.30 bottle.

--- Formula/imap@8.0.rb
+++ Formula/imap@8.0.rb
@@ -20,6 +20,7 @@ class ImapAT80 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sonoma:  "9ebdc50ab126e5f24ac0f61104642879e6ea510b120412b7cf8d089a6a447a8c"
     sha256 cellar: :any,                 arm64_ventura: "e4a896e184d7054dab05bea71d3499688de4789423693ec8910e623e86aaa6e6"
     sha256 cellar: :any,                 ventura:       "5735b19919a5aa759417857cf0043717ba08523a0f3d3c04e545bf6ebf2c6ea3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d0c8c6342ec8be764270ae26b50270c78221f8fdc516b83a85b099765df3298d"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "01fe7a39e4b6bacb911b587ab639065f781dd0218e0456fd978bafe880f213df"
   end
 
