3d0aa43a0f475b1e4d623094e577b916100f85d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 19:05:36 +0000

    redis@5.6: update 4.3.0 bottle.

--- Formula/redis@5.6.rb
+++ Formula/redis@5.6.rb
@@ -22,6 +22,7 @@ class RedisAT56 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_monterey: "4b6d246011629bd0004a5f98522b56f6627ec36cb384145651c599d651a07f3c"
     sha256 cellar: :any,                 ventura:        "428bd61ebddd2ea434bfa46806436e88f867229e46cdef67744bb6de964b8f55"
     sha256 cellar: :any,                 monterey:       "ffd7b9348e64deedf3687c7d2de92007fcc92cb5097acb0bef05298d74ebe66f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:    "cc38ea2cbc1a07b05d5f68090d73f9ce8fab2712a3fb2801772312f723bd1a31"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "ec2742cbf1bd43860efa3b6208157d525d0bee4f401eac4e1bb4d273d918e63a"
   end
 
