d81117188a4f575af9ea492bb7f7f93411d47e1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 10:54:36 +0000

    xdebug@8.2: update 3.4.3 bottle.

--- Formula/xdebug@8.2.rb
+++ Formula/xdebug@8.2.rb
@@ -24,6 +24,7 @@ class XdebugAT82 < AbstractPhpExtension
     sha256 arm64_sonoma:  "17c15d1745b727cb927ce71fa7850c1a4eb217a17ecfe410dd3b72494954181a"
     sha256 arm64_ventura: "768faf8507a4b099ffc0912c301951e093388baf80044149638798c06de45f59"
     sha256 ventura:       "1118bd590d5f7e9a2da37280c6de2964aaca28923f41fae95a3d7df6126d46b8"
+    sha256 arm64_linux:   "693e6395020d8cbc67c6b051ff067134b8d6d52edf51568081af231488dce9c2"
     sha256 x86_64_linux:  "f018e18f0e37052eef22077b864e0c83dbc9e881652ca0d23ad40acf0a68dc5f"
   end
 
