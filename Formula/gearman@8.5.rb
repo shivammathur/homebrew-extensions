82b9c6e864246910caca767a7d263fc1e0f05ec3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 10:52:42 +0000

    gearman@8.5: update 2.1.4 bottle.

--- Formula/gearman@8.5.rb
+++ Formula/gearman@8.5.rb
@@ -24,6 +24,7 @@ class GearmanAT85 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sonoma:  "4d1052e423349f1dcd76c406882f0989357e9597a4645e955ef4e28f32ff2d7d"
     sha256 cellar: :any,                 arm64_ventura: "e09f7eda8f086caf0cb2d2f3677e63705b9832f09b01d1271df4416e145c826c"
     sha256 cellar: :any,                 ventura:       "388cf48ff120a26f4e98f7d9596fdb99138ad5f4db8bb0c2a51c139385c607fd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "75b3182ccf12c29dd77e0a0be84dc71213e2e8c2eb1cf83ca69904dcf6a29c79"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e69672b61a64e9dea292006a35e9ecb8996fb624974ddd3f35abbf293093166"
   end
 
