49a00aaebcb7c34bee10e30f6a0783517085b264
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 10:57:46 +0000

    yaml@7.2: update 2.2.4 bottle.

--- Formula/yaml@7.2.rb
+++ Formula/yaml@7.2.rb
@@ -24,6 +24,7 @@ class YamlAT72 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sonoma:  "fe256cc785c0a465f859481c2a1f10ecf4d39db67d600ad2ce439e5966ddc8d8"
     sha256 cellar: :any,                 arm64_ventura: "da2a85d45f02efc72bc3f1afa22de691ae98dee721010bead82fde0f6c1f3478"
     sha256 cellar: :any,                 ventura:       "d9eeb3b7993076ee19f02e3208d07cddc657452d429f18b5c9c339887d0922e6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cfdab7a6b781cccf856f5466c9c72283fa5a7fa3e936370722bba1ad9f6c4553"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "a4779e3b9d9070e16aa6b264a491aca55e112a92e51b5de17871cbdf57c45053"
   end
 
