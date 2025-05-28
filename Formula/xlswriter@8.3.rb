119774b0e5c5db53eef2325e511a4a17e3feb6be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 10:57:30 +0000

    xlswriter@8.3: update 1.5.8 bottle.

--- Formula/xlswriter@8.3.rb
+++ Formula/xlswriter@8.3.rb
@@ -24,6 +24,7 @@ class XlswriterAT83 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "14c049fbbf0d2f7c6c011ea4c62faddd20f92ecb89a069810a0f3e4fa9485481"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "ac34a458dd619dd08239bce2767d7c546a2ba7fa758ce1a0e231b02bd98d48e5"
     sha256 cellar: :any_skip_relocation, ventura:       "95138ff4afa94190bb203adca0a8678563d234f06a12390add09bb8173864971"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5bc755c3f0cffbcee11d58508bf9cfcc02326985e6b337e5c5ecd6347f8ffd3a"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "7fdd92f8949e44496bfa5059f1adbd7405665af42500578ea8e6f6e7c98cd5ac"
   end
 
