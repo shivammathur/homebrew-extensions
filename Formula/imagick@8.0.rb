e8f11f325a4058b7af0b195f0a68ffde059ed6a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 10:53:57 +0000

    imagick@8.0: update 3.8.0 bottle.

--- Formula/imagick@8.0.rb
+++ Formula/imagick@8.0.rb
@@ -24,6 +24,7 @@ class ImagickAT80 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sonoma:  "37a1a59cfbf3f4f87f12d54b5cb3584d8e786e3cff383c05ecf15f84dc24d817"
     sha256 cellar: :any,                 arm64_ventura: "81f40bd081d7d1720a186b228522b377c7c3ee8c2982e8afded16cc72d694a8e"
     sha256 cellar: :any,                 ventura:       "5c61e6fa77ecf23e5a45fd7ab759b66d6a44bca3c5d73b71abfc6046f53350d3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "71be6a892aa8d3c4faf55c971d1128151707ddb5710cce69daba9c0934fdd3e8"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "82ece2c6402b93a647b8d4594cf41087144ec77b00916eb151921b97a8caeadd"
   end
 
