80baa6e5a992c4b7c20f80536731fc30d3dfa4fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-28 19:06:22 +0000

    memcached@8.2: update 3.3.0 bottle.

--- Formula/memcached@8.2.rb
+++ Formula/memcached@8.2.rb
@@ -24,6 +24,7 @@ class MemcachedAT82 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sonoma:  "9431a3c0a774ef66b3603fd22ccb6543b795371d36019f34e53d908912643f04"
     sha256 cellar: :any,                 arm64_ventura: "b514fce0e11bb59230fadeebb5b8160f52cd18285c8a0a9ed786aafa197edef6"
     sha256 cellar: :any,                 ventura:       "af05118a4d048cdb6009c8bc61a05a2a7d050855a0c64254c76a89a6f701bbdd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6746d62ca57b4962e002a44597004e229eb0fb9cc8ddca7736e343b66794a694"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "f6ede364ec70e8a572f4cec5e34da0532b6b007abbf0da8c1570de3b1d38e906"
   end
 
