445441b60ca5b6e47a1abb7758bc224364853186
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-01 18:06:48 +0000

    vld@7.4: update 0.19.0 bottle.

--- Formula/vld@7.4.rb
+++ Formula/vld@7.4.rb
@@ -20,14 +20,12 @@ class VldAT74 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "153e8967b427b9157100018a917bea53ea496b5517a7f6e10476a6de2782bf1e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "570dfb9b4091cbb18755787af94d79343fd72d4b1167ce0b7568cdab18cc99fb"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c922202ee672994dd349727d0e2a825069f0f06a9d633216949ce000db554201"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4c426ebe9683d071fa9d20603ae5d55ff1c2582cf6d02228140478bf4a8d0654"
-    sha256 cellar: :any_skip_relocation, ventura:        "a7c560c8e8d0289c176f7dc9599d7f491c34a16dc79bc3f948e43eb67375f360"
-    sha256 cellar: :any_skip_relocation, monterey:       "d8376f460768febcc06ced11978662525bc001af2622530ffcafb7782e466d6b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "e2b6b7ce0139f03cb6a2ce79e2043c4bd097bcdd87ddd23d42b2902e93793c8a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "74653c836d099c60aecd72692e6257bb442b085b488d15f948d0bbdd1af38732"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "572b2ad2978c2c55c2b01319295486630a2c0fb9b96a33b41609ebe2ed7d91d8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "933479e7b1d7eee36b9ad4bed0d28894167a5c6c2eb017019bcfc3df39ff1f6f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a200de63fb749cb9c7409e286d0666f60f18443c978b6231b7f0316c103fcafc"
+    sha256 cellar: :any_skip_relocation, ventura:       "1a4830beecbb690a95a40c674b5a8c1a365fc8c284e17c234de5a8d0943e17ad"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2ba50cb508a91127272056e11ee46d6872fdc21c86e97dd788b39111e4bd5e1b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "35b7a774dbce07068deea118185bd019e000d8cd586f574b772a48830817629a"
   end
 
   def install
