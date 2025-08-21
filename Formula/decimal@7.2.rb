7d3260f7e41cf38f21278ba86842377afc05a5b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 19:25:07 +0000

    decimal@7.2: update 1.5.0 bottle.

--- Formula/decimal@7.2.rb
+++ Formula/decimal@7.2.rb
@@ -20,6 +20,12 @@ class DecimalAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_sequoia: "2052ad2f430f4109343fb87859080f18b5d1faf311123ca2529833b783936264"
+    sha256 cellar: :any,                 arm64_sonoma:  "c457c2032a2a872a85ed06e0cb67226221e4b2376194072abcc142e34c6e676c"
+    sha256 cellar: :any,                 arm64_ventura: "d87ccf86b8e9bae5236d4aa6c82a611915f211a43450b39ca052dfe889cdcd9f"
+    sha256 cellar: :any,                 ventura:       "c931a2cf7642031f31198810461db94e6b99310ac6099fab711942de898b7d9c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ec55a0bc16f409d54c4096bfdcd5eb8a51e74e4a68806d49f5ba2255328d0f3f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "84477b42aeb96adce46a9aef5f2a9dc3cd87335802c558bc5977a3f1bec9959c"
   end
 
   depends_on "mpdecimal"
