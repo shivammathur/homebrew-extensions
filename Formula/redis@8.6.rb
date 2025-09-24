897ec1fe7954b1be7d18d1a6ef161eaccaa9ec8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-24 10:47:41 +0530

    redis@8.6: Added new formula

---
+++ Formula/redis@8.6.rb
@@ -0,0 +1,72 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Redis Extension
+class RedisAT86 < AbstractPhpExtension
+  init
+  desc "Redis PHP extension"
+  homepage "https://github.com/phpredis/phpredis"
+  url "https://pecl.php.net/get/redis-6.2.0.tgz"
+  sha256 "5069c13dd22bd9e494bb246891052cb6cc0fc9a1b45c6a572a8be61773101363"
+  head "https://github.com/phpredis/phpredis.git", branch: "develop"
+  license "PHP-3.01"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/redis/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_sequoia: "e9c95b8fd3905f5050e7cb42842a9254cff90dd7df2e51771c91af1a515c8295"
+    sha256 cellar: :any,                 arm64_sonoma:  "25c533081592dce93c0aac3a7fe9a2fb4df63e6d6a12971c35d559782ea7fbf4"
+    sha256 cellar: :any,                 arm64_ventura: "4c655e9a19870f8065c7173b0277093cfe9a2817fa370d4064c48b2e934787e8"
+    sha256 cellar: :any,                 ventura:       "e2ec2419b5af587bfd41ca8dc5946406009b1473145068e56feb40c96435c25b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f644333663ed3d71414c89a61b24f85c337e4754c8c90462babaf52a13a39f13"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9ccc05e0805f82e3f5ee163ab3a56b490ec6a091b4853fa33c7d5c90c366ba9f"
+  end
+
+  depends_on "liblzf"
+  depends_on "lz4"
+  depends_on "shivammathur/extensions/igbinary@8.6"
+  depends_on "shivammathur/extensions/msgpack@8.6"
+  depends_on "zstd"
+
+  def patch_redis
+    %w[igbinary msgpack].each do |e|
+      mkdir_p "include/php/ext/#{e}"
+      headers = Dir["#{Formula["#{e}@8.6"].opt_include}/**/*.h"]
+      (buildpath/"redis-#{version}/include/php/ext/#{e}").install_symlink headers unless headers.empty?
+    end
+    inreplace "common.h", "ext/standard/php_smart_string.h", "Zend/zend_smart_string.h"
+  end
+
+  def install
+    args = %W[
+      --enable-redis
+      --enable-redis-igbinary
+      --enable-redis-lz4
+      --enable-redis-lzf
+      --enable-redis-msgpack
+      --enable-redis-zstd
+      --with-liblz4=#{Formula["lz4"].opt_prefix}
+      --with-libzstd=#{Formula["zstd"].opt_prefix}
+    ]
+
+    on_macos do
+      args << "--with-liblzf=#{Formula["liblzf"].opt_prefix}"
+    end
+
+    Dir.chdir "redis-#{version}"
+    patch_redis
+    patch_spl_symbols
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
