cd2b00bab56fcd7e3f35ae9b1d316fc23f29f0fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-24 09:17:12 +0530

    pecl_http@8.6: Added new formula

---
+++ Formula/pecl_http@8.6.rb
@@ -0,0 +1,67 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Pecl_http Extension
+class PeclHttpAT86 < AbstractPhpExtension
+  init
+  desc "Pecl_http PHP extension"
+  homepage "https://github.com/m6w6/ext-http"
+  url "https://pecl.php.net/get/pecl_http-4.2.6.tgz"
+  sha256 "cd33230050b3f7c5ddb6f4383ce2a81f0bcdb934432029eec72ebf0f942b876d"
+  revision 1
+  head "https://github.com/m6w6/ext-http.git", branch: "master"
+  license "BSD-2-Clause"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/pecl_http/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_sequoia: "dfe96ab3ba5bf19bc8ae0c769f697310e5afbc30e3aa2271380937609b346d2a"
+    sha256 cellar: :any,                 arm64_sonoma:  "9b74f26824c6ab5a6fdb630932afb5406f07312a4d1a0347d7469b7212abab6c"
+    sha256 cellar: :any,                 arm64_ventura: "3494131673fb0c58121de01d7f89cc425b1543c1f71d14387727b5da36c59d8e"
+    sha256 cellar: :any,                 ventura:       "488d27cb6715d05a0bd8c487ee5e839951d6d45264acdf1f828ace69d300b794"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "48d0f024d73467c7ec1ef6b1ed519405a3c041268b398072e53f94cece38e90e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "984342dd0f27ae10d3bf065f63abe10702db9d7b6007e6e8a700c081634a7aeb"
+  end
+
+  depends_on "brotli"
+  depends_on "curl"
+  depends_on "icu4c@77"
+  depends_on "libevent"
+  depends_on "libidn2"
+  depends_on "shivammathur/extensions/raphf@8.6"
+  depends_on "zlib"
+
+  priority "30"
+
+  def install
+    args = %W[
+      --with-http
+      --with-http-libicu-dir=#{Formula["icu4c"].opt_prefix}
+      --with-http-zlib-dir=#{Formula["zlib"].opt_prefix}
+    ]
+    extra_includes = %W[
+      -I#{Formula["shivammathur/extensions/raphf@8.6"].opt_include}/php
+    ]
+    # Work around to support `icu4c` 75, which needs C++17.
+    ENV.append "CXX", "-std=c++17"
+    ENV.libcxx if ENV.compiler == :clang
+    ENV["EXTRA_INCLUDES"] = extra_includes * " "
+    Dir.chdir "pecl_http-#{version}"
+    inreplace "src/php_http_api.h", "ext/raphf", "ext/raphf@8.6"
+    inreplace "src/php_http_message_body.c", "standard/php_lcg.h", "random/php_random.h"
+    inreplace "src/php_http_misc.c", "standard/php_lcg.h", "random/php_random.h"
+    patch_spl_symbols
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+  end
+end
