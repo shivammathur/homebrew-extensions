3961770eb5224d2e75527d76e6bb72204254581a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-25 08:02:34 +0530

    sqlsrv@8.5: revision bump

--- Formula/sqlsrv@8.5.rb
+++ Formula/sqlsrv@8.5.rb
@@ -10,6 +10,7 @@ class SqlsrvAT85 < AbstractPhpExtension
   homepage "https://github.com/Microsoft/msphpsql"
   url "https://pecl.php.net/get/sqlsrv-5.12.0.tgz"
   sha256 "a9ebb880b2a558d3d6684f6e6802c53c5bffa49e1ee60d1473a7124fc9cb72ad"
+  revision 1
   head "https://github.com/Microsoft/msphpsql.git", branch: "dev"
   license "MIT"
 
