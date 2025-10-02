b91e4ddc69abfc95d8ed3f47927169c91b7534ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-02 08:06:33 +0000

    opentelemetry@8.4: update 1.2.1 bottle.

--- Formula/opentelemetry@8.4.rb
+++ Formula/opentelemetry@8.4.rb
@@ -19,12 +19,12 @@ class OpentelemetryAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b1178a973488d2f8aac706a86135ea894be7c02219cd641e867fe692cce670ea"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "62710bcabc50d2e353031c868f241170be338b0ca736fa272e85f84a9683c077"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0597e27e1219d9e9073a208883ae5a6e1aa3bd41f0aec621dd5708df65d33e72"
-    sha256 cellar: :any_skip_relocation, ventura:       "131d4b2503e2356857920e0f9e50d532d14d889fe2bc56125eae6b32cfb28eb3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "bf6c26a89bafb9bb9e72be648489ba6746a606ec9baed9312fb6809361ce37d5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "57d727a6f5535c14c881d1b79b51aa1b9b0fb2fada957d1257d298bb9bc3c85e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "73caf7ceea215848d61b90fe4e99798c2c3f9accafac27b32edfa7cb04773338"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2893a8e2a16f19c1ef3b07ef020e761ac27f17ba337649ad41de51b8ddec47c9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cee20b8754530159e0869102a736125791b8e7ff3d647309e390f5c783126685"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bdcf2d50ac079fa4fdb2aec253f1dbf335968a721c41942de6a62c07451df19f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a1c283ba7d42353a4900d41b000063c24056b45dd30f3aa871b44156c642b63e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "91c150b863897def8f5a069a0e47ea8080592f4975d09512770aa5e551b7e348"
   end
 
   def install
