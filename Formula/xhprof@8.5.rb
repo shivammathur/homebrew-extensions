5b7241de3f33f2272a0d2f2764338c9271b0b0e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-03 22:53:29 +0000

    xhprof@8.5: add 2.3.10 bottle.

--- Formula/xhprof@8.5.rb
+++ Formula/xhprof@8.5.rb
@@ -16,6 +16,16 @@ class XhprofAT85 < AbstractPhpExtension
     regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cc8c7ea18e52881d24c8bdb9dd6891be842bd2e58647ef56d1fd8fe156340d33"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e21845205cf96efc38462e3974e316516ea0d6aea807822521771b64eb0c4930"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d1d269320c2f32c07c8fbb43ab48624c7114c558cec6930a8ce4118811d24c6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5b42822f15994259125d99febeec79b13deca8d260fcab73a8853e86b67947da"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "69578d4a0834c6800416ef14321bb64ef015f13067b3dd51b3042aea6d40afe8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78c51b8a313cb0722d115c15ffd8de169b16eac0ef225546b69e62a31635bfe5"
+  end
+
   def install
     Dir.chdir "xhprof-#{version}/extension"
     safe_phpize
