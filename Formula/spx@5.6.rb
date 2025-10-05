60946d51e08088a2a36bcc407664d5e928e36787
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-05 06:51:50 +0000

    spx@5.6: add 0.4.20 bottle.

--- Formula/spx@5.6.rb
+++ Formula/spx@5.6.rb
@@ -17,6 +17,16 @@ class SpxAT56 < AbstractPhpExtension
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "51606cd9960e8fe099251e4ea7f7e94cff7fc30004acbdd8f11e53d357d96d9f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "05b0a09d8d8217039fc7ed612c98c866ba42111da4e90351f66f486c5d51ce8c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bb8a4100c30a6b48068586bd7baae85821f441503f4bf2d86795f4edb77b984d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "107bb4e0fc5335ae54397a2ad596ff1c83362e7ea8ba51597405449015d4be87"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "add7e459acaeaa9a296797f44cf7fd9cd43833c35b392884d53a626af1fe956d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e133c7d0a9b47ba7294201d0e198dc3066e9e54db19555d3938aca2b65167cbb"
+  end
+
   depends_on "zlib"
 
   def install
