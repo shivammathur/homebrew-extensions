2b8f489f55b58f10173cdef3cb61dab5a238e7d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-13 10:06:14 +0000

    igbinary@8.4: update 3.2.16 bottle.

--- Formula/igbinary@8.4.rb
+++ Formula/igbinary@8.4.rb
@@ -28,6 +28,7 @@ class IgbinaryAT84 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "bebf3c2fc2361ae2770c91bc0e925f9bdf5b21c4a6b3ed4ed802e7b56ff1393a"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "34fbb968e636dfa18bc4def7faf7117ff6d9e49f0eca8a1364307694c94a8d5a"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "cf9d39c06d6544643dd47453c5c82302b7f62a640d901f4293b8860987e04ce4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "96ff465ad17e498e448e5195825b0867354ea369aa6c3fc3287d8dd3cd44fe6d"
     sha256 cellar: :any_skip_relocation, ventura:       "a614f7c25babb6667c2a7f2b8fbb7537626a36d86eb6171b02682daaaaf52755"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "d048cc717798f4d0d6304a7b7b568fed27ef50197d5649c96f47da0a04b3bfba"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "26c47fb4af80908ed52b1fcb7548413fb362bec469a4e80b4092f55dc07d037b"
