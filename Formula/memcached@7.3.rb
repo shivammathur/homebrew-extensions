e1f9220349e5b99d31690fe2d72e7983e52fe179
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-13 10:06:38 +0000

    memcached@7.3: update 3.4.0 bottle.

--- Formula/memcached@7.3.rb
+++ Formula/memcached@7.3.rb
@@ -20,12 +20,10 @@ class MemcachedAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_sequoia: "f3096d409425c1a2bf22e12b83fdaca6bc8171731b7d8e6c83704e7b84c583e4"
-    sha256 cellar: :any,                 arm64_sonoma:  "d8c5272353a404b8f00f84713704116d1cfaf92cefea671faef53681cb5bfe0b"
-    sha256 cellar: :any,                 arm64_ventura: "2eb79e68a8a7b95d04afa1fe64ff7f771eb70c3b9f874cf349de219fc48c9616"
-    sha256 cellar: :any,                 ventura:       "a52bb4ee9c534de880320f3e56cedd522ad94a5cb6dc170bd533d33604c27e96"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "137c768e41eae7815c0b9a6621184651d9904a51c501b95cfd445459d2d29447"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a03fe559f9ac4cabc9e67bc1296982f22148ae4cb61cb55d9884e77b8c6429e"
+    sha256 cellar: :any,                 arm64_sequoia: "f5e3cbec1bf480d956963ee9d38915ea13aa312d5920b0756542a3951b6c09bb"
+    sha256 cellar: :any,                 arm64_sonoma:  "549c09a9a23f02408e4ac94a10b6046544975cdbe7885b9e14dda504794d1832"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b87d52c514b69190d443ecfd58fdae7cd5ec15e91a5e715ecccc6c1aa5f557c9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "06f7f6f37750b4e1b984e066663a8c20fcdc2eb1bc32f06ef679d55b235df2f2"
   end
 
   depends_on "libevent"
