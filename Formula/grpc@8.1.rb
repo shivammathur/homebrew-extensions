18ff040500b0d001da502ade49bc015efece8dcb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-13 14:01:03 +0000

    grpc@8.1: update 1.75.0 bottle.

--- Formula/grpc@8.1.rb
+++ Formula/grpc@8.1.rb
@@ -22,6 +22,7 @@ class GrpcAT81 < AbstractPhpExtension
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "8843f7ace56fe3f353e6281f1acaf03c093aea95ae60ca0adecc62b4b09c3524"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "46251e28f463c4f0bdad52bfd6932c772dca9229f4133634dfcc455256d2e904"
+    sha256 cellar: :any_skip_relocation, sonoma:        "55e5d0476ec7cb3e295f772d4ca5d5dee2de217f96e41e705b17622580f707ce"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "8854c4e4155a0a2c4f76dd42462050176669efb4a14271bfed55cfce9a3e96e4"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b65d21dce46f267efe06f806c10e8c2f65ea0c25810339f654a1dee529c3017"
   end
