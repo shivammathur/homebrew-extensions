1b2bf1f0cf24836fdfefceffb09b0df5d546ab05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-18 21:43:08 +0000

    ast@8.4: update 1.1.3 bottle.

--- Formula/ast@8.4.rb
+++ Formula/ast@8.4.rb
@@ -23,6 +23,7 @@ class AstAT84 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "1c98116e8c1e0e3254444d8392a9aeb58ec2d0371b3a117aff356a07f6696b3a"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "afb8a048f2aa742d56d0fed0d433144abe9a2a161728ac444d5cea353a606d2a"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "886a6ceba599d186831af296ef0d666cfc297982a6e79a8925d36a2014fc50b0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0f6cddd79f060ad7dfe7cff98682bed23d3e9e0d41946a093286538b844f024d"
     sha256 cellar: :any_skip_relocation, ventura:       "c910c9f27f70600ac1ce5b7037d9d691c197313de2f4d63bb6da81989bba7023"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "90de14f77d5fd498253bf50e470b823e0cd2195d07e6b263c7913464078677fd"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "3235104a950832dfdb2909b8c3ae02c294ea3900ba0d0e24b5070a6017a90fb9"
