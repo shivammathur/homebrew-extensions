514daf27cc7113bd47d1f2c42be5f72dd88c2a28
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-18 22:26:55 +0000

    decimal@8.4: update 1.5.0 bottle.

--- Formula/decimal@8.4.rb
+++ Formula/decimal@8.4.rb
@@ -23,6 +23,7 @@ class DecimalAT84 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "536c23b249834302f64e96498f63252fa5238f4c802d259771860b24eed0a0dd"
     sha256 cellar: :any,                 arm64_sonoma:  "6684e7e54918753e579f4fc0aa6194bdcdf2c7cb482b61d6edbf2248bc0f33ff"
     sha256 cellar: :any,                 arm64_ventura: "099512434f8b0ec1de9b1b91e873ba7ad238c15b4b7aeb1988bba1ba42ed3eb5"
+    sha256 cellar: :any,                 sonoma:        "21a0412e1ae7c8c5deeb000ec192687d66a4c904a987fd093bb8935bbb65911a"
     sha256 cellar: :any,                 ventura:       "80772609292458df84c12f1114d91d1493dcdde4fcc6ea1bd54be713eb057a51"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "aed2cc89661f580aa4c4b0094b4328d2f15e638b31f89bc9b4ebe0cdcfa5618b"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "ab829e3b5b3371a11d9a7fe4cb687706972e3afb7af92f38f94bfc0f7cc543f7"
