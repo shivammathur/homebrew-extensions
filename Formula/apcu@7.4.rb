b7ceb8339703f3dd47d4b829ac741f887da74e62
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-18 21:04:21 +0000

    apcu@7.4: update 5.1.27 bottle.

--- Formula/apcu@7.4.rb
+++ Formula/apcu@7.4.rb
@@ -23,6 +23,7 @@ class ApcuAT74 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "db32a1f8f634404e8c4eb9b45b4ffe67c420375621b115dce09ad81c799ba734"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5824e7556f9fede9c34c330225bb7639b47f3190a3bf3409041e6cb0cb417b3d"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "c6011a7c2298e300244aecb51d4271a500cf0c70cf7103d96821d3eadec4103e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "70ee7e70653c71a70d9cdb33c380f61f6244b2adb84ac9d3ee4da7178f3ac1b7"
     sha256 cellar: :any_skip_relocation, ventura:       "9ba175c41c445a3d12034611db31e96b9a4ec8c565c68e9575b9dc4b8e10b467"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "5e6cc132d9b66c69b3ec3f01e12de4f22137a4d59683289e5cfa013e6bf9a596"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "6de7ea96776c7e06d4b546859445d6e49491024235add068e422d059e849b53b"
