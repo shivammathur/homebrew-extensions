ee028fab0e27df38614bb566c3d2a78f166fb839
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-18 23:34:40 +0000

    expect@8.2: update 0.4.0 bottle.

--- Formula/expect@8.2.rb
+++ Formula/expect@8.2.rb
@@ -24,6 +24,7 @@ class ExpectAT82 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "ab2789a38ca29c024b522e58744b777265ce3eb4a95640422126e9d0bd7a97b2"
     sha256 cellar: :any,                 arm64_sonoma:  "48236f6fa03ea1048e16335debdca186caa69ae1ad401c5e7d18fa3c2382ab4d"
     sha256 cellar: :any,                 arm64_ventura: "6d3c65a3313cf6b88fbee66da48ab438843cf4435e15d61ade930b4e9d9ef0e9"
+    sha256 cellar: :any,                 sonoma:        "cd0d56b841f697277bff718072ea03df74f29fb4e1eafb137c7aa92dad187955"
     sha256 cellar: :any,                 ventura:       "e32b3dc0d9fbcb1ed18a6e8a6dbefb03c28153100f2ac98ebdac59530a8a34d7"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "82c175d6db881b19b1ae5ed1ddb262a56721c3538e8031175cd7fe9ef7a9213a"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "628fab7645a959ee03d61c0e81cf29939ca6f35042e065039a001124df65fb4c"
