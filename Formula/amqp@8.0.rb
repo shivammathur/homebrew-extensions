200fc489d2a6945028831b33927bdcc794486f2f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-18 20:48:20 +0000

    amqp@8.0: update 2.1.2 bottle.

--- Formula/amqp@8.0.rb
+++ Formula/amqp@8.0.rb
@@ -24,6 +24,7 @@ class AmqpAT80 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sonoma:   "9baf8cbbd4b5147fe76086ffe02df8641fd4845806fde5eb82abb23ded5e92ba"
     sha256 cellar: :any,                 arm64_ventura:  "fc25b6fc67b3354b3809d0bff4312fe52842202c30b913be93866fd09a49af25"
     sha256 cellar: :any,                 arm64_monterey: "4534512ecd49687b9b4a704917a93425cfb052db144ce4757ff22f37061516e7"
+    sha256 cellar: :any,                 sonoma:         "165007a5f3342f4f5facecad53b8fd8aaad5e4a23e3ce63f624d8c48fe5221c5"
     sha256 cellar: :any,                 ventura:        "61237e27e18f198728b2bd60da6659be2d3d6d141252bbf2dc5bf266bfe5da99"
     sha256 cellar: :any,                 monterey:       "99fe60bf2d8a145e4a9b100f82e9446979601541c6299369006a211daf1926f0"
     sha256 cellar: :any_skip_relocation, arm64_linux:    "0ea27eead6dcbe2c8370200952b47e61132e0fc8fc48ebfc4919a70d1c731210"
