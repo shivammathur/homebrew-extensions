15c371833d227dc84c5a886ac93931e6fda3510e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 09:23:24 +0000

    zstd@7.4: update 0.15.2 bottle.

--- Formula/zstd@7.4.rb
+++ Formula/zstd@7.4.rb
@@ -23,6 +23,7 @@ class ZstdAT74 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "8d400486a91ec73bdd7f21fe586ad2e565b634d118c788f81869694db0e58494"
     sha256 cellar: :any,                 arm64_sonoma:  "4a91807d0b759c43fbead7525d40632248d8546f58c0de7a028dbf5dec47e582"
     sha256 cellar: :any,                 arm64_ventura: "b4edef9422e24f4d79bd29d3d2655e65c470712709480a5dd20e68ab671ce337"
+    sha256 cellar: :any,                 sonoma:        "352bc122bd4b741af213420d6fcac6a271bca3cc67066d3cfaf9875900200a64"
     sha256 cellar: :any,                 ventura:       "6d4eb88ed1b297853206943485dbec9b5b9b036e8a188859a429946042ab372a"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "36bbd79c4ef6a38b71ae8b524f844297742628bba911adb3399ffa21d9472432"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "de08a4014734819e91fe0f5c27a0097d1cb272502af88c92849854b4b15e2b55"
