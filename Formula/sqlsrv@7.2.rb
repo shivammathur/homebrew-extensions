b9972081ecb74d06a7eb7eb5e9e4c08a74f00d4e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 05:33:09 +0000

    sqlsrv@7.2: update 5.9.0 bottle.

--- Formula/sqlsrv@7.2.rb
+++ Formula/sqlsrv@7.2.rb
@@ -20,6 +20,7 @@ class SqlsrvAT72 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_ventura:  "c01c3290a479ef533bd3ffd4478a501d93e7d41f84dea7f674e89a2de8f6a2be"
     sha256 cellar: :any,                 arm64_monterey: "7cd2bd68dc8c6ec66ba2eea8f11b909735c9541df901ea40228342a5bcc94ca5"
     sha256 cellar: :any,                 arm64_big_sur:  "e8b07d6500f01138ae7eb653dd4090a671dd7824d3cb8dc2c1ef0d9ae3489770"
+    sha256 cellar: :any,                 sonoma:         "552e4a577dd4bd627d1410738ba3b4aec817214bee0599883b2ce57b62fd9d69"
     sha256 cellar: :any,                 ventura:        "29a13ef06d117d7c15aa9a2fafd89b49bf8a97f81ab0c6a10a8391bf1b28ae05"
     sha256 cellar: :any,                 monterey:       "81259ea8536efc8a608d8e89ae0ce8d70354ac7cdeef90aed15cf3701513f556"
     sha256 cellar: :any,                 big_sur:        "0a0bfcf787a2307d5330aa530c544e2a0d1419f2757e004f76f6be65a253dd00"
