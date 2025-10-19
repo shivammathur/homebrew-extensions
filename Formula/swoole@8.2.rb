39154238fa641d1294f243f45eb2cd75068f0ba8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 06:21:50 +0000

    swoole@8.2: update 6.0.2 bottle.

--- Formula/swoole@8.2.rb
+++ Formula/swoole@8.2.rb
@@ -23,6 +23,7 @@ class SwooleAT82 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "be0eaed26a1b2c625326a8840b7a1cc8a34707584a754356e49dfa3d1b860906"
     sha256 cellar: :any,                 arm64_sonoma:  "85ebd0085528b536ef91aa6352e89b09b76e25b86d24e794d13c15f5e7b0e96c"
     sha256 cellar: :any,                 arm64_ventura: "773e6fbeb087b5a63e09856129309025befb9521209debad260423d6fc555208"
+    sha256 cellar: :any,                 sonoma:        "a0c1e22585c9dd716525b48d1e8aa6eda00501c80581e65720e16ca44b0c08f7"
     sha256 cellar: :any,                 ventura:       "1405d88a3675d62bf93c84a68ff982ee1a7032133500b2e69d09233587c62b6a"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "b28a93c03fbc06f4c5f0b251e0ce2d094d63a3ac89ea1073fb0dcf2560b59dac"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb10b6b622c80eec84ad130d588ef8a43c5a8f1c154e4a4740314e1d0d196ccc"
