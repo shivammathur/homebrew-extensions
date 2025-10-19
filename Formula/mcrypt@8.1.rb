2ec80fae3263338d02684aa0913aa5377313430f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 01:32:37 +0000

    mcrypt@8.1: update 1.0.9 bottle.

--- Formula/mcrypt@8.1.rb
+++ Formula/mcrypt@8.1.rb
@@ -23,6 +23,7 @@ class McryptAT81 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "a6e9d485a7d36198b93faa92fd9c5361d4bace2e73a160e64b18d2e7cde57d24"
     sha256 cellar: :any,                 arm64_sonoma:  "39b22f7c49d40e94b30284dae8ad7b364e05bcfed68cedbac190da9806d89099"
     sha256 cellar: :any,                 arm64_ventura: "d1d8e641611f0dbecbfe11b815e657cedd9024a416108745a5cacbf337959f8b"
+    sha256 cellar: :any,                 sonoma:        "fdf828f180c9657ea35412da47ae9182cd03bc52421de18fbec93379a0df05f5"
     sha256 cellar: :any,                 ventura:       "b315fb24de4fad9f3683b1ad5d71ca2a9ab1663f8a1b8cf8db56c3bb4c05bc2d"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "dbfc54fd20599364def1bb74456f914f677a29f716c3e6c8447ccaf7a90cb52d"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "6701b08bedc064458fe898334c66e55c0a029ac4b7da9a5462d0482335567185"
