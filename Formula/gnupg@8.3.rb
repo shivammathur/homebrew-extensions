49fb218dac91dca17ab10052a6a39298189cca75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 00:29:58 +0000

    gnupg@8.3: update 1.5.4 bottle.

--- Formula/gnupg@8.3.rb
+++ Formula/gnupg@8.3.rb
@@ -23,6 +23,7 @@ class GnupgAT83 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "3cc0b65f092f8f288ae11a37f36b8c8529edb60b154e1ede9acfb80e22193212"
     sha256 cellar: :any,                 arm64_sonoma:  "cb18d25849726e4864847e600a4cc5f5e0034654320a4130d86f8f9b7004f02e"
     sha256 cellar: :any,                 arm64_ventura: "c2c0189c1e00c7a44eac64aa22b7da08dfa124b33394e7074332cf4627b1a578"
+    sha256 cellar: :any,                 sonoma:        "92a869e8c95cae163f430f9afd5c8b7e14d7be75b82cb4d25041ea433c9b3650"
     sha256 cellar: :any,                 ventura:       "4c7b2d721cbad87d269ef833b01b102e7c8098363be97572fd084080d936cb92"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "e923f30eed8c37b68b04058dc1eaca94103f2106e003139e34ce2c379a754cc4"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "c02eb0a384780c1b48d51b59a065d00c605dea379547d114f9b4eefc1a40eb74"
