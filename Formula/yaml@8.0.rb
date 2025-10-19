73697ac9e20f7623f13228a9e5e7706048be4615
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 08:42:49 +0000

    yaml@8.0: update 2.2.5 bottle.

--- Formula/yaml@8.0.rb
+++ Formula/yaml@8.0.rb
@@ -23,6 +23,7 @@ class YamlAT80 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "7d0615c194d7d021e96ea3e8e9b3a60f4d9ad8f3ff036e47ed2629589335db51"
     sha256 cellar: :any,                 arm64_sonoma:  "98f37fb0953ac377a31d20dececb0e6ad0233c4d8a434f98e76e4ac6308039fc"
     sha256 cellar: :any,                 arm64_ventura: "5f8b7c8277ee5ad0df3d03285e0aa87e21437ab0c7f775546b17cea77a9fe88b"
+    sha256 cellar: :any,                 sonoma:        "07e43ad868c61d799178a1c328e6dbe6a1ab3f58bc8c10a4cc1ca2d1d0200341"
     sha256 cellar: :any,                 ventura:       "54344288f1c8da4b6cb60e16fcfbbc9a043f189555aa187826af38a2fa00e167"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "1603f968e05545c3fe3c11402b06dc7a3994b7bac91c21bd640340d91d08915e"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "801a2e96c928eba0d996d273e259d45fb131abcda4bf8aa4a38c10d66686ed15"
