effa9d47a17fba38f4fccc516735c851d9183cdc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 05:20:57 +0000

    snmp@8.2: update 8.2.29 bottle.

--- Formula/snmp@8.2.rb
+++ Formula/snmp@8.2.rb
@@ -23,6 +23,7 @@ class SnmpAT82 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "6038810fcebf39544b9350aac923d760c61722cf6e76bfbba5fc149f330016bb"
     sha256 cellar: :any,                 arm64_sonoma:  "219212501a28ef5a6dba91c4a2e1912627c5600a86d0e1cc28149fcb59836427"
     sha256 cellar: :any,                 arm64_ventura: "fda8b83b4d719deb2edf4de305d38381e33046226dad3763378b301d08b03258"
+    sha256 cellar: :any,                 sonoma:        "0f2ce0fe178847f8aa0727105627886cb4462689aacd79060fe6aa9d2de91adf"
     sha256 cellar: :any,                 ventura:       "8751cd709fdbdb77a1e6858fed74930f547d62ec7b864f0604c5e124c6e209a4"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "9edc7144a41ce359d904a16d1df46ea139d2dded057cb2474f902e8968e3671c"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "d768504f38113a475033602e83601acfda42c71180e24722e3f39aa2f56676d1"
