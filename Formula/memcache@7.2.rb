f83d8de4d26a70d2d7593f6b65fbd34cb22214e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 01:49:02 +0000

    memcache@7.2: update 4.0.5.2 bottle.

--- Formula/memcache@7.2.rb
+++ Formula/memcache@7.2.rb
@@ -19,6 +19,7 @@ class MemcacheAT72 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "0bffce610faa353a481c72c23527206981586d7ea53d17b464c22c0b2a9b72b7"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a8ca40c91ec709c206fd41a2fbdc72be699baa235969b425b3aa1d62aa04d5e6"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b47e577a71ec37a02000a5df1fcbeda5cf9bce28633daeff6acbde7d147fd7dc"
+    sha256 cellar: :any,                 sonoma:         "abc863f3734cd33c83d6bc6d8ee317b4c81b9b841d73f8a3203b4205c57d5696"
     sha256 cellar: :any_skip_relocation, ventura:        "1da95a1a8e73dc192c442ec449c1b852cb88b5654819b008c82ae921d05111de"
     sha256 cellar: :any_skip_relocation, big_sur:        "42e093cdcfb1b52bcb97b9b56c8eeddb7d0851d391c26ec921b2024fc3184e97"
     sha256 cellar: :any_skip_relocation, catalina:       "a71ae5bd6aa1211c1cfcdeb69dfb6c7a254735d01660efdb56587e43ba7c89ca"
