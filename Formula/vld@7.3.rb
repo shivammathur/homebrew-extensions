5aecb0d46ff5d5c240f0f763be893c0841c7bc3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 07:45:05 +0000

    vld@7.3: update 0.19.1 bottle.

--- Formula/vld@7.3.rb
+++ Formula/vld@7.3.rb
@@ -23,6 +23,7 @@ class VldAT73 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "50d0e199ce892c415e1edeed4f3b5c3f37a4d9cdee25de0bfeca6573d62c3e46"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f0faa8cd35b06cda3c618180a911488d628de5b3e2383160d491b9618903a2da"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "746fd108478a9e3dd306e78c98899fb79c73afc4d67b5c7dcd2123d7b340e22a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3574817cebbfb5341c308c6c7d3f2cb7bc7de53ca79adb274d1e5c69a0cf53dd"
     sha256 cellar: :any_skip_relocation, ventura:       "c0e9330263660e4194c754fd0611bbd1d5242e64cfc612f36db23e5514939afe"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "ffad7f0916c389a6c71c5cec85511277455d68e646f2c8d9e7aa3f4b655b3a57"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "0083d7cae15249b493ac30429f93c3064f27aceb86167051c7352c2e7aa1203a"
