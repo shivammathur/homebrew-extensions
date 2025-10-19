deaa0199a5df95726e0d04c0c68a01376a42bb20
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 08:03:46 +0000

    xdebug@7.4: update 3.1.6 bottle.

--- Formula/xdebug@7.4.rb
+++ Formula/xdebug@7.4.rb
@@ -15,16 +15,17 @@ class XdebugAT74 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 arm64_sequoia:  "6160312dd3a2f90f5a3f6df4d94c7423968cbf9a4390e727a95806f380b916bf"
-    sha256 arm64_ventura:  "23d415f21e02cdc58e73f1a722f556d32b6a208e6606545072de48697b9b773e"
-    sha256 arm64_monterey: "b159d154d361dd85bede60b5e022ef1760845e9b42b8b863095936dce228cd45"
-    sha256 arm64_big_sur:  "ab9bca1022c1243036d51a852412960dfd1f1da57e79eb19fdb1d03c4fc8e3c3"
-    sha256 ventura:        "b2f8e1b2079d0e9fd436dd52f8df4ff4d3606c2bfe3d41c41a8466de463bdc51"
-    sha256 monterey:       "8db8eb07582f265df2044db5251f75d03acfffc87dfff32e63bd1d37ac7611e5"
-    sha256 big_sur:        "b83e210c171ab66180684a86c13beceefd9a472284a005024665cc3020dd39dd"
-    sha256 catalina:       "6ab0c95388ab54af02f6fdd731eec06f3cc61c7b4bf590eb1782716537e6b39a"
-    sha256 arm64_linux:    "4028ecfaaca4bcf81bf03cc3f86e512d9594016d414a91d98711671652500abb"
-    sha256 x86_64_linux:   "e0f9e52312d052a041620c4ddbdbe68b16acc70c145a6ed5a351de903bb78bf1"
+    sha256                               arm64_sequoia:  "6160312dd3a2f90f5a3f6df4d94c7423968cbf9a4390e727a95806f380b916bf"
+    sha256                               arm64_ventura:  "23d415f21e02cdc58e73f1a722f556d32b6a208e6606545072de48697b9b773e"
+    sha256                               arm64_monterey: "b159d154d361dd85bede60b5e022ef1760845e9b42b8b863095936dce228cd45"
+    sha256                               arm64_big_sur:  "ab9bca1022c1243036d51a852412960dfd1f1da57e79eb19fdb1d03c4fc8e3c3"
+    sha256 cellar: :any_skip_relocation, sonoma:         "851040027ba5f32eef0da2e0fa418aac019994d9b792e8021c3d4404d30e5fea"
+    sha256                               ventura:        "b2f8e1b2079d0e9fd436dd52f8df4ff4d3606c2bfe3d41c41a8466de463bdc51"
+    sha256                               monterey:       "8db8eb07582f265df2044db5251f75d03acfffc87dfff32e63bd1d37ac7611e5"
+    sha256                               big_sur:        "b83e210c171ab66180684a86c13beceefd9a472284a005024665cc3020dd39dd"
+    sha256                               catalina:       "6ab0c95388ab54af02f6fdd731eec06f3cc61c7b4bf590eb1782716537e6b39a"
+    sha256                               arm64_linux:    "4028ecfaaca4bcf81bf03cc3f86e512d9594016d414a91d98711671652500abb"
+    sha256                               x86_64_linux:   "e0f9e52312d052a041620c4ddbdbe68b16acc70c145a6ed5a351de903bb78bf1"
   end
 
   uses_from_macos "zlib"
