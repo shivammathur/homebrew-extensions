4495ec2fd89150b51fb5186a78febfb7b51732dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 16:04:37 +0000

    v8js@8.5: update 2.1.2 bottle.

--- Formula/v8js@8.5.rb
+++ Formula/v8js@8.5.rb
@@ -16,6 +16,12 @@ class V8jsAT85 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 arm64_tahoe:   "6d656df4f1df60817c19a4d6ff83ddc4bd8c6ba62280a20424fd123e45506565"
+    sha256 arm64_sequoia: "77a8012195fc69281177d5fe7af95d633f2420f8d3a5c39adff7fa729a1b2487"
+    sha256 arm64_sonoma:  "ffade468ca87f8b8108889753324249342618c60d5aa1cc0885e3e3f868e5621"
+    sha256 sonoma:        "63ddd2a60ea82240bb81f8c8112ffaa460676dcaff3c3d266d997a69956ff284"
+    sha256 arm64_linux:   "97767d1dd57eedbd9aac3f5de477838e761a91354e35ae5042a2bfd21dfa1070"
+    sha256 x86_64_linux:  "ea1c59b76a5d8faa814325ae9671b35cb9f977b81a69cd4e23a359227eba7ee2"
   end
 
   depends_on "v8"
