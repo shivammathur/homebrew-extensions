50dd931cbc4c3089fc37f0b833b9f7cca3e59906
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 04:07:07 +0000

    psr@7.2: update 1.1.0 bottle.

--- Formula/psr@7.2.rb
+++ Formula/psr@7.2.rb
@@ -19,6 +19,7 @@ class PsrAT72 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "2125c6eb09cec11b96886e07c0bb158166efb7d9c81c4317291a983402c83406"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4494e93cbf3c8c7f5854cc8921bd43ad0bb900eb43ee69dd6b883ed9015d8a9c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1174d0390fa8ebdad9c352490709e5e9ba5a3c26307bd662a6d44735bb653b51"
+    sha256 cellar: :any_skip_relocation, sonoma:         "dd3f30a63c7dfde001257be4f925d41637d2422b76e360756cf4fcf0af7e9501"
     sha256 cellar: :any_skip_relocation, ventura:        "7a4c5b62b1166f031a811ce57804dfa3852a51efb637d2235ac86ce3a5354310"
     sha256 cellar: :any_skip_relocation, monterey:       "e8a3e3cbb4cf1f836733f8ae3b68a4ea12727b5a30d0feea3523569faa6e0db3"
     sha256 cellar: :any_skip_relocation, big_sur:        "6f628e993bd86d669febb15b30017def78885cda5a2e861b223f355090a32844"
