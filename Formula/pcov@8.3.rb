1ed4f31a085cef8b0f460a0185638d3ddf4922ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 02:52:41 +0000

    pcov@8.3: update 1.0.12 bottle.

--- Formula/pcov@8.3.rb
+++ Formula/pcov@8.3.rb
@@ -23,6 +23,7 @@ class PcovAT83 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "c9a686bebcddbe6ccfa64563988c045ac814152f6efc7f21b256a595e91d44de"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d3620489e8e59506d2af663d93211956f87416be9bb0b774583b06e81f0c6a37"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "79e927d6bb4a4e8376b88a4ba82fd16626e470cb1900c6a8b549699b568dfe4c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "88727800c0b6d8793e73f955205b9f42830603df23f2ad3c054a7262dba4d3cd"
     sha256 cellar: :any_skip_relocation, ventura:       "4c8062057c0474259953945445436330672897e40a830778eb0519cae8209c2e"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "0372f7cdc70e4ca19de33ee45d9f708052d65ff8aed17e0c2d0a82bd5ee0dc30"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "1bc81f618d0ffe4ac69088ced6e6b019d09e904d4e8f75d71c63257efcebc0be"
