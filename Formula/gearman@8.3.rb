28e07503136403d06303765aafa1a0598f573d6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 00:03:11 +0000

    gearman@8.3: update 2.1.4 bottle.

--- Formula/gearman@8.3.rb
+++ Formula/gearman@8.3.rb
@@ -23,6 +23,7 @@ class GearmanAT83 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "136f87a8e2b4939e042bfbdec1ef34090d2c9236b1388d81a1c67a72b1412453"
     sha256 cellar: :any,                 arm64_sonoma:  "0aadd2eca4c43bf638c0cbb172a161e3a3af945b008b689c85cacd067aa8885a"
     sha256 cellar: :any,                 arm64_ventura: "ff3b10614cad409a6d6905ff114d95a4ecea060d38a84ffcca1d9225d2e29cd2"
+    sha256 cellar: :any,                 sonoma:        "a81652ee9754177d5890a94603a78498bcf547c8e5ac1c2205f53cbd3ef4c744"
     sha256 cellar: :any,                 ventura:       "5d8d6677a0ad86575a346e0c621c8055ef685e4ae0349077e23a04117d0c4a63"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "a0ab6896e61701812d2d05d211b54fafb93f6ad4d7a3002e5693c50a083a2b12"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "82ebbc862d7fdcaeb24af209c78c81cf5ee3f533bd10ff93902e25b329024f09"
