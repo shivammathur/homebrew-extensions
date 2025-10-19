0875a1774687b6b1e20a0234ca877a54fea0d4e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 17:20:45 +0000

    phalcon3@7.1: update 3.4.5 bottle.

--- Formula/phalcon3@7.1.rb
+++ Formula/phalcon3@7.1.rb
@@ -18,6 +18,7 @@ class Phalcon3AT71 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "ba702baae6aad7e7885e646a91c3ac4a1f4c019b1c3db40b216ba895ecbb09bb"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ccb4c43b2a5ddff5991b0f7e28db82c30e7c9077c3795bbb973286a51cbf8883"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0598a203279e0f07695d3e1c7648bf16e797ded88aaad1d49104483d4596c63a"
+    sha256 cellar: :any_skip_relocation, sonoma:         "b5cb55a26b373f2af5226eb11fa0c0abe38eb400d0f36db2df055601b0aef182"
     sha256 cellar: :any_skip_relocation, ventura:        "a8429780e32fceded97d3da7003d5061556faec688e96ad64512e57619f2e6f3"
     sha256 cellar: :any_skip_relocation, big_sur:        "7b45f98d2144ef08f4649fc8ae181787c1bbfc7a7cb0519f07f4a3e6d8401d6b"
     sha256 cellar: :any_skip_relocation, catalina:       "95ba6a141cee048cad040562a79b2b41b08a393f2d525884639dbf9c4bd03c60"
