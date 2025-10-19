fd17c3577d0e76f61e12eb263562a850e15e6c86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 00:58:28 +0000

    lua@7.2: update 2.0.7 bottle.

--- Formula/lua@7.2.rb
+++ Formula/lua@7.2.rb
@@ -25,6 +25,7 @@ class LuaAT72 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_ventura:  "17018d6b760702687a9677913388d0013f308aeff5f7bdf708b98c1bcc785ea4"
     sha256 cellar: :any,                 arm64_monterey: "a3d2669a9cff387d8a82daeeb51caced84b76e322e86208b5945906bb319dc36"
     sha256 cellar: :any,                 arm64_big_sur:  "4a3ee5f70f4d3405073d5d5e70416c59f125821de4a9dc8adcd04917ee84ba7a"
+    sha256 cellar: :any,                 sonoma:         "1309ea6700464e0b493c5ef4aa1fc31cb6f2c29d30529e2b19fe71bb2075af44"
     sha256 cellar: :any,                 ventura:        "185b191335656cbdc2e4fe3ab7dfc0efde86b3139638a29e8925f7d21c48c957"
     sha256 cellar: :any,                 monterey:       "709fa4876039ec3a9bfa786fc84c7cb6ddc6a9d1f76bd87178d64d4db631e1e7"
     sha256 cellar: :any,                 big_sur:        "b1a07d7300e3e79ace0cc4cb6c149321396fb87b407f824d05e180db0056a3bc"
