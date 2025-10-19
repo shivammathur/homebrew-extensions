b1b99d1e95d4ff63f0a3564facebb7f7063cc73a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 07:59:44 +0000

    xdebug@7.1: update 2.9.8 bottle.

--- Formula/xdebug@7.1.rb
+++ Formula/xdebug@7.1.rb
@@ -19,6 +19,7 @@ class XdebugAT71 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f2c7a1031b8e9d0c2caebcf37c370a34185e6f87a5dc5e6f054d22ecbe32dac6"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e5075432964bed41bcbdf45419842173f8177fda94027d45e880ae2e5edbde93"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4fa906e4e575df3df5ecbdf76854c388817ad014689927ee6b202e102cb5efe1"
+    sha256 cellar: :any_skip_relocation, sonoma:         "8268f20c67fb3180a473810797bba138122867ef28295fef0f9dc671f7071f0d"
     sha256 cellar: :any_skip_relocation, ventura:        "c4bb38b1dc9563e610f766f43a02ac9becd07bdeb42c9b8248780c27811b3506"
     sha256 cellar: :any_skip_relocation, big_sur:        "a47a76c5abceaf29d9ce37ca5a5cd299dd604649517a894e885486789f2b7b75"
     sha256 cellar: :any_skip_relocation, catalina:       "6bbd0da640e9b1060634f15909ae53804efb34afdce1fcb0793cbf7bc7f2fcb3"
