2c7c1b7d86b8318a7eb3d9ab68064bcd9b3b486c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 07:45:17 +0000

    vips@8.3: update 1.0.13 bottle.

--- Formula/vips@8.3.rb
+++ Formula/vips@8.3.rb
@@ -24,6 +24,7 @@ class VipsAT83 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "3c98cc6451e0255457dc0ec4b20fc12d78214a0ecf56bcd3a9c13fa1c17d7665"
     sha256 cellar: :any,                 arm64_sonoma:  "72e9d4be9a5c64b328de73bf7eca62d1b022d246a952aa17d714ddf404357930"
     sha256 cellar: :any,                 arm64_ventura: "d94b002f9e4ab1f03686ee7f5ec08a79ebd670ecc8ece646d880904e03d52d89"
+    sha256 cellar: :any,                 sonoma:        "5c1863c67ea306b3e25b4f01345c2adb7a7fe664c07c4199441bce46867246f9"
     sha256 cellar: :any,                 ventura:       "33928feb382e6fab6a34b5aa0e8583ca9af593509da34d2e8d814d967ee79e17"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "52c78cd510a386213e7fcc8722444be91b96ba9dd8a5198af1b51f9bac005ac8"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "73896b7b457c035d4c8aae86ce50404a4eeaca6c61fae13df97d9284c7835c92"
