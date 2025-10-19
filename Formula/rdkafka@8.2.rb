282516546ec81da155f634bc65682b3cc3344524
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 04:41:52 +0000

    rdkafka@8.2: update 6.0.5 bottle.

--- Formula/rdkafka@8.2.rb
+++ Formula/rdkafka@8.2.rb
@@ -23,6 +23,7 @@ class RdkafkaAT82 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "aec9336326ca29fef3f6d002f7bc2f5126b74ee5ffbd491b3a5764db16d2f6e6"
     sha256 cellar: :any,                 arm64_sonoma:  "a5c68cb62c84539765a7ae56c999ced8b585cea11b960e6674b94676e530247c"
     sha256 cellar: :any,                 arm64_ventura: "46ac5bcf8ba5a13e453a2b02d54d90e758989fc8c757fb4a3f5a1bc50587e1dc"
+    sha256 cellar: :any,                 sonoma:        "b0c8fe651c3e1a8ddb2609547bd1892d472d616a04b10cab8fac1eb09164e262"
     sha256 cellar: :any,                 ventura:       "daf689ba8993dcf86de7fec5041637ddd8b8dcc165ae6b428c074e27a61bdb71"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "88c0d3820b2bec18bb97c016af8c273711b13df47c337735ba8bc7edf19d606c"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "83bfc50c5bb638d9514d0625899a6ee7b4c55cf9ac4c307bc3d214263b867bef"
