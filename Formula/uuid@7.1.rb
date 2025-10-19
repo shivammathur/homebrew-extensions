9eb8974cb3e4e722c8df769dd83e7debf8d04a2f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 06:38:20 +0000

    uuid@7.1: update 1.3.0 bottle.

--- Formula/uuid@7.1.rb
+++ Formula/uuid@7.1.rb
@@ -23,6 +23,7 @@ class UuidAT71 < AbstractPhpExtension
     sha256 cellar: :any,                 arm64_sequoia: "0ded6d6816cda75fd9682786a8ce2ef34656d8617233f65d6cf6f9b617292e6d"
     sha256 cellar: :any,                 arm64_sonoma:  "bbdc7d15a1f062ea219485798bd664e413f2ae2e78c94ad7ebd488e1b8864082"
     sha256 cellar: :any,                 arm64_ventura: "b114e21f1970a26a50d81f845dacb040045d6e3d308cf579902fba0fdfa27368"
+    sha256 cellar: :any,                 sonoma:        "1e6aa9c1fcaad9a04ccfc8674b223afb5aafac44a529ff21bc645b3d49e38df4"
     sha256 cellar: :any,                 ventura:       "bf58ac7d7dad15c72f0b0523d1ae14031dbff6a0358b040659f3cd307a9d7cbe"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "f9cca10885383c8d4df9f194a6e5ba09fd0b9242dbc167a6153a713ad6b73a29"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "a85dead91f08617f0c91fdf19fbda63ceffc632097ca8a9ab7e7d5c77e5f9614"
