27e7b336fa77068fe2cb02fb96a1d8daf5661b94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-24 08:06:55 +0000

    grpc@8.1: update 1.76.0 bottle.

--- Formula/grpc@8.1.rb
+++ Formula/grpc@8.1.rb
@@ -20,11 +20,12 @@ class GrpcAT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8843f7ace56fe3f353e6281f1acaf03c093aea95ae60ca0adecc62b4b09c3524"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "46251e28f463c4f0bdad52bfd6932c772dca9229f4133634dfcc455256d2e904"
-    sha256 cellar: :any_skip_relocation, sonoma:        "55e5d0476ec7cb3e295f772d4ca5d5dee2de217f96e41e705b17622580f707ce"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8854c4e4155a0a2c4f76dd42462050176669efb4a14271bfed55cfce9a3e96e4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b65d21dce46f267efe06f806c10e8c2f65ea0c25810339f654a1dee529c3017"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4fb2781bb1987cb2c68e2090de3a926a61cc4e8cc49d555408e2cfb0df3499ae"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "acb2d2e575dc515dca1dd360520e36d203c32961c0c29de7862bdde70068bacb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f0d7042b0818055c09df33cd5276f3da1de66f4300cf4cc12246d48305dd0df3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "032f9b1ad4e9796b785e72114edad84bfee6a4ebca387fc466179f89dd68b40d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "47c0f862a319df31d560d7bacff4da55fdb0e3c72fe8afd5d2c021a39c9d64cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6af2db2457b4a2306d4e2589f6d2c0a9302fa5c3375e771c9beda0896dcc650e"
   end
 
   depends_on "grpc"
