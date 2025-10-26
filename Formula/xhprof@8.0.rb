4a432709844c6714445ce2bfab343813686c42da
Shivam Mathur <shivam_jpr@hotmail.com>
2025-10-27 01:34:54 +0530

    xhprof@8.0: Add missing formula

---
+++ Formula/xhprof@8.0.rb
@@ -0,0 +1,32 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+class XhprofAT80 < AbstractPhpExtension
+  init
+  desc "Hierarchical Profiler for PHP"
+  homepage "https://github.com/longxinH/xhprof"
+  url "https://pecl.php.net/get/xhprof-2.3.10.tgz"
+  sha256 "251aee99c2726ebc6126e1ff0bb2db6e2d5fd22056aa335e84db9f1055d59d95"
+  head "https://github.com/longxinH/xhprof.git", branch: "master"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/xhprof/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "xhprof-#{version}/extension"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-xhprof"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+  end
+end
