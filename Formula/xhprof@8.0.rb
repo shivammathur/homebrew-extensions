ee773c605063fedf6f00b263e025df9095a6cc97
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-26 20:21:29 +0000

    xhprof@8.0: update 2.3.10 bottle.

--- Formula/xhprof@8.0.rb
+++ Formula/xhprof@8.0.rb
@@ -19,6 +19,12 @@ class XhprofAT80 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a5de6442fe34ae303a53069eb17bdd565407ad392eebaf06ae97103e3c600f54"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b58a8b96692cc17a1b175e278155978a23fb6b7e66be2a655408067e927feca0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "90828d1760b04b5a1e2640569a61c4cabc4de3500c8512a45eb772b2487bab46"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fe2a3cd2a091d31cb31dfc8c39425cfcd28d275ca554f17c25cb042804129021"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "778a38ccf4efa3c239212e6b48bc13027d3d1d0c4390e20983d8208fa8dd4819"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8f11d658389acd40399dafeb8e7ebff8d59cdb57dde807cff51df9905b985c8"
   end
 
   def install
