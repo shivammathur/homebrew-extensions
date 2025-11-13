93a51879b4189285e34b67313715cb476a178c8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-13 05:16:40 +0000

    memcache@8.6: update 8.2 bottle.

--- Formula/memcache@8.6.rb
+++ Formula/memcache@8.6.rb
@@ -20,12 +20,13 @@ class MemcacheAT86 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "322b3e42a428dd8bd5f0487fda764c1f7886e2da13e893d42ee6dbcba2bf97d1"
-    sha256 cellar: :any,                 arm64_sequoia: "61dd510a112aeb899aa0f4ce29e44e341e19399f527db604ebded1ab250d1e1d"
-    sha256 cellar: :any,                 arm64_sonoma:  "36685acff4139e25fe8e3a159e511c31a9f07f29958babd85f940069130fdff1"
-    sha256 cellar: :any,                 sonoma:        "7a56fbde3334ec68365d30cd8417c51462b8f8f94e101c3e91321ceff71fb824"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5f45a2c8e57e05b62048ab87325c64df200a8f585662ed133108fea29b8751b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ffdda9b08bd248518486ed0c960df91303b5d33e6680bb09ac65422ba75302ad"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_tahoe:   "c11bcb1ce82d0d9278c83d756dd87e52220ef4cbefe04b697d05dae1d011b0fe"
+    sha256 cellar: :any,                 arm64_sequoia: "ed44e093d119e88a90f7040e3413b859114c0ab94a29f35e5efd180b215cee20"
+    sha256 cellar: :any,                 arm64_sonoma:  "55eb672d92e85fcac5e66e10613502a515a4d7b4a93341ca18dea43413ca7db6"
+    sha256 cellar: :any,                 sonoma:        "d56640e406c662b1af11d3f337e0d57269306e01a1aec79248bfbb6681768d4b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c08ccbba71e3c1fc33d63b4b7b87bc8e93f4ee2b0104351163186623e8fc9bae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c66568073edc1d5718c27b7dda5b7e7a7081323121891c372be2d4b56474b9e2"
   end
 
   depends_on "zlib"
