7e12d5812756e99f04de9f3e9d92472eb9662329
Shivam Mathur <shivam_jpr@hotmail.com>
2025-11-13 22:43:28 +0530

    protobuf@8.6 4.33.1

--- Formula/protobuf@8.6.rb
+++ Formula/protobuf@8.6.rb
@@ -8,8 +8,8 @@ class ProtobufAT86 < AbstractPhpExtension
   init
   desc "Protobuf PHP extension"
   homepage "https://github.com/protocolbuffers/protobuf"
-  url "https://pecl.php.net/get/protobuf-4.33.0.tgz"
-  sha256 "37cf760871f3d846f35ec43ba1b6531e34f4712535ff81a048f47a1ba4f3dbad"
+  url "https://pecl.php.net/get/protobuf-4.33.1.tgz"
+  sha256 "a7cb88ade340f654d78f8050d7915ce915e51b0accb859caa7bc37b9f3ea9f2f"
   head "https://github.com/protocolbuffers/protobuf.git", branch: "main"
   license "BSD-3-Clause"
 
@@ -31,6 +31,10 @@ class ProtobufAT86 < AbstractPhpExtension
   def install
     Dir.chdir "protobuf-#{version}"
     patch_spl_symbols
+    inreplace %w[
+      map.c
+      message.c
+    ], "zval_dtor", "zval_ptr_dtor_nogc"
     safe_phpize
     system "./configure", "--enable-protobuf"
     system "make"
