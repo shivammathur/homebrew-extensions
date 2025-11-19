3dcad1d8f220127ddc2a791abc22c40d8715e0ef
Shivam Mathur <shivam_jpr@hotmail.com>
2025-11-20 04:59:47 +0530

    mongodb@8.0: revision bump (icu4c@78)

--- Formula/mongodb@8.0.rb
+++ Formula/mongodb@8.0.rb
@@ -10,7 +10,7 @@ class MongodbAT80 < AbstractPhpExtension
   homepage "https://github.com/mongodb/mongo-php-driver"
   url "https://pecl.php.net/get/mongodb-1.20.1.tgz"
   sha256 "614e57594918feb621f525e6516d59ce09b78f5172355ba8afb6c2207c1ce900"
-  revision 1
+  revision 2
   head "https://github.com/mongodb/mongo-php-driver.git", branch: "v1.20"
   license "Apache-2.0"
 
@@ -25,7 +25,8 @@ class MongodbAT80 < AbstractPhpExtension
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "23e106cf09940ed63642cb6c5fc470e25ba4154443d896fdf0ed1883d3c4d499"
   end
 
-  depends_on "icu4c@77"
+  depends_on "cyrus-sasl"
+  depends_on "icu4c@78"
   depends_on "openssl@3"
   depends_on "snappy"
   depends_on "zlib"
