8b5a73e6b2cd11728f05910af798740d621296b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-20 01:05:19 +0000

    newrelic@8.4: update 12.2.0.27 bottle.

--- Formula/newrelic@8.4.rb
+++ Formula/newrelic@8.4.rb
@@ -20,13 +20,12 @@ class NewrelicAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_sequoia: "66a3b0bb7fee8effd8f4c0617af2237028caada74fb29bcd685607409a5a1cd4"
-    sha256 cellar: :any,                 arm64_sonoma:  "52a319d9153c51b5d7f3f2c60a42b15d9686a0b1f0c6ef5efb777cb6d9ace988"
-    sha256 cellar: :any,                 arm64_ventura: "78867a24ed345b6312fce42d8f6862337c6e452fa7bfd97690dd29c69558054e"
-    sha256 cellar: :any,                 sonoma:        "834b7d958c53519f84ab8ff9d5a47c9367f8e460462d8a12afbe9c5e3a0000ae"
-    sha256 cellar: :any,                 ventura:       "312a13c53d339dc54ef27586f7d68fe8cfc7b6a44eb9f959bf80cc7c89c265cb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1103d8987b29e7096a6f2ab900d31878f716c221c9d9db5c5d78dcc63e62498b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d95a3196da1cbbee0389b935e06c4e3910024cc6755ae9cb1394ac129e8a4d4f"
+    sha256 cellar: :any,                 arm64_tahoe:   "28de4886c3f3e84156a35be58dee5914356b8350c71c2a39d7ef6643f48906ca"
+    sha256 cellar: :any,                 arm64_sequoia: "3787f24d5a7d3bf2a75b4d71675421abcb6216c78e505ecb03ef1ab57dd0ca32"
+    sha256 cellar: :any,                 arm64_sonoma:  "02a709c512332257c06ce72c9aadcac942eb1a7e87811d6579101d7ddc379ef6"
+    sha256 cellar: :any,                 sonoma:        "f2b30045536193250204aa211e3187d401a87f18331e19a8cd57c00474a2bfaa"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "21acd38a6b357b8483bed3571707a3948ce3f3783ee018e761104143018a3fe6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "91d9604ec95f46ba920b0016e2694251ba43ea82c4949f7205ffff10e512caa5"
   end
 
   # for pcre_compile
