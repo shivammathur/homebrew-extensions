058d63857727508beaa6ec677082d7c3e7eedae2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-28 23:02:22 +0000

    gmagick@8.5: add 2.0.6RC1 bottle.

--- Formula/gmagick@8.5.rb
+++ Formula/gmagick@8.5.rb
@@ -18,6 +18,16 @@ class GmagickAT85 < AbstractPhpExtension
     regex(/<v>(\d+\.\d+(?:\.\d+)?(?:RC\d+)?)(?=<)/i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_tahoe:   "d99ff43ba1900a4e9aa50ca0a328ac7f4666918e399c811897fc74feb4f816d5"
+    sha256 cellar: :any,                 arm64_sequoia: "09d5ab0ac282a21fff9a92077da8a9c45b2e05c7e5b1dc7147312e8e9176d632"
+    sha256 cellar: :any,                 arm64_sonoma:  "1fc1f5f5cf9062fe243c0767ad9b5bc059d674a69b568e8aa67110216b4f7431"
+    sha256 cellar: :any,                 sonoma:        "16f03ac382ff5f33485a27d0a8a8f72e25e1377d998132d72cde73f2e7871479"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "73728a106298be75b94a414685e59adf4e097ce65045a661811d77ac53f74e93"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "286d85c77bf1ce544589b3d8a45d9aebaf989c2d83f16279232b920f518383fe"
+  end
+
   conflicts_with "imagick@8.5",
 because: "both provide PHP image processing extensions and should not be loaded together"
 
