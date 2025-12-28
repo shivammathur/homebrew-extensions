cc334ca24b397ad67f6e42f1101b8a28d123588d
Shivam Mathur <shivam_jpr@hotmail.com>
2025-12-29 05:18:38 +0530

    scalar_objects@7.3: add new formula

---
+++ Formula/scalar_objects@7.3.rb
@@ -0,0 +1,32 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for ScalarObjects Extension
+class ScalarObjectsAT73 < AbstractPhpExtension
+  init
+  desc "Scalar objects PHP extension"
+  homepage "https://github.com/nikic/scalar_objects"
+  url "https://github.com/nikic/scalar_objects/archive/86dbcc0c939732faac93e3c5ea233205df142bf0.tar.gz"
+  sha256 "a0f621772b37a9d15326f40cc9a28051504d9432ba089a734c1803f8081b0b39"
+  version "86dbcc0c939732faac93e3c5ea233205df142bf0"
+  head "https://github.com/nikic/scalar_objects.git", branch: "master"
+  license "MIT"
+
+  livecheck do
+    skip "No tagged releases; using latest commit"
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+  end
+end
