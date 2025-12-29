79e84de774966021760d9e5db3c973eb105ea3c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-29 00:20:36 +0000

    scalar_objects@7.2: update 86dbcc0c939732faac93e3c5ea233205df142bf0 bottle.

--- Formula/scalar_objects@7.2.rb
+++ Formula/scalar_objects@7.2.rb
@@ -20,6 +20,12 @@ class ScalarObjectsAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ecd16ffbcd54bdb4526c57de17c35a5b703512351bc9ee83b37b7e1618f4c1a0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3d074769db6f224a9b0b5fd1e95d69449bef1a4cfcc365287f0313246f0850ad"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ccd40669f1b149600e71a95145eefaaabdc475639b42a854c5bdda605724252c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f8cb6fd8ad53e05df6d4bdf5125799c2e5a735af21156b88b633a21435c5a15f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4aa4a4b9af9ca971837f03f495906af74f9f59a8514bf293d17e94be640aa2bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76fea1e38b2ee37cbbfddee5d532b518b83a7168f5bd3975a51b89fa2cc6ec96"
   end
 
   def install
