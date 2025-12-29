7c1fb5cc52914cab0728bcf22781c1a90ebfd98c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-29 03:09:45 +0000

    swow@8.4: add 1.6.2 bottle.

--- Formula/swow@8.4.rb
+++ Formula/swow@8.4.rb
@@ -18,6 +18,16 @@ class SwowAT84 < AbstractPhpExtension
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256                               arm64_tahoe:   "cbff4a6c46c10ec6781e45a7c505219c939952068e7ae1d12977b66492adadaa"
+    sha256                               arm64_sequoia: "efdeabb231ac775c539cf59fab65863f55791e305373e37e710e034bd7e054c3"
+    sha256                               arm64_sonoma:  "e2bb43184b85fc9e14a96b1048799393a4fb9679dd93ac25f1621fd552c357c6"
+    sha256 cellar: :any,                 sonoma:        "462cf5fbb066bd58468ecdd3dd0502c85e04d0bfe0416ba017ee0ad1a98106c5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9b1c49138c1acb72875f9d02c9e8a88990a517f6fb87a30b3b48f1f8ffd5a821"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c115fa17f8eca91bc0864ecd739da4c874c67041df47e268b83b59f4dd5e94c9"
+  end
+
   depends_on "openssl@3"
   depends_on "libpq"
 
