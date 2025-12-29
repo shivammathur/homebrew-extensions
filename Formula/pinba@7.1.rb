1a94a2548275e820a01860eb80c569bb269eb640
Shivam Mathur <shivam_jpr@hotmail.com>
2025-12-29 11:01:15 +0530

    pinba@7.1: add new formula

---
+++ Formula/pinba@7.1.rb
@@ -0,0 +1,60 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Pinba Extension
+class PinbaAT71 < AbstractPhpExtension
+  init
+  desc "Pinba PHP extension"
+  homepage "https://github.com/tony2001/pinba_extension"
+  url "https://github.com/tony2001/pinba_extension/archive/refs/tags/RELEASE_1_1_2.tar.gz"
+  sha256 "7df27fb3e9de548459b56638394d824781c8f13395da1a0b7f94515f03e15ddc"
+  head "https://github.com/tony2001/pinba_extension.git", branch: "master"
+  license "LGPL-2.1-only"
+
+  depends_on "protobuf-c"
+
+  livecheck do
+    url :stable
+  end
+
+  patch :DATA
+
+  def install
+    args = %W[
+      --enable-pinba=#{Formula["protobuf-c"].opt_prefix}
+    ]
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+  end
+end
+
+__END__
+diff --git a/pinba.proto b/pinba.proto
+index 1596199..b9947ae 100644
+--- a/pinba.proto
++++ b/pinba.proto
+@@ -24,4 +24,6 @@ message Request {
+ 	optional string schema          = 19;
+ 	repeated uint32 tag_name        = 20;
+ 	repeated uint32 tag_value       = 21;
++	repeated float timer_ru_utime    = 22;
++	repeated float timer_ru_stime    = 23;
+ }
+diff --git a/config.m4 b/config.m4
+index b50499b..4475a03 100644
+--- a/config.m4
++++ b/config.m4
+@@ -8,5 +8,7 @@ if test "$PHP_PINBA" != "no"; then
+ AC_CHECK_HEADERS(malloc.h)
+ PHP_CHECK_FUNC(mallinfo)
+ 
+-  PHP_NEW_EXTENSION(pinba, pinba-pb-c.c pinba.c protobuf-c.c, $ext_shared,, -DNDEBUG)
++  PHP_ADD_LIBRARY(protobuf-c, 1, PINBA_SHARED_LIBADD)
++  PHP_SUBST(PINBA_SHARED_LIBADD)
++  PHP_NEW_EXTENSION(pinba, pinba-pb-c.c pinba.c, $ext_shared,, -DNDEBUG)
+ fi
