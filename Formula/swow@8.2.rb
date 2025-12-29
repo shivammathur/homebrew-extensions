87db90ac4a29ef3e9f1ed111e7672cbb4f64b375
Shivam Mathur <shivam_jpr@hotmail.com>
2025-12-29 08:11:22 +0530

    swow@8.2: add new formula

---
+++ Formula/swow@8.2.rb
@@ -0,0 +1,40 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Swow Extension
+class SwowAT82 < AbstractPhpExtension
+  init
+  desc "Concurrent coroutine network communication engine"
+  homepage "https://github.com/swow/swow"
+  url "https://github.com/swow/swow/archive/refs/tags/v1.6.2.tar.gz"
+  sha256 "4939bb0390ad95861e7f98c279df41a7a00ca21fc94383be812a5163d63598e7"
+  head "https://github.com/swow/swow.git", branch: "develop"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "openssl@3"
+  depends_on "libpq"
+
+  conflicts_with "swoole@8.2", because: "both provide swoole-like coroutine functionality"
+
+  def install
+    args = %w[
+      --enable-swow
+      --enable-swow-ssl
+      --enable-swow-curl
+      --enable-swow-pdo-pgsql
+    ]
+    Dir.chdir "ext"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+  end
+end
