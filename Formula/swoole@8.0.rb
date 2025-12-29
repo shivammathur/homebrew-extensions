2eaecabf404f4e5038a42a63217b13b2a564d49a
Shivam Mathur <shivam_jpr@hotmail.com>
2025-12-29 06:45:35 +0530

    swoole@8.0: update formula args

--- Formula/swoole@8.0.rb
+++ Formula/swoole@8.0.rb
@@ -10,6 +10,7 @@ class SwooleAT80 < AbstractPhpExtension
   homepage "https://github.com/swoole/swoole-src"
   url "https://github.com/swoole/swoole-src/archive/v5.1.6.tar.gz"
   sha256 "0df87a2257f800607d38b6c703789facae5e1d9a9e78cd4a52c3fdc9b6fb64eb"
+  revision 1
   head "https://github.com/swoole/swoole-src.git", branch: "master"
   license "Apache-2.0"
 
@@ -25,7 +26,9 @@ class SwooleAT80 < AbstractPhpExtension
   end
 
   depends_on "brotli"
+  depends_on "c-ares"
   depends_on "curl"
+  depends_on "libpq"
   depends_on "openssl@3"
 
   uses_from_macos "zlib"
@@ -33,11 +36,18 @@ class SwooleAT80 < AbstractPhpExtension
   def install
     args = %W[
       --enable-brotli
+      --enable-cares
+      --enable-http2
+      --enable-mysqlnd
       --enable-openssl
+      --with-openssl-dir=#{Formula["openssl@3"].opt_prefix}
+      --enable-sockets
       --enable-swoole
       --enable-swoole-curl
-      --enable-http2
-      --with-openssl-dir=#{Formula["openssl@3"].opt_prefix}
+      --enable-swoole-pgsql
+      --enable-swoole-odbc=unixodbc
+      --enable-swoole-sqlite
+      --enable-zstd
     ]
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig, *args
