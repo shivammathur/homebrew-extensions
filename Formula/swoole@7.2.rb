c8e23b675ee1514aad23b15b6fd95bdbd1568b02
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-29 01:47:33 +0000

    swoole@7.2: update 4.8.11_2 bottle.

--- Formula/swoole@7.2.rb
+++ Formula/swoole@7.2.rb
@@ -16,16 +16,12 @@ class SwooleAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sequoia:  "4dde7267f3582ac59bd628b187bb3a9fa0f61c8db265c7e17e7eccc040a76308"
-    sha256 cellar: :any,                 arm64_sonoma:   "49d88338b65aaadecaeb450ecc7144b8fcf5c01f3e518a0121d054ee71f81c58"
-    sha256 cellar: :any,                 arm64_ventura:  "cd6993d7d3eb7ea619e8d673b792c4abdf33d462f3936d7d4420bedd7fa966e4"
-    sha256 cellar: :any,                 arm64_monterey: "df0e50ae6f944f05b4a8f3b77ea693fb8e35c79fef3f705dab8cadd0c236230b"
-    sha256 cellar: :any,                 sonoma:         "d873b6fa4745e8065e610e5a626559a187f0217214afea8f80db78189ad0a195"
-    sha256 cellar: :any,                 ventura:        "4d15c4667e82b5c01b67741b31f6e38163bcf55a2372223c274e55e9d8425ad2"
-    sha256 cellar: :any,                 monterey:       "743a3f30d52eb1e8671d280b6423ac15de33c9cf2017c0b19cd87f6ef96b22e3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "23e465ecfe01addfe7fb56e6bf303f1d8705df96f9f6fdc60df1645506011999"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3d6ddaf96a0f09db3feae2dd942b59ee2f87ce74e31c5d127af5fed2f28c21b4"
+    sha256 cellar: :any,                 arm64_tahoe:   "768cf4001933551674514006919a5c70de3a5d325149c9a76ec9aedef00c0ad4"
+    sha256 cellar: :any,                 arm64_sequoia: "4f9a958055c1df80ae27fc69344c76d50f270d4b518b0e6ae0379852a39a2cb2"
+    sha256 cellar: :any,                 arm64_sonoma:  "7dbfe54d8367fc116bcd0608a7de0a3d33a52f6e90b8caf1fcac35b380782c8d"
+    sha256 cellar: :any,                 sonoma:        "371a2ca55641b7600cc1cd12aeffac082c7825fae6ba2e63dce0c323252d4e45"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9eba2c105349cfe9af9325b461da8bf11c42be50451aa52f2ca3148ab391162c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "55b832a35556e69805976a0ec146a021d9c9b37f237f281546303bab05cfc4a0"
   end
 
   depends_on "brotli"
