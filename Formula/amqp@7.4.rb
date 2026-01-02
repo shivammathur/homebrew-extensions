e4a90a296a2b0d69137440972a145f438dcb7e1c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-02 18:07:53 +0000

    amqp@7.4: update 2.2.0 bottle.

--- Formula/amqp@7.4.rb
+++ Formula/amqp@7.4.rb
@@ -20,15 +20,12 @@ class AmqpAT74 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_sequoia:  "b4f13acb8d6045623296f99b1b656748f250f0b04b5f430bd64ea42079b31545"
-    sha256 cellar: :any,                 arm64_sonoma:   "300fada8c790c0410792cfed769baa842c28029f375dff140512dc31424f90b4"
-    sha256 cellar: :any,                 arm64_ventura:  "8b29546bf132a9e6acf3f2d9e4bb1f430ed92ae973514b946d27492c67aee831"
-    sha256 cellar: :any,                 arm64_monterey: "d3ac6a5be87d2ebffa5173077427f27f97dddbf46fa76017a4cd57fbe67a1adf"
-    sha256 cellar: :any,                 sonoma:         "05c17ed1b1ea7646c6c330fbcb25885dcff8c3c0025e5637d1a2e5edcf8591f7"
-    sha256 cellar: :any,                 ventura:        "53f42d4caf02bb13d90a376c3ab1adf9a35bec6436a38fb2d24b7ab543cd8050"
-    sha256 cellar: :any,                 monterey:       "4c0760756eb3e2a28252954718f814303f062d45248e4aab8a66441837a35e9f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "a42b472b649d9c2763604a2cb4422d57cf511a3f247f23e0baccd7853adeb721"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2d30b4059a4aa1df6ac597df5d45d9b14916108a9fdd10d30f9377af4fe41b8a"
+    sha256 cellar: :any,                 arm64_tahoe:   "eb2f4a0274773bb1b4e718a425d1971166e45e932ba38a8f6663d150f8c69795"
+    sha256 cellar: :any,                 arm64_sequoia: "42dc65a01e5b705d57ddd9e7645b9edf06b736bfb24b24dee4aec7a8f7473429"
+    sha256 cellar: :any,                 arm64_sonoma:  "17dffbff2cbab309be8fd93b7f7f1c384999ee05fd476d2914abd3f251f0a3fe"
+    sha256 cellar: :any,                 sonoma:        "640bb7639a6d71e833d2655300305e648017b3e7ca56d185db9a21b8a7c432e8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "79723b3fae0dfa6c3f0b4d0f8b0e25885f28af41752e80b8cfbfe0e416e13857"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "375c079ab612cc2d6df9cc46aab43dc04bfa986eed544449b0705b6d4f0f727e"
   end
 
   depends_on "rabbitmq-c"
