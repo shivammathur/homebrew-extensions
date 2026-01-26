ad8b35a1a1a6cc1ae5fa57344c3569b43808aab1
Shivam Mathur <shivam_jpr@hotmail.com>
2026-01-26 17:48:24 +0530

    brotli@7.1: add new formula

---
+++ Formula/brotli@7.1.rb
@@ -0,0 +1,36 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Brotli Extension
+class BrotliAT71 < AbstractPhpExtension
+  init
+  desc "Brotli PHP extension"
+  homepage "https://github.com/kjdev/php-ext-brotli"
+  url "https://pecl.php.net/get/brotli-0.18.3.tgz"
+  sha256 "ed3879ec9858dd42edb34db864af5fb07139b256714eb86f8cf12b9a6221841a"
+  head "https://github.com/kjdev/php-ext-brotli.git", branch: "master"
+  license "MIT"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/brotli/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  depends_on "brotli"
+
+  def install
+    args = %W[
+      --enable-brotli
+      --with-libbrotli=#{Formula["brotli"].opt_prefix}
+    ]
+    Dir.chdir "brotli-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
