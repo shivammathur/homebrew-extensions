2cb8cac50e2af23c71cb3bb55fc2a575dcf06089
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-26 12:59:44 +0000

    brotli@5.6: add 0.14.2 bottle.

--- Formula/brotli@5.6.rb
+++ Formula/brotli@5.6.rb
@@ -18,6 +18,16 @@ class BrotliAT56 < AbstractPhpExtension
     regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_tahoe:   "ef3784dee0c546864d8fb2cae81252b3b4979fb382654e6612c16c6950213167"
+    sha256 cellar: :any,                 arm64_sequoia: "50642717c6ac7a6210463f10196fb8b1db40034dc5b27f7aca34585520cec71f"
+    sha256 cellar: :any,                 arm64_sonoma:  "8dc82e770c77e45df062a7cd7f5de2eafada0be519f1c2eb82269ed7bed944bb"
+    sha256 cellar: :any,                 sonoma:        "cc2f610b857adcfb4e709ca957e50aee6a4bc41fc1d12d3e3951e374eb354c7d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7f45dc50e989fc36113acf25b5381b726a21c6047ca06f9792bfb725f8504e5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "15ac0f0dc75e719005fa2998d7d57c5b39364d6f1f53d3e079388ef90d5f1ad4"
+  end
+
   depends_on "brotli"
 
   def install
