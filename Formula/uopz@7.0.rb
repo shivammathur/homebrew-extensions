1971b9a5d9109ce54e21f9f5e56f96040b1fb31c
Shivam Mathur <shivam_jpr@hotmail.com>
2026-02-06 11:11:01 +0530

    uopz@7.0: add new formula

---
+++ Formula/uopz@7.0.rb
@@ -0,0 +1,36 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Uopz Extension
+class UopzAT70 < AbstractPhpExtension
+  init
+  desc "Uopz PHP extension"
+  homepage "https://github.com/krakjoe/uopz"
+  url "https://pecl.php.net/get/uopz-5.0.2.tgz"
+  sha256 "74bfb68c7f88013be6374862eed41b616e0bd6aa522142ccf3394470d487d5c9"
+  head "https://github.com/krakjoe/uopz.git", branch: "master"
+  license "PHP-3.01"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/uopz/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "uopz-#{version}"
+    inreplace "src/function.c", "static inline uopz_try_addref(zval *z)",
+"static inline void uopz_try_addref(zval *z)"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-uopz"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
