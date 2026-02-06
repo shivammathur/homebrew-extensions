b7da803be43f2661daa1005b5a655e6527d340ee
Shivam Mathur <shivam_jpr@hotmail.com>
2026-02-06 11:10:15 +0530

    seaslog@8.0: add new formula

---
+++ Formula/seaslog@8.0.rb
@@ -0,0 +1,34 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Seaslog Extension
+class SeaslogAT80 < AbstractPhpExtension
+  init
+  desc "Seaslog PHP extension"
+  homepage "https://github.com/SeasX/SeasLog"
+  url "https://pecl.php.net/get/SeasLog-2.2.0.tgz"
+  sha256 "e40a1067075b1e0bcdfd5ff98647b9f83f502eb6b2a3d89e67b067704ea127da"
+  head "https://github.com/SeasX/SeasLog.git", branch: "master"
+  license "PHP-3.01"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/seaslog/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "SeasLog-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--with-seaslog"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
