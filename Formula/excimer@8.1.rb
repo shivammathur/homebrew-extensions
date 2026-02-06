cd2330f050b632b1230364dd86b3e124d319acf6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 06:21:39 +0000

    excimer@8.1: update 1.2.5 bottle.

--- Formula/excimer@8.1.rb
+++ Formula/excimer@8.1.rb
@@ -19,6 +19,12 @@ class ExcimerAT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5cdf5a2a7f66d535a0c9ce69222edbda7eb2f93e8beb3448120bfa46e25a8285"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6d55beaca1e7e88016393d1f3e5866c957115b9ecb095f28871a8427c5d2badc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3125a0fc31a7726e2127150adccca8a8ccb883e2ab114be180bf1bfd3215d058"
+    sha256 cellar: :any_skip_relocation, sonoma:        "61dc4bfdbd0aa215f53f3b31f84ae7f75702f27edcd4c410337ba1b166a10bba"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3b4ee7b495e5a3f37890a583e98629e5a8aa697bb793e93fa1bdfa0b4de21f78"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f062d9cca2f776b567aad9e47dd67c41fb32bf6617bbd110f88d2928929db2b9"
   end
 
   def install
