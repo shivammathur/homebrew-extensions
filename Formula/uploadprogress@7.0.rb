34a56ade214dd1eb0581e4e22d3cae8efb0ac6e1
Shivam Mathur <shivam_jpr@hotmail.com>
2026-02-06 11:12:09 +0530

    uploadprogress@7.0: add new formula

---
+++ Formula/uploadprogress@7.0.rb
@@ -0,0 +1,34 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Uploadprogress Extension
+class UploadprogressAT70 < AbstractPhpExtension
+  init
+  desc "Uploadprogress PHP extension"
+  homepage "https://github.com/php/pecl-php-uploadprogress"
+  url "https://pecl.php.net/get/uploadprogress-1.1.4.tgz"
+  sha256 "9ba0f010f78889b8d69c5c643a9c920d1e140cd1d06817b8bf4ee76f996138c0"
+  head "https://github.com/php/pecl-php-uploadprogress.git", branch: "master"
+  license "PHP-3.01"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/uploadprogress/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "uploadprogress-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-uploadprogress"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
