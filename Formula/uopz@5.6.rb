8a2d6b55359f41e1b348497d9548610be03b5dc7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 11:26:28 +0000

    uopz@5.6: update 2.0.7 bottle.

--- Formula/uopz@5.6.rb
+++ Formula/uopz@5.6.rb
@@ -20,6 +20,12 @@ class UopzAT56 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9e146f46600808f3c0fb6d8f68c46f419bdfb64aedc6b2cee412657eb66b3d99"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bc4584851d5b66c95aa0875282343fbc59c7294b293cc633048abd415dcc6707"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cf829a0be3d537591f6c0bc546ddf84e4a73af1ab7ad28620c4e1e720716f8f4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f510d0b09095d45438aac408b23d813aabbca9f31302d9b76914bba3d9bfae6a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f16e8dabc22df9cc56a050976382e3a5d8ddd76b9bcb0dd31602fd5573da169f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5651f63465e1feaa4e22de0171cd0ad44cbbe3c211f67d590c08479a228f128e"
   end
 
   conflicts_with "xdebug@5.6", because: "uopz requires Xdebug 2.9.4+"
