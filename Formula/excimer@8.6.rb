c632ca04bdc6c2ff594dc6fbd0e5a99a26b8462d
Shivam Mathur <shivam_jpr@hotmail.com>
2026-02-06 11:06:48 +0530

    excimer@8.6: add new formula

---
+++ Formula/excimer@8.6.rb
@@ -0,0 +1,33 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Excimer Extension
+class ExcimerAT86 < AbstractPhpExtension
+  init
+  desc "Excimer PHP extension"
+  homepage "https://www.mediawiki.org/wiki/Excimer"
+  url "https://pecl.php.net/get/excimer-1.2.5.tgz"
+  sha256 "cf49acd81a918ea80af7be4c8085746b4b17ffe30df3421edd191f0919a46d1d"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/excimer/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "excimer-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-excimer"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
