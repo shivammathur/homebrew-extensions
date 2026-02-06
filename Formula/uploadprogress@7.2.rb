1ada13c40be5c209ab848580c9eb01c3797ee804
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 08:15:32 +0000

    uploadprogress@7.2: update 2.0.2 bottle.

--- Formula/uploadprogress@7.2.rb
+++ Formula/uploadprogress@7.2.rb
@@ -20,6 +20,12 @@ class UploadprogressAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5484ffe3b27b909df2dbb6662af2a03e016871f705774ec7419fb49bdfc71540"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c6d604888c20dbbe9df49fd1012fd827e3f3569e81110835300bfa026236e6f5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3734bb673c1c5fb2e019057a5fb631083e054c5fb8a608f3a446adca640cbe8c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "abaf1837f55908f2f9110fd298552815d66e67e5762a87b5be177a52bac455c8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3329ed5d2a83c12e43d6fb5b648d7572a7ca20677d784db4d493ae88e82b337a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "201777ec599e3479e018e038379f4f9ba1d69a9e1f861a2fe2099d50ffd75e84"
   end
 
   def install
