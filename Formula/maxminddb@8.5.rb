fe6aa0af3db2040b8ae1ef2a9f9c2626fb799680
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 06:37:20 +0000

    maxminddb@8.5: update 1.13.1 bottle.

--- Formula/maxminddb@8.5.rb
+++ Formula/maxminddb@8.5.rb
@@ -20,6 +20,12 @@ class MaxminddbAT85 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_tahoe:   "615f375c475daf9ec6deb370e9f55a1e9f096f961fbe9177d4a7567d65243643"
+    sha256 cellar: :any,                 arm64_sequoia: "d722ff2a306773fe44719654dcbb6b62c4da53c888fa32b716ef78fb3222ebaa"
+    sha256 cellar: :any,                 arm64_sonoma:  "6fe2283a7156d9ce2cb23d0cd735fa28742b94783d09abc5f1c64c50b9563cb3"
+    sha256 cellar: :any,                 sonoma:        "837daa7c5790deddef2fbef88f88e5aa5a35a32caa50b17e36ed44a231e7e9f2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b5448d9c9d6b3f5271746183debe2125533eed81006b576014515ec2b2f31007"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13ac89856e9feb531bfd0962fe28ad580648f2cb736d9c9d8834263fa691e81a"
   end
 
   depends_on "libmaxminddb"
