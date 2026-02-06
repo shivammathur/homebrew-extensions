57ce76bba2d4c6f82d6a834fd3698da68fec469e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 08:15:45 +0000

    seaslog@8.3: update 2.2.0 bottle.

--- Formula/seaslog@8.3.rb
+++ Formula/seaslog@8.3.rb
@@ -20,6 +20,12 @@ class SeaslogAT83 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b19d77c6475890c5ebae2fe0be1aab93aa3a2b723d671fff24b806ce0009be1f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aa3e128a26e2b1d01613b34a7f7c8d2913a14056f02226bed28b47b47e6103e8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c8b581210f151130fd1b8d3597bcbe48afa86c6dc691865149bffcec2f009eae"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f6422b3ec6d5425247ed030e696452ae31837305dc0d0fb19760544cb45bd24f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8622553c00338803654679bca434e4edca8910e6b3008f0c7ae5a698eb3c4001"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ea6a3151c4506f27fec01903378ecd8596236a6d012fbbc898c579eade4d237"
   end
 
   def install
