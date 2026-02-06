ec46dce5efdfc6aba8251da15e4c5d8370ee142a
Shivam Mathur <shivam_jpr@hotmail.com>
2026-02-06 11:09:16 +0530

    oauth@8.1: add new formula

---
+++ Formula/oauth@8.1.rb
@@ -0,0 +1,36 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Oauth Extension
+class OauthAT81 < AbstractPhpExtension
+  init
+  desc "Oauth PHP extension"
+  homepage "https://github.com/php/pecl-web_services-oauth"
+  url "https://pecl.php.net/get/oauth-2.0.10.tgz"
+  sha256 "1fd5e074dacf5149603493c454b476d69850bec0a71d7ea69a36a00db728a0fb"
+  head "https://github.com/php/pecl-web_services-oauth.git", branch: "master"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/oauth/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "curl"
+
+  def install
+    Dir.chdir "oauth-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-oauth"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
