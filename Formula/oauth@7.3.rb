a449158f3bf8651ab8c6fcf322d6494e54c08a07
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 11:26:35 +0000

    oauth@7.3: update 2.0.10 bottle.

--- Formula/oauth@7.3.rb
+++ Formula/oauth@7.3.rb
@@ -20,6 +20,12 @@ class OauthAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ae1512182c2a7d88bde167bc10f239af8e71685219fcf6d57debdc316bf5b0c4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "09c6124a21ac5d114f7ef9d181b4a1d3f7c3fed0764aa1255abcb67e55b5df83"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3dc20adced6f3e2fd07e5d6eefc7ecd4c809f6e7b0af867c5f278cab1409f22f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a73ad69dfc8e70ddbc30083003319e235ce2e36516c46e443aaf4d8b75d2566f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "530edc39f9b1c031ed36fde7aca252491b42528062b5497c5a8a0170f191c348"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b9d40e8a6ed5fd91bd543087ae53d89b13a18873a61d217661aab2dd28525a4e"
   end
 
   depends_on "curl"
