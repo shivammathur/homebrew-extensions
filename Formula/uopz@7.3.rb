4de48daac5676f43048bf094daea554359389b12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 10:16:03 +0000

    uopz@7.3: update 6.1.2 bottle.

--- Formula/uopz@7.3.rb
+++ Formula/uopz@7.3.rb
@@ -20,6 +20,12 @@ class UopzAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c05ee7fba0d4d04b0837842aa64d38edc791f24046e37ef0b1a689a548ab41a0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2cfda04a2e5ad58ed198afb626854d635fbb478da014d1f081b35689a976fc37"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5d673212c19837ea26250403ca57b072717f7de4eb09056c62c7ef006dd1770d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "af08f7013ed62a31e05bdfa0948dc072430d103028ce8b32e87be97a420135dd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "681fcfc1e1f11ee07bc61d4b0379d85a8669206942cf922735cc49563c76e8ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cd5384c09bb4f76d74abf7cf138ba329e4ca91516a2aaaf42e0fbaaf37b8c08c"
   end
 
   def install
