c39f504901cac1f925a06d1f347f3080f2b6a3c9
Shivam Mathur <shivam_jpr@hotmail.com>
2026-02-06 11:08:32 +0530

    maxminddb@8.5: add new formula

---
+++ Formula/maxminddb@8.5.rb
@@ -0,0 +1,36 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Maxminddb Extension
+class MaxminddbAT85 < AbstractPhpExtension
+  init
+  desc "MaxMind DB Reader PHP extension"
+  homepage "https://github.com/maxmind/MaxMind-DB-Reader-php-ext"
+  url "https://pecl.php.net/get/maxminddb-1.13.1.tgz"
+  sha256 "362839e6a0a50f6253d46ae11b3cae80520582e2b5528423aed9644577a3a93d"
+  head "https://github.com/maxmind/MaxMind-DB-Reader-php-ext.git", branch: "main"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/maxminddb/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "libmaxminddb"
+
+  def install
+    Dir.chdir "maxminddb-#{version}/ext"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--with-maxminddb"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
