1af681b8e57b3067776d0a3447f354b3f1903ff3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-06 11:26:29 +0000

    xdebug@5.6: update 2.5.5 bottle.

--- Formula/xdebug@5.6.rb
+++ Formula/xdebug@5.6.rb
@@ -15,16 +15,13 @@ class XdebugAT56 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "6c4f07c63798af7a08b11f4749b129cc8909af2e2949ee867a280710d73847a2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "edc7d841bf561cddcbd861d519845b6c7fd655054b4f8e4bbc4c373d793b89a2"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9af4b364d4bfc44502cacc96e832b15416590077d0db510feafcd10b0eee05bb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "41becc4471bec61058f9684636e5b7b5eca8e8b7ae7c2aa4d6427c69ac7b8985"
-    sha256 cellar: :any_skip_relocation, sonoma:         "b7ebe7aa285a629074fe451e9e944059302579229ee4b457f6c53719c822b4ee"
-    sha256 cellar: :any_skip_relocation, ventura:        "aa93a72086aca1726d101fb7b95920cc4e9b01b77ebc68988f26a0e055e645f2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cf4e988a3805069c2fc0ff876cbda1ef84ea1474971a3b358122bf7109956b40"
-    sha256 cellar: :any_skip_relocation, catalina:       "ddc7ec2f15a6615f8b1c07da9819e06419801d74c5b0a2be0f3b44ec1a08699f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "ed86e50b14dbaff7527b1e891ffba447ca7297ceb79699ccf4e975c944a2aad8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7737abdf69947420d8d523e32e482e1c6c42a60ff44fd8c324917c30decc0b12"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "53d32e6996a6ee4b70602020831ada06909c6e04590be2ab8a0a9f7f8868a882"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "46fa1ffb1cabb564f3355b214a602b02cfe74d091be8d6e3dec5554890ac9e40"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7eeb0ffa62c2cd08c291762f1b3ef34bd70c573af952c345f6e1fef253270dbc"
+    sha256 cellar: :any_skip_relocation, sonoma:        "da1957b11216c8a8f4b17a84f3fd63058fb75be3f44b3bf83dc76973589fb534"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "25109ed2684223609dd44e97cd8971910c2c5bc1257e2c7d94b8cb86f48dc282"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bee0b8a7a9826c6894ae414945f30c549d939a9c5052b0d4f10fbd59b70eef22"
   end
 
   conflicts_with "uopz@5.6", because: "uopz for PHP 5.6 conflicts with Xdebug."
