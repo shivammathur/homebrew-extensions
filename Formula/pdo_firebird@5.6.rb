e5b6b7dcd44312e288753d76a32810c04ce536e0
Shivam Mathur <shivam_jpr@hotmail.com>
2026-02-08 05:21:31 +0530

    pdo_firebird@5.6: add new formula

---
+++ Formula/pdo_firebird@5.6.rb
@@ -0,0 +1,32 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Pdo Firebird Extension
+class PdoFirebirdAT56 < AbstractPhpExtension
+  env :std
+  init
+  desc "PDO Firebird PHP extension"
+  homepage "https://github.com/php/php-src"
+  url "https://github.com/shivammathur/php-src-backports/archive/6cfe49e294414185452ec89bad39b1bd42cc72c9.tar.gz"
+  version "5.6.40"
+  sha256 "c7aea2d4742a6daadfa333dce1e6707bd648b2ed54e36238674db026e27d43cf"
+  license "PHP-3.01"
+  depends_on "shivammathur/extensions/firebird-client@3"
+
+  def install
+    fb_prefix = Formula["shivammathur/extensions/firebird-client@3"].opt_prefix
+    args = %W[
+      --with-pdo-firebird=shared,#{fb_prefix}
+    ]
+    Dir.chdir buildpath/"ext/pdo_firebird" do
+      safe_phpize
+      system "./configure", "--prefix=#{prefix}", phpconfig, *args
+      system "make"
+      prefix.install "modules/#{extension}.so"
+      write_config_file
+      add_include_files
+    end
+  end
+end
