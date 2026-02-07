64b913504a6e4e7d5eae680f67d64a0c19646da1
Shivam Mathur <shivam_jpr@hotmail.com>
2026-02-08 05:29:13 +0530

    interbase@8.2: add new formula

---
+++ Formula/interbase@8.2.rb
@@ -0,0 +1,32 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Interbase Extension
+class InterbaseAT82 < AbstractPhpExtension
+  env :std
+  init
+  desc "Interbase (Firebird) PHP extension"
+  homepage "https://github.com/FirebirdSQL/php-firebird"
+  url "https://github.com/FirebirdSQL/php-firebird/archive/refs/tags/v3.0.1.tar.gz"
+  sha256 "019300f18b118cca7da01c72ac167f2a5d6c3f93702168da3902071bde2238f9"
+  license "PHP-3.01"
+
+  depends_on "shivammathur/extensions/firebird-client"
+
+  def install
+    fb_prefix = Formula["shivammathur/extensions/firebird-client"].opt_prefix
+    args = %W[
+      --with-interbase=shared,#{fb_prefix}
+    ]
+    Dir.chdir buildpath do
+      safe_phpize
+      system "./configure", "--prefix=#{prefix}", phpconfig, *args
+      system "make"
+      prefix.install "modules/#{extension}.so"
+      write_config_file
+      add_include_files
+    end
+  end
+end
