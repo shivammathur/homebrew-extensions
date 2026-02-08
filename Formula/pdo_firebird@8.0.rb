d06d76ca20be015aace25baec00f78ebbd92172f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-08 01:13:02 +0000

    pdo_firebird@8.0: add 8.0.30 bottle.

--- Formula/pdo_firebird@8.0.rb
+++ Formula/pdo_firebird@8.0.rb
@@ -6,6 +6,16 @@ require File.expand_path("../Abstract/abstract-php-extension", __dir__)
 # Class for Pdo Firebird Extension
 class PdoFirebirdAT80 < AbstractPhpExtension
   env :std
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_tahoe:   "75b6c04c22c37baa27603ee2dd8401a664d762d37546d7d1889968cd6a893c3b"
+    sha256 cellar: :any,                 arm64_sequoia: "4ee46fb401285961e207bb601040ac3f5df824731d950f0faa3a8814acd80a49"
+    sha256 cellar: :any,                 arm64_sonoma:  "33c82318065a5cacb357bd111cc48d03e4ba2ffa0095edd42684903a88f94462"
+    sha256 cellar: :any,                 sonoma:        "87fea1ad017a4341b088eed2af24b8f3438d49aa556d37ce5e23d20e58afa774"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2e027ac3cf6d87d7a818861e009bb8c77ed8729c9de22a9d87ecf142d87d7af0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "884fc2b9c4b9bdd2399fc5ec37341e18e1394adff1f9387d6a7ac0dca0021b53"
+  end
   init
   desc "PDO Firebird PHP extension"
   homepage "https://github.com/php/php-src"
