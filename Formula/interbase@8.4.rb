a8e911c5584933268bdda0f3b954f78a073dc755
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-08 01:42:10 +0000

    interbase@8.4: add 6.1.1-RC.2 bottle.

--- Formula/interbase@8.4.rb
+++ Formula/interbase@8.4.rb
@@ -6,6 +6,16 @@ require File.expand_path("../Abstract/abstract-php-extension", __dir__)
 # Class for Interbase Extension
 class InterbaseAT84 < AbstractPhpExtension
   env :std
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any,                 arm64_tahoe:   "1600cc9deb37461f79fa158fe3aeb3b426793ffc4b0a926a86d173efaa7ef8c7"
+    sha256 cellar: :any,                 arm64_sequoia: "434d41669032d3ea40164f084e24de3f013cd72a6dd46055d521c9cc70f1af78"
+    sha256 cellar: :any,                 arm64_sonoma:  "4f98c736ce0505bd1749a63a021b31c3f9b047466894dbdefff6edd8f49fd619"
+    sha256 cellar: :any,                 sonoma:        "2290048516957223a2155a7dd3699227e732b4a258ed568d2012fcad5f96674b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a0edfeb59acbb9f52a7b063dc4a4052c16656f0dd6b513ba1ae0b6b6e932aa8d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1153476af7cb0481fc60e5f67ecbf6f1ed17759c7bd724f87c5ece748178d364"
+  end
   init
   desc "Interbase (Firebird) PHP extension"
   homepage "https://github.com/FirebirdSQL/php-firebird"
