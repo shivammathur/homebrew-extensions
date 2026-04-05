bf0be9eb719a22dab07c20b33e2cc98b70365d72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-05 00:19:21 +0000

    ssh2@8.1: update 1.5.0 bottle.

--- Formula/ssh2@8.1.rb
+++ Formula/ssh2@8.1.rb
@@ -20,15 +20,12 @@ class Ssh2AT81 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_sequoia:  "693d6dfe60f901fd0dc04f138164e70c80e7518ccad408f24f313daa7c9a5c5b"
-    sha256 cellar: :any,                 arm64_sonoma:   "238b25bdbd8e70720f52bf93b61ab94f2d3454c070de18eaee013c8fda5b1005"
-    sha256 cellar: :any,                 arm64_ventura:  "a47c6aaafbf8a062d3eb561a57a32b46016953d96ca8d0c49dea3cec33bf713a"
-    sha256 cellar: :any,                 arm64_monterey: "9922c76011a790f7705c1c1dd6f3db7099c1e1594d4850cd7ff11cacbd0a92a6"
-    sha256 cellar: :any,                 sonoma:         "8cde14cc85bf063c7b1ec4222343c4647f8ad1d5148e34a149e7de0fc5f02c90"
-    sha256 cellar: :any,                 ventura:        "21b3afdbfc5eb1d7be2d6ab53299e47f597f61494fdfcc44375b5bb7d1c13080"
-    sha256 cellar: :any,                 monterey:       "fc9f83062e526e79eff29fe6fc4d9cb2aff26236d4a3cc370f264d4ab61582cc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "d8591cde92ea336b1f5d55f32e1e9d70a176e02659b47903f87fde5e8734bba1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ca24fcc534a083dad8f7b19b9110ec11175bb9058607b2795c7094a15425f7e0"
+    sha256 cellar: :any,                 arm64_tahoe:   "744065684e0bf2c6bade430cd12bf06e8febb8061d2c34230d7ca4030037d437"
+    sha256 cellar: :any,                 arm64_sequoia: "75cd792727c32e5f8791ffd1fc8599d0a7e0c9f3299d19b7d4e2d747e0f99e2b"
+    sha256 cellar: :any,                 arm64_sonoma:  "96288a05a84609b1c320b0e44e15d1a12c2c2ab322ab6c44be7c82d14f60bd51"
+    sha256 cellar: :any,                 sonoma:        "ad0662c8ca81704f051ef7056dea10cda3e418147477d83d63cc409a68e24ed8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b9a07af507a3f23d6ac0cc40c09a3b5452dd4dc16092d911b81e3524447cc333"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "632344629eebf8cd16e3fba59d1f60e9b15f1a09127454f7054ccddc32175e39"
   end
 
   depends_on "libssh2"
