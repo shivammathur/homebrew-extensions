cede5005ebadbc4b7ead589b51189b08658884b1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-07 18:21:50 +0000

    gearman@8.5: update 2.2.1 bottle.

--- Formula/gearman@8.5.rb
+++ Formula/gearman@8.5.rb
@@ -20,12 +20,12 @@ class GearmanAT85 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "e60c9cfab9bdae4394ea5129d710e8436b61e08541aba3bf29ef20ec8bcb4b5c"
-    sha256 cellar: :any,                 arm64_sequoia: "d22ec60097a9458351b8a4628b8c8b43212b3a31310d7ed8d707caf945f1fb7b"
-    sha256 cellar: :any,                 arm64_sonoma:  "7c909f2445c51432d13b431d9e9d5e5e3514e16961ff03ccb712f7a969a9b627"
-    sha256 cellar: :any,                 sonoma:        "f4567df16e518b0aea8772bb54f2a8dbbc623298913974c65016f95983b82962"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e8f0f800690605e9c0563c181d3f792d957e887f826322961a441f9c237baa35"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bdf7401b05440aab854c1acd72bd6d6517d0401e61f9dbd7a90fe93abaeaf195"
+    sha256 cellar: :any,                 arm64_tahoe:   "aa4c0bb4e9ff612c75921b05f45696f49991a644e34873bf42ff609a1e180e93"
+    sha256 cellar: :any,                 arm64_sequoia: "decb1387a9a360e2a12caf90802ad8ca93e4f325475a05903108766588db27e8"
+    sha256 cellar: :any,                 arm64_sonoma:  "80f3010f1e45faa947ec455022b38303c527af74cee18ae80489c7b8ccfe967c"
+    sha256 cellar: :any,                 sonoma:        "d87814e5c60c05ac7a85e2a8ea55d322343a71efff28592210887f97889b66e9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d530a8a2ed05628a69d9dff741fdfbec14d7e0cb266d627f2d40d215baa7ba3b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "94e2884df0142d921634817a6e97c8096ec8bdf1e9a5257835c019c789e30b2c"
   end
 
   depends_on "gearman"
