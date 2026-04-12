339b01dc4bc734a4b9950e63bb426a97e6b15ed7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-12 02:32:12 +0000

    decimal@8.2 2.0.1

--- Formula/decimal@8.2.rb
+++ Formula/decimal@8.2.rb
@@ -8,8 +8,8 @@ class DecimalAT82 < AbstractPhpExtension
   init
   desc "Decimal PHP extension"
   homepage "https://github.com/php-decimal/ext-decimal"
-  url "https://pecl.php.net/get/decimal-1.5.3.tgz"
-  sha256 "168bdcc445e1557b889df5e46313825f2abc77c5d7cfb7a4215063d2f7ca4a97"
+  url "https://pecl.php.net/get/decimal-2.0.1.tgz"
+  sha256 "026e30f71016d25f267f9b38ab80a94bed4779e05e9ff5f48d9b08bf1c18d204"
   head "https://github.com/php-decimal/ext-decimal.git", branch: "master"
   license "MIT"
 
