8a0a37b52f3af68c2ee02c190485a913512353f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-16 15:51:51 +0000

    vips@8.3: update 1.0.13_1 bottle.

--- Formula/vips@8.3.rb
+++ Formula/vips@8.3.rb
@@ -21,14 +21,12 @@ class VipsAT83 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 14
-    sha256 cellar: :any,                 arm64_sequoia: "3c98cc6451e0255457dc0ec4b20fc12d78214a0ecf56bcd3a9c13fa1c17d7665"
-    sha256 cellar: :any,                 arm64_sonoma:  "72e9d4be9a5c64b328de73bf7eca62d1b022d246a952aa17d714ddf404357930"
-    sha256 cellar: :any,                 arm64_ventura: "d94b002f9e4ab1f03686ee7f5ec08a79ebd670ecc8ece646d880904e03d52d89"
-    sha256 cellar: :any,                 sonoma:        "5c1863c67ea306b3e25b4f01345c2adb7a7fe664c07c4199441bce46867246f9"
-    sha256 cellar: :any,                 ventura:       "33928feb382e6fab6a34b5aa0e8583ca9af593509da34d2e8d814d967ee79e17"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "52c78cd510a386213e7fcc8722444be91b96ba9dd8a5198af1b51f9bac005ac8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "73896b7b457c035d4c8aae86ce50404a4eeaca6c61fae13df97d9284c7835c92"
+    sha256 cellar: :any,                 arm64_tahoe:   "fa11824ffe8a3ead93160c4055b570884b45848576902f769e052dc271c27f7e"
+    sha256 cellar: :any,                 arm64_sequoia: "4f5908699fc1db6a3d45de7e67b5de84d64e8f963e6c3b1779e0caef230ff67a"
+    sha256 cellar: :any,                 arm64_sonoma:  "27d3637351109b419c4ad28a652e9579d43b077491012edd3efe02cb0ce43bf6"
+    sha256 cellar: :any,                 sonoma:        "86d6901d5b1a234b940b15cb6521314a8ad9b3cc9c3532882a7121e155ad186e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3bd1a86a36b02d6651c1d4bff37c116b85854032c83797cf362f463972e34467"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7c943ff117457133802108546da978e553530bb708487d9e0d5ff21cf0770d8f"
   end
 
   depends_on "gettext"
