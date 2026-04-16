df060aed1be126b9c3a209611a919809b5a0bc7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-16 08:37:41 +0000

    zmq@8.3: update 1.1.3 bottle.

--- Formula/zmq@8.3.rb
+++ Formula/zmq@8.3.rb
@@ -16,16 +16,13 @@ class ZmqAT83 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 6
-    sha256 cellar: :any,                 arm64_sequoia:  "bccae1e1ad474b4b383f9013dcde8358b8fac73673ff1274f0d69ffdb784938e"
-    sha256 cellar: :any,                 arm64_sonoma:   "f9b9d023ea340bf93e81bc08cb2d34ad2ea3748431c8d6f6819e9b988d2a3506"
-    sha256 cellar: :any,                 arm64_ventura:  "ec1e1f900bcc40aedfa084b4994336f7e7bfcc956e1919558036081b358ee5f6"
-    sha256 cellar: :any,                 arm64_monterey: "855a34d1ce27488a572172a4e4a45b05bb7a626b23b2a4f570403c2366a429a3"
-    sha256 cellar: :any,                 sonoma:         "98b79acfdb729fab32e689db06c43106e59fc6ec4b38f3817c57369a5b4f5e75"
-    sha256 cellar: :any,                 ventura:        "dc732bd37594c87ee2a8c4f59479d08d65b9e28873dc81ba7b506a2e19c117e4"
-    sha256 cellar: :any,                 monterey:       "415b004ae5d9dde70e44d8ae8731712177d2e09fc8a217e6099b8f5e4df3380d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "579a51509ec5e530262729811b097ee287b478d8c7e9ab41052d3aa4e1012eba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "14adf691cc818de518a3da089df1269fae87c5d46a93e8deb575c2a749b1958f"
+    rebuild 7
+    sha256 cellar: :any,                 arm64_tahoe:   "c5cc81745fe4d280bf7cdec1a089963ef924d203b0c537263728abea628c8f13"
+    sha256 cellar: :any,                 arm64_sequoia: "b8e9a70cf4c7cf1f6faf61f4f233792a15bd9b032025d90ce13bf40bcd978537"
+    sha256 cellar: :any,                 arm64_sonoma:  "e265fbea9888f13a42086f4a5e11117b6deea061f890a9de1958c468d0e8fed4"
+    sha256 cellar: :any,                 sonoma:        "eecd97b4f244eff2b96811dcc3ff352978151a8c35c00d0145f4d40fb177120b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4ff907f075853dee73bedabc71ed64e1e302aaabb97806ba8e48e922e9b4daec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fa5ab4119dcff5f857b88ff7470cb9409ea8c121698e0273f71f8ed06683d007"
   end
 
   depends_on "zeromq"
