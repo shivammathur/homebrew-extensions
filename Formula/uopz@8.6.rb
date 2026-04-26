4fce7dec58a0d1882f0b2b593a8bda6f90d2afa3
Shivam Mathur <shivam_jpr@hotmail.com>
2026-04-27 01:10:40 +0530

    uopz@8.6: fix PHP 8.6 build

--- Formula/uopz@8.6.rb
+++ Formula/uopz@8.6.rb
@@ -10,6 +10,7 @@ class UopzAT86 < AbstractPhpExtension
   homepage "https://github.com/krakjoe/uopz"
   url "https://pecl.php.net/get/uopz-7.1.1.tgz"
   sha256 "50fa50a5340c76fe3495727637937eaf05cfe20bf93af19400ebf5e9d052ece3"
+  revision 1
   head "https://github.com/krakjoe/uopz.git", branch: "master"
   license "PHP-3.01"
 
@@ -54,6 +55,44 @@ class UopzAT86 < AbstractPhpExtension
     File.write("uopz-pr-185.patch", filtered_patch.join)
     system "patch", "-p1", "-i", "uopz-pr-185.patch"
     inreplace "uopz.c", "zend_exception_get_default()", "zend_ce_exception"
+    inreplace "uopz.c", "INI_INT(", "zend_ini_long_literal("
+    inreplace "uopz.c", '"uopz.exit",	"0"', '"uopz.exit",	"1"'
+    inreplace "uopz.c" do |s|
+      s.gsub!("\tuopz_handlers_init();\n", "")
+      s.gsub!("\tuopz_handlers_shutdown();\n", "")
+      s.gsub!("uopz_disabled_guard();", "uopz_disabled_guard();\n\tuopz_activate();")
+    end
+    inreplace "uopz.h", "zend_bool   disable;", "zend_bool   disable;\n\tzend_bool   active;"
+    inreplace "src/handlers.c" do |s|
+      s.sub!("_uopz_vm_dispatch(UOPZ_OPCODE_HANDLER_ARGS_PASSTHRU)", "ZEND_USER_OPCODE_DISPATCH")
+      s.sub!("{ /* {{{ */\n\tCACHE_PTR(EX(opline)->result.num, NULL);",
+             "{ /* {{{ */\n\tif (!zend_hash_num_elements(&UOPZ(returns)) && " \
+             "!zend_hash_num_elements(&UOPZ(hooks)) && " \
+             "!zend_hash_num_elements(&UOPZ(mocks))) {\n" \
+             "\t\tUOPZ_VM_DISPATCH();\n" \
+             "\t}\n\n\tCACHE_PTR(EX(opline)->result.num, NULL);")
+    end
+    inreplace "src/util.h", "void uopz_request_init(void);",
+                            "void uopz_activate(void);\nvoid uopz_request_init(void);"
+    inreplace "src/util.c" do |s|
+      s.sub!("#include \"return.h\"", "#include \"handlers.h\"\n#include \"return.h\"")
+      s.sub!("void uopz_request_init(void) { /* {{{ */\n\tUOPZ(copts) = CG(compiler_options);",
+             "void uopz_activate(void) { /* {{{ */\n\tif (UOPZ(active)) {\n" \
+             "\t\treturn;\n\t}\n\n\tUOPZ(copts) = CG(compiler_options);")
+      s.sub!("\tzend_hash_init(&UOPZ(returns), 8, NULL, uopz_table_dtor, 0);",
+             "\tuopz_handlers_init();\n\tuopz_callers_init();\n\tUOPZ(active) = 1;\n" \
+             "} /* }}} */\n\nvoid uopz_request_init(void) { /* {{{ */\n" \
+             "\tUOPZ(active) = 0;\n\n\tzend_hash_init(&UOPZ(returns), 8, NULL, uopz_table_dtor, 0);")
+      s.sub!("zend_hash_init(&UOPZ(hooks), 8, NULL, uopz_table_dtor, 0);",
+             "zend_hash_init(&UOPZ(hooks), 8, NULL, uopz_table_dtor, 0);\n\n" \
+             "\tif (!UOPZ(exit)) {\n\t\tuopz_activate();\n\t}")
+      s.sub!("\n\tuopz_callers_init();", "")
+      s.sub!("void uopz_request_shutdown(void) { /* {{{ */\n\tCG(compiler_options) = UOPZ(copts);",
+             "void uopz_request_shutdown(void) { /* {{{ */\n\tif (UOPZ(active)) {\n" \
+             "\t\tCG(compiler_options) = UOPZ(copts);\n\t\tuopz_handlers_shutdown();")
+      s.sub!("\n\tuopz_callers_shutdown();",
+             "\n\t\tuopz_callers_shutdown();\n\t\tUOPZ(active) = 0;\n\t}")
+    end
     inreplace "src/constant.c", "zval_dtor", "zval_ptr_dtor_nogc"
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-uopz"
