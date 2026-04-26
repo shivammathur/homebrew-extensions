f975f655257ad8b78284ed3a79da1b5da28013ac
Shivam Mathur <shivam_jpr@hotmail.com>
2026-04-27 01:12:00 +0530

    scalar_objects@8.6: rebuild

--- Formula/scalar_objects@8.6.rb
+++ Formula/scalar_objects@8.6.rb
@@ -11,6 +11,7 @@ class ScalarObjectsAT86 < AbstractPhpExtension
   url "https://github.com/nikic/scalar_objects/archive/86dbcc0c939732faac93e3c5ea233205df142bf0.tar.gz"
   sha256 "a0f621772b37a9d15326f40cc9a28051504d9432ba089a734c1803f8081b0b39"
   version "86dbcc0c939732faac93e3c5ea233205df142bf0"
+  revision 1
   head "https://github.com/nikic/scalar_objects.git", branch: "master"
   license "MIT"
 
@@ -29,6 +30,44 @@ class ScalarObjectsAT86 < AbstractPhpExtension
   end
 
   def install
+    inreplace "scalar_objects.c",
+              '#include "php_scalar_objects.h"',
+              "#include \"Zend/zend_vm.h\"\n#include \"php_scalar_objects.h\""
+    inreplace "php_scalar_objects.h",
+              ";\nZEND_END_MODULE_GLOBALS(scalar_objects)",
+              ";\n\tzend_bool has_handlers;\nZEND_END_MODULE_GLOBALS(scalar_objects)"
+    inreplace "scalar_objects.c" do |s|
+      s.sub! "\tobj =",
+              "\tif (!SCALAR_OBJECTS_G(has_handlers)) {\n" \
+              "\t\treturn ZEND_USER_OPCODE_DISPATCH;\n" \
+              "\t}\n\n" \
+              "\tobj ="
+      s.sub! "\tzend_set_user_opcode_handler(ZEND_INIT_METHOD_CALL, scalar_objects_method_call_handler);\n", ""
+      s.sub! "zend_class_entry *ce = NULL;\n",
+              "zend_class_entry *ce = NULL;\n" \
+              "\tzend_execute_data *ex;\n" \
+              "\tzend_op_array *op_array;\n" \
+              "\tuint32_t i;\n"
+      s.sub! "SCALAR_OBJECTS_G(handlers)[type] = ce;\n",
+              "SCALAR_OBJECTS_G(handlers)[type] = ce;\n" \
+              "\tif (!SCALAR_OBJECTS_G(has_handlers)) {\n" \
+              "\t\tzend_set_user_opcode_handler(ZEND_INIT_METHOD_CALL, scalar_objects_method_call_handler);\n" \
+              "\t\tex = execute_data->prev_execute_data;\n" \
+              "\t\tif (ex && ex->func && ZEND_USER_CODE(ex->func->type)) {\n" \
+              "\t\t\top_array = &ex->func->op_array;\n" \
+              "\t\t\tfor (i = 0; i < op_array->last; i++) {\n" \
+              "\t\t\t\tif (op_array->opcodes[i].opcode == ZEND_INIT_METHOD_CALL) {\n" \
+              "\t\t\t\t\tzend_vm_set_opcode_handler(&op_array->opcodes[i]);\n" \
+              "\t\t\t\t}\n" \
+              "\t\t\t}\n" \
+              "\t\t}\n" \
+              "\t}\n" \
+              "\tSCALAR_OBJECTS_G(has_handlers) = 1;\n"
+      s.sub!("sizeof(zend_class_entry *));\n\n\treturn SUCCESS;",
+             "sizeof(zend_class_entry *));\n" \
+             "\tSCALAR_OBJECTS_G(has_handlers) = 0;\n\n" \
+             "\treturn SUCCESS;")
+    end
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig
     system "make"
