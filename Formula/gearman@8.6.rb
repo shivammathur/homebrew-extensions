caf4b93e8715956fd643abaf98ee6cbe40ae338c
Shivam Mathur <shivam_jpr@hotmail.com>
2026-04-30 18:35:24 +0530

    gearman@8.6: simplify offsetof patch

--- Formula/gearman@8.6.rb
+++ Formula/gearman@8.6.rb
@@ -40,18 +40,13 @@ class GearmanAT86 < AbstractPhpExtension
     if File.read("php_gearman.c").include?("zend_exception_get_default()")
       inreplace("php_gearman.c") { |s| s.gsub! "zend_exception_get_default()", "zend_ce_exception" }
     end
-    Dir["**/*.{c,h}"].each do |f|
-      contents = File.read(f)
-      next if contents.exclude?("XtOffsetOf") && contents.exclude?("zval_dtor")
-
-      needs_stddef = contents.include?("XtOffsetOf")
-
-      inreplace f do |s|
-        s.gsub! "XtOffsetOf", "offsetof" if needs_stddef
-        s.gsub! "zval_dtor", "zval_ptr_dtor_nogc" if contents.include?("zval_dtor")
-        s.sub!(/\A/, "#include <stddef.h>\n") if needs_stddef
-      end
-    end
+    inreplace %w[
+      php_gearman.c
+      php_gearman_client.c
+      php_gearman_job.c
+      php_gearman_task.c
+      php_gearman_worker.c
+    ], "XtOffsetOf", "offsetof"
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig, *args
     system "make"
