01105f71853b903877386c5e3f8f745ffe88f565
Shivam Mathur <shivam_jpr@hotmail.com>
2026-04-30 18:35:20 +0530

    excimer@8.6: simplify offsetof patch

--- Formula/excimer@8.6.rb
+++ Formula/excimer@8.6.rb
@@ -31,11 +31,8 @@ class ExcimerAT86 < AbstractPhpExtension
 
   def install
     Dir.chdir "excimer-#{version}"
-    inreplace "excimer.c" do |s|
-      s.gsub! "INI_INT(", "zend_ini_long_literal("
-      s.gsub! "XtOffsetOf", "offsetof"
-      s.sub!(/\A/, "#include <stddef.h>\n")
-    end
+    inreplace "excimer.c", "INI_INT(", "zend_ini_long_literal("
+    inreplace "excimer.c", "XtOffsetOf", "offsetof"
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-excimer"
     system "make"
