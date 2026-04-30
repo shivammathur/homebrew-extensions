60f5da61eba0c917131c63b33dd5be2bc15bd70d
Shivam Mathur <shivam_jpr@hotmail.com>
2026-04-30 15:40:42 +0530

    maxminddb@8.6: fix PHP 8.6 build

--- Formula/maxminddb@8.6.rb
+++ Formula/maxminddb@8.6.rb
@@ -33,6 +33,14 @@ class MaxminddbAT86 < AbstractPhpExtension
 
   def install
     Dir.chdir "maxminddb-#{version}/ext"
+    Dir["**/*.{c,h}"].each do |f|
+      next unless File.read(f).include?("XtOffsetOf")
+
+      inreplace f do |s|
+        s.gsub! "XtOffsetOf", "offsetof"
+        s.sub!(/\A/, "#include <stddef.h>\n")
+      end
+    end
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig, "--with-maxminddb"
     system "make"
