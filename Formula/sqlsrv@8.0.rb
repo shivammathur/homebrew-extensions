405942a684f920009896b779c03d5d35eb8fc79a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-30 17:59:25 +0000

    sqlsrv@8.0: update 5.13.1 bottle.

--- Formula/sqlsrv@8.0.rb
+++ Formula/sqlsrv@8.0.rb
@@ -20,12 +20,12 @@ class SqlsrvAT80 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "831cc4b97f751036001f54164276ade80daefa259f2563bc1f69a8f01b516584"
-    sha256 cellar: :any,                 arm64_sequoia: "93a0c438633b7a985eb4ee9323637420ca934d07466c2bf8bb5d21c39780f7fb"
-    sha256 cellar: :any,                 arm64_sonoma:  "3934d073b0f347bf72d0a97ca787721983b2d476968bc58ed0af14e2f969de81"
-    sha256 cellar: :any,                 sonoma:        "4d582660a4d31ad1f84541a93283cc0445c5146fff75309cebaaf92e2ac1595b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f15b903d9468380720749ba3e6843d8b698d521fc81981d754a20e4ee64dd7f1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8e9c5e3c9bc0778d51f0ea06cb93b01cb26f61511c628e55f92b3ae4191bbd4"
+    sha256 cellar: :any,                 arm64_tahoe:   "f9760dc096e333f9f0b67f0ed3a69592a5a63c12c44d457ee86929beddd0a927"
+    sha256 cellar: :any,                 arm64_sequoia: "ba90035caf749008df2310284106b9cf2b7fe950fb873d8b0fbdf0c7cdc6fdab"
+    sha256 cellar: :any,                 arm64_sonoma:  "1c257c217e4520f43f6721792a3d87d8645056d7f2619a2a52d847d5e267618a"
+    sha256 cellar: :any,                 sonoma:        "8b561c6293a5c6a78027280a1a6518cd3431771e8ee7b4012c3c4af264f0bd63"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "43bfc5dd855313922e637e82c9aa8701364e8b73febe724650717352df487bae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "05f268dafb61935fa279974edbd53d794d24381754a50201fe33db0de6887dde"
   end
 
   depends_on "unixodbc"
