018de3d1e48a58cca67decc37c81318684c6f10d
Shivam Mathur <shivam_jpr@hotmail.com>
2026-04-30 15:40:09 +0530

    ds@8.6: fix PHP 8.6 build

--- Formula/ds@8.6.rb
+++ Formula/ds@8.6.rb
@@ -32,6 +32,14 @@ class DsAT86 < AbstractPhpExtension
 
   def install
     Dir.chdir "ds-#{version}"
+    Dir["src/php/**/*.{c,h}"].each do |f|
+      next unless File.read(f).include?("XtOffsetOf")
+
+      inreplace f do |s|
+        s.gsub! "XtOffsetOf", "offsetof"
+        s.sub!(/\A/, "#include <stddef.h>\n")
+      end
+    end
     safe_phpize
     system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-ds"
     system "make"
