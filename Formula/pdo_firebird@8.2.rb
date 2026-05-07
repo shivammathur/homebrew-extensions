c04f672ef63a2e82bb45192788cf46a21b87515d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-07 17:18:02 +0000

    pdo_firebird@8.2 8.2.31

--- Formula/pdo_firebird@8.2.rb
+++ Formula/pdo_firebird@8.2.rb
@@ -8,8 +8,8 @@ class PdoFirebirdAT82 < AbstractPhpExtension
   init
   desc "PDO Firebird PHP extension"
   homepage "https://github.com/php/php-src"
-  url "https://www.php.net/distributions/php-8.2.30.tar.xz"
-  sha256 "bc90523e17af4db46157e75d0c9ef0b9d0030b0514e62c26ba7b513b8c4eb015"
+  url "https://www.php.net/distributions/php-8.2.31.tar.xz"
+  sha256 "95eae411d594fe6f6e5678b76645dc13ae47d3c0a5325c1d969b58dea56ee45a"
   head "https://github.com/php/php-src.git", branch: "PHP-8.2"
   license "PHP-3.01"
 
