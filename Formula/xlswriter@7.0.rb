11c8735fa16629690ff4c2a37a1dd2f1e0a83d85
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-16 13:49:03 +0000

    xlswriter@7.0: update 2.0.0 bottle.

--- Formula/xlswriter@7.0.rb
+++ Formula/xlswriter@7.0.rb
@@ -20,13 +20,12 @@ class XlswriterAT70 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe59075da253d5a87f2aa298b92178bdca6ff80ad2a7f92e7b6084ac816fbab1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5bec8b068289b3894af10eed8edea828207dc6ab8c8c5238e0dbf33b0f25e209"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "35cc57cb6d28b33629cb51dcccd79a0ff687a9f281a1112c360dad59dea5d155"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b270e0bb1050fc78d8c5c7915c0b17f791ea4dc401749d2b664e425c3dd4f64d"
-    sha256 cellar: :any_skip_relocation, ventura:       "c0975423161546c768ac9bb46237bbb04ff4f1cc45434a5a0703d095047a9a3b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "539d09edf464dd1b993ebca30bacae35a843f725bdccb926a03d4faa7138123f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e991fe6d3c87943027230adf715ab901d7e493ae7d188a2ae87c2bc91816a10"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bc0cea5b53dd3c409cbd47a119829d0c7103337ee48403d68f9c28ae31f1c899"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "151bf18a472b26ca1f260404aace53d8dd7ec47332e33c759cabf1c2e505e9f8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5fd0f7347a91f6cda27b7f6a0dee1e59f24116b4385fafbd606114dc627a298c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0936711fdd051ed2e0167de1ff1de12aeb2fc6d4f00d94adfb0e7669441cd2c2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "32f6546935e4a5a691ee5ea048f30c74cc15e5865c254b5d8bd0fc5aa7be6ce9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7d6b40b7b84703b3a57c4ad0eb4824e49b63033ba8aa7100c31f49e7b25fb0e7"
   end
 
   def install
