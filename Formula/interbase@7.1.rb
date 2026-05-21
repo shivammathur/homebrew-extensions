0610130808201780ea5473138b7a3ae2fd121930
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-21 07:41:36 +0000

    interbase@7.1: update 7.1.33 bottle.

--- Formula/interbase@7.1.rb
+++ Formula/interbase@7.1.rb
@@ -9,12 +9,13 @@ class InterbaseAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "95b48720157f312e4107fa719f90b773b40db07cebe2c13423dbf3b69f26e0dc"
-    sha256 cellar: :any,                 arm64_sequoia: "190928ee02d171b653cb1c19e51e42d99f5541161b027b7b4ad9cde4c21dff93"
-    sha256 cellar: :any,                 arm64_sonoma:  "c4bda933c3050e4ed995c3648007c2020cb7243292f8010be3ec7e578b5875f9"
-    sha256 cellar: :any,                 sonoma:        "1d461cc4ed5004cf132e448d3888b7af451bf43751df60a136c294dd272b5039"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "32b16a943a9f552883022f54c4d84b6a6b6403257ba99b0c4e27c39a348a05e0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "86c6fa78abf80ed40c748f544fce0ebb5d1b707e2471df5c24b872593e558396"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_tahoe:   "b3b58307224560697274f839d2a22c78150645043c381b4b9a13fe118c80061e"
+    sha256 cellar: :any,                 arm64_sequoia: "62d162a7d202e71d50120d869579e4288301f7181f206c76a9e86b4d1ea2f0dc"
+    sha256 cellar: :any,                 arm64_sonoma:  "751a1f27abd95b9d28283f52db12b238c1111ef82878c517cac85676ab3ae05e"
+    sha256 cellar: :any,                 sonoma:        "9e288c109bf61dda07ea00f13a159515e4ded727aeee8ae808b0b45fa172aa6d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1984d50857e94f74817b774a1b3554032dd32c92e6f90a3711d1017dd70f2031"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fdecc13e2a2688eaee3144815411e6265bd8c272352982b37f2dd3c66e29ff77"
   end
   init
   desc "Interbase (Firebird) PHP extension"
