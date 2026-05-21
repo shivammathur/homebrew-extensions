7740094433ebca886605b2b00d9fb216e6fee46c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-21 06:02:01 +0000

    Update mcrypt@7.1

--- Formula/mcrypt@7.1.rb
+++ Formula/mcrypt@7.1.rb
@@ -8,9 +8,9 @@ class McryptAT71 < AbstractPhpExtension
   init
   desc "Mcrypt PHP extension"
   homepage "https://github.com/php/php-src"
-  url "https://github.com/shivammathur/php-src-backports/archive/45db7daedb330abded7576b9c4dadf5ed13e2f0b.tar.gz"
+  url "https://github.com/shivammathur/php-src-backports/archive/4cd450adf633ff3b756586f5ce8fb31a7c7f8359.tar.gz"
   version "7.1.33"
-  sha256 "c83694b44f2c2fedad3617f86d384d05e04c605fa61a005f5d51dfffaba39772"
+  sha256 "632a98f29d7e023b0dc4d3ae9680877f8f7aafed162345ca3318f5e9d1f87db7"
   head "https://github.com/shivammathur/php-src-backports.git", branch: "PHP-7.1-security-backports"
   license "PHP-3.01"
 
