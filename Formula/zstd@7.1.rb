52a4684a1b98c451846377e2401b9bbe16602c35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-05 08:59:22 +0000

    zstd@7.1: update 0.16.0 bottle.

--- Formula/zstd@7.1.rb
+++ Formula/zstd@7.1.rb
@@ -20,13 +20,12 @@ class ZstdAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_sequoia: "7d9ecf3a3fddb715bf6bde550b780b92bf70cff94e4bee0c3e4dab8236c9ea9f"
-    sha256 cellar: :any,                 arm64_sonoma:  "7dbd1487e064ba287996c5de31fa8da30ec72922550ec6fd97891bec0e513a3b"
-    sha256 cellar: :any,                 arm64_ventura: "bba299291cae7294b1903d4c2d65dd9cf69ba83c04f5a428f11991dd615dcf76"
-    sha256 cellar: :any,                 sonoma:        "4a91297c89c258c2b32648c3a8aee656b355c82342d4bdcf9ce5087ef0b16175"
-    sha256 cellar: :any,                 ventura:       "cb219578b840854904366bf3ba848ae592f51a0138630418e91a02a379d11ede"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6d937eb49befdac6857eb022d65614613e8bc312a62f742c615d174723a92c23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "31e50fefb41532300118a2e817fd68a749820106ab4dbd4949a83895b72aebed"
+    sha256 cellar: :any, arm64_tahoe:   "417473b8cad935469d334d8104ab7b0eb57312b0bb35589468d6b4c55a52eaa2"
+    sha256 cellar: :any, arm64_sequoia: "ab106d32d4b3761635fb0f0a6ba0b915da6481412762d892ebaa6a13dee9661a"
+    sha256 cellar: :any, arm64_sonoma:  "081775a513a5422953d5ecb9eca4ee8d59d2d3220d0b1e71763c564f0ac4e440"
+    sha256 cellar: :any, sonoma:        "3b0f37653ce0b9436a05bf2f41bd4df358eb01fa3643f70b8949c04e50cacf2e"
+    sha256 cellar: :any, arm64_linux:   "f8e1934a52e72325c8b81923a25ed4e7fc333f8080b37e689e0100e0376b0303"
+    sha256 cellar: :any, x86_64_linux:  "5932f1d40061dd844b6a94f0005209bde12ce7ed680b3cdf7d34d281976f82a2"
   end
 
   depends_on "zstd"
