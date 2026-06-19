1e966428bb33254649ffb576f8f61dbfcde12533
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-19 02:36:01 +0000

    phalcon5@8.4 5.15.0

--- Formula/phalcon5@8.4.rb
+++ Formula/phalcon5@8.4.rb
@@ -8,8 +8,8 @@ class Phalcon5AT84 < AbstractPhpExtension
   init
   desc "Phalcon5 PHP extension"
   homepage "https://github.com/phalcon/cphalcon"
-  url "https://pecl.php.net/get/phalcon-5.14.0.tgz"
-  sha256 "9160c586227f3ae64a282eae5eec4241107087ca66689ac44498fbcd2b3f3c52"
+  url "https://pecl.php.net/get/phalcon-5.15.0.tgz"
+  sha256 "c0dbe13169a1e03d65a7c8f5a8aca9f00b4e13557337e4651e54a16a393d40d5"
   head "https://github.com/phalcon/cphalcon.git", branch: "master"
   license "BSD-3-Clause"
 
