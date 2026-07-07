7526676baa995fadad873ce582e37fd8b44d850b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-07 07:09:02 +0000

    imagick@7.1: update 3.8.1_2 bottle.

--- Formula/imagick@7.1.rb
+++ Formula/imagick@7.1.rb
@@ -24,12 +24,12 @@ because: "both provide PHP image processing extensions and should not be loaded 
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any, arm64_tahoe:   "ace2f72522f3c0142f26595fec04d06d87407a03d1eb689b399522e00b7b4769"
-    sha256 cellar: :any, arm64_sequoia: "c2851962b5095922056b2e6f9772faa2a4b39f2c87558d4cacf40c7f7f6051e1"
-    sha256 cellar: :any, arm64_sonoma:  "6f7ee3c89f3587814d273b83aae1de4d57909819e3d65d0653122e4f3c8ffcfc"
-    sha256 cellar: :any, sonoma:        "87d1d74eacfb1f3dc9ee37aca5547073d453787cf2b9db66a0e36b196a408399"
-    sha256 cellar: :any, arm64_linux:   "790b0d5793b8da0fe1b7ebfd1fe9b094418739417580a9547165583a73c7f48a"
-    sha256 cellar: :any, x86_64_linux:  "de9a3562783c3feb65ce700be5106cad35082574ab0ae98a4bb9fbc8cd8e0bb9"
+    sha256 cellar: :any, arm64_tahoe:   "acd6bea7ebf9dd2c053f75656e109f0c9e1459fe4654cdefdf88c890ac02fc79"
+    sha256 cellar: :any, arm64_sequoia: "87027516ea46adfd68cbe0418372d889eb8f0a4387e5fc57a30f2ca2cdf9e2b3"
+    sha256 cellar: :any, arm64_sonoma:  "62e6a68488d7338789b96e63da1b5c78b5e52592fab6bf35d1b537eb3f86c91c"
+    sha256 cellar: :any, sonoma:        "37e7e2040490c1b2b4242b0505a8880cf94d19f5b984384b197c16f028026b8e"
+    sha256 cellar: :any, arm64_linux:   "cc461defd0a3a91d4c4891a5f88cc64818cf7242cdd25006f8c0f835e767e921"
+    sha256 cellar: :any, x86_64_linux:  "07659b2886898a2dd3d6e0f8994a2c125b299cad7a827eadc655b2f2874fd5e7"
   end
 
   depends_on "imagemagick"
