1854117f117bb24e8adccd652b1865980c626dfa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-16 12:55:07 +0000

    event@8.3: update 3.1.6 bottle.

--- Formula/event@8.3.rb
+++ Formula/event@8.3.rb
@@ -20,15 +20,12 @@ class EventAT83 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_sequoia:  "67aedc0f97709cfe2f6eb4522d8e8b0eb0bcd19fbcadcb5f60b6b82e9f98b34d"
-    sha256 cellar: :any,                 arm64_sonoma:   "74d43bb263fb459b14fd5c50309789728aa56ee4a9c163e4a349c8549922f1e2"
-    sha256 cellar: :any,                 arm64_ventura:  "a9eb1417da6f31a5a8424be54dbc659c0c5f0bebabc4c353d0d89f8c8624d76b"
-    sha256 cellar: :any,                 arm64_monterey: "43389e01e0bee805661dfa111d6d5ed97a9017cd0fcf6e64c9c7546b5916f684"
-    sha256 cellar: :any,                 sonoma:         "cdb902d9922edb54dd5d7382e3194677ee3d88652599da56e60ddc64433250ab"
-    sha256 cellar: :any,                 ventura:        "542b58154ad229bb6ca2cd07df4326e5b17fc71b3122e4578c0deafc901c89b5"
-    sha256 cellar: :any,                 monterey:       "770d1331d08ebf77caf79766c707839eb4e8e9cb5d386b336723fa5c2ec597b7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "2a3bfa0ff879f5a883afb375f3059b97ad4b4d9b98d76e02b465a7a1afad8eb6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61b62952c14f921f8036ae5b7095c87bc15b82e99b31193349d0a1c3fc73c4a1"
+    sha256 cellar: :any, arm64_tahoe:   "a39ae21f2d72cf47c17d8c4059d4db2fadd2d15c0e6390fe089011e548a3b6aa"
+    sha256 cellar: :any, arm64_sequoia: "39373edfc5390dcb9d809c7ad138249bc9ece677ab1c10d44978fb5a2c3c57e8"
+    sha256 cellar: :any, arm64_sonoma:  "1a1cccb8d059befbb7b4ea0321988371e7696fbf809e3533f1b8af0bd4de42a7"
+    sha256 cellar: :any, sonoma:        "7d0812c208b3d96f98495883fddf8e5e38d089a11bf72b609dde9572c8b43403"
+    sha256 cellar: :any, arm64_linux:   "48c37203e62ff84197573fd7c361ef900b25af16b539776e3e34886e179c5352"
+    sha256 cellar: :any, x86_64_linux:  "e89ffa8a5f7c773a4c265534baf4aae5b21735e8aef7e5e1d40194cda2e6fc55"
   end
 
   depends_on "libevent"
