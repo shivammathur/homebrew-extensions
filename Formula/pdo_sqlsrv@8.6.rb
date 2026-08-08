1dbce8ec0868f6b500bbc812990a21017ee08907
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-08 00:18:51 +0000

    pdo_sqlsrv@8.6: update 5.13.3 bottle.

--- Formula/pdo_sqlsrv@8.6.rb
+++ Formula/pdo_sqlsrv@8.6.rb
@@ -20,12 +20,12 @@ class PdoSqlsrvAT86 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any, arm64_tahoe:   "36447fbf2e732ce79ebf81b94cd590ea42285b4c9ff1259c0533f4d62e28c650"
-    sha256 cellar: :any, arm64_sequoia: "ff74d1cc0d2ea6fde7f943d9512451e7bfd94b11cdeb9234998c7ed93733f9c8"
-    sha256 cellar: :any, arm64_sonoma:  "b8984c766f7e99fd7b9e0cd801dcc672b9a22438b34bf96be0add240fdd1957c"
-    sha256 cellar: :any, sonoma:        "ff75869a893abb2f0ab9787abc3499ce3c631316f3a91136754453754f0f70c3"
-    sha256 cellar: :any, arm64_linux:   "1eb96581acf20430e7e44fcbb9517913dbf0e1ee1a81bc47108c792789c1e1fe"
-    sha256 cellar: :any, x86_64_linux:  "4894c62bef83f219ac366c7845797ec669a068f2d546cc3b56e8c6fa5a2bed35"
+    sha256 cellar: :any, arm64_tahoe:   "f9f930119e2fe4e421425aa12db3916f4f90e5900980bc17ec35418c8f03377a"
+    sha256 cellar: :any, arm64_sequoia: "a80815dec785cd476cc5bef2a691c7f7bff3ca71e34c6dd00c632e008269a26b"
+    sha256 cellar: :any, arm64_sonoma:  "9adcb1cc0cf43b8bcb7ce0449fba8096d91c272a29daab59710554c6b8b8f238"
+    sha256 cellar: :any, sonoma:        "30fe93e79e039be80b1b15ef83b22e96980d5be63aa6f627de386dfc4614365f"
+    sha256 cellar: :any, arm64_linux:   "5c478f73b0697d9be2c47131f3eec5b0b609011beacd9ddbd30335f7ab92ba04"
+    sha256 cellar: :any, x86_64_linux:  "c6a982ce79b9b18cc303c440a34167e399acad63edce2567b0caa449429a40de"
   end
 
   depends_on "unixodbc"
