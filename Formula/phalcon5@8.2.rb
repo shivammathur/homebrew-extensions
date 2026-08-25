55f7492e15db223f3580386f937ec00c31ad2012
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-25 08:12:53 +0000

    phalcon5@8.2: update 5.20.1 bottle.

--- Formula/phalcon5@8.2.rb
+++ Formula/phalcon5@8.2.rb
@@ -20,12 +20,12 @@ class Phalcon5AT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "73be7d31bbc9b44cb495582245f4e038574bfae8dea4d9e378217786463e6e30"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c34206449def5d2e16b5ba735f277afecf9fded1e19f21304c6303dd22588f68"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2f09b652456bab4e60db2e11ceffd16dbf08c0c82eb0e81cec93b830e056ca98"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a626936cf1b71c3e66cd92ce72991803e0334536d8ba4755ed6e68f443cc7abb"
-    sha256 cellar: :any,                 arm64_linux:   "30adacf4d1e508018d42196ea0622e675efb10040534d1147da44ce74b897eea"
-    sha256 cellar: :any,                 x86_64_linux:  "b385f280ae0c6e89d61c83adc8db9454a84668b409c4a7b5c4df76e107290307"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d4ffe3e2c5ef963468a67c4900dfeca4ab11203321dba61f5d4496d8e94e8a18"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "277e95cb31dd41d60a90d393863cb534c79b99237c3e135ca713381861b2727f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "44d94ea787c423a237e4671331b0e0e408b368440530ac03dedc35ad16ae9856"
+    sha256 cellar: :any_skip_relocation, sonoma:        "52b8754602be1d0d7015fa1bf4f6250b4184f98b03db54f8631bace87fd2e987"
+    sha256 cellar: :any,                 arm64_linux:   "c8f3836d36f00daf38baf5004d429c2edf6427163caa4025b577925eb0073d8b"
+    sha256 cellar: :any,                 x86_64_linux:  "0eafa2c8a14776b23d62642d61b2f1f1f52cfa6568880616453b16e08ae2ec8c"
   end
 
   depends_on "pcre"
