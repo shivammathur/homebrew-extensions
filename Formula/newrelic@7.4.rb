f9291ba75e5aad32ff16824275181d3a6300b860
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-31 21:42:51 +0000

    newrelic@7.4 12.10.0.39

--- Formula/newrelic@7.4.rb
+++ Formula/newrelic@7.4.rb
@@ -8,8 +8,8 @@ class NewrelicAT74 < AbstractPhpExtension
   init
   desc "Newrelic PHP extension"
   homepage "https://github.com/newrelic/newrelic-php-agent"
-  url "https://github.com/newrelic/newrelic-php-agent/archive/refs/tags/v12.9.0.38.tar.gz"
-  sha256 "f3a90afd9a9fbb380d953f3a6fff7a4ce14231f8077c61f0794d17027d2f2d69"
+  url "https://github.com/newrelic/newrelic-php-agent/archive/refs/tags/v12.10.0.39.tar.gz"
+  sha256 "9c778d5f6f13c58e67efb469e02e8a3131719d9065bf03334b971a623799020a"
   head "https://github.com/newrelic/newrelic-php-agent.git", branch: "main"
   license "Apache-2.0"
 
