c178b14befc9d1e856c68bfde8058e133d96b840
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 06:18:58 +0000

    xdebug@8.6: update 3.5.0_1 bottle.

--- Formula/xdebug@8.6.rb
+++ Formula/xdebug@8.6.rb
@@ -17,13 +17,12 @@ class XdebugAT86 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    rebuild 11
-    sha256                               arm64_tahoe:   "b8b9c98878a4ceaef572e51bac3d8aef6b4e566c107a4a192f33a8c57398c0f6"
-    sha256                               arm64_sequoia: "83e09f718f671ef995b9e84d5318e509222923648c9e39e9313e21524b04ac2b"
-    sha256                               arm64_sonoma:  "4a74a9d63eb69dccda2c4765ce1ef0705dc5b6b51bc7149aa281c572e29d411b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b33d4f6306fd2fc474bfb2f8404b382df773f362d051bb900d8ded6ce5f7770c"
-    sha256                               arm64_linux:   "38a359ee1049d579ee92109e001a5e0e9e857a7ba1adf08716462bed7e317bc7"
-    sha256                               x86_64_linux:  "e966c6c894dbecc4bcfa99b7fb2e7ef027e744faa63de72a55206525f985fb43"
+    rebuild 12
+    sha256 arm64_tahoe:   "caabc3d1d5401c8006b2de596d871c93ff59d6685c82b69455073430e385aa58"
+    sha256 arm64_sequoia: "453d3d095fe21361486cc70678cad59c7d56ed49ef1fd95966cd5aed12f53386"
+    sha256 arm64_sonoma:  "8cb940bf41088c23aee49aca763b6b522256156ea268e3f028203d712041a16d"
+    sha256 arm64_linux:   "7813e2fc6a7f174993a3c44e154479902453e8260b0872620f0fba19986b4a86"
+    sha256 x86_64_linux:  "d8f8072d15429015a0a67346fbad6febe835e3830a56d09a466e86314be94d35"
   end
 
   on_linux do
