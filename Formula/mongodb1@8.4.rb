27ae3fa3f146bfd13cc1ae6a76e365fd3c902bd0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-03 17:42:26 +0000

    mongodb1@8.4: update 1.21.8 bottle.

--- Formula/mongodb1@8.4.rb
+++ Formula/mongodb1@8.4.rb
@@ -20,12 +20,11 @@ class Mongodb1AT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any, arm64_tahoe:   "ba0949e8fa5a1e4c67e03caff6d1cc6f2921e97947c8d72114199a5c0ab13e8b"
-    sha256 cellar: :any, arm64_sequoia: "f3e704871b9070bbe9e69591094d3c25b201cdb9e06f30d24696322a7f5bcaff"
-    sha256 cellar: :any, arm64_sonoma:  "649b267e7088f8230c653d8356aeb9f8c154d9e983564778509ec2d484ddc5c5"
-    sha256 cellar: :any, sonoma:        "f4bb0a9d7f9b1b956a213441f376c4f29b794e4177ebd8b61ec433b333db6382"
-    sha256 cellar: :any, arm64_linux:   "04b0e8b2d3034d1d92b79020e4939af63e62a7241845629d47660366af3aa0e9"
-    sha256 cellar: :any, x86_64_linux:  "21eaf964ef8f4747bfec5354a9006b4e4fb42eda0e9e915b697c85701d6fab25"
+    sha256 cellar: :any, arm64_tahoe:   "5889037369f7d76fc1cb73a534d9a3312dc79f0edf95d4321f760e6ceb72a029"
+    sha256 cellar: :any, arm64_sequoia: "914150dd2ba9a71ea788cf62e601df8a6fb843781d5f1aae7ca686604bc404b4"
+    sha256 cellar: :any, arm64_sonoma:  "d0899e71508cab96d25c869f8e5021f2d1f933a6b989ba55896cef7b23b80b2f"
+    sha256 cellar: :any, arm64_linux:   "deb174effb34720407d483de2ebc983f33aec367ffc91205448b23fe118cf3ac"
+    sha256 cellar: :any, x86_64_linux:  "dc644abf855825483e8e8b7a7134fcf51e8c85765dbea20481e4f8f6053ae79e"
   end
 
   depends_on "cyrus-sasl"
