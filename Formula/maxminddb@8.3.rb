177831986eb8b7b46b2e94a41548d172b2831035
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-11 04:04:19 +0000

    maxminddb@8.3: update 1.14.0 bottle.

--- Formula/maxminddb@8.3.rb
+++ Formula/maxminddb@8.3.rb
@@ -20,12 +20,11 @@ class MaxminddbAT83 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "37261386d826b3b5d4daec3de1e16546a9aca2864c3002f381fda5da172a4461"
-    sha256 cellar: :any,                 arm64_sequoia: "965b7435d97f73982d455725cdc2079886451951fa5831937190f5071697594d"
-    sha256 cellar: :any,                 arm64_sonoma:  "0214e9e065b3b75e7514513533adb9be5f37be1706200122c51117ea715056ea"
-    sha256 cellar: :any,                 sonoma:        "657f37db7cd72dadf066262a38af23bbffab2af686673a5e5a782a42c8a7d45e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3405e66be213536ac3189d04c5ff4da8d26b3e5ee9da7084b8b1060f289e7338"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d2e9f0a3ab1f58a157ee46b315034228af8078a3c4ef6fcd5d6a21412c2da58b"
+    sha256 cellar: :any, arm64_tahoe:   "0b84b45b563bf83023eaaa83fadbf2cd27459ab95b4757587d34c21c3c7b1e11"
+    sha256 cellar: :any, arm64_sequoia: "671902c0cf34999925657ea1ef45901c74ac29fb3e4e2b82023c81f365b32c75"
+    sha256 cellar: :any, arm64_sonoma:  "ad9a9d20e520f94cc20f64fd7ce599ddcc43fbf9afe10b479d86439de40d78b2"
+    sha256 cellar: :any, arm64_linux:   "69de10b59ed4079954d23097b4d67fcb0d7fae3cb53137076b60f2f9ab1c1197"
+    sha256 cellar: :any, x86_64_linux:  "13f5048352c59cb6c463361d4dcc9f95027ea0b9df19bcff88eb089f02248587"
   end
 
   depends_on "libmaxminddb"
