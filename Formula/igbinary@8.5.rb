77f509161b3deb50693f47aa7684c5645384a8ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 17:00:48 +0000

    igbinary@8.5: update 3.2.16_1 bottle.

--- Formula/igbinary@8.5.rb
+++ Formula/igbinary@8.5.rb
@@ -25,12 +25,13 @@ class IgbinaryAT85 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7e2ed132c40c96126799fac8350562d693d1059ab94a342ebf4a3202fee1300e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "27eb75834d9311ffcf9439f231d3bbedd950a069076150d5a60fe92cbbacdc81"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5b791ee3009a2321d5f8c8ad763b1b12fdaddfa0802e8fe0affde4d6ee43e48e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f91424b35a755f17948a007e7803b0dd60995c9aafc4590fe2279f9a53c23680"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "07e9215af3dc24f9f933a5438a1ecafe48908869a8c62554c803cd0d89f49b4d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3a18780a3237c0110331b696e1cffde1534cdf36a7d137280bf9bd6a09db9d5f"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "562ed20acdceb752ce7fdb7fb10bd9dd90e694a1d1cadb7f40f04694b2025530"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "7e2ed132c40c96126799fac8350562d693d1059ab94a342ebf4a3202fee1300e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "27eb75834d9311ffcf9439f231d3bbedd950a069076150d5a60fe92cbbacdc81"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "5b791ee3009a2321d5f8c8ad763b1b12fdaddfa0802e8fe0affde4d6ee43e48e"
+    sha256 cellar: :any_skip_relocation, sonoma:            "f91424b35a755f17948a007e7803b0dd60995c9aafc4590fe2279f9a53c23680"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "07e9215af3dc24f9f933a5438a1ecafe48908869a8c62554c803cd0d89f49b4d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "3a18780a3237c0110331b696e1cffde1534cdf36a7d137280bf9bd6a09db9d5f"
   end
 
   def install
