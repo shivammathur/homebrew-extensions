7a59935d7802d66679a995939ea4a0eda9615f61
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 18:24:00 +0000

    excimer@7.3: update 1.2.6 bottle.

--- Formula/excimer@7.3.rb
+++ Formula/excimer@7.3.rb
@@ -19,12 +19,13 @@ class ExcimerAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3200c6a0dcfa34da373c0e603f64a56aabb4ed6c9c8a6c8a9e14840bb1c77ef9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "acca57f68f265ae23e59f276a9c3978a716f6a810580a77a8c2239510a064e8e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "523992f5bafd873d877edd1a98405e6a9b2894ad5aa0b1a8e83ffec84fa1af91"
-    sha256 cellar: :any_skip_relocation, sonoma:        "12e4684c12e886ea780a555df5e54dc8289a6d89bc7180ed29b2554b75ce4706"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "2e3d6ceaafd41403ccd03f73cb48670fd2a4a00f7c730780972a0c26ba5ddfd3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "28298c681126b4a8e07a97e0a1d1405f700aca26b5bb2e84589f7c8de880fca9"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "1d6c65657f8d69c8c13ebc7551b6dbcd7b6b70ef4c906ce05857c5c3dce673b1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "3200c6a0dcfa34da373c0e603f64a56aabb4ed6c9c8a6c8a9e14840bb1c77ef9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "acca57f68f265ae23e59f276a9c3978a716f6a810580a77a8c2239510a064e8e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "523992f5bafd873d877edd1a98405e6a9b2894ad5aa0b1a8e83ffec84fa1af91"
+    sha256 cellar: :any_skip_relocation, sonoma:            "12e4684c12e886ea780a555df5e54dc8289a6d89bc7180ed29b2554b75ce4706"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "2e3d6ceaafd41403ccd03f73cb48670fd2a4a00f7c730780972a0c26ba5ddfd3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "28298c681126b4a8e07a97e0a1d1405f700aca26b5bb2e84589f7c8de880fca9"
   end
 
   def install
