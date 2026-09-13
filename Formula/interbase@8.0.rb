a810563128549d92cee6beba5f5519ba3db9c6c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 22:41:02 +0000

    interbase@8.0: update 3.0.1 bottle.

--- Formula/interbase@8.0.rb
+++ Formula/interbase@8.0.rb
@@ -9,12 +9,13 @@ class InterbaseAT80 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "2c49e2a4edf3daa7c2f52185f0322b082d02a95f5643c7a39aa6452ebada2235"
-    sha256 cellar: :any,                 arm64_sequoia: "4bebbaa7ec9d9b53fa501fb5a7f563c6982a06a9eb594303c57c67039f3af64a"
-    sha256 cellar: :any,                 arm64_sonoma:  "0e9016ef7616dbf16644e359a9a2d203b31774920875354e24cc8454b7d9c01d"
-    sha256 cellar: :any,                 sonoma:        "4fa59f3d5e9656dcf67d02bf3b45cf2164a27225909251d1e5f1905169959093"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ec1a450609a53ba220ecfd33be03e1885495ea2aad8eacd1d4ad4094694c0019"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8b6bec49c0846bfcc792ea623e688768174e542d473acd83571a3ef62523f625"
+    sha256 cellar: :any,                 arm64_golden_gate: "d03a2b21c3f1ca9af063c2a3f5d1a77283f3524f43f08bea60664c0e5bb6e791"
+    sha256 cellar: :any,                 arm64_tahoe:       "2c49e2a4edf3daa7c2f52185f0322b082d02a95f5643c7a39aa6452ebada2235"
+    sha256 cellar: :any,                 arm64_sequoia:     "4bebbaa7ec9d9b53fa501fb5a7f563c6982a06a9eb594303c57c67039f3af64a"
+    sha256 cellar: :any,                 arm64_sonoma:      "0e9016ef7616dbf16644e359a9a2d203b31774920875354e24cc8454b7d9c01d"
+    sha256 cellar: :any,                 sonoma:            "4fa59f3d5e9656dcf67d02bf3b45cf2164a27225909251d1e5f1905169959093"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "ec1a450609a53ba220ecfd33be03e1885495ea2aad8eacd1d4ad4094694c0019"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "8b6bec49c0846bfcc792ea623e688768174e542d473acd83571a3ef62523f625"
   end
   init
   desc "Interbase (Firebird) PHP extension"
