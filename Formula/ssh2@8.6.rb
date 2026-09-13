d5f02e827ae85e4c4ed028fecf2dd2141e38a295
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 22:00:22 +0000

    ssh2@8.6: update 1.5.0_1 bottle.

--- Formula/ssh2@8.6.rb
+++ Formula/ssh2@8.6.rb
@@ -21,12 +21,13 @@ class Ssh2AT86 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "f85ea4b0c75b467059c95b6ede9fa39cc168c3123adf4193834afdc0a608720e"
-    sha256 cellar: :any,                 arm64_sequoia: "a5ee99ba9efa39608fcd5a812e680b91f8503a40028b35a133c5e070369d2101"
-    sha256 cellar: :any,                 arm64_sonoma:  "08b15b070e1cf38d666a507aea94cd3e9d5f299a7855b08977bc8c9cb5493010"
-    sha256 cellar: :any,                 sonoma:        "02219562e505037a1fc143016702a0d1a2d571654c54cf93e48d5d15e19da259"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "867b32388d44873c941e3f7c738522e04810f864f83ef186e6b5ed37bc2895b6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "139be996a0794794b7dd5b7f31da42645f2a5ce6846e65551ce3bc9bee7784c7"
+    sha256 cellar: :any,                 arm64_golden_gate: "94c5fafd2fecbd95b406471b105c6cf152634b9fc9890aee6028d4ba9e19dba1"
+    sha256 cellar: :any,                 arm64_tahoe:       "f85ea4b0c75b467059c95b6ede9fa39cc168c3123adf4193834afdc0a608720e"
+    sha256 cellar: :any,                 arm64_sequoia:     "a5ee99ba9efa39608fcd5a812e680b91f8503a40028b35a133c5e070369d2101"
+    sha256 cellar: :any,                 arm64_sonoma:      "08b15b070e1cf38d666a507aea94cd3e9d5f299a7855b08977bc8c9cb5493010"
+    sha256 cellar: :any,                 sonoma:            "02219562e505037a1fc143016702a0d1a2d571654c54cf93e48d5d15e19da259"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "867b32388d44873c941e3f7c738522e04810f864f83ef186e6b5ed37bc2895b6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "139be996a0794794b7dd5b7f31da42645f2a5ce6846e65551ce3bc9bee7784c7"
   end
 
   depends_on "libssh2"
