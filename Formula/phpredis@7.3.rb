fed22538ea673008bff4d52d7c5d3fae946b2648
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 22:20:30 +0000

    phpredis@7.3: update 6.1.0 bottle.

--- Formula/phpredis@7.3.rb
+++ Formula/phpredis@7.3.rb
@@ -15,12 +15,13 @@ class PhpredisAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "e85295163a23ad9a72455d9b685fc2c76b7a095857d843bfe702633a45d9ddcb"
-    sha256 cellar: :any,                 arm64_sequoia: "6a7f6ee11da60750043134a9d632ba98b3c634de36331ea120cc2d7fb3ab11b8"
-    sha256 cellar: :any,                 arm64_sonoma:  "1172407d30541e728c1de2d3031e9b387d327e912b99fbd8a0d54af7cdbe980e"
-    sha256 cellar: :any,                 sonoma:        "e85cd96f0eaaacdb7edbe27b201e2c48fb7c2722d144940a3c1a3fd9222dacd4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "eb654194089ff50499cc3927fe8706e4b6bcf4bec9f27a7ceedcec3bd1caf80b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "68f25877dc968b2239052ac4617584c2c6d09279b78dc0f54fb5ca8436e6f3ce"
+    sha256 cellar: :any,                 arm64_golden_gate: "9e172aed43be3ec0d3200880b7914d6a22bcb17b1639b24f112517de28de0c69"
+    sha256 cellar: :any,                 arm64_tahoe:       "e85295163a23ad9a72455d9b685fc2c76b7a095857d843bfe702633a45d9ddcb"
+    sha256 cellar: :any,                 arm64_sequoia:     "6a7f6ee11da60750043134a9d632ba98b3c634de36331ea120cc2d7fb3ab11b8"
+    sha256 cellar: :any,                 arm64_sonoma:      "1172407d30541e728c1de2d3031e9b387d327e912b99fbd8a0d54af7cdbe980e"
+    sha256 cellar: :any,                 sonoma:            "e85cd96f0eaaacdb7edbe27b201e2c48fb7c2722d144940a3c1a3fd9222dacd4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "eb654194089ff50499cc3927fe8706e4b6bcf4bec9f27a7ceedcec3bd1caf80b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "68f25877dc968b2239052ac4617584c2c6d09279b78dc0f54fb5ca8436e6f3ce"
   end
 
   depends_on "liblzf"
