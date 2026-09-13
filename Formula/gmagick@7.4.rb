9250767eaf9ca6bbc285254739bb0248bb8a7f0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 18:50:37 +0000

    gmagick@7.4: update 2.0.6RC1 bottle.

--- Formula/gmagick@7.4.rb
+++ Formula/gmagick@7.4.rb
@@ -20,12 +20,13 @@ class GmagickAT74 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "93bdadf36f6ab5283b7c81c00b905c557e7df18a07758f898bf15825272b07a0"
-    sha256 cellar: :any,                 arm64_sequoia: "3091df2525acdfe8429528cdabc5340f84620274a1cd2320b09cba831219e210"
-    sha256 cellar: :any,                 arm64_sonoma:  "71e634671acd8f2cdc6f8f251879ce315919a523a3fba8df4edd5984311dca69"
-    sha256 cellar: :any,                 sonoma:        "37dec1dfc22e4d32a6b6f043f0c4151d0614dc024aad0d80b6f9a4552345ba99"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "45b98221a60e1bd31d8346aefe16fcfcfcc5eefbc9e57949eaae76d6682c2bab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ecc19f5cedb5af59c0d935daa2ba74cddefe2fc85c6a466f328b0bba316c8de7"
+    sha256 cellar: :any,                 arm64_golden_gate: "bc9719e28de2f92d2a9d151fd665f16ec9626bf1af2c0a88ab44f8cb2a9bd905"
+    sha256 cellar: :any,                 arm64_tahoe:       "93bdadf36f6ab5283b7c81c00b905c557e7df18a07758f898bf15825272b07a0"
+    sha256 cellar: :any,                 arm64_sequoia:     "3091df2525acdfe8429528cdabc5340f84620274a1cd2320b09cba831219e210"
+    sha256 cellar: :any,                 arm64_sonoma:      "71e634671acd8f2cdc6f8f251879ce315919a523a3fba8df4edd5984311dca69"
+    sha256 cellar: :any,                 sonoma:            "37dec1dfc22e4d32a6b6f043f0c4151d0614dc024aad0d80b6f9a4552345ba99"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "45b98221a60e1bd31d8346aefe16fcfcfcc5eefbc9e57949eaae76d6682c2bab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "ecc19f5cedb5af59c0d935daa2ba74cddefe2fc85c6a466f328b0bba316c8de7"
   end
 
   conflicts_with "imagick@7.4",
