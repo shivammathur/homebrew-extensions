2d9c2f54763b6ce0f378afcb14c8ee810cbe0fa7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 18:30:26 +0000

    memcache@7.3: update 4.0.5.2 bottle.

--- Formula/memcache@7.3.rb
+++ Formula/memcache@7.3.rb
@@ -15,16 +15,17 @@ class MemcacheAT73 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "1ec3fb26bf95ec5f6124cb70380466c336095a816fab498581b3d5a824b616a9"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1e39d9b2face840e6cc2f1f840b05f329642fd0517bb23ed88dedf3ace1fe532"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "739671b51d3555fbd4293e9fe04c67498111bc5e4a44103a933235bdeb9383b9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ec185b645c3c5b5197784cadd6e10796b47a1d7fc3300cf71652746c9afec79e"
-    sha256 cellar: :any,                 sonoma:         "14e39d7f0463bab7789b7ffbc2ec70fb3b5471633ee6f718cfb292a017742101"
-    sha256 cellar: :any_skip_relocation, ventura:        "bc84a3c6e261a4c4e3c65800827bee1b71f0ebe05d7068e30f5284ad751743ad"
-    sha256 cellar: :any_skip_relocation, big_sur:        "4ea2ed416c4e7986948946d69e06d7dcace83b6262224716cee78e55dcc22e5e"
-    sha256 cellar: :any_skip_relocation, catalina:       "9cd96f363b7748e9616c12caa85247c1faff4502aa9eb20e1c364c1e7bd70737"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "9a3c42a62c56aee23bebf30e32fa418460a725ef5df547f81918d96303fb06c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "456a04eaf5ce8e5ca29f0422af1093606dc32edf94bc27476a65d7f57f8046f4"
+    sha256 cellar: :any,                 arm64_golden_gate: "2fc1c893acd3e3d4fecb0cc6677da9ce0a5682fb85416ad5f0c4b6d86060054d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "1ec3fb26bf95ec5f6124cb70380466c336095a816fab498581b3d5a824b616a9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:     "1e39d9b2face840e6cc2f1f840b05f329642fd0517bb23ed88dedf3ace1fe532"
+    sha256 cellar: :any_skip_relocation, arm64_monterey:    "739671b51d3555fbd4293e9fe04c67498111bc5e4a44103a933235bdeb9383b9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:     "ec185b645c3c5b5197784cadd6e10796b47a1d7fc3300cf71652746c9afec79e"
+    sha256 cellar: :any,                 sonoma:            "14e39d7f0463bab7789b7ffbc2ec70fb3b5471633ee6f718cfb292a017742101"
+    sha256 cellar: :any_skip_relocation, ventura:           "bc84a3c6e261a4c4e3c65800827bee1b71f0ebe05d7068e30f5284ad751743ad"
+    sha256 cellar: :any_skip_relocation, big_sur:           "4ea2ed416c4e7986948946d69e06d7dcace83b6262224716cee78e55dcc22e5e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "9a3c42a62c56aee23bebf30e32fa418460a725ef5df547f81918d96303fb06c7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "456a04eaf5ce8e5ca29f0422af1093606dc32edf94bc27476a65d7f57f8046f4"
+    sha256 cellar: :any_skip_relocation, x86_64_catalina:   "9cd96f363b7748e9616c12caa85247c1faff4502aa9eb20e1c364c1e7bd70737"
   end
 
   depends_on "zlib"
