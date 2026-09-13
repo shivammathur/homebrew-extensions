62911b2ae2322da379f1a89bfb4193975e593688
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 18:06:55 +0000

    mailparse@7.2: update 3.1.3 bottle.

--- Formula/mailparse@7.2.rb
+++ Formula/mailparse@7.2.rb
@@ -15,16 +15,17 @@ class MailparseAT72 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "7f64dd521b52d973a90b986e4c533078212637b5c87fb5eba43a551084610799"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "71f7ef538b072c8d872e932b97d0ef8b4ab421b08600d3c8230b16bd028e838f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "902a18e74b6888c2e18cf2b999c80f8e183880a5b71603a71e7ef84a90cc30e5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f06e175c966786e514a46a5383c926caa58ea572df9b384a2158a39c942e299d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a2c658ee3da63298ecc7b64e94d094379a7d601433e5797ceb840913e4214e07"
-    sha256 cellar: :any_skip_relocation, ventura:        "58396970e056c0f755298539d2d3a88833eaa0c11b2126a6c93ab00e73084031"
-    sha256 cellar: :any_skip_relocation, big_sur:        "32a5f199a260d8a1f1d78e9e0690cdbbc0f0c917da5e907e867ae42f0edb8ba9"
-    sha256 cellar: :any_skip_relocation, catalina:       "cf6f724472d2760e884f8be30df6f0b1fa1832c02adc9f5f91a07be24c7a1048"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "b0be4b55a82cb6327da602d719d4e67aa7a1b463cda5ba274c92d731e65259b2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ba871a50281191fff23a2cb9e5a4d0ab6d5ab528536311767b4917e09c10212"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "564295d2d16d5212af0137d63a246a9b6619728415e8493ab9897591e07f5141"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "7f64dd521b52d973a90b986e4c533078212637b5c87fb5eba43a551084610799"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:     "71f7ef538b072c8d872e932b97d0ef8b4ab421b08600d3c8230b16bd028e838f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey:    "902a18e74b6888c2e18cf2b999c80f8e183880a5b71603a71e7ef84a90cc30e5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:     "f06e175c966786e514a46a5383c926caa58ea572df9b384a2158a39c942e299d"
+    sha256 cellar: :any_skip_relocation, sonoma:            "a2c658ee3da63298ecc7b64e94d094379a7d601433e5797ceb840913e4214e07"
+    sha256 cellar: :any_skip_relocation, ventura:           "58396970e056c0f755298539d2d3a88833eaa0c11b2126a6c93ab00e73084031"
+    sha256 cellar: :any_skip_relocation, big_sur:           "32a5f199a260d8a1f1d78e9e0690cdbbc0f0c917da5e907e867ae42f0edb8ba9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "b0be4b55a82cb6327da602d719d4e67aa7a1b463cda5ba274c92d731e65259b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "3ba871a50281191fff23a2cb9e5a4d0ab6d5ab528536311767b4917e09c10212"
+    sha256 cellar: :any_skip_relocation, x86_64_catalina:   "cf6f724472d2760e884f8be30df6f0b1fa1832c02adc9f5f91a07be24c7a1048"
   end
 
   def install
