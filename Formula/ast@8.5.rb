c81b36299a54209cdd88b11ecc102575fc53f9d7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 21:21:55 +0000

    ast@8.5: update 1.1.3_1 bottle.

--- Formula/ast@8.5.rb
+++ Formula/ast@8.5.rb
@@ -21,12 +21,13 @@ class AstAT85 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "617bd089677c8ed2e5b05d4737333f79a59973195b55a52249cbc5907deb46c2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9289640e66d933287f310fb275c10c387388a87d341db6ed1cbc7d4e2c93318a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1689b09243a1d8bf62b8adc1205a1833c85dab4573945b63f5e0aed53b42da1b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "acea329d97778a48643bf189481b8884e299a03bdfbbea7b898ed34d6145bbdc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4bc9043bc6de5636e216f0fc96d6e2e76e0b2f29d233e1a9f95ad9e31b6ba10d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "70189e4f2f02cdaf78ab94d678e1a8843efe9ecb6149591a757dec16e091e82e"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "7fc5a0eaaf7654c2b20a482d6e787412ad2f6d2bee408d829a634611a2bd3483"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "617bd089677c8ed2e5b05d4737333f79a59973195b55a52249cbc5907deb46c2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "9289640e66d933287f310fb275c10c387388a87d341db6ed1cbc7d4e2c93318a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "1689b09243a1d8bf62b8adc1205a1833c85dab4573945b63f5e0aed53b42da1b"
+    sha256 cellar: :any_skip_relocation, sonoma:            "acea329d97778a48643bf189481b8884e299a03bdfbbea7b898ed34d6145bbdc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "4bc9043bc6de5636e216f0fc96d6e2e76e0b2f29d233e1a9f95ad9e31b6ba10d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "70189e4f2f02cdaf78ab94d678e1a8843efe9ecb6149591a757dec16e091e82e"
   end
 
   def install
