f06a43c4e493bcf11d40e1fd7231b06a2ca02b96
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 16:48:20 +0000

    raphf@7.1: update 2.0.2 bottle.

--- Formula/raphf@7.1.rb
+++ Formula/raphf@7.1.rb
@@ -21,14 +21,15 @@ class RaphfAT71 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b6f2b2d0ef1c1440f7a53c07caf347f7933228c44ed7a2b19d05ae483a6f1fc2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0d2d2c0eccf5e8977fb73878f3ce2510ff38fd28856a530a7d64c5531eca6d4d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cc549bd36b3610c9fd43555c6830275d9a8fc834661fcebcf8c9d8c64cc5c618"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "db787d2b70a03037be7f1534d1393017c3a5dfed847607ffd50f82a9dcfef270"
-    sha256 cellar: :any_skip_relocation, sonoma:        "de23ab315237e27a5fccf6e44fc2e46a98c9e679bd1d79b1163b6884ceae3baf"
-    sha256 cellar: :any_skip_relocation, ventura:       "a7de0d76002ef98698a2defc2fe25478205eca7972af8fbea31613b7b431c14a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a40c1581a54d435b13b88a8fdd0cac9a65f308d80a881724817973d0510a823b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5742c80fe6aa54ede467bfd21a974a4c100d717a4bf2e1ba6bbc7eeceb38c1be"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "596f6013fb1d5bfad8e0a21f6a46d26fb844a5ab884c71c7b13b79dd0f902929"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "b6f2b2d0ef1c1440f7a53c07caf347f7933228c44ed7a2b19d05ae483a6f1fc2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "0d2d2c0eccf5e8977fb73878f3ce2510ff38fd28856a530a7d64c5531eca6d4d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "cc549bd36b3610c9fd43555c6830275d9a8fc834661fcebcf8c9d8c64cc5c618"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:     "db787d2b70a03037be7f1534d1393017c3a5dfed847607ffd50f82a9dcfef270"
+    sha256 cellar: :any_skip_relocation, sonoma:            "de23ab315237e27a5fccf6e44fc2e46a98c9e679bd1d79b1163b6884ceae3baf"
+    sha256 cellar: :any_skip_relocation, ventura:           "a7de0d76002ef98698a2defc2fe25478205eca7972af8fbea31613b7b431c14a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "a40c1581a54d435b13b88a8fdd0cac9a65f308d80a881724817973d0510a823b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "5742c80fe6aa54ede467bfd21a974a4c100d717a4bf2e1ba6bbc7eeceb38c1be"
   end
 
   def install
