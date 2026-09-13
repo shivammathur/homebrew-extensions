164d44adc8dcdcc85cb0ed5d199438ca95341595
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 17:07:15 +0000

    memcache@5.6: update 3.0.8 bottle.

--- Formula/memcache@5.6.rb
+++ Formula/memcache@5.6.rb
@@ -16,15 +16,16 @@ class MemcacheAT56 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "6ed209a01d995af840cf8105be50a81111a59f7dc5775a9626d2c1a9a49cc8d1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "247d5ba60ad3a00a3cbb5a910f9be6399e65b9e6ec439cd9818cf41086618d1d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "07e71c24e4d89d38467ecb611436e61b1f6625abb599d9b42ef05ce13f30e376"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b532920875491f228d3db7761c3ea8319a15db0bf339a2316ee12eab3f547897"
-    sha256 cellar: :any_skip_relocation, sonoma:         "c8126b340fb1dfbd20a9e7cf1ebd7c406a9df996dcb64c1fbe287ec2ebe280de"
-    sha256 cellar: :any_skip_relocation, ventura:        "b02431a40ffb9363b54fd763d074357c5c4627e892e0d6d35a5eca95f5a83335"
-    sha256 cellar: :any_skip_relocation, monterey:       "b53981195df335a32b8eaa3b9a33b5d4169899433b43becb2f62c610086f2712"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "674243b1ecc6c93073430d76dbf457722de5343e1ee7dbd79de57d3267d4da58"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "294a7c6ee68c5c92623fe32ccd0f5d9836ecdd202cdd625a65db9468f08d591e"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "62f4c575d5d354363d8b8dec5462e11a15e5450e1b147ae65d2aba66f87efb5e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "6ed209a01d995af840cf8105be50a81111a59f7dc5775a9626d2c1a9a49cc8d1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "247d5ba60ad3a00a3cbb5a910f9be6399e65b9e6ec439cd9818cf41086618d1d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:     "07e71c24e4d89d38467ecb611436e61b1f6625abb599d9b42ef05ce13f30e376"
+    sha256 cellar: :any_skip_relocation, arm64_monterey:    "b532920875491f228d3db7761c3ea8319a15db0bf339a2316ee12eab3f547897"
+    sha256 cellar: :any_skip_relocation, sonoma:            "c8126b340fb1dfbd20a9e7cf1ebd7c406a9df996dcb64c1fbe287ec2ebe280de"
+    sha256 cellar: :any_skip_relocation, ventura:           "b02431a40ffb9363b54fd763d074357c5c4627e892e0d6d35a5eca95f5a83335"
+    sha256 cellar: :any_skip_relocation, monterey:          "b53981195df335a32b8eaa3b9a33b5d4169899433b43becb2f62c610086f2712"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "674243b1ecc6c93073430d76dbf457722de5343e1ee7dbd79de57d3267d4da58"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "294a7c6ee68c5c92623fe32ccd0f5d9836ecdd202cdd625a65db9468f08d591e"
   end
 
   resource "inline_patch" do
