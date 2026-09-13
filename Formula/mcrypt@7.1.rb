ed02529900c17eb144f5a99d455ce6870e76c4e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 17:47:02 +0000

    mcrypt@7.1: update 7.1.33 bottle.

--- Formula/mcrypt@7.1.rb
+++ Formula/mcrypt@7.1.rb
@@ -17,12 +17,13 @@ class McryptAT71 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 24
-    sha256 cellar: :any, arm64_tahoe:   "e1741c1bfdbdbcdf99dd21906794a37f8e2ae3c4f3654e48846a103efa44e83c"
-    sha256 cellar: :any, arm64_sequoia: "3503c6d406b4f2eab2c7ff9b3011fcf278c9535f713ce8645af9aebaa7c92079"
-    sha256 cellar: :any, arm64_sonoma:  "5157ae504717fcd86c1c2325e35ba07eaf772b1a60948c6a815ab9e6db60afb3"
-    sha256 cellar: :any, sonoma:        "6a3a35ac4d1a7a73b68c778c5da369fd322af026ea3f64003902412271a35bfc"
-    sha256 cellar: :any, arm64_linux:   "dfc23da351ff297283aabf60298d084ac9aab71f849c2016b18c788a78b7a596"
-    sha256 cellar: :any, x86_64_linux:  "125d530574c175c96a8715a2c3c2ab25605523c76c43bfbcbd5df7b748cfe600"
+    sha256 cellar: :any, arm64_golden_gate: "431119551d7d14d4ce6310a8a832b113e811b45f79133067c03f965e8704ef15"
+    sha256 cellar: :any, arm64_tahoe:       "e1741c1bfdbdbcdf99dd21906794a37f8e2ae3c4f3654e48846a103efa44e83c"
+    sha256 cellar: :any, arm64_sequoia:     "3503c6d406b4f2eab2c7ff9b3011fcf278c9535f713ce8645af9aebaa7c92079"
+    sha256 cellar: :any, arm64_sonoma:      "5157ae504717fcd86c1c2325e35ba07eaf772b1a60948c6a815ab9e6db60afb3"
+    sha256 cellar: :any, sonoma:            "6a3a35ac4d1a7a73b68c778c5da369fd322af026ea3f64003902412271a35bfc"
+    sha256 cellar: :any, arm64_linux:       "dfc23da351ff297283aabf60298d084ac9aab71f849c2016b18c788a78b7a596"
+    sha256 cellar: :any, x86_64_linux:      "125d530574c175c96a8715a2c3c2ab25605523c76c43bfbcbd5df7b748cfe600"
   end
 
   depends_on "automake" => :build
