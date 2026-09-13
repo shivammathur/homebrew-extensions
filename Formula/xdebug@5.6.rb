5aab1afb1d6c3c0ca88490defea5907c8ad143e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 17:14:02 +0000

    xdebug@5.6: update 2.5.5 bottle.

--- Formula/xdebug@5.6.rb
+++ Formula/xdebug@5.6.rb
@@ -16,12 +16,13 @@ class XdebugAT56 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "53d32e6996a6ee4b70602020831ada06909c6e04590be2ab8a0a9f7f8868a882"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "46fa1ffb1cabb564f3355b214a602b02cfe74d091be8d6e3dec5554890ac9e40"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7eeb0ffa62c2cd08c291762f1b3ef34bd70c573af952c345f6e1fef253270dbc"
-    sha256 cellar: :any_skip_relocation, sonoma:        "da1957b11216c8a8f4b17a84f3fd63058fb75be3f44b3bf83dc76973589fb534"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "25109ed2684223609dd44e97cd8971910c2c5bc1257e2c7d94b8cb86f48dc282"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bee0b8a7a9826c6894ae414945f30c549d939a9c5052b0d4f10fbd59b70eef22"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "dc4bb035361c1593166d57c981d7e3066d41d6feada649f248a2fdf16d105648"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "53d32e6996a6ee4b70602020831ada06909c6e04590be2ab8a0a9f7f8868a882"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "46fa1ffb1cabb564f3355b214a602b02cfe74d091be8d6e3dec5554890ac9e40"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "7eeb0ffa62c2cd08c291762f1b3ef34bd70c573af952c345f6e1fef253270dbc"
+    sha256 cellar: :any_skip_relocation, sonoma:            "da1957b11216c8a8f4b17a84f3fd63058fb75be3f44b3bf83dc76973589fb534"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "25109ed2684223609dd44e97cd8971910c2c5bc1257e2c7d94b8cb86f48dc282"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "bee0b8a7a9826c6894ae414945f30c549d939a9c5052b0d4f10fbd59b70eef22"
   end
 
   conflicts_with "uopz@5.6", because: "uopz for PHP 5.6 conflicts with Xdebug."
