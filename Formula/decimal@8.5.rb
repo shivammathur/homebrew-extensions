e67a2e5c8ce5358142987d6f1cc4331619b4be6f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 21:25:13 +0000

    decimal@8.5: update 2.0.1 bottle.

--- Formula/decimal@8.5.rb
+++ Formula/decimal@8.5.rb
@@ -20,12 +20,13 @@ class DecimalAT85 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "f5635b569ffb7b4e3b650864f03ce7f95959fdf79e030eb9649b19f64120ebd5"
-    sha256 cellar: :any,                 arm64_sequoia: "e38bb0008054ee05c23b80b914c29c75bfed8faf125b5dc5c7e3ab77c34938b1"
-    sha256 cellar: :any,                 arm64_sonoma:  "41b57a09efe817502d6b7e2be442f7712da8c7c8d4fe1ef67ad379281d16935a"
-    sha256 cellar: :any,                 sonoma:        "76c62d558e81b7ff2c89a8d5e9daa8be60eefb6e59ef1080bef4b0eacba32ba7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6f7a056eee1e0a30af29766a9673b5216c9a23d3a3ea7fcf34543524217a9c74"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ddaffdc6cb77ab828dbee284d5235b0fb71f2c19fbcaa6649ede395c506ee38b"
+    sha256 cellar: :any,                 arm64_golden_gate: "04aadc304c58b86780dff3624e244e618ec30c3acb7b00730ade29e3c9705faa"
+    sha256 cellar: :any,                 arm64_tahoe:       "f5635b569ffb7b4e3b650864f03ce7f95959fdf79e030eb9649b19f64120ebd5"
+    sha256 cellar: :any,                 arm64_sequoia:     "e38bb0008054ee05c23b80b914c29c75bfed8faf125b5dc5c7e3ab77c34938b1"
+    sha256 cellar: :any,                 arm64_sonoma:      "41b57a09efe817502d6b7e2be442f7712da8c7c8d4fe1ef67ad379281d16935a"
+    sha256 cellar: :any,                 sonoma:            "76c62d558e81b7ff2c89a8d5e9daa8be60eefb6e59ef1080bef4b0eacba32ba7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "6f7a056eee1e0a30af29766a9673b5216c9a23d3a3ea7fcf34543524217a9c74"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "ddaffdc6cb77ab828dbee284d5235b0fb71f2c19fbcaa6649ede395c506ee38b"
   end
 
   depends_on "mpdecimal"
