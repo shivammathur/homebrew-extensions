2c6e409a6d39e51d1e585765a5754c79ac86a673
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 18:46:18 +0000

    amqp@7.4: update 2.2.0 bottle.

--- Formula/amqp@7.4.rb
+++ Formula/amqp@7.4.rb
@@ -20,12 +20,13 @@ class AmqpAT74 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "eb2f4a0274773bb1b4e718a425d1971166e45e932ba38a8f6663d150f8c69795"
-    sha256 cellar: :any,                 arm64_sequoia: "42dc65a01e5b705d57ddd9e7645b9edf06b736bfb24b24dee4aec7a8f7473429"
-    sha256 cellar: :any,                 arm64_sonoma:  "17dffbff2cbab309be8fd93b7f7f1c384999ee05fd476d2914abd3f251f0a3fe"
-    sha256 cellar: :any,                 sonoma:        "640bb7639a6d71e833d2655300305e648017b3e7ca56d185db9a21b8a7c432e8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "79723b3fae0dfa6c3f0b4d0f8b0e25885f28af41752e80b8cfbfe0e416e13857"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "375c079ab612cc2d6df9cc46aab43dc04bfa986eed544449b0705b6d4f0f727e"
+    sha256 cellar: :any,                 arm64_golden_gate: "e2cd409bc61013736bcf741aff1e944c3add264bd0cf4235f07c6c0d091e5bf5"
+    sha256 cellar: :any,                 arm64_tahoe:       "eb2f4a0274773bb1b4e718a425d1971166e45e932ba38a8f6663d150f8c69795"
+    sha256 cellar: :any,                 arm64_sequoia:     "42dc65a01e5b705d57ddd9e7645b9edf06b736bfb24b24dee4aec7a8f7473429"
+    sha256 cellar: :any,                 arm64_sonoma:      "17dffbff2cbab309be8fd93b7f7f1c384999ee05fd476d2914abd3f251f0a3fe"
+    sha256 cellar: :any,                 sonoma:            "640bb7639a6d71e833d2655300305e648017b3e7ca56d185db9a21b8a7c432e8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "79723b3fae0dfa6c3f0b4d0f8b0e25885f28af41752e80b8cfbfe0e416e13857"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "375c079ab612cc2d6df9cc46aab43dc04bfa986eed544449b0705b6d4f0f727e"
   end
 
   depends_on "rabbitmq-c"
