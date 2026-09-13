f43bf4bbd441a589a6a7b194f678dc42bcf0bb01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 21:17:13 +0000

    uopz@8.4: update 7.1.1 bottle.

--- Formula/uopz@8.4.rb
+++ Formula/uopz@8.4.rb
@@ -26,12 +26,13 @@ class UopzAT84 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8a572f5dc0917b746b2f8b94df5fac065eaeb08ca7373168b97844fd1c52e73a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6313ca70311341075314dfe9ca383a9a104f29a48618fc6d34ebb5b9c4fe3d0e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bbd19a90041da7e2aa64dd15a117eb45718d8f6deae66734e770234e630e20af"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b0c2bc858463dba79d8bb40408094122dca3c93500f5d9f441c44e62244d0473"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a27797040913a0dd2ab28bc43bf164282a389c86317ecdffb283d4755561a377"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "87c94f81e34bb57f7336f1294ab5b5d710ec906944914e4a75a466113e7761b8"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "d58bca2d20b8e087df5c5b292343f652b449601f2c3da9ff565da1d4fb376e2d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "8a572f5dc0917b746b2f8b94df5fac065eaeb08ca7373168b97844fd1c52e73a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "6313ca70311341075314dfe9ca383a9a104f29a48618fc6d34ebb5b9c4fe3d0e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "bbd19a90041da7e2aa64dd15a117eb45718d8f6deae66734e770234e630e20af"
+    sha256 cellar: :any_skip_relocation, sonoma:            "b0c2bc858463dba79d8bb40408094122dca3c93500f5d9f441c44e62244d0473"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "a27797040913a0dd2ab28bc43bf164282a389c86317ecdffb283d4755561a377"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "87c94f81e34bb57f7336f1294ab5b5d710ec906944914e4a75a466113e7761b8"
   end
 
   priority "10"
