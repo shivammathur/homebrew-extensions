ad2f0aae6e8437c344d1106e001de0a158e9d90f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 21:24:28 +0000

    event@8.5: update 3.1.6 bottle.

--- Formula/event@8.5.rb
+++ Formula/event@8.5.rb
@@ -20,12 +20,13 @@ class EventAT85 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any, arm64_tahoe:   "0143dca773a2d02d1afdb49ffc7ef3607f96cf8794d03796e8943d74e292fff4"
-    sha256 cellar: :any, arm64_sequoia: "6ffed0bce0c30f6410c2f88f0f9940cc9e1a41dc6d1029f0681806c2a821e292"
-    sha256 cellar: :any, arm64_sonoma:  "707b6d221fdca094d431640804622ca98c847a2d783bbc4c69e7aa5e69dac3db"
-    sha256 cellar: :any, sonoma:        "4ff5a6444420493ae2d824dbcda0035ffca0dc5881e398437a4f332101ee3449"
-    sha256 cellar: :any, arm64_linux:   "e904ee4248ed5610e2ebb48ad60600c17f71e593eff494f96a71fab9cdc70dc5"
-    sha256 cellar: :any, x86_64_linux:  "09192304bc1a4ceba6011c371e54667ae18063e3f7f41ce8dd4ff6bbe03c9d3f"
+    sha256 cellar: :any, arm64_golden_gate: "433b014be9629cd0cf600ea2773c79c0e6ff4d8f077ac430afe459c151602b9f"
+    sha256 cellar: :any, arm64_tahoe:       "0143dca773a2d02d1afdb49ffc7ef3607f96cf8794d03796e8943d74e292fff4"
+    sha256 cellar: :any, arm64_sequoia:     "6ffed0bce0c30f6410c2f88f0f9940cc9e1a41dc6d1029f0681806c2a821e292"
+    sha256 cellar: :any, arm64_sonoma:      "707b6d221fdca094d431640804622ca98c847a2d783bbc4c69e7aa5e69dac3db"
+    sha256 cellar: :any, sonoma:            "4ff5a6444420493ae2d824dbcda0035ffca0dc5881e398437a4f332101ee3449"
+    sha256 cellar: :any, arm64_linux:       "e904ee4248ed5610e2ebb48ad60600c17f71e593eff494f96a71fab9cdc70dc5"
+    sha256 cellar: :any, x86_64_linux:      "09192304bc1a4ceba6011c371e54667ae18063e3f7f41ce8dd4ff6bbe03c9d3f"
   end
 
   depends_on "libevent"
