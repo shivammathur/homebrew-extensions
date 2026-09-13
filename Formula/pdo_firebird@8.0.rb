9ccd54dd5019c710fbd2f91b3fdd8cc9e6733aa3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 22:11:14 +0000

    pdo_firebird@8.0: update 8.0.30 bottle.

--- Formula/pdo_firebird@8.0.rb
+++ Formula/pdo_firebird@8.0.rb
@@ -16,12 +16,13 @@ class PdoFirebirdAT80 < AbstractPhpExtension
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
     rebuild 3
-    sha256 cellar: :any, arm64_tahoe:   "b27c33bb4f219c671e60f1928baa136a19cd2d283f46a5a3c02c4411cebe8a6e"
-    sha256 cellar: :any, arm64_sequoia: "3970af65b7ed573c0a282c6b377d0b2e5490caa57665a2ac5a026a042c2fdfe0"
-    sha256 cellar: :any, arm64_sonoma:  "2e4503cc6b808558af7c41bf4732f20b5988707a22ab3bcf18fcac51d787a7b9"
-    sha256 cellar: :any, sonoma:        "9c3d358934a53c678ae6fd945c7eb8a8bc45a186b3f1411cfe4a43b2c42efcf7"
-    sha256 cellar: :any, arm64_linux:   "a690758413a36fac8ae26a2bf199865d9cad657c92287e0bfc0314e00e552ce5"
-    sha256 cellar: :any, x86_64_linux:  "6aa890c84cda404f99039493a8f96c3acc1a8e8fe278e9b4b1aa9f32f3588018"
+    sha256 cellar: :any, arm64_golden_gate: "989fa400a4c1b50ad17d525d0c99f1f23abecbeb0446396cd3da225a6f48d298"
+    sha256 cellar: :any, arm64_tahoe:       "b27c33bb4f219c671e60f1928baa136a19cd2d283f46a5a3c02c4411cebe8a6e"
+    sha256 cellar: :any, arm64_sequoia:     "3970af65b7ed573c0a282c6b377d0b2e5490caa57665a2ac5a026a042c2fdfe0"
+    sha256 cellar: :any, arm64_sonoma:      "2e4503cc6b808558af7c41bf4732f20b5988707a22ab3bcf18fcac51d787a7b9"
+    sha256 cellar: :any, sonoma:            "9c3d358934a53c678ae6fd945c7eb8a8bc45a186b3f1411cfe4a43b2c42efcf7"
+    sha256 cellar: :any, arm64_linux:       "a690758413a36fac8ae26a2bf199865d9cad657c92287e0bfc0314e00e552ce5"
+    sha256 cellar: :any, x86_64_linux:      "6aa890c84cda404f99039493a8f96c3acc1a8e8fe278e9b4b1aa9f32f3588018"
   end
 
   depends_on "shivammathur/extensions/firebird-client@3"
