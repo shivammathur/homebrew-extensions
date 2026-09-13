f8c7454d628774ecd5aa23a43c3c1bcc4f5d1e1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 22:00:58 +0000

    uploadprogress@8.6: update 2.0.2_1 bottle.

--- Formula/uploadprogress@8.6.rb
+++ Formula/uploadprogress@8.6.rb
@@ -21,12 +21,13 @@ class UploadprogressAT86 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "42dab277bebea480b0cb3ff7cac130134ede111df245e7dae13950e50425a1d5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "460929e09c35010ad12357201331c08ba01af8bcd4d65ac74efe8bb644235ad4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "74d407063313abd9da6b148a94163da3d063bdc67770ef54b4b3494c71b1ea79"
-    sha256 cellar: :any_skip_relocation, sonoma:        "805828186fef3c92e26b6bb36805a0a88913227937f40736a3bbdd2801089d12"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b4cfa1286cf4094d8781d60ab43a6974d415f1f338b73154f7fb8b154c160e52"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8730604895f03bd1672ccd814e3623d1fd986538fb89265057a69e6a2b60da3"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "aea5b0f651ce722466bffbb3d3dabf23c72a08e04690b14cb57160743039c81b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "42dab277bebea480b0cb3ff7cac130134ede111df245e7dae13950e50425a1d5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "460929e09c35010ad12357201331c08ba01af8bcd4d65ac74efe8bb644235ad4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "74d407063313abd9da6b148a94163da3d063bdc67770ef54b4b3494c71b1ea79"
+    sha256 cellar: :any_skip_relocation, sonoma:            "805828186fef3c92e26b6bb36805a0a88913227937f40736a3bbdd2801089d12"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "b4cfa1286cf4094d8781d60ab43a6974d415f1f338b73154f7fb8b154c160e52"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "b8730604895f03bd1672ccd814e3623d1fd986538fb89265057a69e6a2b60da3"
   end
 
   def install
