58545dd8d78532b6b518c75c2874e99611c66bc4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 21:26:54 +0000

    gnupg@8.5: update 1.5.4_1 bottle.

--- Formula/gnupg@8.5.rb
+++ Formula/gnupg@8.5.rb
@@ -21,12 +21,13 @@ class GnupgAT85 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "ba405f650b2f79c0387bf16aba156c11ee3a3094e4194902a7ec2bb6f328415e"
-    sha256 cellar: :any,                 arm64_sequoia: "5c81a5b532edb1fd03c45e21e4ce0915a366cfc673d856bc523d4489931317eb"
-    sha256 cellar: :any,                 arm64_sonoma:  "8517d35113f60d4994bba0df671876f0890ad9c0520542a83720264612d9be90"
-    sha256 cellar: :any,                 sonoma:        "03ada1ceb8c781a577a6583ad62f5e4c0439f031f5e15225152c9ad8ebd8af33"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c18a77114d62582058aeb43b97f043cf2b6f9d13c7bad3790a6b713edc3178ef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "16c01aa5b3b7a6a70a0ebba88c02e37a449155c0fd344f8e373443db0a9567ef"
+    sha256 cellar: :any,                 arm64_golden_gate: "37284f143893e13dffd3c17f5f6066e233ea6fe0f6026dd361b5cb88693df064"
+    sha256 cellar: :any,                 arm64_tahoe:       "ba405f650b2f79c0387bf16aba156c11ee3a3094e4194902a7ec2bb6f328415e"
+    sha256 cellar: :any,                 arm64_sequoia:     "5c81a5b532edb1fd03c45e21e4ce0915a366cfc673d856bc523d4489931317eb"
+    sha256 cellar: :any,                 arm64_sonoma:      "8517d35113f60d4994bba0df671876f0890ad9c0520542a83720264612d9be90"
+    sha256 cellar: :any,                 sonoma:            "03ada1ceb8c781a577a6583ad62f5e4c0439f031f5e15225152c9ad8ebd8af33"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "c18a77114d62582058aeb43b97f043cf2b6f9d13c7bad3790a6b713edc3178ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "16c01aa5b3b7a6a70a0ebba88c02e37a449155c0fd344f8e373443db0a9567ef"
   end
 
   depends_on "gpgme"
