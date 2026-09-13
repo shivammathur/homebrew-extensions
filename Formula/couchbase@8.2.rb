1d60f15f38623af3f64bac84abe9cec21b993790
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 20:09:58 +0000

    couchbase@8.2: update 4.5.0 bottle.

--- Formula/couchbase@8.2.rb
+++ Formula/couchbase@8.2.rb
@@ -20,12 +20,13 @@ class CouchbaseAT82 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_tahoe:   "968d6ec5c0fb86fccf2977869da31ab0a59fceb54e4b04d3aca07080999a37f7"
-    sha256 cellar: :any,                 arm64_sequoia: "d9c3924edc1b3af9cdd504a0b07a91d0740424cc5c2c917b16f3aae78c0c7986"
-    sha256 cellar: :any,                 arm64_sonoma:  "baa9dfc701a82872956494fa954573c75b9074353860833f71281ade07b77c0c"
-    sha256 cellar: :any,                 sonoma:        "1dc2d80026fa2d3df6beec72179ab97e814436043f9c601207e6ad3e4caa7e51"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d1ce2953f2c3658dc1bbee8cb817ab153ff15b98f8aa73457400b601c60447ff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b637d5c6b281cef1c50b2b96e7f524f4251e6604babb703de78bc759c4d2d2e0"
+    sha256 cellar: :any,                 arm64_golden_gate: "0e3326316c6e02780d52277821c8738d360e8e108fb07f482f83f9bc2efdfc2a"
+    sha256 cellar: :any,                 arm64_tahoe:       "968d6ec5c0fb86fccf2977869da31ab0a59fceb54e4b04d3aca07080999a37f7"
+    sha256 cellar: :any,                 arm64_sequoia:     "d9c3924edc1b3af9cdd504a0b07a91d0740424cc5c2c917b16f3aae78c0c7986"
+    sha256 cellar: :any,                 arm64_sonoma:      "baa9dfc701a82872956494fa954573c75b9074353860833f71281ade07b77c0c"
+    sha256 cellar: :any,                 sonoma:            "1dc2d80026fa2d3df6beec72179ab97e814436043f9c601207e6ad3e4caa7e51"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "d1ce2953f2c3658dc1bbee8cb817ab153ff15b98f8aa73457400b601c60447ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "b637d5c6b281cef1c50b2b96e7f524f4251e6604babb703de78bc759c4d2d2e0"
   end
 
   depends_on "cmake" => :build
