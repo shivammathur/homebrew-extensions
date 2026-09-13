b4ef4661cde5244e5bea739c49823bf9220adca8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 17:12:22 +0000

    uuid@5.6: update 1.0.5 bottle.

--- Formula/uuid@5.6.rb
+++ Formula/uuid@5.6.rb
@@ -15,16 +15,17 @@ class UuidAT56 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any,                 arm64_sequoia:  "9f9bb838c74ca441e669dfce9175948250794d2c5d2801aed74e114c47adf309"
-    sha256 cellar: :any,                 arm64_ventura:  "da9c6203abe7105ff76d12a344a177685bde5800fb89ca8561f6cf24408764f8"
-    sha256 cellar: :any,                 arm64_monterey: "94a8612ef072ecf2caf2863dabfa8424da514596ea2c4311474f7ca9a43783e5"
-    sha256 cellar: :any,                 arm64_big_sur:  "018111e9ffb6b71e0b7fcca6cdff33c2e5e352f35e668ba805dcebb03f9c6b08"
-    sha256 cellar: :any,                 sonoma:         "b2c63ffd1eacfc4db0b944ec4de6e0c3c757bbfabc52558ecad1b60bd3d0eea0"
-    sha256 cellar: :any,                 ventura:        "ec6e80ef18b683b2e8053c7913c65d585d26a44758467088fc895e6214e7edba"
-    sha256 cellar: :any,                 monterey:       "51bac70303e9639a0586e8022e638fe91e1c392d243fbf29b9ebaa106a07931d"
-    sha256 cellar: :any,                 big_sur:        "40c4bf9e704f9bec958334b562e909cb0d297dc3570649811eafdd13e72768a8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "b8ef52dd57aa1ef6b19ce0cc5d3540e300f4258871ab0e5bf0a7ce8a87aa6647"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "046eed04ced266750613dbe22c0df1eca13a50625e7b91997cc12a28d634337b"
+    sha256 cellar: :any,                 arm64_golden_gate: "a7d59ce57d127f02038fae7967e2f26df19c72e6a03310335720340d677f3dca"
+    sha256 cellar: :any,                 arm64_sequoia:     "9f9bb838c74ca441e669dfce9175948250794d2c5d2801aed74e114c47adf309"
+    sha256 cellar: :any,                 arm64_ventura:     "da9c6203abe7105ff76d12a344a177685bde5800fb89ca8561f6cf24408764f8"
+    sha256 cellar: :any,                 arm64_monterey:    "94a8612ef072ecf2caf2863dabfa8424da514596ea2c4311474f7ca9a43783e5"
+    sha256 cellar: :any,                 arm64_big_sur:     "018111e9ffb6b71e0b7fcca6cdff33c2e5e352f35e668ba805dcebb03f9c6b08"
+    sha256 cellar: :any,                 sonoma:            "b2c63ffd1eacfc4db0b944ec4de6e0c3c757bbfabc52558ecad1b60bd3d0eea0"
+    sha256 cellar: :any,                 ventura:           "ec6e80ef18b683b2e8053c7913c65d585d26a44758467088fc895e6214e7edba"
+    sha256 cellar: :any,                 monterey:          "51bac70303e9639a0586e8022e638fe91e1c392d243fbf29b9ebaa106a07931d"
+    sha256 cellar: :any,                 big_sur:           "40c4bf9e704f9bec958334b562e909cb0d297dc3570649811eafdd13e72768a8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "b8ef52dd57aa1ef6b19ce0cc5d3540e300f4258871ab0e5bf0a7ce8a87aa6647"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "046eed04ced266750613dbe22c0df1eca13a50625e7b91997cc12a28d634337b"
   end
 
   def uuid_dependency
