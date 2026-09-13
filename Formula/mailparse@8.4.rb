5a29a01f30c737a55b283bd4bd78e0fd959c0173
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 21:04:49 +0000

    mailparse@8.4: update 3.2.0 bottle.

--- Formula/mailparse@8.4.rb
+++ Formula/mailparse@8.4.rb
@@ -20,12 +20,13 @@ class MailparseAT84 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "aa056b762e1739def6b5c65d43263efb7b6cd4502962dd6cb7b439382e560efc"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9abc5881960739b1eedaeeac62e88574d3b7f94744329bf99e50fafcda2110ae"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "01144e658faba2051c674d86f5a6fd8c4da1014354efea9db499e0f6fc210a75"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d8235b9a4c1f390a155e61205e6bbd8aa3d92505279c5332589b140832e18275"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "753227ad981b23f4295c3e229ef65694e816ef264b4eae55cec7fe940306ee75"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "18a5d58401ea66f0602d6c11554b609def9a36b986c735001a313a06c4cbed00"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "34e76b466ce5533db3c1b1d41cd5be46b7bd93a33f97d6ac4dba20782c9d1882"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "aa056b762e1739def6b5c65d43263efb7b6cd4502962dd6cb7b439382e560efc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "9abc5881960739b1eedaeeac62e88574d3b7f94744329bf99e50fafcda2110ae"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "01144e658faba2051c674d86f5a6fd8c4da1014354efea9db499e0f6fc210a75"
+    sha256 cellar: :any_skip_relocation, sonoma:            "d8235b9a4c1f390a155e61205e6bbd8aa3d92505279c5332589b140832e18275"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "753227ad981b23f4295c3e229ef65694e816ef264b4eae55cec7fe940306ee75"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "18a5d58401ea66f0602d6c11554b609def9a36b986c735001a313a06c4cbed00"
   end
 
   def install
