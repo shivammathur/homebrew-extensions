e8edecd03f81c189b52f13dd426e5b3f52f8ef96
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 22:10:44 +0000

    protobuf@8.3: update 5.36.2 bottle.

--- Formula/protobuf@8.3.rb
+++ Formula/protobuf@8.3.rb
@@ -20,12 +20,11 @@ class ProtobufAT83 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "03999f2958fc5a4e9cc241f2af5604e2de765d407639500600ba49f2576b778b"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "8d4a8adbdf85019b382bc6ff5d3288ae0b910d507eb428da1546700ce131c4a6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "cf21c4ee890e4593728157360263fc0eb19456429b61190a51f125008876388a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "61bbcda51eb6f7b8fb174c1db43387b34560ed6225bd347c25b50da4a0d40183"
-    sha256 cellar: :any,                 arm64_linux:       "9c8916a8a5a8685fd6b30aa2244eafa91615779f462c8bef9b1c209316ac3ff2"
-    sha256 cellar: :any,                 x86_64_linux:      "f7be43056ef7878928d08795826f6105da178a9decd2c82a883374cd68e04f49"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "99fe167bea3bbeb8324b870b4f7fa6f44c02862bde736157bc0d63a7984429c8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "c00013b96c2c3e6aad0f56a2b05b0fb976de5bca9f5421a94ad1a8b8f755ff35"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "ead873322fa75a1c08599fa69678c30136c34c1d2176ba558ea9bda3ec63585a"
+    sha256 cellar: :any,                 arm64_linux:       "08c477bfb27a6754b2431d250adf0bbb0c633acb2bd7d80a149c404064af4006"
+    sha256 cellar: :any,                 x86_64_linux:      "cb9af7ea0fc5e039f9db272acc1671f20ef712d9e6435c976babd808b162c193"
   end
 
   def install
