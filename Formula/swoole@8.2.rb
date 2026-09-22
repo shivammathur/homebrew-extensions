8b0b721efbdbb3af1ea1a8c8bdfffd4bafb6998d
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-22 22:46:16 +0530

    Fix Swoole 6.2.3 build on macOS

--- Formula/swoole@8.2.rb
+++ Formula/swoole@8.2.rb
@@ -44,6 +44,7 @@ class SwooleAT82 < AbstractPhpExtension
   conflicts_with "swow@8.2", because: "both provide coroutine networking extensions"
 
   def install
+    inreplace "src/core/misc.cc", "sw_usleep(1000);", "usleep(1000);" if OS.mac?
     args = %W[
       --enable-brotli
       --enable-cares
