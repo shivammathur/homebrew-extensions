5e6d5f4629c40f00899be631a7951368e4a77c82
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 04:14:26 +0000

    apcu@8.7: update 5.1.28 bottle.

--- Formula/apcu@8.7.rb
+++ Formula/apcu@8.7.rb
@@ -20,6 +20,11 @@ class ApcuAT87 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "713851739c7300f53d917836841bc8a37d74baf242aa29e37b72fccd7173b686"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "54b226930c8260eceb6ed5ae6cbd6da08da34cdf8b43d53b14d55479810d1713"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "e64c2700fd6acc7d0c6e6de36c64669d697ff9ff83d3ab3fa546c763eb706404"
+    sha256 cellar: :any,                 arm64_linux:       "bc29c33c2ad982c17f9c1569785bb8b6f22546fbe3916636a36c2fa51338200d"
+    sha256 cellar: :any,                 x86_64_linux:      "1749f0e1e1aee980b3c63b1f8484fa7d732447afc30cb4cd5b092b32c204532b"
   end
 
   def install
