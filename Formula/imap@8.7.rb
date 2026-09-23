63ff4d72388d16604101a4d5eaddc7b84344662f
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:24 +0530

    Add imap@8.7 formula

---
+++ Formula/imap@8.7.rb
@@ -0,0 +1,46 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Imap Extension
+class ImapAT87 < AbstractPhpExtension
+  init
+  desc "Imap PHP extension"
+  homepage "https://github.com/php/pecl-mail-imap"
+  url "https://pecl.php.net/get/imap-1.0.3.tgz"
+  sha256 "0c2c0b1f94f299004be996b85a424e3d11ff65ac0a3c980db3213289a4a3faaf"
+  head "https://github.com/php/pecl-mail-imap.git", branch: "main"
+  license "PHP-3.01"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/imap/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "krb5"
+  depends_on "openssl@3"
+  depends_on "shivammathur/extensions/imap-uw"
+
+  def install
+    Dir.chdir "imap-#{version}"
+    inreplace "php_imap.c", "0, Z_L(0)", "Z_L(0)"
+    inreplace "php_imap.c", "INI_STR(", "zend_ini_string_literal("
+    inreplace "php_imap.c", "XtOffsetOf", "offsetof"
+    safe_phpize
+    system "./configure",
+           "--prefix=#{prefix}",
+           phpconfig,
+           "--with-imap=shared, #{Utils::Path.formula_opt_prefix("imap-uw")}",
+           "--with-imap-ssl=#{Utils::Path.formula_opt_prefix("openssl@3")}",
+           "--with-kerberos"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
