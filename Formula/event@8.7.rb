3590f4166d716d5991299800f8161fdd24fed169
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 04:14:15 +0000

    event@8.7: update 3.1.6 bottle.

--- Formula/event@8.7.rb
+++ Formula/event@8.7.rb
@@ -20,6 +20,11 @@ class EventAT87 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any, arm64_golden_gate: "8205c194ffb141c4e638ec875d031db2e223f0371fb52b4ab45aa9436fe350f9"
+    sha256 cellar: :any, arm64_tahoe:       "aa1c760294f782f9405dd63e196bb029e1e123591bdf230c63157dfd8fcae01d"
+    sha256 cellar: :any, arm64_sequoia:     "5797a6ac53863d16877cbb01042d4add661a701bcc9fd341493c2936a0dff708"
+    sha256 cellar: :any, arm64_linux:       "53d4aaf5ff69c0374e46f1dd4e139039f370da217dbeab587002e41bda1dff0b"
+    sha256 cellar: :any, x86_64_linux:      "fea5d2f278e3730e90a61e28fbc952106418c6140ce2851159990519b0d1a7c1"
   end
 
   depends_on "libevent"
