fc7b4b063d2d2068ba78220aced68f00711f70e0
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:27 +0530

    Add uuid@8.7 formula

---
+++ Formula/uuid@8.7.rb
@@ -0,0 +1,53 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Uuid Extension
+class UuidAT87 < AbstractPhpExtension
+  init
+  desc "Uuid PHP extension"
+  homepage "https://github.com/php/pecl-networking-uuid"
+  url "https://pecl.php.net/get/uuid-1.3.0.tgz"
+  sha256 "b7af055e2c409622f8c5e6242d1c526c00e011a93c39b10ca28040b908da3f37"
+  head "https://github.com/php/pecl-networking-uuid.git", branch: "master"
+  license "LGPL-2.1-only"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/uuid/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def uuid_dependency
+    if OS.linux?
+      "util-linux"
+    else
+      "e2fsprogs"
+    end
+  end
+
+  on_macos do
+    depends_on "e2fsprogs"
+  end
+
+  on_linux do
+    depends_on "util-linux"
+  end
+
+  def install
+    args = %W[
+      --with-uuid=#{Utils::Path.formula_opt_prefix(uuid_dependency)}
+    ]
+    Dir.chdir "uuid-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
