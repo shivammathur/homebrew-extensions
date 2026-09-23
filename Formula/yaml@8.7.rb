24a61e123eb7cba7ef6b0861023e0562a5672b92
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:28 +0530

    Add yaml@8.7 formula

---
+++ Formula/yaml@8.7.rb
@@ -0,0 +1,46 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Yaml Extension
+class YamlAT87 < AbstractPhpExtension
+  init
+  desc "Yaml PHP extension"
+  homepage "https://github.com/php/pecl-file_formats-yaml"
+  url "https://pecl.php.net/get/yaml-2.3.0.tgz"
+  sha256 "bc8404807a3a4dc896b310af21a7f8063aa238424ff77f27eb6ffa88b5874b8a"
+  head "https://github.com/php/pecl-file_formats-yaml.git", branch: "php7"
+  license "PHP-3.01"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/yaml/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "libyaml"
+
+  def install
+    args = %W[
+      --with-yaml=#{Utils::Path.formula_opt_prefix("libyaml")}
+    ]
+    Dir.chdir "yaml-#{version}"
+    inreplace "detect.c", "ZEND_ATOL(*lval, buf)", "*lval = ZEND_ATOL(buf)"
+    %w[
+      yaml.c
+      parse.c
+    ].each do |f|
+      inreplace f, "zval_dtor", "zval_ptr_dtor_nogc"
+    end
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
