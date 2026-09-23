497c0813da47b9180d210891d50dfa1b066ae83b
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:27 +0530

    Add rdkafka@8.7 formula

---
+++ Formula/rdkafka@8.7.rb
@@ -0,0 +1,58 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Rdkafka Extension
+class RdkafkaAT87 < AbstractPhpExtension
+  init
+  desc "Rdkafka PHP extension"
+  homepage "https://github.com/arnaud-lb/php-rdkafka"
+  url "https://pecl.php.net/get/rdkafka-6.0.5.tgz"
+  sha256 "0af6b665c963c8c7d1109cec738034378d9c8863cbf612c0bd3235e519a708f1"
+  head "https://github.com/arnaud-lb/php-rdkafka.git", branch: "6.x"
+  license "MIT"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/rdkafka/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "librdkafka"
+
+  def install
+    Dir.chdir "rdkafka-#{version}"
+    patch_spl_symbols
+    inreplace %w[
+      conf.c
+      kafka_consumer.c
+      metadata.c
+      metadata_broker.c
+      metadata_collection.c
+      metadata_partition.c
+      metadata_topic.c
+      php_rdkafka_priv.h
+      queue.c
+      rdkafka.c
+      topic.c
+      topic_partition.c
+    ], "XtOffsetOf", "offsetof"
+    inreplace %w[
+      metadata_broker.c
+      metadata_collection.c
+      metadata_partition.c
+      metadata_topic.c
+    ], "zval_dtor", "zval_ptr_dtor_nogc"
+    inreplace "rdkafka.c", "EMPTY_SWITCH_DEFAULT_CASE()", "default: ZEND_UNREACHABLE()"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--with-rdkafka=#{Utils::Path.formula_opt_prefix("librdkafka")}"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
