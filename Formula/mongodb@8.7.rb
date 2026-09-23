bd491c7ade05ddc3023d01ae6987d9f29ee14eb4
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:25 +0530

    Add mongodb@8.7 formula

---
+++ Formula/mongodb@8.7.rb
@@ -0,0 +1,54 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Mongodb Extension
+class MongodbAT87 < AbstractPhpExtension
+  init
+  desc "Mongodb PHP extension"
+  homepage "https://github.com/mongodb/mongo-php-driver"
+  url "https://pecl.php.net/get/mongodb-2.5.2.tgz"
+  sha256 "7d47d1e92c754ca3999d0671b0c4b71878787d65de35f3b0e8c14f6345476ad9"
+  head "https://github.com/mongodb/mongo-php-driver.git", branch: "v2.x"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/mongodb/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "cyrus-sasl"
+  depends_on "icu4c@78"
+  depends_on "openssl@3"
+  depends_on "snappy"
+  depends_on "zlib"
+  depends_on "zstd"
+
+  def install
+    # Work around to support `icu4c` 75, which needs C++17.
+    ENV.append "CXX", "-std=c++17"
+    ENV.libcxx if ENV.compiler == :clang
+    Dir.chdir "mongodb-#{version}"
+    server_api = "src/MongoDB/ServerApi.c"
+    if File.read(server_api).include?("ZVAL_IS_NULL") || File.read(server_api).include?("zval_is_true")
+      inreplace server_api do |s|
+        s.gsub! "ZVAL_IS_NULL", "Z_ISNULL_P" if File.read(server_api).include?("ZVAL_IS_NULL")
+        s.gsub! "zval_is_true", "zend_is_true" if File.read(server_api).include?("zval_is_true")
+      end
+    end
+    if File.read("src/MongoDB/Cursor.c").include?("zval_dtor")
+      inreplace "src/MongoDB/Cursor.c", "zval_dtor", "zval_ptr_dtor_nogc"
+    end
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-mongodb"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
