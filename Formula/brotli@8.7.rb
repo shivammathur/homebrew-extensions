03aae27a226a30378b33874d036c294c6f2beac0
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:23 +0530

    Add brotli@8.7 formula

---
+++ Formula/brotli@8.7.rb
@@ -0,0 +1,40 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Brotli Extension
+class BrotliAT87 < AbstractPhpExtension
+  init
+  desc "Brotli PHP extension"
+  homepage "https://github.com/kjdev/php-ext-brotli"
+  url "https://pecl.php.net/get/brotli-0.21.0.tgz"
+  sha256 "97a69edd4f71046b1bd285d914741a60478e69a1db785c2b42aed940ab1fa18f"
+  head "https://github.com/kjdev/php-ext-brotli.git", branch: "master"
+  license "MIT"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/brotli/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "brotli"
+
+  def install
+    args = %W[
+      --enable-brotli
+      --with-libbrotli=#{Utils::Path.formula_opt_prefix("brotli")}
+    ]
+    Dir.chdir "brotli-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
