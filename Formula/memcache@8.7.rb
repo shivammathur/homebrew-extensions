8ee3d081de35fc4f1b76056d6d28a0e5c0d202d1
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:25 +0530

    Add memcache@8.7 formula

---
+++ Formula/memcache@8.7.rb
@@ -0,0 +1,61 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Memcache Extension
+class MemcacheAT87 < AbstractPhpExtension
+  init
+  desc "Memcache PHP extension"
+  homepage "https://github.com/websupport-sk/pecl-memcache"
+  url "https://pecl.php.net/get/memcache-8.2.tgz"
+  sha256 "b3f0640eacdeb9046c6c86a1546d7fb8a4e9f219e5d9a36a287e59b2dd8208e5"
+  head "https://github.com/websupport-sk/pecl-memcache.git", branch: "main"
+  license "PHP-3.0"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/memcache/allreleases.xml"
+    regex(/<v>(\d+\.\d+(?:\.\d+)?(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "zlib"
+
+  def install
+    args = %W[
+      --enable-memcache
+      --with-zlib-dir=#{Utils::Path.formula_opt_prefix("zlib")}
+    ]
+    Dir.chdir "memcache-#{version}"
+    inreplace %w[
+      src/memcache_ascii_protocol.c
+      src/memcache_binary_protocol.c
+      src/memcache_pool.c
+      src/memcache_session.c
+    ], "ext/standard/php_smart_string.h", "Zend/zend_smart_string.h"
+    inreplace "src/memcache_pool.h", "ext/standard/php_smart_string_public.h", "Zend/zend_smart_string.h"
+    inreplace %w[
+      src/memcache.c
+      src/memcache_binary_protocol.c
+      src/memcache_pool.c
+      src/memcache_session.c
+    ], "zval_dtor", "zval_ptr_dtor_nogc"
+    inreplace "src/memcache_session.c", "ZEND_EXTERN_MODULE_GLOBALS(memcache)", <<~EOS
+      ZEND_EXTERN_MODULE_GLOBALS(memcache)
+      #define ps_create_sid_memcache php_session_create_id
+      #define ps_validate_sid_memcache php_session_validate_sid
+    EOS
+    inreplace "src/memcache_session.c", "path = save_path;", "path = ZSTR_VAL(save_path);"
+    inreplace "src/memcache.c", "WRONG_PARAM_COUNT;", "zend_wrong_param_count(); RETURN_THROWS();"
+    inreplace "src/memcache_session.c", "INI_INT(", "zend_ini_long_literal("
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
