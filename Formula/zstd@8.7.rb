84c2b562f7d71653a8ae3326a395b38f0961068a
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:28 +0530

    Add zstd@8.7 formula

---
+++ Formula/zstd@8.7.rb
@@ -0,0 +1,36 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Zstd Extension
+class ZstdAT87 < AbstractPhpExtension
+  init
+  desc "Zstd Extension for PHP"
+  homepage "https://github.com/kjdev/php-ext-zstd"
+  url "https://pecl.php.net/get/zstd-0.18.0.tgz"
+  sha256 "223d0f77eb5a5e73cf5e7a0652dd8fde7ffdcc843e7f30eeb3998283dec847b9"
+  head "https://github.com/kjdev/php-ext-zstd.git", branch: "master"
+  license "MIT"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/zstd/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "zstd"
+
+  def install
+    Dir.chdir "zstd-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", "--with-libzstd", phpconfig
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
