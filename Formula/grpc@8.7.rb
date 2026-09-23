ae5c79667e4a30780b6e0fef029346647ab78b65
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:24 +0530

    Add grpc@8.7 formula

---
+++ Formula/grpc@8.7.rb
@@ -0,0 +1,62 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for GRPC Extension
+class GrpcAT87 < AbstractPhpExtension
+  init
+  desc "gRPC PHP extension"
+  homepage "https://github.com/grpc/grpc"
+  url "https://pecl.php.net/get/grpc-1.84.0.tgz"
+  sha256 "555716233a5cc9a6baa605719f87b7688a88fb8bfdd9b4493396276244d56625"
+  head "https://github.com/grpc/grpc.git", branch: "master"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/grpc/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "grpc"
+
+  def install
+    Dir.chdir "grpc-#{version}"
+    patch_spl_symbols
+    inreplace "src/php/ext/grpc/call.c", "zend_exception_get_default(TSRMLS_C)", "zend_ce_exception"
+    promise_factory = "src/core/lib/promise/detail/promise_factory.h"
+    if File.read(promise_factory).include?("GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION\n    absl::enable_if_t")
+      inreplace promise_factory do |s|
+        s.gsub! "GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION\n    absl::enable_if_t",
+                "GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION inline\n    absl::enable_if_t"
+      end
+    end
+    inreplace "src/core/lib/promise/try_seq.h" do |s|
+      s.gsub! "GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION auto TrySeq",
+              "GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION inline auto TrySeq"
+    end
+    loop_h = "src/core/lib/promise/loop.h"
+    if File.read(loop_h).include?("GPR_NO_UNIQUE_ADDRESS union {")
+      inreplace loop_h, "GPR_NO_UNIQUE_ADDRESS union {", "union {"
+    end
+    inreplace %w[
+      src/php/ext/grpc/call.h
+      src/php/ext/grpc/call_credentials.h
+      src/php/ext/grpc/channel.h
+      src/php/ext/grpc/channel_credentials.h
+      src/php/ext/grpc/php7_wrapper.h
+      src/php/ext/grpc/server.h
+      src/php/ext/grpc/server_credentials.h
+      src/php/ext/grpc/timeval.h
+    ], "XtOffsetOf", "offsetof"
+    safe_phpize
+    system "./configure", "--enable-grpc"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+  end
+end
