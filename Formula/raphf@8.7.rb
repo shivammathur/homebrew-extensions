ae3c3b357725c4da68cfbf4c391f021aca61e262
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 04:50:26 +0000

    raphf@8.7: update 2.0.2 bottle.

--- Formula/raphf@8.7.rb
+++ Formula/raphf@8.7.rb
@@ -21,6 +21,11 @@ class RaphfAT87 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "b25fa18526fbcfab4dd7d98b1c315db1154445fcece5dfde274e56db53188167"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "3323bce7c3b757bf140d60f5ba944a849d9957ac7039ab3294d3dab359e58929"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "8588f9fe3fa1049a84bf4c38cd06847f3f4b6d48432194cea51ea13b4ca90d4d"
+    sha256 cellar: :any,                 arm64_linux:       "ae1a71ea18b09d78c3a61c9c037a9997ea59e7e4b37b01fd73f84607023870c3"
+    sha256 cellar: :any,                 x86_64_linux:      "6ee7e582945161d88c38b0082d7401e850882c6f6520a82105c81912b1c5247c"
   end
 
   def install
