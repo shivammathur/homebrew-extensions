7938db2233b7cd7d96681c5bcc408ab6e75b249b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 04:51:04 +0000

    gmagick@8.7: update 2.0.6RC1 bottle.

--- Formula/gmagick@8.7.rb
+++ Formula/gmagick@8.7.rb
@@ -20,6 +20,11 @@ class GmagickAT87 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any, arm64_golden_gate: "99ce8f5211b8112522510df228424ec1f776bf56bc24857f55818eb42af89ce7"
+    sha256 cellar: :any, arm64_tahoe:       "0d04e859fda5443bf4e5356f2561c60e476bb57944e89f893c7429c3eb3d6bb1"
+    sha256 cellar: :any, arm64_sequoia:     "89a1a083cb9b5f785812d9fb8901fec466b69aa222a8e717c31a343f3096668d"
+    sha256 cellar: :any, arm64_linux:       "bf423b1d9b49ac1053c11d83562d039845253a57009bf0fef104be0d41c7f14a"
+    sha256 cellar: :any, x86_64_linux:      "163221f6b3f5c43d46b0aa5934d7199cd2fcb8fe6c569c6feb112793f84f6ba7"
   end
 
   conflicts_with "imagick@8.7",
