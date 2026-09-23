3de4b0e4b7a08eeea1354ffaa2e2e4e549257921
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:24 +0530

    Add mailparse@8.7 formula

---
+++ Formula/mailparse@8.7.rb
@@ -0,0 +1,39 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Mailparse Extension
+class MailparseAT87 < AbstractPhpExtension
+  init
+  desc "Mailparse PHP extension"
+  homepage "https://github.com/php/pecl-mail-mailparse"
+  url "https://pecl.php.net/get/mailparse-3.2.0.tgz"
+  sha256 "cc5111ae17bfa36efcc5ef23dcf75b6501593ac264c196aad3e39cd4ad765332"
+  head "https://github.com/php/pecl-mail-mailparse.git", branch: "master"
+  license "PHP-3.01"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/mailparse/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "re2c" => :build
+
+  def install
+    # Work around configure issues with Xcode 16
+    ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
+
+    Dir.chdir "mailparse-#{version}"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-mailparse"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
