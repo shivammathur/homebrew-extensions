a26db8d960dc78cdfce41457a188d8736cf61414
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:22 +0530

    Add apcu@8.7 formula

---
+++ Formula/apcu@8.7.rb
@@ -0,0 +1,41 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Apcu Extension
+class ApcuAT87 < AbstractPhpExtension
+  init
+  desc "Apcu PHP extension"
+  homepage "https://github.com/krakjoe/apcu"
+  url "https://pecl.php.net/get/apcu-5.1.28.tgz"
+  sha256 "ca9c1820810a168786f8048a4c3f8c9e3fd941407ad1553259fb2e30b5f057bf"
+  head "https://github.com/krakjoe/apcu.git", branch: "master"
+  license "PHP-3.01"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/apcu/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    Dir.chdir "apcu-#{version}"
+    inreplace "apc.c", 'php_verror(NULL, "", verbosity, format, args);',
+                       "php_verror(NULL, verbosity, format, args);"
+    inreplace "apc_persist.c", "EMPTY_SWITCH_DEFAULT_CASE()", "default: ZEND_UNREACHABLE();"
+    if File.read("apc_cache.c").include?("zval_dtor")
+      inreplace("apc_cache.c") { |s| s.gsub! "zval_dtor", "zval_ptr_dtor_nogc" }
+    end
+    inreplace %w[apc_cache.h apc_iterator.c apc_iterator.h], "XtOffsetOf", "offsetof"
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-apcu"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
