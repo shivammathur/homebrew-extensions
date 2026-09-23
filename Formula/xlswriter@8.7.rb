a2afb48820a455df0726705a9b2339a1b14f9f13
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:28 +0530

    Add xlswriter@8.7 formula

---
+++ Formula/xlswriter@8.7.rb
@@ -0,0 +1,52 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Xlswriter Extension
+class XlswriterAT87 < AbstractPhpExtension
+  init
+  desc "Xlswriter PHP extension"
+  homepage "https://github.com/viest/php-ext-xlswriter"
+  url "https://pecl.php.net/get/xlswriter-3.0.0.tgz"
+  sha256 "a17986ad5ac09529513fc59b2871ca2b53eaec1c2c55cf00be60a292e85ade73"
+  head "https://github.com/viest/php-ext-xlswriter.git", branch: "master"
+  license "BSD-2-Clause"
+
+  livecheck do
+    url "https://pecl.php.net/rest/r/xlswriter/allreleases.xml"
+    regex(/<v>(\d+\.\d+\.\d+(?:\.\d+)?)(?=<)/i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    args = %w[
+      --with-xlswriter
+      --enable-reader
+    ]
+    ENV.append "CFLAGS", "-std=gnu17"
+    Dir.chdir "xlswriter-#{version}"
+    inreplace "kernel/common.c", "lxlsx_datetime timestamp_to_datetime", <<~C
+      #if PHP_VERSION_ID >= 80600
+      static int xlswriter_php_idate(char format, time_t ts, bool localtime)
+      {
+          int result = 0;
+          php_idate(format, ts, localtime, &result);
+          return result;
+      }
+      #define php_idate(format, ts, localtime) xlswriter_php_idate(format, ts, localtime)
+      #endif
+
+      lxlsx_datetime timestamp_to_datetime
+    C
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}", phpconfig, *args
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    add_include_files
+  end
+end
