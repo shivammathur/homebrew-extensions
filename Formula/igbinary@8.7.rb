1549f2588ce0315febbc48b375364dd88d28988a
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:24 +0530

    Add igbinary@8.7 formula

---
+++ Formula/igbinary@8.7.rb
@@ -0,0 +1,47 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Igbinary Extension
+class IgbinaryAT87 < AbstractPhpExtension
+  init
+  desc "Igbinary PHP extension"
+  homepage "https://github.com/igbinary/igbinary"
+  url "https://github.com/igbinary/igbinary/archive/3.2.16.tar.gz"
+  sha256 "941f1cf2ccbecdc1c221dbfae9213439d334be5d490a2f3da2be31e8a00b0cdb"
+  compatibility_version 1
+  head "https://github.com/igbinary/igbinary.git", branch: "master"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url :homepage
+    strategy :git do |tags|
+      semver_tags = tags.map(&:to_s).grep(/^v?\d+(\.\d+)+$/)
+      semver_tags.max_by { |tag| Version.new(tag.delete_prefix("v")) }
+    end
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  def install
+    patch_spl_symbols
+    safe_phpize
+    inreplace "src/php7/php_igbinary.h", "ext/standard/php_smart_string.h", "Zend/zend_smart_string.h"
+    inreplace "src/php7/igbinary.c" do |s|
+      s.gsub! "zval_dtor", "zval_ptr_dtor_nogc"
+      s.gsub! "const char* user_func_name;", "zend_string *user_func_name;"
+      s.gsub! "(user_func_name == NULL) || (user_func_name[0] == '\\0')", "user_func_name == NULL"
+      s.gsub! "ZVAL_STRING(&user_func, user_func_name)", "ZVAL_STR_COPY(&user_func, user_func_name)"
+      s.gsub! "PG(unserialize_callback_func));", "ZSTR_VAL(PG(unserialize_callback_func)));"
+    end
+    system "./configure", "--prefix=#{prefix}", phpconfig, "--enable-igbinary"
+    system "make"
+    prefix.install "modules/#{extension}.so"
+    write_config_file
+    Dir.chdir "src/php7"
+    add_include_files
+  end
+end
