bdd8efd030ddbc02e939d4af3f5d1b1e0fe015ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 04:50:50 +0000

    brotli@8.7: update 0.21.0 bottle.

--- Formula/brotli@8.7.rb
+++ Formula/brotli@8.7.rb
@@ -20,6 +20,11 @@ class BrotliAT87 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any, arm64_golden_gate: "c5974622d224bfef8962f6d6b329cd794edf10d4cb113401478208bcb30214f7"
+    sha256 cellar: :any, arm64_tahoe:       "f6bd47f37ee44884758eaf8ef6ac66aee87b56a899ce285f920322f7a47b50a9"
+    sha256 cellar: :any, arm64_sequoia:     "74214dfd3228864070c91bc7a8fd962f3ea25958ada5c357f5351afe7714663e"
+    sha256 cellar: :any, arm64_linux:       "620b2f9f0a0ca4a6133f88ba02cbd57969a7861a57ddaa7d8b1171ee900f3100"
+    sha256 cellar: :any, x86_64_linux:      "f61d5c193ea4cf4c857d811422207bac5432715944119e109134fd8f9ac528e4"
   end
 
   depends_on "brotli"
