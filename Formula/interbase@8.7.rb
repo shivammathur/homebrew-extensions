a32067470d99fbe77937ecbe3e7b7a70c132215e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 04:14:02 +0000

    interbase@8.7: update 5.0.2 bottle.

--- Formula/interbase@8.7.rb
+++ Formula/interbase@8.7.rb
@@ -9,6 +9,11 @@ class InterbaseAT87 < AbstractPhpExtension
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/extensions"
+    sha256 cellar: :any, arm64_golden_gate: "4a2cac372112aedb4a22f168f99ea1d54ef6fae5d3ab0f4f5e1da3bcd58a752b"
+    sha256 cellar: :any, arm64_tahoe:       "adbcdf64f8baf922fe5ba24db4f42ab8676979bb48142d9e3009c802b199802a"
+    sha256 cellar: :any, arm64_sequoia:     "f82971557585ebf6c7073ba4b824af6cea70de00e3d922f91154b00d5334557a"
+    sha256 cellar: :any, arm64_linux:       "0c44ea49d6b490868a73a184d54e7d0572270a106c00b3c81068871043aeb869"
+    sha256 cellar: :any, x86_64_linux:      "52e6adfb0eab16c185a96e22d4101266b103f93e0e206061fd6b89a45f1f50d7"
   end
   init
   desc "Interbase (Firebird) PHP extension"
