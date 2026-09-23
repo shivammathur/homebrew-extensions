83f840b4b8a982dd0ffe5f9619181f6134f7bb28
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-23 08:17:26 +0530

    Add pdo_firebird@8.7 formula

---
+++ Formula/pdo_firebird@8.7.rb
@@ -0,0 +1,38 @@
+# typed: true
+# frozen_string_literal: true
+
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+# Class for Pdo Firebird Extension
+class PdoFirebirdAT87 < AbstractPhpExtension
+  init
+  desc "PDO Firebird PHP extension"
+  homepage "https://github.com/php/php-src"
+  url "https://github.com/php/php-src/archive/10a2b0dae015c155a0e1d7486989738c22493a02.tar.gz?commit=10a2b0dae015c155a0e1d7486989738c22493a02"
+  version "8.7.0"
+  sha256 "5111af9822cf0422a45fdb1e5563590b2a6f5b2db5b035f13e5de6147d7a6849"
+  head "https://github.com/php/php-src.git", branch: "master"
+  license "PHP-3.01"
+
+  bottle do
+    root_url "https://ghcr.io/v2/shivammathur/extensions"
+  end
+
+  depends_on "shivammathur/extensions/firebird-client"
+
+  def install
+    fb_prefix = Utils::Path.formula_opt_prefix("shivammathur/extensions/firebird-client")
+    args = %W[
+      --with-pdo-firebird=shared,#{fb_prefix}
+    ]
+    Dir.chdir buildpath/"ext/pdo_firebird" do
+      safe_phpize
+      ENV.append "CFLAGS", "-Wno-incompatible-function-pointer-types" if OS.mac?
+      system "./configure", "--prefix=#{prefix}", phpconfig, *args
+      system "make"
+      prefix.install "modules/#{extension}.so"
+      write_config_file
+      add_include_files
+    end
+  end
+end
